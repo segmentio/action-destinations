38171661b9d859c11d2a5ffe061afb6e05cf1334
Gabriel Samson <gpsamson@users.noreply.github.com>
2021-08-10 17:53:35 -0700

    LIBWEB-778 Support UTM and referrer in Amplitude Actions (#28)
    * initial utm work
    
    * property definitions
    
    * generated types
    
    * track properties
    
    * generated types
    
    * identify impl
    
    * cr feedback
    
    * swap default placement
    
    * add temporary referrer description
    
    * generated
    
    * refactor to format referrer correctly
    
    * generated types
    
    * better user-prop merge
    
    * update doc
    
    * more test + fixes
    
    * fixes for unintended user_properties
    
    * reorganize tests to proper describes
    
    * improve tests + CR feedback
    
    Co-authored-by: Dan Lasky <dan.lasky@segment.com>
    Co-authored-by: Dan <dlasky@users.noreply.github.com>

---
+++ packages/destination-actions/src/destinations/amplitude/utm.ts
@@ -0,0 +1,60 @@
+import { AmplitudeEvent } from './logEvent'
+import { AmplitudeUserProperties } from './merge-user-properties'
+
+interface Payload {
+  utm_properties?: UTMProperties
+  user_properties?: AmplitudeUserProperties
+}
+
+interface UTMProperties {
+  utm_source?: string
+  utm_medium?: string
+  utm_campaign?: string
+  utm_term?: string
+  utm_content?: string
+}
+
+interface InitialUTMProperties {
+  initial_utm_source?: string
+  initial_utm_medium?: string
+  initial_utm_campaign?: string
+  initial_utm_term?: string
+  initial_utm_content?: string
+}
+
+/**
+ * Take a compatible payload that contains a `utm_properties` key and converts it to a user_properties object suitable for injection into an amplitude event
+ *
+ * @param payload an event payload that contains a utm_properties property
+ * @returns a user_properties object suitable for amplitude events
+ */
+export function convertUTMProperties(payload: Payload): AmplitudeUserProperties {
+  const { utm_properties, ...rest } = payload
+
+  if (!utm_properties) return {}
+
+  const cleanedPayload = rest as AmplitudeEvent
+  const set: UTMProperties = {}
+  const setOnce: InitialUTMProperties = {}
+
+  Object.entries(utm_properties).forEach(([key, value]) => {
+    set[key as keyof UTMProperties] = value
+    setOnce[`initial_${key}` as keyof InitialUTMProperties] = value
+  })
+
+  let userProperties: AmplitudeUserProperties
+
+  if (cleanedPayload.user_properties) {
+    userProperties = { ...cleanedPayload.user_properties }
+
+    userProperties.$set = { ...userProperties.$set, ...set }
+    userProperties.$setOnce = { ...userProperties.$setOnce, ...setOnce }
+  } else {
+    userProperties = {
+      $set: set,
+      $setOnce: setOnce
+    }
+  }
+
+  return userProperties
+}
