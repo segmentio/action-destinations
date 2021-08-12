5d2b61aa38b22d2496f8df924f0008453157ebae
Julio <julio.farah@gmail.com>
2021-08-11 17:21:15 -0700

    Allow user to select braze sdk version

--- packages/browser-destinations/src/destinations/braze/__tests__/initialization.test.ts
+++ packages/browser-destinations/src/destinations/braze/__tests__/initialization.test.ts
@@ -2,8 +2,30 @@ import appboy from '@braze/web-sdk'
 import { Analytics, Context } from '@segment/analytics-next'
 import * as jsdom from 'jsdom'
 import brazeDestination from '../index'
+import { destination } from '../index'
 
 describe('initialization', () => {
+  const settings = {
+    safariWebsitePushId: 'safari',
+    allowCrawlerActivity: true,
+    doNotLoadFontAwesome: true,
+    enableLogging: true,
+    localization: 'pt',
+    minimumIntervalBetweenTriggerActionsInSeconds: 60,
+    openInAppMessagesInNewTab: true,
+    sessionTimeoutInSeconds: 60,
+    requireExplicitInAppMessageDismissal: true,
+    enableHtmlInAppMessages: true,
+    devicePropertyAllowlist: ['ay', 'Dios', 'mio'],
+    devicePropertyWhitelist: ['foo', 'bar'],
+    // version: 3,
+    // logPurchaseWhenRevenuePresent: false,
+    // onlyTrackKnownUsersOnWeb: false,
+    allowUserSuppliedJavascript: true,
+    contentSecurityNonce: 'bar',
+    endpoint: 'endpoint'
+  }
+
   beforeEach(async () => {
     jest.restoreAllMocks()
     jest.resetAllMocks()
@@ -33,27 +55,6 @@ describe('initialization', () => {
   })
 
   test('load initialization options', async () => {
-    const settings = {
-      safariWebsitePushId: 'safari',
-      allowCrawlerActivity: true,
-      doNotLoadFontAwesome: true,
-      enableLogging: true,
-      localization: 'pt',
-      minimumIntervalBetweenTriggerActionsInSeconds: 60,
-      openInAppMessagesInNewTab: true,
-      sessionTimeoutInSeconds: 60,
-      requireExplicitInAppMessageDismissal: true,
-      enableHtmlInAppMessages: true,
-      devicePropertyAllowlist: ['ay', 'Dios', 'mio'],
-      devicePropertyWhitelist: ['foo', 'bar'],
-      // version: 3,
-      // logPurchaseWhenRevenuePresent: false,
-      // onlyTrackKnownUsersOnWeb: false,
-      allowUserSuppliedJavascript: true,
-      contentSecurityNonce: 'bar',
-      endpoint: 'endpoint'
-    }
-
     const initialize = jest.spyOn(appboy, 'initialize')
 
     const [logCustomEvent] = await brazeDestination({
@@ -81,5 +82,38 @@ describe('initialization', () => {
 
     const { endpoint, ...expectedSettings } = settings
     expect(initialize).toHaveBeenCalledWith('b_123', { baseUrl: endpoint, ...expectedSettings })
+  })
+
+  test('loads sdk version 3.3 by default', async () => {
+    const withVersion = {
+      ...settings
+    }
+
+    const dependencies = {
+      loadScript: jest.fn()
+    }
+
+    // @ts-expect-error
+    await destination.initialize({ settings: withVersion }, dependencies)
+
+    expect(dependencies.loadScript).toHaveBeenCalledWith(`https://js.appboycdn.com/web-sdk/3.3/service-worker.js`)
+  })
+
+  test('load different sdk versions', async () => {
+    const withVersion = {
+      ...settings,
+      sdkVersion: '3.0'
+    }
+
+    const dependencies = {
+      loadScript: jest.fn()
+    }
+
+    // @ts-expect-error
+    await destination.initialize({ settings: withVersion }, dependencies)
+
+    expect(dependencies.loadScript).toHaveBeenCalledWith(
+      `https://js.appboycdn.com/web-sdk/${withVersion.sdkVersion}/service-worker.js`
+    )
   })
 })
--- packages/browser-destinations/src/destinations/braze/index.ts
+++ packages/browser-destinations/src/destinations/braze/index.ts
@@ -18,6 +18,30 @@ export const destination: BrowserDestinationDefinition<Settings, typeof appboy> 
   slug: 'actions-braze-web',
   mode: 'device',
   settings: {
+    sdkVersion: {
+      description: 'The version of the SDK to use. Defaults to 3.3.',
+      label: 'SDK Version',
+      type: 'string',
+      choices: [
+        {
+          value: '3.0',
+          label: '3.0'
+        },
+        {
+          value: '3.1',
+          label: '3.1'
+        },
+        {
+          value: '3.2',
+          label: '3.2'
+        },
+        {
+          value: '3.3',
+          label: '3.3'
+        }
+      ],
+      required: false
+    },
     api_key: {
       description: 'Created under Developer Console in the Braze Dashboard.',
       label: 'API Key',
@@ -216,21 +240,12 @@ export const destination: BrowserDestinationDefinition<Settings, typeof appboy> 
       enableHtmlInAppMessages: settings.enableHtmlInAppMessages,
       devicePropertyAllowlist: settings.devicePropertyAllowlist,
       devicePropertyWhitelist: settings.devicePropertyWhitelist,
-      // openNewsFeedCardsInNewTab: false,
-      // automaticallyDisplayMessages: true,
-      // trackAllPages: false,
-      // trackNamedPages: false,
-      // customEndpoint: '',
-      // changed from 1 to 3
-      // version: settings.appVersion ?? 3,
-      // logPurchaseWhenRevenuePresent: false,
-      // onlyTrackKnownUsersOnWeb: false,
       baseUrl: settings.endpoint,
       allowUserSuppliedJavascript: settings.allowUserSuppliedJavascript,
       contentSecurityNonce: settings.contentSecurityNonce
     }
 
-    await dependencies.loadScript('https://js.appboycdn.com/web-sdk/3.3/service-worker.js')
+    await dependencies.loadScript(`https://js.appboycdn.com/web-sdk/${settings.sdkVersion ?? '3.3'}/service-worker.js`)
 
     const initialized = appboy.initialize(settings.api_key, config)
     if (!initialized) {
