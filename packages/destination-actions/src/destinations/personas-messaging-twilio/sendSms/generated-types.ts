87de9f075f34312591dc361782255fff78d8684f
Lauren Fries Brink <lbrink@twilio.com>
2021-08-12 10:03:17 -0600

    GROW-26 Add Twilio destination specific to Personas Messaging product

---
+++ packages/destination-actions/src/destinations/personas-messaging-twilio/sendSms/generated-types.ts
@@ -0,0 +1,16 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * User ID in Segment
+   */
+  userId: string
+  /**
+   * Which number to send SMS from
+   */
+  fromNumber: string
+  /**
+   * Message to send
+   */
+  body: string
+}
