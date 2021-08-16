34a5719b6c278f3ea755d1fade012e39dfc2a73c
Dan Lasky <dan.lasky@segment.com>
2021-08-16 13:18:50 -0400

    address CR feedback

--- packages/destination-actions/src/destinations/amplitude/user-agent.ts
+++ packages/destination-actions/src/destinations/amplitude/user-agent.ts
@@ -1,5 +1,4 @@
 import UaParser from '@amplitude/ua-parser-js'
-import { removeUndefined } from '@segment/actions-core'
 import { omit } from 'lodash'
 
 interface Payload {
@@ -16,12 +15,16 @@ export function parseUserAgent(payload: Payload & ParsedUA): ParsedUA {
   if (!payload?.userAgent) {
     return omit(payload, 'userAgent')
   }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   const parser = new UaParser(payload?.userAgent)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   const device = parser.getDevice()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   const os = parser.getOS()
-  return removeUndefined({
+
+  return {
     os_name: payload.os_name ?? os.name,
     os_version: payload.os_version ?? os.version,
     device_model: payload.device_model ?? device.model
-  })
+  }
 }
