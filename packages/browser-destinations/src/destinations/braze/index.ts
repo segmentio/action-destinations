130c74a4964aaff09c7c2457131dcbb5e2c3b1ce
Netto Farah <nettofarah@gmail.com>
2021-08-16 12:34:16 -0500

    Braze debounce middleware

--- packages/browser-destinations/src/destinations/braze/index.ts
+++ packages/browser-destinations/src/destinations/braze/index.ts
@@ -5,6 +5,7 @@ import appboy from '@braze/web-sdk'
 import logCustomEvent from './logCustomEvent'
 import logUser from './logUser'
 import logPurchase from './logPurchase'
+import debounce, { resetUserCache } from './debounce'
 
 declare global {
   interface Window {
@@ -234,13 +235,16 @@ export const destination: BrowserDestinationDefinition<Settings, typeof appboy> 
     }
 
     appboy.openSession()
+    resetUserCache()
+
     return appboy
   },
 
   actions: {
     logUser,
     logCustomEvent,
-    logPurchase
+    logPurchase,
+    debounce
   }
 }
 
