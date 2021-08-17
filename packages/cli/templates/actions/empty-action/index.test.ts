09ad0a5e43e74ba1a421126c19639792931707cc
Dalton Scharff <dalton.scharff@segment.com>
2021-08-17 11:12:03 -0500

    fills empty-action test file with testing scaffold

--- packages/cli/templates/actions/empty-action/index.test.ts
+++ packages/cli/templates/actions/empty-action/index.test.ts
@@ -1,1 +1,11 @@
-// TEST FILE
+import nock from 'nock'
+import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import Action from '../index'
+
+const testDestination = createTestIntegration(Action)
+
+describe('{{destination}}', () => {
+  describe('{{name}}', () => {
+    // TODO: Test your action
+  })
+})
