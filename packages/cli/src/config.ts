f837d333bde750d4168e3bd1319df596ff27aa1c
Julio <julio.farah@gmail.com>
2021-08-17 16:59:24 -0700

    fix conditional

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -1,6 +1,4 @@
 export const ASSET_PATH =
   process.env.NODE_ENV === 'production' || process.env.SERVER_ENVIRONMENT === 'production'
     ? 'https://cdn.segment.com/next-integrations/actions'
-    : process.env.NODE_ENV === 'stage'
-    ? 'https://cdn.segment.build/next-integrations/actions'
-    : undefined
+    : 'https://cdn.segment.build/next-integrations/actions'
