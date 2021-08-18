157596b01b7c5305235028f16a5d9357f59236ee
gpsamson <gabriel.samson@segment.com>
2021-08-18 15:17:42 -0700

    Move browser-destination jest config to file and conditionally run tests

---
+++ packages/browser-destinations/jest.config.js
@@ -0,0 +1,27 @@
+module.exports = async () => {
+    const config = {
+        rootDir: './',
+        moduleNameMapper: {
+            '@segment/actions-core': '<rootDir>/../core/src/index.ts'
+        },
+        transform: {
+            '^.+\\.[t|j]sx?$': 'babel-jest'
+        },
+        transformIgnorePatterns: [
+            '/node_modules/(?!@segment/analytics-next).+\\.js$'
+        ],
+        forceExit: true
+    }
+
+    try {
+        await import('@segment/analytics-next')
+    } catch (_) {
+        config.testPathIgnorePatterns = [
+            '/node_modules/',
+            '<rootDir>/src/'
+        ]
+        console.warn('optional dependency @segment/analytics-next not found, ignoring all tests')
+    }
+
+    return config
+}
