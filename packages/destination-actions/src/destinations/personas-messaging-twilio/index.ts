f0844aa457b66c2e13f8c53b6b6f002a11673e26
Lauren Fries Brink <6043178+laurenfb@users.noreply.github.com>
2021-08-23 16:40:53 -0600

    GROW-26: add Twilio destination action #minor (#41)
    * GROW-26 Add Twilio destination specific to Personas Messaging product
    
    * Update packages/destination-actions/src/destinations/personas-messaging-twilio/sendSms/index.ts
    
    Co-authored-by: Matt Shwery <matthew.shwery@segment.com>
    
    * Remove `form` helper
    
    * GROW-26 add cloud mode to destination definition
    
    * GROW-26 add GET request to twilio API to validate credentials
    
    * GROW-26 add comments to indicate what can be removed post-CPS
    
    * GROW-26 use type = password instead of type = string
    
    Co-authored-by: Matt Shwery <matthew.shwery@segment.com>
    
    * GROW-26 omit extra header
    
    Co-authored-by: Matt Shwery <matthew.shwery@segment.com>
    
    * GROW-26 remove twilio phone number fallback
    
    * GROW-26 remove handlebars 4.7.7
    
    * GROW-26 use Mustache instead of Handlebars
    
    * GROW-26 oops, missed a place to use type = password
    
    Co-authored-by: Matt Shwery <matthew.shwery@segment.com>
    
    * GROW-26 remove conditional default and specify (hopefully more correct) defaults for fromNumber and userId
    
    * GROW-26 bring id & action name up to date with production data
    
    * GROW-26 temp comment out fromNumber default to see impact on test
    
    * Revert "GROW-26 temp comment out fromNumber default to see impact on test"
    
    This reverts commit 5828e2dea7d6af5c763ccc5b8f9af86eb0ed9b59.
    
    * GROW-26 use user_id rather than profile.user_id because mustache gonna mustache
    
    * GROW-26 use profile.traits.phone & adjust tests to pass
    
    * Publish
    
     - @segment/ajv-human-errors@1.3.2-alpha.0
     - @segment/browser-destinations@3.4.1-alpha.0
     - @segment/actions-cli@3.4.1-alpha.0
     - @segment/actions-core@3.4.1-alpha.0
     - @segment/action-destinations@3.4.1-alpha.0
     - @segment/destination-subscriptions@3.3.1-alpha.0
    
    * GROW-26 add exclamation to comment
    
    * Publish
    
     - @segment/actions-cli@3.4.1-alpha.1
     - @segment/action-destinations@3.4.1-alpha.1
    
    * GROW-26 bye bye exclamation point
    
    * Publish
    
     - @segment/actions-cli@3.4.1-alpha.2
     - @segment/action-destinations@3.4.1-alpha.2
    
    * Revert "GROW-26 bye bye exclamation point"
    
    This reverts commit 3f52cf02b0c7821976e03f588b2f180a90ab91cb.
    
    * Publish
    
     - @segment/actions-cli@3.4.1-alpha.3
     - @segment/action-destinations@3.4.1-alpha.3
    
    * GROW-26 go back to using externalId for phone
    
    * Publish
    
     - @segment/actions-cli@3.4.1-alpha.4
     - @segment/action-destinations@3.4.1-alpha.4
    
    * GROW-26 we're doing this exclamation point dance again trying to lerna publish :eyeroll:
    
    * Publish
    
     - @segment/actions-cli@3.4.1-alpha.5
     - @segment/action-destinations@3.4.1-alpha.5
    
    * GROW-26 add default for message body
    
    * Publish
    
     - @segment/ajv-human-errors@1.4.0
     - @segment/browser-destinations@3.5.0
     - @segment/actions-cli@3.5.0
     - @segment/actions-core@3.5.0
     - @segment/action-destinations@3.5.0
     - @segment/destination-subscriptions@3.4.0
    
    Co-authored-by: Vadim Demedes <vadimdemedes@hey.com>
    Co-authored-by: Matt Shwery <matthew.shwery@segment.com>

---
+++ packages/destination-actions/src/destinations/personas-messaging-twilio/index.ts
@@ -0,0 +1,59 @@
+import type { DestinationDefinition } from '@segment/actions-core'
+import type { Settings } from './generated-types'
+import sendSms from './sendSms'
+
+const destination: DestinationDefinition<Settings> = {
+  name: 'Actions Personas Messaging Twilio',
+  mode: 'cloud',
+  authentication: {
+    scheme: 'custom',
+    fields: {
+      twilioAccountId: {
+        label: 'Twilio Account ID',
+        description: 'Twilio Account ID',
+        type: 'string',
+        required: true
+      },
+      twilioAuthToken: {
+        label: 'Twilio Auth Token',
+        description: 'Twilio Auth Token',
+        type: 'password',
+        required: true
+      },
+      profileApiEnvironment: {
+        label: 'Profile API Environment',
+        description: 'Profile API Environment',
+        type: 'string',
+        required: true
+      },
+      profileApiSpaceId: {
+        label: 'Profile API Space ID',
+        description: 'Profile API Space ID',
+        type: 'string',
+        required: true
+      },
+      profileApiAccessToken: {
+        label: 'Profile API Access Token',
+        description: 'Profile API Access Token',
+        type: 'password',
+        required: true
+      }
+    },
+    testAuthentication: (request) => {
+      return request('https://api.twilio.com/2010-04-01')
+    }
+  },
+  // TODO: GROW-259 we'll uncomment this once we remove the calls to the profiles API,
+  // but for now this would extend those requests and result in 401s
+  // extendRequest: ({ settings }) => {
+  //   return {
+  //     username: settings.twilioAccountId,
+  //     password: settings.twilioAuthToken
+  //   }
+  // },
+  actions: {
+    sendSms
+  }
+}
+
+export default destination
---
+++ packages/destination-actions/src/destinations/personas-messaging-twilio/sendSms/index.ts
@@ -0,0 +1,125 @@
+import Mustache from 'mustache'
+import type { ActionDefinition, RequestOptions } from '@segment/actions-core'
+import type { Settings } from '../generated-types'
+import type { Payload } from './generated-types'
+
+const getProfileApiEndpoint = (environment: string): string => {
+  return `https://profiles.segment.${environment === 'production' ? 'com' : 'build'}`
+}
+
+type RequestFn = (url: string, options?: RequestOptions) => Promise<Response>
+
+const fetchProfileTraits = async (
+  request: RequestFn,
+  settings: Settings,
+  profileId: string
+): Promise<Record<string, string>> => {
+  const endpoint = getProfileApiEndpoint(settings.profileApiEnvironment)
+  const response = await request(
+    `${endpoint}/v1/spaces/${settings.profileApiSpaceId}/collections/users/profiles/user_id:${profileId}/traits?limit=200`,
+    {
+      headers: {
+        authorization: `Basic ${Buffer.from(settings.profileApiAccessToken + ':').toString('base64')}`,
+        'content-type': 'application/json'
+      }
+    }
+  )
+
+  const body = await response.json()
+  return body.traits
+}
+
+const fetchProfileExternalIds = async (
+  request: RequestFn,
+  settings: Settings,
+  profileId: string
+): Promise<Record<string, string>> => {
+  const endpoint = getProfileApiEndpoint(settings.profileApiEnvironment)
+  const response = await request(
+    `${endpoint}/v1/spaces/${settings.profileApiSpaceId}/collections/users/profiles/user_id:${profileId}/external_ids?limit=25`,
+    {
+      headers: {
+        authorization: `Basic ${Buffer.from(settings.profileApiAccessToken + ':').toString('base64')}`,
+        'content-type': 'application/json'
+      }
+    }
+  )
+
+  const body = await response.json()
+  const externalIds: Record<string, string> = {}
+
+  for (const externalId of body.data) {
+    externalIds[externalId.type] = externalId.id
+  }
+
+  return externalIds
+}
+
+interface Profile {
+  user_id?: string
+  anonymous_id?: string
+  phone?: string
+  traits: Record<string, string>
+}
+
+const action: ActionDefinition<Settings, Payload> = {
+  title: 'Send SMS',
+  description: 'Send SMS using Twilio',
+  defaultSubscription: 'type = "track" and event = "Audience Entered"',
+  fields: {
+    userId: {
+      label: 'User ID',
+      description: 'User ID in Segment',
+      type: 'string',
+      required: true,
+      default: { '@path': '$.userId' }
+    },
+    fromNumber: {
+      label: 'From Number',
+      description: 'Which number to send SMS from',
+      type: 'string',
+      required: true,
+      default: { '@path': '$.properties.fromNumber' }
+    },
+    body: {
+      label: 'Message',
+      description: 'Message to send',
+      type: 'text',
+      required: true,
+      default: { '@path': '$.properties.body' }
+    }
+  },
+  perform: async (request, { settings, payload }) => {
+    const [traits, externalIds] = await Promise.all([
+      fetchProfileTraits(request, settings, payload.userId),
+      fetchProfileExternalIds(request, settings, payload.userId)
+    ])
+
+    const profile: Profile = {
+      ...externalIds,
+      traits
+    }
+
+    if (!profile.phone) {
+      return
+    }
+
+    // TODO: GROW-259 remove this when we can extend the request
+    // and we no longer need to call the profiles API first
+    const token = Buffer.from(`${settings.twilioAccountId}:${settings.twilioAuthToken}`).toString('base64')
+
+    return request(`https://api.twilio.com/2010-04-01/Accounts/${settings.twilioAccountId}/Messages.json`, {
+      method: 'POST',
+      headers: {
+        authorization: `Basic ${token}`
+      },
+      body: new URLSearchParams({
+        Body: Mustache.render(payload.body, profile),
+        From: payload.fromNumber,
+        To: profile.phone
+      })
+    })
+  }
+}
+
+export default action
