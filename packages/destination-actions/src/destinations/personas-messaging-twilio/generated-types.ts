8eb1684eccf2160c4f022aa4b044a18645ca4cd2
Lauren Fries Brink <6043178+laurenfb@users.noreply.github.com>
2021-08-23 16:40:53 -0600

    GROW-26: add Twilio destination action #minor (#41)
    * GROW-26 Add Twilio destination specific to Personas Messaging product
    
    * Update packages/destination-actions/src/destinations/personas-messaging-twilio/sendSms/index.ts
    
    Co-authored-by: Matt Shwery <matthew.shwery@segment.com>
    
    * Remove `form` helper
    
    * GROW-26 add cloud mode to destination definition
    
    * GROW-26 add GET request to twilio API to validate credentials
    
    * GROW-26 add comments to indicate what can be removed post-CPS
    
    * GROW-26 use type = password instead of type = string
    
    Co-authored-by: Matt Shwery <matthew.shwery@segment.com>
    
    * GROW-26 omit extra header
    
    Co-authored-by: Matt Shwery <matthew.shwery@segment.com>
    
    * GROW-26 remove twilio phone number fallback
    
    * GROW-26 remove handlebars 4.7.7
    
    * GROW-26 use Mustache instead of Handlebars
    
    * GROW-26 oops, missed a place to use type = password
    
    Co-authored-by: Matt Shwery <matthew.shwery@segment.com>
    
    * GROW-26 remove conditional default and specify (hopefully more correct) defaults for fromNumber and userId
    
    * GROW-26 bring id & action name up to date with production data
    
    * GROW-26 temp comment out fromNumber default to see impact on test
    
    * Revert "GROW-26 temp comment out fromNumber default to see impact on test"
    
    This reverts commit f42ea912edf1f040135bcd5b614bb0b424254e15.
    
    * GROW-26 use user_id rather than profile.user_id because mustache gonna mustache
    
    * GROW-26 use profile.traits.phone & adjust tests to pass
    
    * Publish
    
     - @segment/ajv-human-errors@1.3.2-alpha.0
     - @segment/browser-destinations@3.4.1-alpha.0
     - @segment/actions-cli@3.4.1-alpha.0
     - @segment/actions-core@3.4.1-alpha.0
     - @segment/action-destinations@3.4.1-alpha.0
     - @segment/destination-subscriptions@3.3.1-alpha.0
    
    * GROW-26 add exclamation to comment
    
    * Publish
    
     - @segment/actions-cli@3.4.1-alpha.1
     - @segment/action-destinations@3.4.1-alpha.1
    
    * GROW-26 bye bye exclamation point
    
    * Publish
    
     - @segment/actions-cli@3.4.1-alpha.2
     - @segment/action-destinations@3.4.1-alpha.2
    
    * Revert "GROW-26 bye bye exclamation point"
    
    This reverts commit eddc95e08536f447c0b9c839c4d584091094c1c3.
    
    * Publish
    
     - @segment/actions-cli@3.4.1-alpha.3
     - @segment/action-destinations@3.4.1-alpha.3
    
    * GROW-26 go back to using externalId for phone
    
    * Publish
    
     - @segment/actions-cli@3.4.1-alpha.4
     - @segment/action-destinations@3.4.1-alpha.4
    
    * GROW-26 we're doing this exclamation point dance again trying to lerna publish :eyeroll:
    
    * Publish
    
     - @segment/actions-cli@3.4.1-alpha.5
     - @segment/action-destinations@3.4.1-alpha.5
    
    * GROW-26 add default for message body
    
    * Publish
    
     - @segment/ajv-human-errors@1.4.0
     - @segment/browser-destinations@3.5.0
     - @segment/actions-cli@3.5.0
     - @segment/actions-core@3.5.0
     - @segment/action-destinations@3.5.0
     - @segment/destination-subscriptions@3.4.0
    
    Co-authored-by: Vadim Demedes <vadimdemedes@hey.com>
    Co-authored-by: Matt Shwery <matthew.shwery@segment.com>

---
+++ packages/destination-actions/src/destinations/personas-messaging-twilio/generated-types.ts
@@ -0,0 +1,24 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Settings {
+  /**
+   * Twilio Account ID
+   */
+  twilioAccountId: string
+  /**
+   * Twilio Auth Token
+   */
+  twilioAuthToken: string
+  /**
+   * Profile API Environment
+   */
+  profileApiEnvironment: string
+  /**
+   * Profile API Space ID
+   */
+  profileApiSpaceId: string
+  /**
+   * Profile API Access Token
+   */
+  profileApiAccessToken: string
+}
