56f177dd28f2d4961e3e3281236647dae6ae9258
Julio <julio.farah@gmail.com>
2021-08-30 11:51:56 -0700

    [braze web] Fix traits being passed as custom_attributes on logUserProfile

--- packages/browser-destinations/src/destinations/braze/updateUserProfile/index.ts
+++ packages/browser-destinations/src/destinations/braze/updateUserProfile/index.ts
@@ -174,9 +174,9 @@ const action: BrowserActionDefinition<Settings, typeof appboy, Payload> = {
       }
     }
 
+    const reservedFields = [...Object.keys(action.fields ?? {}), 'firstName', 'lastName']
     if (payload.custom_attributes !== undefined) {
       Object.entries(payload.custom_attributes).forEach(([key, value]) => {
-        const reservedFields = Object.keys(action.fields ?? {})
         if (!reservedFields.includes(key)) {
           user.setCustomUserAttribute(key, value as string | number | boolean | Date | string[] | null)
         }
