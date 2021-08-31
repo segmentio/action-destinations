deee267ecfc505d44b2ee919253240be4cd8ec50
Jina Park <jina.park@segment.com>
2021-08-31 10:52:33 -0400

    Add autoprompt for destination argument

--- packages/cli/src/commands/generate/snapshots.ts
+++ packages/cli/src/commands/generate/snapshots.ts
@@ -1,9 +1,11 @@
 import { Command, flags } from '@oclif/command'
+import { renderTemplates } from '../../lib/templates'
+import { exec } from 'child_process'
+import { autoPrompt } from '../../lib/prompt'
 import chalk from 'chalk'
 import path from 'path'
 import ora from 'ora'
-import { renderTemplates } from '../../lib/templates'
-import { exec } from 'child_process'
+import globby from 'globby'
 
 export default class GenerateSnapshots extends Command {
   private spinner: ora.Ora = ora()
@@ -18,26 +20,55 @@ export default class GenerateSnapshots extends Command {
     help: flags.help({ char: 'h' })
   }
 
-  static args = [{ name: 'destination', description: 'destination name', required: true }]
+  static args = [{ name: 'destination', description: 'destination name' }]
 
   async run() {
     const { args } = this.parse(GenerateSnapshots)
     const integrationsGlob = './packages/destination-actions/src/destinations'
-    const targetPath = `${integrationsGlob}/${args.destination}`
+
+    let destinationName = args.destination
+    if (!destinationName) {
+      const integrationsPath = `${integrationsGlob}/*`
+      const integrationDirs = await globby(integrationsPath, {
+        expandDirectories: false,
+        onlyDirectories: true,
+        gitignore: true,
+        ignore: ['node_modules']
+      })
+
+      const { selectedDestination } = await autoPrompt<{ selectedDestination: { name: string } }>(args, {
+        type: 'select',
+        name: 'selectedDestination',
+        message: 'Which destination?',
+        choices: integrationDirs.map((integrationPath) => {
+          const [name] = integrationPath.split(path.sep).reverse()
+          return {
+            title: name,
+            value: { name }
+          }
+        })
+      })
+
+      if (selectedDestination) {
+        destinationName = selectedDestination.name
+      }
+    }
+
+    const targetPath = `${integrationsGlob}/${destinationName}`
     const templatePath = path.join(__dirname, '../../../templates/actions/snapshot')
 
     try {
-      this.spinner.start(`Creating ${chalk.bold(args.name)}`)
+      this.spinner.start(`Creating snapshot tests for ${chalk.bold(destinationName)} destination`)
       renderTemplates(
         templatePath,
         targetPath,
         {
-          destination: args.destination,
+          destination: destinationName,
           integrationsGlob
         },
         true
       )
-      this.spinner.succeed(`Create snapshot test for ${args.destination} ${args.action} action`)
+      this.spinner.succeed(`Created snapshot tests for ${destinationName} destination`)
     } catch (err) {
       this.spinner.fail(`Snapshot test creation failed: ${chalk.red(err.message)}`)
       this.exit()
