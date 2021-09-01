110ba27274a474a2be641318899ec524e63c9ff8
Nick Aguilar <nicholas.aguilar@segment.com>
2021-08-31 20:47:57 -0700

    Extended test

--- packages/destination-actions/src/destinations/actions-facebook-conversions/purchase/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/actions-facebook-conversions/purchase/__tests__/index.test.ts
@@ -52,4 +52,43 @@ describe('purchase', () => {
     expect(responses.length).toBe(1)
     expect(responses[0].status).toBe(201)
   })
+
+  it('should throw an error if no user_data keys are included', async () => {
+    nock(`https://graph.facebook.com/v11.0/${settings.pixelId}`, {
+      reqheaders: {
+        bearer: settings.token
+      }
+    })
+      .post('/events')
+      .reply(200, {})
+
+    const event = createTestEvent({
+      event: 'Order Completed',
+      userId: '7b17fb0bd173f625b58636fb796407c22b3d16fc78302d79f0fd30c2fc2fc068', // Pre -hashed for simplicity
+      timestamp: '1629827640',
+      properties: {
+        action_source: 'email',
+        currency: 'USD',
+        value: 12.12
+      }
+    })
+
+    try {
+      await testDestination.testAction('purchase', {
+        event,
+        settings,
+        mapping: {
+          currency: {
+            '@path': '$.properties.currency'
+          },
+          value: {
+            '@path': '$.properties.value'
+          }
+          //No user data mapping
+        }
+      })
+    } catch (e) {
+      expect(e.message).toBe('Must include at least one user data property')
+    }
+  })
 })
