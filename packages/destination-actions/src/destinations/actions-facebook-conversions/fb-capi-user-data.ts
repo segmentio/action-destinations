86b7a14f987169d486b8c281bfa7e26cb42407c3
Nick Aguilar <nicholas.aguilar@segment.com>
2021-08-31 20:31:45 -0700

    removed unused code

--- packages/destination-actions/src/destinations/actions-facebook-conversions/fb-capi-user-data.ts
+++ packages/destination-actions/src/destinations/actions-facebook-conversions/fb-capi-user-data.ts
@@ -1,55 +1,32 @@
-import { InputField } from "@segment/actions-core/src/destination-kit/types"
+import { InputField } from '@segment/actions-core/src/destination-kit/types'
 
 // Implementation of Facebook user data object
 // https://developers.facebook.com/docs/marketing-api/conversions-api/parameters/customer-information-parameters
 
 export const user_data: InputField = {
-    label: 'User Data',
-    description: 'User Data',
-    type: 'object',
-    properties: {
-        email: {
-            label: 'Email',
-            description: 'User Email',
-            type: 'string'
-        },
-        phone: {
-            label: 'Phone',
-            description: 'User phone number',
-            type: 'string'
-        },
-        gender: {
-            label: 'Gender',
-            description: 'User gender',
-            type: 'string'
-        },
-        client_user_agent: {
-            label: 'Client User Agent',
-            description: 'Client User Agent',
-            type: 'string'
-        }
+  label: 'User Data',
+  description: 'User Data',
+  type: 'object',
+  properties: {
+    email: {
+      label: 'Email',
+      description: 'User Email',
+      type: 'string'
+    },
+    phone: {
+      label: 'Phone',
+      description: 'User phone number',
+      type: 'string'
+    },
+    gender: {
+      label: 'Gender',
+      description: 'User gender',
+      type: 'string'
+    },
+    client_user_agent: {
+      label: 'Client User Agent',
+      description: 'Client User Agent',
+      type: 'string'
     }
-}
-
-// TODO: Can we check if these values are hashed with SHA256?
-// FB will reject if we send non hashed values for many of these
-export interface UserData {
-    em?: string
-    ph?: string
-    ge?: string
-    db?: string
-    ln?: string
-    fn?: string
-    ct?: string
-    state?: string
-    zp?: string
-    country?: string
-    external_id?: string
-    client_ip_address?: string
-    client_user_agent?: string
-    fbc?: string
-    fbp?: string
-    subscription_id?: string
-    lead_id?: string
-    fb_login_id?: string
+  }
 }
--- packages/destination-actions/src/destinations/actions-facebook-conversions/generated-types.ts
+++ packages/destination-actions/src/destinations/actions-facebook-conversions/generated-types.ts
@@ -5,8 +5,4 @@ export interface Settings {
    * The Pixel ID
    */
   pixelId: string
-  /**
-   * Access Token, for testing purposes only, will not be included in final destination.
-   */
-  token: string
 }
