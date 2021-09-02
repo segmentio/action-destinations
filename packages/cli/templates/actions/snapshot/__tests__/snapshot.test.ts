fc63625a21430d1c41869f46b40e78136747cf4b
Jina Park <jina.park@segment.com>
2021-09-02 17:50:17 -0400

    Prettify snapshots

--- packages/cli/templates/actions/snapshot/__tests__/snapshot.test.ts
+++ packages/cli/templates/actions/snapshot/__tests__/snapshot.test.ts
@@ -1,5 +1,6 @@
 import { createTestIntegration } from '@segment/actions-core'
 import destination from '../index'
+import prettier from 'prettier'
 
 const testDestination = createTestIntegration(destination)
 
@@ -7,17 +8,31 @@ describe('Testing snapshot for {{destination}} action:', () => {
   for (const actionSlug in destination.actions) {
     it(`${actionSlug} [required]`, async () => {
       const responses = await testDestination.snapshotAction(destination, actionSlug, true)
-      const requestBody = responses[0].request.body
-      if (requestBody) {
-        expect(requestBody.toString()).toMatchSnapshot()
+      const request = responses[0].request
+
+      expect(request.headers).toMatchSnapshot()
+
+      if (request.body) {
+        const bodyString = decodeURIComponent(request.body.toString())
+        if (bodyString[0] === '{') {
+          expect(prettier.format(request.body.toString(), { parser: 'json' })).toMatchSnapshot()
+        } else {
+          expect(request.body.toString()).toMatchSnapshot()
+        }
       }
     })
 
     it(`${actionSlug} [all fields]`, async () => {
       const responses = await testDestination.snapshotAction(destination, actionSlug, false)
-      const requestBody = responses[0].request.body
-      if (requestBody) {
-        expect(requestBody.toString()).toMatchSnapshot()
+      const request = responses[0].request
+
+      if (request.body) {
+        const bodyString = decodeURIComponent(request.body.toString())
+        if (bodyString[0] === '{') {
+          expect(prettier.format(request.body.toString(), { parser: 'json' })).toMatchSnapshot()
+        } else {
+          expect(request.body.toString()).toMatchSnapshot()
+        }
       }
     })
   }
