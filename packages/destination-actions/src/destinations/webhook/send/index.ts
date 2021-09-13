6d0e2f2139f18f950850803142c332f9653f6c40
Tyson Mote <tyson@segment.com>
2021-09-13 15:34:18 -0700

    Remove unused request methods

--- packages/destination-actions/src/destinations/webhook/send/index.ts
+++ packages/destination-actions/src/destinations/webhook/send/index.ts
@@ -2,19 +2,7 @@ import type { ActionDefinition } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 
-type RequestMethod =
-  | 'get'
-  | 'post'
-  | 'put'
-  | 'delete'
-  | 'patch'
-  | 'head'
-  | 'GET'
-  | 'POST'
-  | 'PUT'
-  | 'DELETE'
-  | 'PATCH'
-  | 'HEAD'
+type RequestMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Send',
