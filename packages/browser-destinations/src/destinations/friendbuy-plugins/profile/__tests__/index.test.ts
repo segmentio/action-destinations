22a0df71040aabe7b0e48992613e6e19c959b001
David Hull <david.hull@friendbuy.com>
2021-10-06 16:17:32 -0700

    Add profile friendbuy-plugins browser plugin.

--- packages/browser-destinations/src/destinations/friendbuy-plugins/profile/__tests__/index.test.ts
+++ packages/browser-destinations/src/destinations/friendbuy-plugins/profile/__tests__/index.test.ts
@@ -1,7 +1,113 @@
-// import { Analytics, Context } from '@segment/analytics-next'
-// import * as jsdom from 'jsdom'
-// import { destination } from '../../index'
+import { Analytics, Context, Plugin } from '@segment/analytics-next'
+import * as jsdom from 'jsdom'
+import browserPluginsDestination from '../..'
+import { Subscription } from '../../../../lib/browser-destinations'
+import { friendbuyLocalStorageKey } from '../index'
+import { get } from 'lodash'
 
-describe('FriendbuyPlugins.profile', () => {
-  test('empty test', () => {})
+const example: Subscription[] = [
+  {
+    partnerAction: 'profile',
+    name: 'profile',
+    enabled: true,
+    subscribe: 'type = "track"',
+    mapping: {}
+  }
+]
+
+let browserActions: Plugin[]
+let profilePlugin: Plugin
+let ajs: Analytics
+
+const profile = 'JWT'
+const friendbuyLocalStorage = JSON.stringify({ tracker: JSON.stringify({ tracker: profile }) })
+
+beforeEach(async () => {
+  jest.restoreAllMocks()
+  jest.resetAllMocks()
+
+  const html = `
+  <!DOCTYPE html>
+    <head>
+      <script>'hi'</script>
+    </head>
+    <body>
+    </body>
+  </html>
+  `.trim()
+
+  const jsd = new jsdom.JSDOM(html, {
+    runScripts: 'dangerously',
+    resources: 'usable',
+    url: 'https://localhost'
+  })
+
+  const windowSpy = jest.spyOn(window, 'window', 'get')
+  windowSpy.mockImplementation(() => jsd.window as unknown as Window & typeof globalThis)
+
+  browserActions = await browserPluginsDestination({ subscriptions: example })
+  profilePlugin = browserActions[0]
+
+  ajs = new Analytics({
+    writeKey: 'w_123'
+  })
+})
+
+describe('ajs-integration', () => {
+  test('runs as an enrichment middleware', async () => {
+    await ajs.register(profilePlugin)
+    jest.spyOn(profilePlugin, 'track')
+
+    const ctx = new Context({
+      type: 'track',
+      event: 'greet',
+      properties: {
+        greeting: 'Oi!'
+      }
+    })
+
+    await ajs.track(ctx.event)
+
+    expect(profilePlugin.track).toHaveBeenCalled()
+    expect(ajs.queue.plugins).toEqual([
+      expect.objectContaining({
+        name: 'Friendbuy Plugins profile',
+        type: 'enrichment'
+      })
+    ])
+  })
+})
+
+describe('profile', () => {
+  beforeEach(async () => {
+    await profilePlugin.load(Context.system(), ajs)
+  })
+
+  test('no profile in local storage', async () => {
+    const ctx = new Context({
+      type: 'track',
+      event: 'greet',
+      properties: {
+        greeting: 'Oi!'
+      }
+    })
+
+    const updatedCtx = await profilePlugin.track?.(ctx)
+    expect(get(updatedCtx, ['event', 'integrations', 'Actions Friendbuy'])).toBeUndefined()
+  })
+
+  test('existing profile in local storage', async () => {
+    window.localStorage.setItem(friendbuyLocalStorageKey, friendbuyLocalStorage)
+
+    const ctx = new Context({
+      type: 'track',
+      event: 'greet',
+      properties: {
+        greeting: 'Oi!'
+      }
+    })
+
+    const updatedCtx = await profilePlugin.track?.(ctx)
+    expect(get(updatedCtx, ['event', 'integrations', 'Actions Friendbuy', 'profile'])).toBe(profile)
+  })
 })
--- packages/browser-destinations/src/destinations/friendbuy-plugins/profile/index.ts
+++ packages/browser-destinations/src/destinations/friendbuy-plugins/profile/index.ts
@@ -2,14 +2,35 @@ import type { BrowserActionDefinition } from '../../../lib/browser-destinations'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 
+export const friendbuyLocalStorageKey = 'persist:friendbuy-msdk-06192019-root'
+
 // Change from unknown to the partner SDK types
 const action: BrowserActionDefinition<Settings, unknown, Payload> = {
   title: 'Profile',
-  description: '',
+  description: 'Attaches the Friendbuy profile to browser events',
   platform: 'web',
+  defaultSubscription: 'type = "track" or type = "identify"',
   fields: {},
-  perform: (_client) => {
-    // Invoke Partner SDK here
+  lifecycleHook: 'enrichment',
+  perform: (_client, { context, _payload }) => {
+    const ls = window.localStorage
+    try {
+      const friendbuyLocalStorageJson = ls.getItem(friendbuyLocalStorageKey)
+      if (!friendbuyLocalStorageJson) {
+        return
+      }
+      const trackerJson = JSON.parse(friendbuyLocalStorageJson).tracker
+      if (!trackerJson) {
+        return
+      }
+      const profile = JSON.parse(trackerJson).tracker
+      if (!profile) {
+        return
+      }
+      context.updateEvent('integrations.Actions Friendbuy.profile', profile)
+    } catch (e) {
+      /* comment to prevent eslint from complaining that block is empty */
+    }
   }
 }
 
