e3e7d8af7783c9de8e754822b527cf1eb6ad37da
David Hull <david.hull@friendbuy.com>
2021-10-08 17:38:39 -0700

    ./bin/run init -n Friendbuy -s friendbuy -t browser

---
+++ packages/browser-destinations/src/destinations/friendbuy/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+// import { Analytics, Context } from '@segment/analytics-next'
+// import * as jsdom from 'jsdom'
+// import plugins, { destination } from '../index'
+
+describe('Friendbuy', () => {
+  test('empty test', () => {})
+})
