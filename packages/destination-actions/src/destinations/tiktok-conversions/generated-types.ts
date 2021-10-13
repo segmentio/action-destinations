c7225278e5838d1850a332cb70786dacd55556f0
Corey Ching <imcoreyching@gmail.com>
2021-10-13 15:29:11 -0700

    types need to be generated from commits that were made in github (#261)

--- packages/destination-actions/src/destinations/tiktok-conversions/generated-types.ts
+++ packages/destination-actions/src/destinations/tiktok-conversions/generated-types.ts
@@ -2,15 +2,15 @@
 
 export interface Settings {
   /**
-   * TikTok Long Term Access Token. You can generate this from the TikTok Marketing API portal. Please following TikToks [Authorization guide](https://ads.tiktok.com/athena/docs/index.html?plat_id=-1&doc_id=100010&id=100681&key=e98b971a296ae45d8e35a22fba032d1c06f5973de9aab73ce07b82f230cf3afd) for more info.
+   * TikTok Long Term Access Token. You can generate this from the TikTok Marketing API portal. Please follow TikTok's [Authorization guide](https://ads.tiktok.com/athena/docs/index.html?plat_id=-1&doc_id=100010&id=100681&key=e98b971a296ae45d8e35a22fba032d1c06f5973de9aab73ce07b82f230cf3afd) for more info.
    */
   accessToken: string
   /**
-   * TikTok app secret key. You can find this key in the "Basic Information" tab of your TikTok app.
+   * TikTok App Secret Key. You can find this key in the "Basic Information" tab of your TikTok app.
    */
   secretKey: string
   /**
-   * TikTok app id. You can find this key in the "Basic Information" tab of your TikTok app.
+   * TikTok App Id. You can find this key in the "Basic Information" tab of your TikTok app.
    */
   appId: string
   /**
