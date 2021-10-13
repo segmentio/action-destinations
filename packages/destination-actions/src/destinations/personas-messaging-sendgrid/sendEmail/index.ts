8c0217bb85999e5629b8774214f8fa49765e9cb7
Lauren Fries Brink <6043178+laurenfb@users.noreply.github.com>
2021-10-13 15:44:57 -0600

    GROW-407 - block demo workspace email domains from execution  (#260)
    * GROW-407 throw an error when a restricted domain is passed as toEmail
    
    * GROW-407 better descriptive names for tests
    
    * GROW-407 add unit test for restricted domain
    
    * GROW-407 adjust happy path test to be more resilient - responses should exist, not be a prescribed length
    
    * GROW-407 test for all 4 restricted domains... maybe overkill?
    
    * GROW-407 DRY up tests a little by using a helper function where it is possible to do so
    
    * GROW-407 add a one-liner to the readme about debugging
    
    * GROW-407 make README re: node flags a lil clearer

--- packages/destination-actions/src/destinations/personas-messaging-sendgrid/sendEmail/index.ts
+++ packages/destination-actions/src/destinations/personas-messaging-sendgrid/sendEmail/index.ts
@@ -1,4 +1,4 @@
-import type { ActionDefinition, RequestOptions } from '@segment/actions-core'
+import { ActionDefinition, IntegrationError, RequestOptions } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 import Mustache from 'mustache'
@@ -54,6 +54,18 @@ const fetchProfileExternalIds = async (
   }
 
   return externalIds
+}
+
+const isRestrictedDomain = (email: string): boolean => {
+  const restricted = ['gmailx.com', 'yahoox.com', 'aolx.com', 'hotmailx.com']
+  const matches = /^.+@(.+)$/.exec(email.toLowerCase())
+
+  if (!matches) {
+    return false
+  }
+
+  const domain = matches[1]
+  return restricted.includes(domain)
 }
 
 interface Profile {
@@ -182,6 +194,14 @@ const action: ActionDefinition<Settings, Payload> = {
 
     if (!toEmail) {
       return
+    }
+
+    if (isRestrictedDomain(toEmail)) {
+      throw new IntegrationError(
+        'Emails with gmailx.com, yahoox.com, aolx.com, and hotmailx.com domains are blocked.',
+        'Invalid input',
+        400
+      )
     }
 
     let name
