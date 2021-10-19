6aa0eb1ccf88a676860c30dca317ef013e944c7a
Matt Shwery <matthew.shwery@segment.com>
2021-10-19 13:55:06 -0500

    use project references to build in webpack

--- packages/browser-destinations/webpack.config.js
+++ packages/browser-destinations/webpack.config.js
@@ -61,6 +61,8 @@ module.exports = {
           {
             loader: 'ts-loader',
             options: {
+              configFile: 'tsconfig.build.json',
+              projectReferences: true,
               transpileOnly: true
             }
           }
