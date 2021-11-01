4c535cb31fc01b5d3dd03d50e576e58cc6c29ff5
Gabriel Samson <gpsamson@users.noreply.github.com>
2021-11-01 10:37:41 -0700

    Add onDelete support to Customer.io (#297)
    * Add onDelete support to Customer.io
    
    * nit changes

---
+++ packages/destination-actions/src/destinations/customerio/__tests__/delete.test.ts
@@ -0,0 +1,41 @@
+import nock from 'nock'
+import { createTestIntegration } from '@segment/actions-core'
+import CustomerIO from '../index'
+import { DecoratedResponse } from '@segment/actions-core'
+import { AccountRegion } from '../utils'
+
+const testDestination = createTestIntegration(CustomerIO)
+
+describe('Customer.io', () => {
+    describe('onDelete', () => {
+        it('should support user deletions, defaulting to the US', async () => {
+            nock('https://track.customer.io').delete('/api/v1/customers/sloth@segment.com').reply(200, {})
+            expect(testDestination.onDelete).toBeDefined()
+
+            if (testDestination.onDelete) {
+                const response = await testDestination?.onDelete(
+                    { type: 'track', userId: 'sloth@segment.com' },
+                    { siteId: 'foo', apiKey: 'bar' }
+                )
+                const resp = response as DecoratedResponse
+                expect(resp.status).toBe(200)
+                expect(resp.data).toMatchObject({})
+            }
+        })
+
+        it('should support regional user deletions', async () => {
+            nock('https://track-eu.customer.io').delete('/api/v1/customers/sloth@segment.com').reply(200, {})
+            expect(testDestination.onDelete).toBeDefined()
+
+            if (testDestination.onDelete) {
+                const response = await testDestination?.onDelete(
+                    { type: 'track', userId: 'sloth@segment.com' },
+                    { siteId: 'foo', apiKey: 'bar', accountRegion: AccountRegion.EU }
+                )
+                const resp = response as DecoratedResponse
+                expect(resp.status).toBe(200)
+                expect(resp.data).toMatchObject({})
+            }
+        })
+    })
+})
