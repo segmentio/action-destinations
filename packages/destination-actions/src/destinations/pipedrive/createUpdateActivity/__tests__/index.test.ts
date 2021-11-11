f34bd7c461eb20ad9058aae51b374fd143f96bbf
Ingvar Lond <ingvar.lond@gmail.com>
2021-11-11 14:50:51 +0200

    unit tests not working

--- packages/destination-actions/src/destinations/pipedrive/createUpdateActivity/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/pipedrive/createUpdateActivity/__tests__/index.test.ts
@@ -1,9 +1,22 @@
 import nock from 'nock'
-import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import { createTestIntegration } from '@segment/actions-core'
 import Destination from '../../index'
 
 const testDestination = createTestIntegration(Destination)
 
+const PIPEDRIVE_API_KEY = 'random string'
+const PIPEDRIVE_DOMAIN = 'companydomain'
+
 describe('Pipedrive.createUpdateActivity', () => {
-  // TODO: Test your action
+  it('should work', async () => {
+    nock(`https://${PIPEDRIVE_DOMAIN}.pipedrive.com/api/v1`)
+      .post('/activities', { subject: 'Some Name' })
+      .query({params: {api_token : PIPEDRIVE_API_KEY}})
+      .reply(200)
+
+    await testDestination.testAction('createUpdateActivity', {
+      mapping: { subject: 'Some Name' },
+      settings: { apiToken: PIPEDRIVE_API_KEY, domain: PIPEDRIVE_DOMAIN }
+    })
+  })
 })
