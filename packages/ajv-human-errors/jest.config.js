2a35deef4375c6462613b396798ec404f77edcfa
Matt Shwery <matthew.shwery@segment.com>
2021-11-15 13:11:10 -0600

    run tests without build

--- packages/ajv-human-errors/jest.config.js
+++ packages/ajv-human-errors/jest.config.js
@@ -3,14 +3,9 @@ module.exports = {
   preset: 'ts-jest',
   globals: {
     'ts-jest': {
-      isolatedModules: false,
-    },
+      isolatedModules: false
+    }
   },
-  testRegex: "((\\.|/)(test))\\.(tsx?|json)$",
-  modulePathIgnorePatterns: [
-    "<rootDir>/dist/"
-  ],
-  moduleNameMapper: {
-    "src/lib/(.*)": "<rootDir>/src/lib/$1"
-  },
-};
+  testRegex: '((\\.|/)(test))\\.(tsx?|json)$',
+  modulePathIgnorePatterns: ['<rootDir>/dist/']
+}
