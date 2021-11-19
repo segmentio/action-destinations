fbb36e468a3179acedfe50d4a0004ddee0caa2b0
Nick Aguilar <nicholas.aguilar@segment.com>
2021-11-19 13:59:41 -0800

    Arrayify array fields when a user passes in a scalar

--- packages/core/src/arrify.ts
+++ packages/core/src/arrify.ts
@@ -1,7 +1,32 @@
-import { isArray } from './real-type-of'
+import type { JSONSchema4 } from 'json-schema'
+import { isArray, isObject } from './real-type-of'
 
-export function arrify<T>(value: T | T[] | undefined): T[] {
-  if (value === undefined || value === null) return []
+export function arrify<T>(value: T | T[] | undefined | null, treatNullAsEmpty?: true): T[]
+export function arrify<T>(value: T | T[] | undefined | null, treatNullAsEmpty: false): T[] | undefined | null
+export function arrify<T>(value: T | T[] | undefined | null, treatNullAsEmpty = true): T[] | undefined | null {
+  if (value === undefined || value === null) return treatNullAsEmpty ? [] : (value as undefined | null)
   if (isArray(value)) return value
   return [value]
+}
+
+// Coerces non-arrays into arrays based on the jsonschema.
+// Mutates the original object
+export function arrifyFields(obj: unknown, schema: JSONSchema4 = {}): unknown {
+  if (!isObject(obj)) {
+    return obj
+  }
+
+  if (!schema.properties) return obj
+
+  for (const key of Object.keys(obj)) {
+    const fieldSchema = schema.properties[key]
+    if (!fieldSchema) continue
+    if (Array.isArray(fieldSchema.type)) continue
+
+    if (fieldSchema.type === 'array') {
+      obj[key] = arrify(obj[key], false)
+    }
+  }
+
+  return obj
 }
