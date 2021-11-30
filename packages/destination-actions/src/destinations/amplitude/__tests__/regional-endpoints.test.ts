41dc2dd4deb7c2fd96239e43ce12bfd809106d3c
Dalton Scharff <dalton.scharff@segment.com>
2021-11-30 10:48:38 -0600

    adds inline snapshot for endpoint mapping

--- packages/destination-actions/src/destinations/amplitude/__tests__/regional-endpoints.test.ts
+++ packages/destination-actions/src/destinations/amplitude/__tests__/regional-endpoints.test.ts
@@ -17,4 +17,38 @@ describe('Amplitude - Regional endpoints', () => {
     const result = getEndpointByRegion('httpapi', 'NONE')
     expect(result).toEqual(endpoints.httpapi.north_america)
   })
+  it('should return the correct endpoints', () => {
+    expect(endpoints).toMatchInlineSnapshot(`
+      Object {
+        "batch": Object {
+          "europe": "https://api.eu.amplitude.com/batch",
+          "north_america": "https://api2.amplitude.com/batch",
+        },
+        "deletions": Object {
+          "europe": "https://analytics.eu.amplitude.com/api/2/deletions/users",
+          "north_america": "https://amplitude.com/api/2/deletions/users",
+        },
+        "groupidentify": Object {
+          "europe": "https://api.eu.amplitude.com/groupidentify",
+          "north_america": "https://api2.amplitude.com/groupidentify",
+        },
+        "httpapi": Object {
+          "europe": "https://api.eu.amplitude.com/2/httpapi",
+          "north_america": "https://api2.amplitude.com/2/httpapi",
+        },
+        "identify": Object {
+          "europe": "https://api.eu.amplitude.com/identify",
+          "north_america": "https://api2.amplitude.com/identify",
+        },
+        "usermap": Object {
+          "europe": "https://api.eu.amplitude.com/usermap",
+          "north_america": "https://api.amplitude.com/usermap",
+        },
+        "usersearch": Object {
+          "europe": "https://analytics.eu.amplitude.com/api/2/usersearch",
+          "north_america": "https://amplitude.com/api/2/usersearch",
+        },
+      }
+    `)
+  })
 })
