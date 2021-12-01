3f5450d352f333c1776e388a82e07f119daaf3cd
Dalton Scharff <daltonscharff@users.noreply.github.com>
2021-12-01 09:17:47 -0600

    [BE-246] Amplitude Actions EU Endpoint (#356)
    * implements function to generate region specific amplitude endpoints
    
    * adds endpoint label to amplitude index.ts
    
    * makes generate-api-endpoint more readable
    
    * adds createEndpoint to all requests in amplitude
    
    * adds tests for amplitude eu endpoint
    
    * updates northAmerica to north_america
    
    * updates generated file description
    
    * fixes typo
    
    * simplify endpoint description
    
    Co-authored-by: Gabriel Samson <gpsamson@users.noreply.github.com>
    
    * replaces create-endpoint with regional-endpoints to avoid dynamic URL construction
    
    * adds returns text
    
    * increases endpoint readability
    
    Co-authored-by: Gabriel Samson <gpsamson@users.noreply.github.com>
    
    * adds inline snapshot for endpoint mapping
    
    Co-authored-by: Gabriel Samson <gpsamson@users.noreply.github.com>

--- packages/destination-actions/src/destinations/amplitude/groupIdentifyUser/index.ts
+++ packages/destination-actions/src/destinations/amplitude/groupIdentifyUser/index.ts
@@ -2,6 +2,7 @@ import type { ActionDefinition } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 import dayjs from '../../../lib/dayjs'
+import { getEndpointByRegion } from '../regional-endpoints'
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Group Identify User',
@@ -84,7 +85,7 @@ const action: ActionDefinition<Settings, Payload> = {
     }
 
     // Associate user to group
-    await request('https://api.amplitude.com/identify', {
+    await request(getEndpointByRegion('identify', settings.endpoint), {
       method: 'post',
       body: new URLSearchParams({
         api_key: settings.apiKey,
@@ -104,7 +105,7 @@ const action: ActionDefinition<Settings, Payload> = {
     })
 
     // Associate group properties
-    return request('https://api.amplitude.com/groupidentify', {
+    return request(getEndpointByRegion('groupidentify', settings.endpoint), {
       method: 'post',
       body: new URLSearchParams({
         api_key: settings.apiKey,
