eae229fea5361fd26bb1c452917d0ac15497d170
Jack Yiu <jackinyiu@gmail.com>
2021-12-01 16:05:01 -0500

    Built initial version of SA segment browser destination

---
+++ packages/browser-destinations/src/destinations/stackadapt/trackEvent/index.ts
@@ -0,0 +1,42 @@
+import type { BrowserActionDefinition } from '../../../lib/browser-destinations'
+import type { Settings } from '../generated-types'
+import type { Payload } from './generated-types'
+
+type StackAdaptSDK = (name: string, pixelId: string, payload: Record<string, string | number>) => void
+
+const action: BrowserActionDefinition<Settings, StackAdaptSDK, Payload> = {
+  title: 'Track Event',
+  description: 'Track events',
+  platform: 'web',
+  defaultSubscription: 'type = "track"',
+  fields: {
+    eventName: {
+      description: 'The name of the event.',
+      label: 'Name',
+      required: true,
+      type: 'string',
+      default: {
+        '@path': '$.event'
+      }
+    },
+    eventProperties: {
+      type: 'object',
+      required: false,
+      description: 'Hash of properties for this event.',
+      label: 'Event Properties',
+      default: {
+        '@path': '$.properties'
+      }
+    }
+  },
+  perform: (saq, event) => {
+    const pixelId = event.settings.universalPixelId
+
+    saq('ts', pixelId, {
+      eventName: event.payload.eventName,
+      ...(event.payload.eventProperties ?? {})
+    })
+  }
+}
+
+export default action
