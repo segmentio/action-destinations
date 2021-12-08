540df6d9da00f7f37fe840049bc9c1c1f89573fa
gpsamson <gabriel.samson@segment.com>
2021-12-07 16:09:42 -0800

    Remove path from tests

--- packages/destination-actions/src/destinations/webhook/__test__/webhook.test.ts
+++ packages/destination-actions/src/destinations/webhook/__test__/webhook.test.ts
@@ -9,20 +9,19 @@ describe('Webhook', () => {
   describe('send', () => {
     it('should work with default mapping', async () => {
       const url = 'https://mme-e2e.segment.com'
-      const path = '/1234'
       const event = createTestEvent({
         timestamp,
         event: 'Test Event'
       })
 
       nock(url)
-        .post(path, event as any)
+        .post('/', event as any)
         .reply(200)
 
       const responses = await testDestination.testAction('send', {
         event,
         mapping: {
-          url: url + path
+          url
         },
         useDefaultMappings: true
       })
@@ -33,19 +32,19 @@ describe('Webhook', () => {
 
     it('supports customizations', async () => {
       const url = 'https://mme-e2e.segment.build'
-      const path = '/1234'
+
       const event = createTestEvent({
         timestamp,
         event: 'Test Event'
       })
       const data = { cool: true }
 
-      nock(url).put(path, data).reply(200)
+      nock(url).put('/', data).reply(200)
 
       const responses = await testDestination.testAction('send', {
         event,
         mapping: {
-          url: url + path,
+          url,
           method: 'PUT',
           data: { cool: true }
         }
@@ -57,19 +56,18 @@ describe('Webhook', () => {
 
     it('supports customizations', async () => {
       const url = 'https://mme-e2e.segment.build'
-      const path = '/1234'
       const event = createTestEvent({
         timestamp,
         event: 'Test Event'
       })
       const data = { cool: true }
 
-      nock(url).put(path, data).reply(200)
+      nock(url).put('/', data).reply(200)
 
       const responses = await testDestination.testAction('send', {
         event,
         mapping: {
-          url: url + path,
+          url,
           method: 'PUT',
           data: { cool: true }
         }
@@ -81,20 +79,19 @@ describe('Webhook', () => {
 
     it('restricts usage to mme-e2e urls', async () => {
       const url = 'https://api.acme.com/webhook'
-      const path = '/1234'
       const event = createTestEvent({
         timestamp,
         event: 'Test Event'
       })
       const data = { cool: true }
 
-      nock(url).put(path, data).reply(200)
+      nock(url).put('/', data).reply(200)
 
       try {
         await testDestination.testAction('send', {
           event,
           mapping: {
-            url: url + path,
+            url,
             method: 'PUT',
             data: { cool: true }
           }
@@ -104,7 +101,7 @@ describe('Webhook', () => {
         const error = err as IntegrationError
         expect(error.status).toBe(400)
         expect(error.code).toBe('Bad Request')
-        expect(error.message).toBe("invalid url 'https://api.acme.com/webhook/1234'")
+        expect(error.message).toBe("invalid url 'https://api.acme.com/webhook'")
       }
     })
   })
