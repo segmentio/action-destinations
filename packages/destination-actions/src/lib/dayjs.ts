ba061440e669d59daeb075a917c517b828058c47
Marcus Ericsson <36717+mericsson@users.noreply.github.com>
2022-01-05 11:40:03 -0800

    Revert "[Customer.io] Only convert ISO 8601 dates" (#400)
    This reverts commit faae0f2f2acaf8b20b818803bcf7f241e350825f.

--- packages/destination-actions/src/lib/dayjs.ts
+++ packages/destination-actions/src/lib/dayjs.ts
@@ -1,10 +1,8 @@
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
-import customParseFormat from 'dayjs/plugin/customParseFormat'
 
 dayjs.extend(utc)
 dayjs.extend(advancedFormat)
-dayjs.extend(customParseFormat)
 
 export default dayjs
