dac7c7ed2210cdffe3503b083a9520326b68f4b9
Nick Aguilar <nicholas.aguilar@segment.com>
2022-01-10 09:22:05 -0800

    WIP Update operation

--- packages/destination-actions/src/destinations/salesforce/lead/index.ts
+++ packages/destination-actions/src/destinations/salesforce/lead/index.ts
@@ -1,9 +1,9 @@
-import type { ActionDefinition } from '@segment/actions-core'
+import { ActionDefinition, IntegrationError } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 import { buildQuery } from '../sf_utils'
-import { lookupRecordId, insertRecord, updateRecord } from '../sf_operations'
 
+const API_VERSION = 'v53.0'
 interface ExternalIDLookupResponse {
   Id: string
 }
@@ -17,19 +17,27 @@ const action: ActionDefinition<Settings, Payload> = {
       description: 'Operation',
       type: 'string',
       required: true,
-      choices: [{ label: 'Create', value: 'create' }, { label: 'Update', value: 'update'}, { label: 'Upsert', value: 'upsert' }]
+      choices: [
+        { label: 'Create', value: 'create' },
+        { label: 'Update', value: 'update' },
+        { label: 'Upsert', value: 'upsert' }
+      ]
     },
     lookup_criteria: {
       label: 'Lookup Criteria',
       description: 'Which criteria to use for update/upsert lookups',
       required: true,
       type: 'string',
-      choices: [{ label: 'External ID', value: 'external_id'}, { label: 'Trait', value: 'trait'}, { label: 'Record ID', value: 'record_id'}]
+      choices: [
+        { label: 'External ID', value: 'external_id' },
+        { label: 'Trait', value: 'trait' },
+        { label: 'Record ID', value: 'record_id' }
+      ]
     },
     external_id_field: {
       label: 'External ID Field',
       description: 'External ID Field',
-      type: 'string',
+      type: 'string'
     },
     external_id_value: {
       label: 'External ID Value',
@@ -100,23 +108,21 @@ const action: ActionDefinition<Settings, Payload> = {
     }
   },
   perform: async (request, { settings, payload }) => {
-      if (payload.operation === 'create') {
-        return insertRecord(request, settings.instanceUrl, 'Lead', payload)
-
-        return request(`${settings.instanceUrl}/services/data/${API_VERSION}/sobjects/Lead`, {
-          method: 'post',
-          json: {
-            LastName: payload.last_name,
-            Company: payload.company,
-            FirstName: payload.first_name,
-            State: payload.state,
-            Street: payload.street,
-            Country: payload.country,
-            PostalCode: payload.postal_code,
-            City: payload.city,
-            Email: payload.email
-          }
-        })
+    if (payload.operation === 'create') {
+      return request(`${settings.instanceUrl}/services/data/${API_VERSION}/sobjects/Lead`, {
+        method: 'post',
+        json: {
+          LastName: payload.last_name,
+          Company: payload.company,
+          FirstName: payload.first_name,
+          State: payload.state,
+          Street: payload.street,
+          Country: payload.country,
+          PostalCode: payload.postal_code,
+          City: payload.city,
+          Email: payload.email
+        }
+      })
     }
 
     if (payload.operation === 'update') {
@@ -125,20 +131,36 @@ const action: ActionDefinition<Settings, Payload> = {
       let recordId = ''
 
       if (payload.lookup_criteria === 'record_id') {
+        if (payload.record_id === undefined) {
+          throw new IntegrationError('Undefined Record ID', 'Misconfigured required field', 400)
+        }
+
         recordId = payload.record_id
       } else if (payload.lookup_criteria === 'external_id') {
-         const { Id } = await request<ExternalIDLookupResponse>(`${settings.instanceUrl}/services/data/${API_VERSION}/sobjects/Lead/${payload.external_id_field}/${external_id_value}`)
-         recordId = Id
-      } else { // trait lookup
-        const SOQLQuery = buildQuery(payload.trait_field, payload.trait_value)
+        const { Id } = await request<ExternalIDLookupResponse>(
+          `${settings.instanceUrl}/services/data/${API_VERSION}/sobjects/Lead/${payload.external_id_field}/${payload.external_id_value}`
+        )
+        recordId = Id
+      } else {
+        // trait lookup
+        if (payload.trait_field === undefined || payload.trait_value === undefined) {
+          throw new IntegrationError('Undefined trait field or trait value', 'Misconfigured required field', 400)
+        }
 
-        const { Id } = await request<ExternalIDLookupResponse>(`${settings.instanceUrl}/services/data/${API_VERSION}/query/${SOQLQuery}`)
+        const SOQLQuery = buildQuery(payload.trait_field, payload.trait_value, 'Lead')
+
+        const { Id } = await request<ExternalIDLookupResponse>(
+          `${settings.instanceUrl}/services/data/${API_VERSION}/query/${SOQLQuery}`
+        )
         recordId = Id
       }
 
       // was there a record?
+      if (recordId === '') {
+        throw new IntegrationError('No record with lookup criteria found', 'Missing record', 404)
+      }
 
-      //if so Perform update 
+      //if so Perform update
       return request(`${settings.instanceUrl}/services/data/${API_VERSION}/sobjects/Lead/${recordId}`, {
         method: 'post',
         json: {
@@ -153,20 +175,7 @@ const action: ActionDefinition<Settings, Payload> = {
           Email: payload.email
         }
       })
-
-      // else error out
     }
-
-    if (payload.operation === 'upsert') {
-      // perform lookup based on user criteria
-
-      // was there a record?
-
-      // if so update that record
-
-      // else insert a new record
-    }
-
   }
 }
 
--- packages/destination-actions/src/destinations/salesforce/sf_utils.ts
+++ packages/destination-actions/src/destinations/salesforce/sf_utils.ts
@@ -1,5 +1,4 @@
-
-export const buildQuery = (trait_field: string, trait_value: string): string => {
-    // TODO: Do this
-    return trait_field + "." + trait_value
+export const buildQuery = (trait_field: string, trait_value: string, sobject: string): string => {
+  // TODO: Expand this
+  return `SELECT Id FROM ${sobject} WHERE ${trait_field} = '${trait_value}'`
 }
