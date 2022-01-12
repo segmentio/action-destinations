1e7cd2d61cb55c0e7689ed001f6ebb16d16b2cf7
Nick Wolfe <nwolfe@aptrinsic.com>
2022-01-12 15:08:00 -0800

    Gainsight PX Cloud Action Use $event to pass through entire Segment event

--- packages/destination-actions/src/destinations/gainsight-px-cloud-action/trackEvent/index.ts
+++ packages/destination-actions/src/destinations/gainsight-px-cloud-action/trackEvent/index.ts
@@ -2,37 +2,27 @@ import type { ActionDefinition } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 import { getEndpointByRegion } from '../regional-endpoints'
-import { commonFields } from "../common-fields";
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Track Event',
   description: 'Send an event to Gainsight PX',
   defaultSubscription: 'type = "track"',
   fields: {
-    event: {
-      label: 'Event Name',
-      type: 'string',
-      description: 'A unique identifier for your event.',
+    segmentEvent: {
+      label: 'Segment Event',
+      type: 'object',
+      description: 'The raw Segment event',
       required: true,
       default: {
-        '@path': '$.event'
+        '@path': '$event'
       }
-    },
-    properties: {
-      label: 'Event Properties',
-      type: 'object',
-      description: 'An object of key-value pairs that represent additional data to be sent along with the event.',
-      default: {
-        '@path': '$.properties'
-      }
-    },
-    ...commonFields
+    }
   },
   perform: (request, { payload, settings }) => {
 
     return request(getEndpointByRegion('track', settings.dataCenter), {
       method: 'post',
-      json: payload
+      json: payload.segmentEvent
     })
   }
 }
