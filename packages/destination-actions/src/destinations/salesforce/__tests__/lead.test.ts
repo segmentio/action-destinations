b42b53d27bcd41f2c0c71f1a24003bdb44f6e56b
Nick Aguilar <nicholas.aguilar@segment.com>
2022-01-12 12:35:59 -0800

    Created testing files for lead and sf-operations

---
+++ packages/destination-actions/src/destinations/salesforce/__tests__/lead.test.ts
@@ -0,0 +1,3 @@
+describe('Salesforce', () => {
+  describe('Lead', () => {})
+})
---
+++ packages/destination-actions/src/destinations/salesforce/__tests__/sf-operations.test.ts
@@ -0,0 +1,9 @@
+describe('Salesforce', () => {
+  describe('Operations', () => {
+    it('should create a record', async () => {})
+
+    it('should update a record', async () => {})
+
+    it('should upsert a record', async () => {})
+  })
+})
