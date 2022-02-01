b7958fd9e1b06279f8bb708b5fb4d4fb9c823d64
Nick Aguilar <nickaguilarnew@gmail.com>
2022-02-01 15:31:23 -0800

    Update packages/destination-actions/src/destinations/salesforce/sf-properties.ts
    Co-authored-by: Marcus Ericsson <36717+mericsson@users.noreply.github.com>

--- packages/destination-actions/src/destinations/salesforce/sf-properties.ts
+++ packages/destination-actions/src/destinations/salesforce/sf-properties.ts
@@ -18,7 +18,7 @@ export const traits: InputField = {
   label: 'Record Matchers',
   description: `The fields used to find Salesforce Lead records for updates. This is required if the Operation is Update or Upsert.
 
-  Any field can be matched on, including Record ID, External IDs, standard fields and custom fields. On the left-hand side, input the Salesforce field name. On the right-hand side, map the Segment field that contains the value.
+  Any field can function as a matcher, including Record ID, External IDs, standard fields and custom fields. On the left-hand side, input the Salesforce field API name. On the right-hand side, map the Segment field that contains the value.
   
   If multiple records are found, no updates will be made. Please use fields that result in unique records.`,
   type: 'object'
