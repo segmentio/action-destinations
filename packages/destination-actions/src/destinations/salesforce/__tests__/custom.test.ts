60636554804806f1d84548eef498b89c5ccb1d9d
Nick Aguilar <nicholas.aguilar@segment.com>
2022-02-02 12:25:55 -0800

    Added unit test for custom action

---
+++ packages/destination-actions/src/destinations/salesforce/__tests__/custom.test.ts
@@ -0,0 +1,62 @@
+import nock from 'nock'
+import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import Destination from '../index'
+import { API_VERSION } from '../sf-operations'
+
+const testDestination = createTestIntegration(Destination)
+
+const settings = {
+  instanceUrl: 'https://test.com'
+}
+
+describe('Salesforce', () => {
+  describe('Custom', () => {
+    it('should create a custom object record', async () => {
+      const event = createTestEvent({
+        event: 'Identify',
+        traits: {
+          email: 'sponge@seamail.com',
+          company: 'Krusty Krab',
+          last_name: 'Squarepants'
+        }
+      })
+
+      nock(`${settings.instanceUrl}/services/data/${API_VERSION}/sobjects`).post('/TestCustom__c').reply(201, {})
+
+      const responses = await testDestination.testAction('custom', {
+        event,
+        settings,
+        mapping: {
+          operation: 'create',
+          sobject: 'TestCustom',
+          custom_fields: {
+            '@path': '$.traits'
+          }
+        }
+      })
+
+      expect(responses.length).toBe(1)
+      expect(responses[0].status).toBe(201)
+
+      expect(responses[0].request.headers).toMatchInlineSnapshot(`
+        Headers {
+          Symbol(map): Object {
+            "authorization": Array [
+              "Bearer undefined",
+            ],
+            "content-type": Array [
+              "application/json",
+            ],
+            "user-agent": Array [
+              "Segment (Actions)",
+            ],
+          },
+        }
+      `)
+
+      expect(responses[0].options.body).toMatchInlineSnapshot(
+        `"{\\"email\\":\\"sponge@seamail.com\\",\\"company\\":\\"Krusty Krab\\",\\"last_name\\":\\"Squarepants\\"}"`
+      )
+    })
+  })
+})
