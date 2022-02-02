75bca8822df8532f487d62e3e417a4607ae16370
Nick Aguilar <nicholas.aguilar@segment.com>
2022-02-01 16:15:32 -0800

    Generated types

--- packages/destination-actions/src/destinations/salesforce/custom/generated-types.ts
+++ packages/destination-actions/src/destinations/salesforce/custom/generated-types.ts
@@ -1,3 +1,28 @@
 // Generated file. DO NOT MODIFY IT BY HAND.
 
-export interface Payload {}
+export interface Payload {
+  /**
+   * Operation
+   */
+  operation: string
+  /**
+   * Traits
+   */
+  traits?: {
+    [k: string]: unknown
+  }
+  /**
+   * The name of the Salesforce object that records will be added or updated within. The object must be predefined in your Salesforce account.
+   */
+  sobject?: string
+  /**
+   *
+   *   Custom fields to send to Salesforce. Fields must be predefined in your Salesforce account.
+   *
+   *   On the left-hand side, input the Salesforce field name with __c appended. On the right-hand side, map the Segment field that contains the value.
+   *
+   */
+  custom_fields?: {
+    [k: string]: unknown
+  }
+}
--- packages/destination-actions/src/destinations/salesforce/custom/index.ts
+++ packages/destination-actions/src/destinations/salesforce/custom/index.ts
@@ -1,17 +1,38 @@
 import type { ActionDefinition } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
+import { operation, traits, custom_fields, validateLookup } from '../sf-properties'
+import Salesforce from '../sf-operations'
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Custom Object',
-  description: '',
-  fields: {},
-  perform: (request, data) => {
-    // Make your partner api request here!
-    // return request('https://example.com', {
-    //   method: 'post',
-    //   json: data.payload
-    // })
+  description: 'Custom Object',
+  fields: {
+    operation: operation,
+    traits: traits,
+    sobject: {
+      label: 'Salesforce Object',
+      description: 'The name of the Salesforce object that records will be added or updated within. The object must be predefined in your Salesforce account.',
+      type: 'string',
+    },
+    custom_fields: custom_fields
+  },
+  perform: async (request, { settings, payload }) => {
+    const sf: Salesforce = new Salesforce(settings.instanceUrl, request)
+
+    if (payload.operation === 'create') {
+      return await sf.createRecord(payload, 'Lead')
+    }
+
+    validateLookup(payload)
+
+    if (payload.operation === 'update') {
+      return await sf.updateRecord(payload, 'Lead')
+    }
+
+    if (payload.operation === 'upsert') {
+      return await sf.upsertRecord(payload, 'Lead')
+    }
   }
 }
 
