20988227cc61834a2195bba53feca4f18944b4cb
Christopher Radek <christopher.radek@segment.com>
2022-02-03 11:16:45 -0800

    add IE 11 support

--- packages/browser-destinations/webpack.config.es5.js
+++ packages/browser-destinations/webpack.config.es5.js
@@ -4,19 +4,30 @@ const TerserPlugin = require('terser-webpack-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const webpack = require('webpack')
-
-const files = globby.sync('./src/destinations/*/index.ts')
+console.log(`MODE:: ${process.env.NODE_ENV}`)
+const files = globby.sync('./dist/web/*/*.js')
 const isProd = process.env.NODE_ENV === 'production'
 
 const entries = files.reduce((acc, current) => {
-  const [_dot, _src, _destinations, destination, ..._rest] = current.split('/')
+  const [_dot, _src, _destinations, destination, rest] = current.split('/')
+  const originalHash = rest.split('.')[0]
+  const name = originalHash ? `${destination}/${originalHash}` : destination
   return {
     ...acc,
-    [destination]: current
+    [name]: {
+      import: current,
+      library: {
+        name: `${destination}Destination`,
+        type: 'umd'
+      }
+    }
   }
 }, {})
 
 const plugins = [new webpack.DefinePlugin({ 'process.env.ASSET_ENV': JSON.stringify(process.env.ASSET_ENV) })]
+if (isProd) {
+  plugins.push(new CompressionPlugin())
+}
 
 if (process.env.ANALYZE) {
   plugins.push(
@@ -36,31 +47,49 @@ module.exports = {
   entry: entries,
   mode: process.env.NODE_ENV || 'development',
   devtool: 'source-map',
+  target: ["browserslist"],
   output: {
-    filename: process.env.NODE_ENV === 'development' ? '[name]/[name].js' : '[name]/[contenthash].js',
+    filename: (pathData) => {
+      const [ name, hash ] = pathData.chunk.name.split('/')
+
+      if (process.env.NODE_ENV === 'development' || !hash) {
+        return `${name}.js`
+      }
+
+      return `${name}/${hash}.js`
+    },
     path: path.resolve(__dirname, 'dist/web'),
     publicPath: 'auto', // Needed for customers using custom CDNs with analytics.js
-    library: {
-      name: '[name]Destination',
-      type: 'umd',
-      export: 'default'
-    }
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.js$/,
         use: [
           {
             loader: 'ts-loader',
             options: {
-              configFile: 'tsconfig.build.json',
-              projectReferences: true,
+              configFile: 'tsconfig.build.es5.json',
               transpileOnly: true
             }
           }
         ]
-      }
+      },
+      {
+        test: /\.js$/,
+        use: [ // Still need babel to handle downleveling some things TypeScript doesn't handle.
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                ['@babel/plugin-proposal-unicode-property-regex', {
+                  useUnicodeFlag: false
+                }]
+              ]
+            }
+          }
+        ]
+      },
     ]
   },
   resolve: {
@@ -84,6 +113,24 @@ module.exports = {
   },
   performance: {
     hints: 'warning'
+  },
+  optimization: {
+    moduleIds: 'deterministic',
+    minimize: isProd,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          ecma: 'es5',
+          mangle: true,
+          compress: true,
+          sourceMap: true,
+          output: {
+            comments: false
+          }
+        }
+      })
+    ]
   },
   plugins
 }
