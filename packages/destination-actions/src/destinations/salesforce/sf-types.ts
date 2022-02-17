f1f0771aa74074898e648c688e6b8f7aa5c5e01f
Logan Luque <logan.luque@segment.com>
2022-02-17 15:40:18 -0800

    Complete opportunity action payload shape

--- packages/destination-actions/src/destinations/salesforce/sf-types.ts
+++ packages/destination-actions/src/destinations/salesforce/sf-types.ts
@@ -1,8 +1,9 @@
 import type { Payload as LeadPayload } from './lead/generated-types'
 import type { Payload as CustomPayload } from './customObject/generated-types'
 import type { Payload as CasePayload } from './case/generated-types'
+import type { Payload as OpportunityPayload } from './opportunity/generated-types'
 
-export type GenericPayload = Partial<LeadPayload & CustomPayload & CasePayload>
+export type GenericPayload = Partial<LeadPayload & CustomPayload & CasePayload & OpportunityPayload>
 
 export type LeadBaseShapeType = {
   LastName?: string
@@ -20,4 +21,12 @@ export type CaseBaseShapeType = {
   Description?: string
 }
 
-export type GenericBaseShape = Partial<LeadBaseShapeType & CaseBaseShapeType>
+export type OpportunityBaseShapeType = {
+  Amount?: string
+  CloseDate?: string
+  Description?: string
+  Name?: string
+  StageName?: string
+}
+
+export type GenericBaseShape = Partial<LeadBaseShapeType & CaseBaseShapeType & OpportunityBaseShapeType>
