97224c09a1e72922fa819d21cb1b7b5afdbe3917
Dan Lasky <dan.lasky@segment.com>
2022-02-28 15:52:56 -0500

    error types

--- packages/cli/src/lib/server.ts
+++ packages/cli/src/lib/server.ts
@@ -149,7 +149,8 @@ function setupRoutes(def: DestinationDefinition | null): void {
       try {
         await destination.testAuthentication(req.body)
         res.status(200).json({ ok: true })
-      } catch (error) {
+      } catch (e) {
+        const error = e as ResponseError
         const fields: Record<string, string> = {}
 
         if (error.name === 'AggregateAjvError') {
