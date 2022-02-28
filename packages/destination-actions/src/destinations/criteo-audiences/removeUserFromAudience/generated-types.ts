8cf355f51c3dd4b0eb83442b9009fbaed78d4cc4
mdillar_criteo <m.dillar@criteo.com>
2022-02-28 17:11:34 +0100

    generated types

---
+++ packages/destination-actions/src/destinations/criteo-audiences/removeUserFromAudience/generated-types.ts
@@ -0,0 +1,12 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * User ID in Segment
+   */
+  user_id: string
+  /**
+   * The user's email
+   */
+  email?: string | null
+}
