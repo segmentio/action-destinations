7d1f0583ecd4e08e3df0fae94f5e519c3d6835f9
mdillar_criteo <m.dillar@criteo.com>
2022-02-28 17:12:39 +0100

    changed import type

--- packages/destination-actions/src/destinations/criteo-audiences/addUserToAudience/index.ts
+++ packages/destination-actions/src/destinations/criteo-audiences/addUserToAudience/index.ts
@@ -1,4 +1,4 @@
-import type { ActionDefinition, RequestOptions, IntegrationError } from '@segment/actions-core'
+import { ActionDefinition, RequestOptions, IntegrationError } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 
