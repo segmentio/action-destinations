698968f2757614bdc709283a138b52f09a95a6ef
Maryam Sharif <maryam.sharif@segment.com>
2022-03-01 14:17:41 -0800

    created Contact action

--- packages/destination-actions/src/destinations/salesforce/sf-object-to-shape.ts
+++ packages/destination-actions/src/destinations/salesforce/sf-object-to-shape.ts
@@ -1,4 +1,10 @@
-import { GenericPayload, GenericBaseShape, LeadBaseShapeType, CaseBaseShapeType } from './sf-types'
+import {
+  GenericPayload,
+  GenericBaseShape,
+  LeadBaseShapeType,
+  CaseBaseShapeType,
+  ContactBaseShapeType
+} from './sf-types'
 
 const LeadShape = (payload: GenericPayload): LeadBaseShapeType => {
   return {
@@ -20,16 +26,30 @@ const CaseShape = (payload: GenericPayload): CaseBaseShapeType => {
   }
 }
 
+const ContactShape = (payload: GenericPayload): ContactBaseShapeType => {
+  return {
+    LastName: payload.last_name,
+    FirstName: payload.first_name,
+    Email: payload.email,
+    MailingState: payload.mailing_state,
+    MailingStreet: payload.mailing_street,
+    MailingCountry: payload.mailing_country,
+    MailingPostalCode: payload.mailing_postal_code,
+    MailingCity: payload.mailing_city
+  }
+}
+
 const objectToShape = new Map<string, (payload: GenericPayload) => LeadBaseShapeType | CaseBaseShapeType>([
   ['lead', LeadShape],
-  ['case', CaseShape]
+  ['case', CaseShape],
+  ['contact', ContactShape]
 ])
 
 export const mapObjectToShape = (payload: GenericPayload, sobject: string): GenericBaseShape => {
   const shapeFunction = objectToShape.get(sobject.toLowerCase())
 
   if (!shapeFunction) {
-    throw new Error(`Undefined Object Shape: ${sobject}`)
+    throw new Error(`: ${sobject}`)
   }
 
   return shapeFunction(payload)
--- packages/destination-actions/src/destinations/salesforce/sf-types.ts
+++ packages/destination-actions/src/destinations/salesforce/sf-types.ts
@@ -1,8 +1,9 @@
 import type { Payload as LeadPayload } from './lead/generated-types'
 import type { Payload as CustomPayload } from './customObject/generated-types'
 import type { Payload as CasePayload } from './case/generated-types'
+import type { Payload as ContactPayload } from './contact/generated-types'
 
-export type GenericPayload = Partial<LeadPayload & CustomPayload & CasePayload>
+export type GenericPayload = Partial<LeadPayload & CustomPayload & CasePayload & ContactPayload>
 
 export type LeadBaseShapeType = {
   LastName?: string
@@ -18,6 +19,17 @@ export type LeadBaseShapeType = {
 
 export type CaseBaseShapeType = {
   Description?: string
+}
+
+export type ContactBaseShapeType = {
+  LastName?: string
+  FirstName?: string
+  MailingState?: string
+  MailingStreet?: string
+  MailingCountry?: string
+  MailingPostalCode?: string
+  MailingCity?: string
+  Email?: string
 }
 
 export type GenericBaseShape = Partial<LeadBaseShapeType & CaseBaseShapeType>
