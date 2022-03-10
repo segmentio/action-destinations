456465ab221bb6fe81261921a1c4955c437a6bc8
mdillar_criteo <m.dillar@criteo.com>
2022-03-10 18:53:57 +0100

    removed comment

--- packages/destination-actions/src/destinations/criteo-audiences/index.ts
+++ packages/destination-actions/src/destinations/criteo-audiences/index.ts
@@ -40,13 +40,6 @@ const destination: DestinationDefinition<Settings> = {
       return getAccessToken(request, credentials)
     }
   },
-  //we might not need this function
-  /*onDelete: async (request, { settings, payload }) => {
-    // Return a request that performs a GDPR delete for the provided Segment userId or anonymousId
-    // provided in the payload. If your destination does not support GDPR deletion you should not
-    // implement this function and should remove it completely.
-  },*/
-
   actions: {
     addUserToAudience,
     removeUserFromAudience
