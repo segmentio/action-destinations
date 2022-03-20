52c0397e97260cd45c6da68937792c4b2418bf77
Nick Aguilar <nicholas.aguilar@segment.com>
2022-03-20 12:49:14 -0700

    Generated types (for all sf actions as well)

--- packages/destination-actions/src/destinations/salesforce/generated-types.ts
+++ packages/destination-actions/src/destinations/salesforce/generated-types.ts
@@ -5,4 +5,8 @@ export interface Settings {
    * The user specific instance URL returned by Salesforce Oauth. This setting is hidden to the user and set by Oauth Service.
    */
   instanceUrl: string
+  /**
+   * Turn this on if you would like to oauth into a sandbox environment instead.
+   */
+  isSandbox?: boolean
 }
