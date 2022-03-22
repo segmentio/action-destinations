ed2df7bbb61a513f757a4ce45d67ce67e096991b
Mikhail Gunkov <painter@3lines.club>
2022-03-22 12:44:18 +0100

    supported multiple fields

--- packages/destination-actions/src/destinations/talon-one/updateCustomerProfilesAudiences/index.ts
+++ packages/destination-actions/src/destinations/talon-one/updateCustomerProfilesAudiences/index.ts
@@ -1,28 +1,20 @@
 import type { ActionDefinition } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
-import { addAudienceId, customerProfileId, deleteAudienceId } from '../t1-properties'
+import { dataItem } from '../t1-properties'
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Update Customer Profiles Audiences',
   description: 'This synchronizes audience data for multiple customer profiles.',
   fields: {
-    deleteAudienceIds: { ...deleteAudienceId },
-    addAudienceIds: { ...addAudienceId },
-    customerProfileId: { ...customerProfileId }
+    data: { ...dataItem }
   },
   perform: (request, { payload }) => {
     // Make your partner api request here!
     return request(`https://integration.talon.one/segment/customer_profiles/audiences`, {
       method: 'put',
       json: {
-        data: [
-          {
-            customerProfileId: payload.customerProfileId,
-            adds: payload.addAudienceIds,
-            deletes: payload.deleteAudienceIds
-          }
-        ]
+        data: payload.data
       }
     })
   }
