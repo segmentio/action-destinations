dfc826e0c62da31bf41da74533175f6702fc7dea
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2022-03-23 19:08:26 +0200

    [HEAP-29018] Fix loading test for Heap (#4)

--- packages/browser-destinations/src/destinations/heap/__tests__/index.test.ts
+++ packages/browser-destinations/src/destinations/heap/__tests__/index.test.ts
@@ -1,22 +1,29 @@
 import { Analytics, Context } from '@segment/analytics-next'
 import heapDestination, { destination } from '../index'
 import nock from 'nock'
-import { HeapApi } from '../types'
+
+const subscriptions = [
+  {
+    partnerAction: 'trackEvent',
+    name: 'Track Event',
+    enabled: true,
+    subscribe: 'type = "track"',
+    mapping: {}
+  }
+]
 
 describe('Heap', () => {
-  const appId = 'fakeAppId'
+  const appId = '1'
 
   test('loading', async () => {
-    const expectedHeap: HeapApi = [['appId', appId]] as any
-
     jest.spyOn(destination, 'initialize')
 
     nock('https://cdn.heapanalytics.com').get(`/js/heap-${appId}.js`).reply(200, {})
 
-    const [event] = await heapDestination({ appId })
+    const [event] = await heapDestination({ appId, subscriptions })
 
     await event.load(Context.system(), {} as Analytics)
     expect(destination.initialize).toHaveBeenCalled()
-    expect(window.heap).toEqual(expectedHeap)
+    expect(window.heap.appid).toEqual(appId)
   })
 })
