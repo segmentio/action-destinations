409e459155b08bf9f29098f79a943ec58a154c65
Michael Gunkoff <54396113+kaatinga@users.noreply.github.com>
2022-04-13 00:48:34 +0200

    Talon.One - fix `updateCustomerProfilesAttributes` action slug (#554)
    * add create audience action
    
    * try enable CI
    
    * try fix
    
    * return ci how it was
    
    * add updateAudience, deleteAudience, trackEvent
    
    * check workflow
    
    * fix tests
    
    * fix tests
    
    * fix description
    
    * improve test
    
    * fix labels and descriptions
    
    * generate types
    
    * return workflows how it was before
    
    * change destination name Talon One -> Talon.One
    
    * review
    
    * new files
    
    * added 3 endpoints
    
    * added generated files
    
    * implemented 3 endppoints
    
    * tests
    
    * fixed tests, added right json tags
    
    * updated models
    
    * updated test models
    
    * update models
    
    * updated models
    
    * tests generated
    
    * updated models
    
    * tests generated
    
    * Revert "Merge pull request #2 from talon-one/sc-7503-add-destination-endpoints-to-work-with-customer"
    
    This reverts commit d391a8e426bf0ef0ea75a55cc0f0157eeb00306c, reversing
    changes made to 68718631ea49ae357f6d1e952538791b9a65c5f0.
    
    * Revert "Revert "Merge pull request #2 from talon-one/sc-7503-add-destination-endpoints-to-work-with-customer""
    
    This reverts commit b7e7d3146860cac65836c276ec56d020905f85c0.
    
    * added more endpoints and fixed some minor things
    
    * deleted an unnecessarily added file
    
    * updated snapshots, thank you @jinapark202
    
    * fixed request model
    
    * updated tests
    
    * update generated models
    
    * updated tests for update multiple customer profiles attributes
    
    Co-authored-by: kkupreeva <kupreeva@talon.one>
    Co-authored-by: Kristina Kupreeva <86834674+kkupreeva@users.noreply.github.com>

--- packages/destination-actions/src/destinations/talon-one/updateCustomerProfilesAttributes/generated-types.ts
+++ packages/destination-actions/src/destinations/talon-one/updateCustomerProfilesAttributes/generated-types.ts
@@ -2,17 +2,22 @@
 
 export interface Payload {
   /**
-   * The customer profile integration identifier to use in Talon.One.
+   * An array of JSON objects that contains customer profile identifier and list of attributes and their values. Customer profile ID is required.
    */
-  customerProfileId: string
+  data: {
+    /**
+     * The customer profile integration identifier to use in Talon.One.
+     */
+    customerProfileId: string
+    /**
+     * Extra attributes associated with the customer profile. [See more info](https://docs.talon.one/docs/product/account/dev-tools/managing-attributes).
+     */
+    attributes?: {
+      [k: string]: unknown
+    }
+  }[]
   /**
-   * Extra attributes associated with the customer profile. [See more info](https://docs.talon.one/docs/product/account/dev-tools/managing-attributes).
-   */
-  attributes?: {
-    [k: string]: unknown
-  }
-  /**
-   * Extra attributes associated with the customer profile. [See more info](https://docs.talon.one/docs/product/account/dev-tools/managing-attributes).
+   * This may contain mutual list of attributes and their values for every customer profile in the "data" array.
    */
   mutualAttributes?: {
     [k: string]: unknown
--- packages/destination-actions/src/destinations/talon-one/updateCustomerProfilesAttributes/index.ts
+++ packages/destination-actions/src/destinations/talon-one/updateCustomerProfilesAttributes/index.ts
@@ -7,21 +7,31 @@ const action: ActionDefinition<Settings, Payload> = {
   title: 'Update Multiple Customer Profiles’ Attributes',
   description: 'This updates attributes for multiple customer profiles.',
   fields: {
-    customerProfileId: { ...customerProfileId },
-    attributes: { ...attribute },
-    mutualAttributes: { ...attribute }
+    data: {
+      label: 'Data item to change customer profile attributes',
+      description:
+        'An array of JSON objects that contains customer profile identifier and list of attributes and their values. Customer profile ID is required.',
+      type: 'object',
+      multiple: true,
+      properties: {
+        customerProfileId: { ...customerProfileId },
+        attributes: { ...attribute }
+      },
+      required: true
+    },
+    mutualAttributes: {
+      ...attribute,
+      label: 'Mutual Attribute-Value pairs',
+      description:
+        'This may contain mutual list of attributes and their values for every customer profile in the "data" array.'
+    }
   },
   perform: (request, { payload }) => {
     // Make your partner api request here!
     return request(`https://integration.talon.one/segment/customer_profiles/attributes`, {
       method: 'put',
       json: {
-        data: [
-          {
-            customerProfileId: payload.customerProfileId,
-            attributes: payload.attributes
-          }
-        ],
+        data: payload.data,
         mutualAttributes: payload.mutualAttributes
       }
     })
