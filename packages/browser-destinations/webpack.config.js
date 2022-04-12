146d53a8e7d4c915cdc3811715014715e5c00425
Ariel Silvestri <ariel.silvestri@segment.com>
2022-04-12 16:21:09 -0400

    Simplify the obfuscated output config

--- packages/browser-destinations/webpack.config.js
+++ packages/browser-destinations/webpack.config.js
@@ -107,77 +107,14 @@ const unobfuscatedOutput = {
 }
 
 const obfuscatedOutput = {
-  entry: entries,
-  mode: process.env.NODE_ENV || 'development',
-  devtool: 'source-map',
+  ...unobfuscatedOutput,
   output: {
-    filename: (file) => {
-      const obfuscatedOutput = Buffer.from(file.chunk.name).toString('base64').replace(/=/g, '');
-      return process.env.NODE_ENV === 'development' ? `${obfuscatedOutput}.js` : `${obfuscatedOutput}/[contenthash].js`
-    },
-    path: path.resolve(__dirname, 'dist/web'),
-    publicPath: 'auto', // Needed for customers using custom CDNs with analytics.js
-    library: '[name]Destination',
-    libraryTarget: 'umd',
-    libraryExport: 'default'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              configFile: 'tsconfig.build.json',
-              projectReferences: true,
-              transpileOnly: true
-            }
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    modules: [
-      // use current node_modules directory first (e.g. for tslib)
-      path.resolve(__dirname, 'node_modules'),
-      'node_modules'
-    ],
-    mainFields: ['exports', 'module', 'browser', 'main'],
-    extensions: ['.ts', '.js'],
-    fallback: {
-      vm: require.resolve('vm-browserify')
-    }
-  },
-  devServer: {
-    liveReload: true,
-    port: 9000,
-    static: {
-      directory: path.resolve(__dirname)
-    }
-  },
-  performance: {
-    hints: 'warning'
-  },
-  optimization: {
-    moduleIds: 'deterministic',
-    minimize: isProd,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-        terserOptions: {
-          ecma: '2015',
-          mangle: true,
-          compress: true,
-          output: {
-            comments: false
-          }
-        }
-      })
-    ]
-  },
-  plugins
-}
+    ...unobfuscatedOutput.output,
+     filename: (file) => {
+       const obfuscatedOutputName = Buffer.from(file.chunk.name).toString('base64').replace(/=/g, '');
+       return process.env.NODE_ENV === 'development' ? `${obfuscatedOutputName}.js` : `${obfuscatedOutputName}/[contenthash].js`
+     },
+  }
+ }
 
 module.exports = [unobfuscatedOutput, obfuscatedOutput]
