4abe32841bdad1c1a85b52830988fa5afdf5be6a
abhishek.g <abhishek.g@clevertap.com>
2022-04-26 09:15:08 +0530

    change html description to markdown tags

--- packages/destination-actions/src/destinations/clevertap/index.ts
+++ packages/destination-actions/src/destinations/clevertap/index.ts
@@ -10,30 +10,28 @@ const destination: DestinationDefinition<Settings> = {
   name: 'Clevertap (Actions)',
   slug: 'actions-clevertap',
   mode: 'cloud',
-  description: '<p data-renderer-start-pos="72">CleverTap is a customer engagement and retention platform that provides the functionality to integrate app analytics and marketing. The platform helps customers increase user engagement in three ways:</p>' +
-    '<ul class="ak-ul" data-indent-level="1">' +
-    '<li><p data-renderer-start-pos="276">Tracks actions users are taking and analyzes how people use the product.</p></li>' +
-    '<li><p data-renderer-start-pos="352"><a class="sc-1ko78hw-0 fiVZLH" href="https://docs.clevertap.com/docs/segments" title="https://docs.clevertap.com/docs/segments" data-renderer-mark="true">Segment</a> users based on their actions and run targeted <a class="sc-1ko78hw-0 fiVZLH" href="https://docs.clevertap.com/docs/intro-to-campaigns" title="https://docs.clevertap.com/docs/intro-to-campaigns" data-renderer-mark="true">campaigns</a> to these segments.</p></li>' +
-    '<li><p data-renderer-start-pos="438"><a class="sc-1ko78hw-0 fiVZLH" href="https://docs.clevertap.com/docs/intro-to-reports" title="https://docs.clevertap.com/docs/intro-to-reports" data-renderer-mark="true">Analyze</a> each campaign to understand its effect on user engagement and business metrics.</p></li>' +
-    '</ul>',
+  description: 'CleverTap is a customer engagement and retention platform that provides the functionality to integrate app analytics and marketing. The platform helps customers increase user engagement in three ways:\n' +
+    '* Tracks actions users are taking and analyzes how people use the product.\n' +
+    '* [Segment](https://docs.clevertap.com/docs/segments) users based on their actions and run targeted [campaigns](https://docs.clevertap.com/docs/intro-to-campaigns) to these  segments.\n' +
+    '* [Analyze](https://docs.clevertap.com/docs/intro-to-reports) each campaign to understand its effect on user engagement and business metrics',
   authentication: {
     scheme: 'custom',
     fields: {
       clevertapAccountId: {
         label: 'CleverTap Account ID',
-        description: 'CleverTap Account Id. This can be found under <a href="https://developer.clevertap.com/docs/authentication#getting-your-account-credentials" title="https://developer.clevertap.com/docs/authentication#getting-your-account-credentials">Settings Page</a>.',
+        description: 'CleverTap Account Id. This can be found under [Settings Page](https://developer.clevertap.com/docs/authentication#getting-your-account-credentials).',
         type: 'string',
         required: true
       },
       clevertapPasscode: {
         label: 'CleverTap Account Passcode',
-        description: '<p>CleverTap Passcode. This can be found under <a href="https://developer.clevertap.com/docs/authentication#getting-your-account-credentials" title="https://developer.clevertap.com/docs/authentication#getting-your-account-credentials">Settings Page</a>.',
+        description: 'CleverTap Passcode. This can be found under [Settings Page](https://developer.clevertap.com/docs/authentication#getting-your-account-credentials).',
         type: 'string',
         required: true
       },
       clevertapEndpoint: {
         label: 'REST Endpoint',
-        description: '<p>Learn More about <a href="https://docs.clevertap.com/docs/build-segment-destination#set-up-segment-destination-action" title="https://docs.clevertap.com/docs/build-segment-destination#set-up-segment-destination-action">Account regions</a>.</p>',
+        description: 'Learn More about [Account regions](https://docs.clevertap.com/docs/build-segment-destination#set-up-segment-destination-action).',
         type: 'string',
         format: 'uri',
         choices: [
