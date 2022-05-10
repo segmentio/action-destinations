9761a401ff3a4715194ad82f67db2b538338ba26
Maryam Sharif <maryam.sharif@segment.com>
2022-05-10 11:46:06 -0700

    scaffolded new google sheets destination
    Fix build

---
+++ packages/destination-actions/src/destinations/google-sheets/postSheet/index.ts
@@ -0,0 +1,20 @@
+import type { ActionDefinition } from '@segment/actions-core'
+import type { Settings } from '../generated-types'
+import type { Payload } from './generated-types'
+
+const action: ActionDefinition<Settings, Payload> = {
+  title: 'Post Sheet',
+  description: '',
+  fields: {},
+  perform: (request, data) => {
+    console.log(request)
+    console.log(data)
+    // Make your partner api request here!
+    // return request('https://example.com', {
+    //   method: 'post',
+    //   json: data.payload
+    // })
+  }
+}
+
+export default action
