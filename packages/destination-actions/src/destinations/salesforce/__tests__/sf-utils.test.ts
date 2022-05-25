bb26b8bacd284d8689b55ba4fb1ddff3b5e45f53
Nick Aguilar <nicholas.aguilar@segment.com>
2022-05-24 17:50:49 -0700

    CSV generation with incomplete data working against 1 test case

--- packages/destination-actions/src/destinations/salesforce/__tests__/sf-utils.test.ts
+++ packages/destination-actions/src/destinations/salesforce/__tests__/sf-utils.test.ts
@@ -4,8 +4,43 @@ import { buildCSVData } from '../sf-utils'
 describe('Salesforce Utils', () => {
   describe('CSV', () => {
     it('should correctly build a CSV from many payloads with incomplete data', async () => {
-      //todo
-      return true
+      const incompletePayloads: GenericPayload[] = [
+        {
+          operation: 'bulkUpsert',
+          traits: {
+            externalIdFieldName: 'test__c',
+            externalIdFieldValue: '00'
+          },
+          name: 'SpongeBob Squarepants',
+          email: 'sponge@seamail.com',
+          customFields: {
+            FavoriteFood: 'Krabby Patty'
+          }
+        },
+        {
+          operation: 'bulkUpsert',
+          traits: {
+            externalIdFieldName: 'test__c',
+            externalIdFieldValue: '01'
+          },
+          name: 'Patrick Star',
+          phone: '123-456-7890'
+        },
+        {
+          operation: 'bulkUpsert',
+          traits: {
+            externalIdFieldName: 'test__c',
+            externalIdFieldValue: '11'
+          },
+          name: 'Sandy Cheeks',
+          state: 'Texas'
+        }
+      ]
+
+      const csv = buildCSVData(incompletePayloads, 'test__c')
+      const expected = `name,state,phone,email,FavoriteFood,test__c\n"SpongeBob Squarepants",#N/A,#N/A,"sponge@seamail.com","Krabby Patty","00"\n"Patrick Star",#N/A,"123-456-7890",#N/A,#N/A,"01"\n"Sandy Cheeks","Texas",#N/A,#N/A,#N/A,"11"\n`
+
+      expect(csv).toEqual(expected)
     })
 
     it('should throw an error for invalid customFields', async () => {
--- packages/destination-actions/src/destinations/salesforce/sf-utils.ts
+++ packages/destination-actions/src/destinations/salesforce/sf-utils.ts
@@ -1,8 +1,10 @@
 import { IntegrationError } from '@segment/actions-core'
 import { GenericPayload } from './sf-types'
-import camelCase from 'lodash/camelCase'
+// import camelCase from 'lodash/camelCase'
 
 const isSettingsKey = new Set<string>(['operation', 'traits', 'customFields'])
+
+const NO_VALUE = `#N/A`
 
 const validateHeaderField = (field: string): IntegrationError | false => {
   if (field.includes(',')) {
@@ -15,72 +17,96 @@ const validateHeaderField = (field: string): IntegrationError | false => {
   return false
 }
 
-export const buildCSVData = (payloads: GenericPayload[], externalIdFieldName: string): string => {
-  let headers = buildHeader(payloads, externalIdFieldName) + '\n'
+const buildHeaderMap = (payloads: GenericPayload[]): Map<string, [[string, number]]> => {
+  const headerMap = new Map<string, [[string, number]]>()
 
-  payloads.forEach((payload) => {
-    headers += buildRow(payload) + '\n'
-  })
+  //iterate in reverse over payloads
+  for (let i = payloads.length - 1; i >= 0; i--) {
+    const payload = payloads[i]
+    Object.entries(payload).forEach(([key, value]) => {
+      if (!isSettingsKey.has(key)) {
+        const actualValue = value as string
+        if (headerMap.has(key)) {
+          headerMap.get(key)?.push([actualValue, i])
+        } else {
+          headerMap.set(key, [[actualValue, i]])
+        }
+      }
+      if (key === 'customFields') {
+        const customFields = value as object
+        Object.entries(customFields).forEach(([customFieldName, customFieldValue]) => {
+          const fieldErr = validateHeaderField(customFieldName)
+          if (fieldErr) {
+            throw fieldErr
+          }
 
+          if (!headerMap.has(customFieldName)) {
+            headerMap.set(customFieldName, [[customFieldValue, i]])
+          } else {
+            headerMap.get(customFieldName)?.push([customFieldValue, i])
+          }
+        })
+      }
+    })
+  }
+
+  return headerMap
+}
+
+const buildHeaders = (headerMap: Map<string, [[string, number]]>): string => {
+  let headers = ''
+  for (const [key, _] of headerMap.entries()) {
+    headers += `${key},`
+  }
   return headers
 }
 
-const buildRow = (payload: GenericPayload): string => {
-  let row = ''
+const buildCSVFromHeaderMap = (
+  payloads: GenericPayload[],
+  headerMap: Map<string, [[string, number]]>,
+  n: number
+): string => {
+  let rows = ''
 
-  // Iterate over the keys in payload that map to a SF object field
-  // Add each value to the row, separated by a comma
-  // The last value in the row should be the externalIDField value.
-  // All valid rows will include a value for the externalIDField,
-  // it is included as the last value in the row for convenience.
+  for (let i = 0; i < n; i++) {
+    let row = ''
+    for (const [key, _] of headerMap.entries()) {
+      let noValueFound = true
+      if (headerMap.has(key)) {
+        const column = headerMap.get(key) as [[string, number]]
 
-  Object.entries(payload).forEach(([key, value]) => {
-    if (!isSettingsKey.has(key)) {
-      row += `"${value}",`
+        if (column !== undefined && column.length > 0 && column[column.length - 1][1] === i) {
+          const value = column.pop()
+          if (value !== undefined) {
+            row += `"${value[0]}",`
+            noValueFound = false
+          }
+        }
+      }
+
+      if (noValueFound) {
+        row += `${NO_VALUE},`
+      }
     }
-    if (key === 'customFields') {
-      const customFields = value as object
-      Object.entries(customFields).forEach(([_, value]) => {
-        row += `"${value}",`
-      })
-    }
-  })
-  row += `"${payload?.traits?.externalIdFieldValue}"`
-
-  return row
+    const externalIdFieldValue = payloads[i].traits?.externalIdFieldValue as string
+    rows += `${row}"${externalIdFieldValue}"\n`
+  }
+  return rows
 }
 
-const buildHeader = (payloads: GenericPayload[], externalIdFieldName: string): string => {
-  const err = validateHeaderField(externalIdFieldName)
-  if (err) {
-    throw err
-  }
+export const buildCSVData = (payloads: GenericPayload[], externalIdFieldName: string): string => {
+  const headerMap = buildHeaderMap(payloads)
+  let csv = buildHeaders(headerMap)
+  console.log('csv', csv)
 
-  let header = ''
-  Object.keys(payloads[0]).forEach((key) => {
-    if (!isSettingsKey.has(key)) {
-      header += `"${snakeCaseToPascalCase(key)}",`
-    }
-    if (key === 'customFields') {
-      const customFields = payloads[0].customFields as object
-      Object.keys(customFields).forEach((customFieldName) => {
-        const fieldErr = validateHeaderField(customFieldName)
-        if (fieldErr) {
-          throw fieldErr
-        }
+  csv += `${externalIdFieldName}\n` + buildCSVFromHeaderMap(payloads, headerMap, payloads.length)
 
-        header += `"${customFieldName}",`
-      })
-    }
-  })
-  header += `"${externalIdFieldName}"`
-
-  return header
+  return csv
 }
 
 // Our key names are in snake case, but Salesforce's field names are in Pascal case.
 // I.E. 'last_name' is our key, but 'LastName' is the Salesforce field name.
-const snakeCaseToPascalCase = (key: string): string => {
-  const token = camelCase(key)
-  return token.charAt(0).toUpperCase() + token.slice(1)
-}
+// const snakeCaseToPascalCase = (key: string): string => {
+//   const token = camelCase(key)
+//   return token.charAt(0).toUpperCase() + token.slice(1)
+// }
