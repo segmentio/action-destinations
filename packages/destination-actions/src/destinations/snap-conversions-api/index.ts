00b148d242c4442ba51ff946ddb1dc1b07313650
rvadera12 <89420099+rvadera12@users.noreply.github.com>
2022-05-31 16:56:49 -0400

    Snap Conversions API Destination (#608)
    * added snap capi destination
    
    * fixed commented out lines
    
    * added snap-conversions-api to index.ts file
    
    * Update Test Files
    
    * Update Snapshots File
    
    * added placeholder action
    
    * added placeholder action
    
    * revert tsconfig.build.json
    
    * POC for report conversion event action
    
    * POC for report conversion event action
    
    * remove data_use field mapping
    
    * updated to fix tests
    
    * updated to fix tests
    
    * added settings to payload to remove object typescript error
    
    * debugging object error with settings
    
    * removed unused type setting
    
    * remove lodash
    
    * added description to action to pass validate test
    
    * fixed defaults
    
    * added validation and unit tests
    
    * updated unit tests
    
    * add preset list view
    
    * Update packages/destination-actions/src/destinations/snap-conversions-api/snap-capi-properties.ts
    
    Co-authored-by: Marín Alcaraz <marin.alcarazcordova@segment.com>
    
    * Update packages/destination-actions/src/destinations/snap-conversions-api/snap-capi-properties.ts
    
    Co-authored-by: Marín Alcaraz <marin.alcarazcordova@segment.com>
    
    * Update packages/destination-actions/src/destinations/snap-conversions-api/snap-capi-properties.ts
    
    Co-authored-by: Marín Alcaraz <marin.alcarazcordova@segment.com>
    
    * updates from PR comments
    
    * adressed PR comments
    
    * more PR comments
    
    Co-authored-by: Marín Alcaraz <marin.alcarazcordova@segment.com>

---
+++ packages/destination-actions/src/destinations/snap-conversions-api/index.ts
@@ -0,0 +1,199 @@
+import { defaultValues, DestinationDefinition } from '@segment/actions-core'
+import type { Settings } from './generated-types'
+
+import reportConversionEvent from './reportConversionEvent'
+
+const DEFAULT_VALS = {
+  ...defaultValues(reportConversionEvent.fields)
+}
+
+const ACCESS_TOKEN_URL = 'https://accounts.snapchat.com/login/oauth2/access_token'
+
+interface RefreshTokenResponse {
+  access_token: string
+}
+
+const presets: DestinationDefinition['presets'] = [
+  {
+    name: 'Add Billing',
+    subscribe: 'event = "Payment Info Entered"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'ADD_BILLING'
+    }
+  },
+  {
+    name: 'Add to Cart',
+    subscribe: 'event = "Product Added"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'ADD_CART'
+    }
+  },
+  {
+    name: 'Add to Wishlist',
+    subscribe: 'event = "Product Added to Wishlist"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'ADD_TO_WISHLIST'
+    }
+  },
+  {
+    name: 'App Install',
+    subscribe: 'event = "Application Installed"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'APP_INSTALL'
+    }
+  },
+  {
+    name: 'List View',
+    subscribe: 'event = "Product List Viewed"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'LIST_VIEW'
+    }
+  },
+  {
+    name: 'App Open',
+    subscribe: 'event = "Application Opened"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'APP_OPEN'
+    }
+  },
+  {
+    name: 'Login',
+    subscribe: 'event = "Signed In"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'LOGIN'
+    }
+  },
+  {
+    name: 'Page View',
+    subscribe: 'type = "page"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'PAGE_VIEW'
+    }
+  },
+  {
+    name: 'Purchase',
+    subscribe: 'event = "Order Completed"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'PURCHASE'
+    }
+  },
+  {
+    name: 'Search',
+    subscribe: 'event = "Products Searched"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'SEARCH'
+    }
+  },
+  {
+    name: 'Share',
+    subscribe: 'event = "Product Shared"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'SHARE'
+    }
+  },
+  {
+    name: 'Sign Up',
+    subscribe: 'event = "Signed Up"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'SIGN_UP'
+    }
+  },
+  {
+    name: 'Start Checkout',
+    subscribe: 'event = "Checkout Started"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'START_CHECKOUT'
+    }
+  },
+  {
+    name: 'View Content',
+    subscribe: 'event = "Product Viewed"',
+    partnerAction: 'reportConversionEvent',
+    mapping: {
+      ...DEFAULT_VALS,
+      event_type: 'VIEW_CONTENT'
+    }
+  }
+]
+
+const destination: DestinationDefinition<Settings> = {
+  name: 'Snap Conversions Api',
+  slug: 'actions-snap-conversions',
+  mode: 'cloud',
+
+  authentication: {
+    scheme: 'oauth2',
+    fields: {
+      pixel_id: {
+        label: 'Pixel ID',
+        description: 'The Pixel ID for your Snapchat Ad Account. Required for web and offline events.',
+        type: 'string'
+      },
+      snap_app_id: {
+        label: 'Snap App ID',
+        description:
+          'The Snap App ID associated with your app. This is a unique code generated in Snapchat Ads Manager and included in your MMP dashboard. Required for app events.',
+        type: 'string'
+      },
+      app_id: {
+        label: 'App ID',
+        description:
+          'The unique ID assigned for a given application. It should be numeric for iOS, and the human interpretable string for Android. Required for app events.',
+        type: 'string'
+      }
+    },
+    refreshAccessToken: async (request, { auth }) => {
+      // Return a request that refreshes the access_token if the API supports it
+      const res = await request<RefreshTokenResponse>(ACCESS_TOKEN_URL, {
+        method: 'POST',
+        body: new URLSearchParams({
+          refresh_token: auth.refreshToken,
+          client_id: auth.clientId,
+          client_secret: auth.clientSecret,
+          grant_type: 'refresh_token'
+        })
+      })
+
+      return { accessToken: res.data.access_token }
+    }
+  },
+  extendRequest({ auth }) {
+    return {
+      headers: {
+        authorization: `Bearer ${auth?.accessToken}`
+      }
+    }
+  },
+  presets,
+  actions: {
+    reportConversionEvent
+  }
+}
+
+export default destination
