57820ad7851e8a30ba8b4fba48d6f5bea2e26486
Robert Hall <rhall@twilio.com>
2022-06-06 13:00:21 -0500

    add refreshAccessToken implementation for Google Sheets

--- packages/destination-actions/src/destinations/google-sheets/index.ts
+++ packages/destination-actions/src/destinations/google-sheets/index.ts
@@ -2,6 +2,12 @@ import type { DestinationDefinition } from '@segment/actions-core'
 import type { Settings } from './generated-types'
 
 import postSheet from './postSheet'
+interface RefreshTokenResponse {
+  access_token: string
+  scope: string
+  expires_in: number
+  token_type: string
+}
 
 const destination: DestinationDefinition<Settings> = {
   name: 'Google Sheets',
@@ -10,26 +16,25 @@ const destination: DestinationDefinition<Settings> = {
 
   authentication: {
     scheme: 'oauth2',
-    fields: {}
+    fields: {},
     // testAuthentication: (request) => {
     //   // Return a request that tests/validates the user's credentials.
     //   // If you do not have a way to validate the authentication fields safely,
     //   // you can remove the `testAuthentication` function, though discouraged.
     // },
-    // refreshAccessToken: async (request, { auth }) => {
-    //   // Return a request that refreshes the access_token if the API supports it
-    //   // const res = await request('https://www.example.com/oauth/refresh', {
-    //   //   method: 'POST',
-    //   //   body: new URLSearchParams({
-    //   //     refresh_token: auth.refreshToken,
-    //   //     client_id: auth.clientId,
-    //   //     client_secret: auth.clientSecret,
-    //   //     grant_type: 'refresh_token'
-    //   //   })
-    //   // })
+    refreshAccessToken: async (request, { auth }) => {
+      const res = await request<RefreshTokenResponse>('https://www.googleapis.com/oauth2/v4/token', {
+        method: 'POST',
+        body: new URLSearchParams({
+          refresh_token: auth.refreshToken,
+          client_id: auth.clientId,
+          client_secret: auth.clientSecret,
+          grant_type: 'refresh_token'
+        })
+      })
 
-    //   return { accessToken: '' }
-    // }
+      return { accessToken: res.data.access_token }
+    }
   },
   extendRequest({ auth }) {
     return {
