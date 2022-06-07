f3ca1e303ab29661c4d99b836f63109fac375e93
rvadera12 <rajul.vadera@segment.com>
2022-06-07 11:49:38 -0700

    added to payload

--- packages/destination-actions/src/destinations/snap-conversions-api/reportConversionEvent/generated-types.ts
+++ packages/destination-actions/src/destinations/snap-conversions-api/reportConversionEvent/generated-types.ts
@@ -1,27 +1,30 @@
 // Generated file. DO NOT MODIFY IT BY HAND.
 
 export interface Payload {
-  item_category?: string;
-  item_ids?: string;
-  description?: string;
-  number_items?: string | number;
-  price?: string | number;
-  currency?: string;
-  transaction_id?: string;
-  level?: string;
-  client_dedup_id?: string;
-  search_string?: string;
-  page_url?: string;
-  sign_up_method?: string;
-  ip_address?: string;
-  user_agent?: string;
-  idfv?: string;
-  uuid_c1?: string;
-  event_tag?: string;
-  event_conversion_type: string;
-  event_type: string;
-  mobile_ad_id?: string;
-  email?: string;
-  phone_number?: string;
-  timestamp: string;
+  snap_app_id?: string
+  app_id?: string
+  pixel_id?: string
+  item_category?: string
+  item_ids?: string
+  description?: string
+  number_items?: string | number
+  price?: string | number
+  currency?: string
+  transaction_id?: string
+  level?: string
+  client_dedup_id?: string
+  search_string?: string
+  page_url?: string
+  sign_up_method?: string
+  ip_address?: string
+  user_agent?: string
+  idfv?: string
+  uuid_c1?: string
+  event_tag?: string
+  event_conversion_type: string
+  event_type: string
+  mobile_ad_id?: string
+  email?: string
+  phone_number?: string
+  timestamp: string
 }
