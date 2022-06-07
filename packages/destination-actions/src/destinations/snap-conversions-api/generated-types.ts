65770c5da966417d46d87c334fae97f6f5e839e5
rvadera12 <rajul.vadera@segment.com>
2022-06-07 13:03:29 -0700

    added setting types

--- packages/destination-actions/src/destinations/snap-conversions-api/generated-types.ts
+++ packages/destination-actions/src/destinations/snap-conversions-api/generated-types.ts
@@ -1,3 +1,16 @@
 // Generated file. DO NOT MODIFY IT BY HAND.
 
-export interface Settings {}
+export interface Settings {
+  /**
+   * The Pixel ID for your Snapchat Ad Account. Required for web and offline events.
+   */
+  pixelId?: string
+  /**
+   * The Snap App ID associated with your app. This is a unique code generated in Snapchat Ads Manager and included in your MMP dashboard. Required for app events.
+   */
+  snapAppId?: string
+  /**
+   * The unique ID assigned for a given application. It should be numeric for iOS, and the human interpretable string for Android. Required for app events.
+   */
+  appId?: string
+}
