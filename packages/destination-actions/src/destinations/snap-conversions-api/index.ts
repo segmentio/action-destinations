3cf85e88cf9f08d3efbe77d11bdcc448b50515d1
rvadera12 <rajul.vadera@segment.com>
2022-06-07 12:56:44 -0700

    added settings

--- packages/destination-actions/src/destinations/snap-conversions-api/index.ts
+++ packages/destination-actions/src/destinations/snap-conversions-api/index.ts
@@ -10,7 +10,25 @@ const destination: DestinationDefinition<Settings> = {
 
   authentication: {
     scheme: 'oauth2',
-    fields: {}
+    fields: {
+      pixelId: {
+        label: 'Pixel ID',
+        description: 'The Pixel ID for your Snapchat Ad Account. Required for web and offline events.',
+        type: 'string'
+      },
+      snapAppId: {
+        label: 'Snap App ID',
+        description:
+          'The Snap App ID associated with your app. This is a unique code generated in Snapchat Ads Manager and included in your MMP dashboard. Required for app events.',
+        type: 'string'
+      },
+      appId: {
+        label: 'App ID',
+        description:
+          'The unique ID assigned for a given application. It should be numeric for iOS, and the human interpretable string for Android. Required for app events.',
+        type: 'string'
+      }
+    }
     //   testAuthentication: (request) => {
     //     // Return a request that tests/validates the user's credentials.
     //     // If you do not have a way to validate the authentication fields safely,
