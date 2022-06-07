acb5e94cec4f011be8b17044e4500461019b5115
rvadera12 <rajul.vadera@segment.com>
2022-06-07 15:10:05 -0700

    possible oauth issue

--- packages/destination-actions/src/destinations/snap-conversions-api/index.ts
+++ packages/destination-actions/src/destinations/snap-conversions-api/index.ts
@@ -7,11 +7,11 @@ const DEFAULT_VALS = {
   ...defaultValues(reportConversionEvent.fields)
 }
 
-const ACCESS_TOKEN_URL = 'https://accounts.snapchat.com/login/oauth2/access_token'
+// const ACCESS_TOKEN_URL = 'https://accounts.snapchat.com/login/oauth2/access_token'
 
-interface RefreshTokenResponse {
-  access_token: string
-}
+// interface RefreshTokenResponse {
+//   access_token: string
+// }
 
 const presets: DestinationDefinition['presets'] = [
   {
@@ -141,7 +141,6 @@ const presets: DestinationDefinition['presets'] = [
     }
   }
 ]
-
 const destination: DestinationDefinition<Settings> = {
   name: 'Snap Conversions Api',
   slug: 'actions-snap-conversions',
@@ -167,28 +166,40 @@ const destination: DestinationDefinition<Settings> = {
           'The unique ID assigned for a given application. It should be numeric for iOS, and the human interpretable string for Android. Required for app events.',
         type: 'string'
       }
-    },
-    refreshAccessToken: async (request, { auth }) => {
-      // Return a request that refreshes the access_token if the API supports it
-      const res = await request<RefreshTokenResponse>(ACCESS_TOKEN_URL, {
-        method: 'POST',
-        body: new URLSearchParams({
-          refresh_token: auth.refreshToken,
-          client_id: auth.clientId,
-          client_secret: auth.clientSecret,
-          grant_type: 'refresh_token'
-        })
-      })
+    }
+    //   testAuthentication: (request) => {
+    //     // Return a request that tests/validates the user's credentials.
+    //     // If you do not have a way to validate the authentication fields safely,
+    //     // you can remove the `testAuthentication` function, though discouraged.
+    //   },
+    //   refreshAccessToken: async (request, { auth }) => {
+    //     // Return a request that refreshes the access_token if the API supports it
+    //     const res = await request('https://www.example.com/oauth/refresh', {
+    //       method: 'POST',
+    //       body: new URLSearchParams({
+    //         refresh_token: auth.refreshToken,
+    //         client_id: auth.clientId,
+    //         client_secret: auth.clientSecret,
+    //         grant_type: 'refresh_token'
+    //       })
+    //     })
 
-      return { accessToken: res.data.access_token }
-    }
-  },
-  extendRequest({ auth }) {
-    return {
-      headers: {
-        authorization: `Bearer ${auth?.accessToken}`
-      }
-    }
+    //     return { accessToken: res.body.access_token }
+    //   }
+    // },
+    // extendRequest({ auth }) {
+    //   return {
+    //     headers: {
+    //       authorization: `Bearer ${auth?.accessToken}`
+    //     }
+    //   }
+    // },
+
+    // onDelete: async (request, { settings, payload }) => {
+    //   // Return a request that performs a GDPR delete for the provided Segment userId or anonymousId
+    //   // provided in the payload. If your destination does not support GDPR deletion you should not
+    //   // implement this function and should remove it completely.
+    // },
   },
   presets,
   actions: {
