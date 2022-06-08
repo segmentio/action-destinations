998b737729be39acbcce03e9fe8560df51411b31
Dan <dlasky@users.noreply.github.com>
2022-06-08 13:18:47 -0400

    add default value to sandbox (#619)

--- packages/destination-actions/src/destinations/salesforce/index.ts
+++ packages/destination-actions/src/destinations/salesforce/index.ts
@@ -31,7 +31,8 @@ const destination: DestinationDefinition<Settings> = {
         label: 'Sandbox Instance',
         description:
           'Enable to authenticate into a sandbox instance. You can log in to a sandbox by appending the sandbox name to your Salesforce username. For example, if a username for a production org is user@acme.com and the sandbox is named test, the username to log in to the sandbox is user@acme.com.test. If you are already authenticated, please disconnect and reconnect with your sandbox username.',
-        type: 'boolean'
+        type: 'boolean',
+        default: false
       }
     },
     refreshAccessToken: async (request, { auth, settings }) => {
