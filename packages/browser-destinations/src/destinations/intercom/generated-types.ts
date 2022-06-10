6ea1a0b5d3e17a5b37e510061d73be79224dd737
Ibrahim <khajanchi.ibrahim@gmail.com>
2022-06-10 16:38:03 -0700

    intercom trackEvent first commit

---
+++ packages/browser-destinations/src/destinations/intercom/generated-types.ts
@@ -0,0 +1,8 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Settings {
+  /**
+   * The app_id of your Intercom app which will indicate where to store any data
+   */
+  appId: string
+}
---
+++ packages/browser-destinations/src/destinations/intercom/trackEvent/__tests__/index.test.ts
@@ -0,0 +1,6 @@
+import { Analytics, Context } from '@segment/analytics-next'
+import { destination } from '../../index'
+
+describe('Intercom.trackEvent', () => {
+  // TODO: Test your action
+})
