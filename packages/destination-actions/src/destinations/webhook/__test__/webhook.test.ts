0dc437d4ef68b259d5adb3f632c295a086d6b8e3
Tyson Mote <tyson@segment.com>
2022-06-27 19:58:09 -0700

    destinations/webhook: Add request-signing support

--- packages/destination-actions/src/destinations/webhook/__test__/webhook.test.ts
+++ packages/destination-actions/src/destinations/webhook/__test__/webhook.test.ts
@@ -1,18 +1,15 @@
 import nock from 'nock'
 import { createTestEvent, createTestIntegration } from '@segment/actions-core'
 import Webhook from '../index'
+import { createHmac } from 'crypto'
 
 const testDestination = createTestIntegration(Webhook)
-const timestamp = new Date().toISOString()
 
 describe('Webhook', () => {
   describe('send', () => {
     it('should work with default mapping', async () => {
       const url = 'https://example.com'
-      const event = createTestEvent({
-        timestamp,
-        event: 'Test Event'
-      })
+      const event = createTestEvent()
 
       nock(url)
         .post('/', event as any)
@@ -32,10 +29,7 @@ describe('Webhook', () => {
 
     it('supports customizations', async () => {
       const url = 'https://example.build'
-      const event = createTestEvent({
-        timestamp,
-        event: 'Test Event'
-      })
+      const event = createTestEvent()
       const headerField = 'Custom-Header'
       const headerValue = 'Custom-Value'
       const data = { cool: true }
@@ -50,6 +44,35 @@ describe('Webhook', () => {
           headers: { [headerField]: headerValue },
           data
         }
+      })
+
+      expect(responses.length).toBe(1)
+      expect(responses[0].status).toBe(200)
+    })
+
+    it('supports request signing', async () => {
+      const url = 'https://example.com'
+      const event = createTestEvent({
+        properties: { cool: true }
+      })
+      const payload = event.properties
+      const sharedSecret = 'abc123'
+
+      const digest = createHmac('sha1', sharedSecret).update(JSON.stringify(payload), 'utf8').digest('hex')
+
+      nock(url)
+        .matchHeader('x-signature', digest)
+        .post('/', payload as any)
+        .reply(200)
+
+      const responses = await testDestination.testAction('send', {
+        event,
+        mapping: {
+          url,
+          data: { '@path': '$.properties' }
+        },
+        settings: { sharedSecret },
+        useDefaultMappings: true
       })
 
       expect(responses.length).toBe(1)
