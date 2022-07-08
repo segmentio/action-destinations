1a38d707d5c0676286414146f58ddbe25db6fe65
Kevin Wang <kwang@twilio.com>
2022-07-08 16:02:13 -0700

    Add unit test for prospect upsert

--- packages/destination-actions/src/destinations/pardot/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/pardot/__tests__/index.test.ts
@@ -1,19 +1,76 @@
 import nock from 'nock'
-import { createTestIntegration } from '@segment/actions-core'
+import { createTestIntegration, createTestEvent } from '@segment/actions-core'
 import Definition from '../index'
+import { API_VERSION } from '../pa-operations'
 
 const testDestination = createTestIntegration(Definition)
 
-describe('Pardot', () => {
-  describe('testAuthentication', () => {
-    it('should validate authentication inputs', async () => {
-      nock('https://your.destination.endpoint').get('*').reply(200, {})
+const settings = {
+  businessUnitID: 'xyz321',
+  accountID: 'abc123',
+  isSandbox: false
+}
+const auth = {
+  refreshToken: 'xyz321',
+  accessToken: 'abc123'
+}
 
-      // This should match your authentication.fields
-      //const authData = {}
-      //TODO
-      testDestination
-      //await expect(testDestination.testAuthentication(authData)).resolves.not.toThrowError()
+const baseUrl = 'https://pi.pardot.com'
+
+describe('Pardot', () => {
+  describe('Prospects', () => {
+    it('should create prospects record', async () => {
+      nock(`${baseUrl}/api/${API_VERSION}/prospects/do`).post('/upsertLatestByEmail').reply(201, {})
+
+      const event = createTestEvent({
+        type: 'track',
+        event: 'Create or update Prospect',
+        properties: {
+          email: 'test@segment.com',
+          first_name: 'Kevin'
+        }
+      })
+
+      const responses = await testDestination.testAction('prospects', {
+        event,
+        settings,
+        auth,
+        mapping: {
+          email: {
+            '@path': '$.properties.email'
+          },
+          firstName: {
+            '@path': '$.properties.first_name'
+          },
+          secondaryDeletedSearch: true
+        }
+      })
+
+      expect(responses.length).toBe(1)
+      expect(responses[0].status).toBe(201)
+
+      expect(responses[0].request.headers).toMatchInlineSnapshot(`
+        Headers {
+          Symbol(map): Object {
+            "authorization": Array [
+              "Bearer abc123",
+            ],
+            "content-type": Array [
+              "application/json",
+            ],
+            "pardot-business-unit-id": Array [
+              "xyz321",
+            ],
+            "user-agent": Array [
+              "Segment (Actions)",
+            ],
+          },
+        }
+      `)
+
+      expect(responses[0].options.body).toMatchInlineSnapshot(
+        `"{\\"matchEmail\\":\\"test@segment.com\\",\\"prospect\\":{\\"email\\":\\"test@segment.com\\"},\\"secondaryDeletedSearch\\":true}"`
+      )
     })
   })
 })
