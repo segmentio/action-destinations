d781ea96bb6b5baa2d1eef86e4d93a24ce88aa4c
maryamsharif <99763167+maryamsharif@users.noreply.github.com>
2022-07-19 14:37:37 -0700

    GA4 updates (#676)
    * [STRATCONN-1413] event parameters check
    
    * [STRATCONN-1356] add timestamp micros
    
    * simplified some logic with comments suggestions
    
    * adding definition change and modify request_object

--- packages/destination-actions/src/destinations/google-analytics-4/ga4-functions.ts
+++ packages/destination-actions/src/destinations/google-analytics-4/ga4-functions.ts
@@ -7,3 +7,51 @@ export function verifyCurrency(currency: string): void {
     throw new IntegrationError(`${currency} is not a valid currency code.`, 'Incorrect value format', 400)
   }
 }
+
+// Ensure the values in params match Googles expectations
+export function verifyParams(params: object | undefined): void {
+  if (!params) {
+    return
+  }
+
+  Object.values(params).forEach((value) => {
+    if (typeof value != 'string' && typeof value != 'number') {
+      throw new IntegrationError(
+        'GA4 only accepts string or number values for event parameters and item parameters. Please ensure you are not including null, array, or nested values.',
+        'Invalid value',
+        400
+      )
+    }
+  })
+}
+
+export function verifyUserProps(userProperties: object | undefined): void {
+  if (!userProperties) {
+    return
+  }
+
+  Object.values(userProperties).forEach((value) => {
+    if (typeof value != 'string' && typeof value != 'number' && value != null) {
+      throw new IntegrationError(
+        'GA4 only accepts string, number or null values for user properties. Please ensure you are not including array or nested values.',
+        'Invalid value',
+        400
+      )
+    }
+  })
+}
+
+// Google expects timestamps to be in Unix microseconds
+export function convertTimestamp(timestamp: string | undefined): number | undefined {
+  if (!timestamp) {
+    return undefined
+  }
+
+  // verify that timestamp is not already in unix
+  if (!isNaN(+timestamp)) {
+    return +timestamp
+  }
+
+  // converts non-unix timestamp to unix microseconds
+  return Date.parse(timestamp) * 1000
+}
