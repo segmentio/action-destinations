b780c8073b4e67cb50dcbc95033cee95899a0957
Ibrahim Khazanchi <65377513+ibbykhazanchi@users.noreply.github.com>
2022-07-21 14:11:40 -0700

    Apply suggestions from code review
    Co-authored-by: kdaswani <49517136+kdaswani@users.noreply.github.com>

--- packages/browser-destinations/src/destinations/intercom/sharedCompanyProperties.ts
+++ packages/browser-destinations/src/destinations/intercom/sharedCompanyProperties.ts
@@ -2,8 +2,8 @@ import { InputField } from '@segment/actions-core'
 
 export const getCompanyProperties = (): Record<string, InputField> => ({
   company_id: {
-    description: 'The company id of the company.',
-    label: 'Company Id',
+    description: 'The unique identifier of the company.',
+    label: 'Company ID',
     type: 'string',
     required: true
   },
@@ -15,36 +15,36 @@ export const getCompanyProperties = (): Record<string, InputField> => ({
   },
   created_at: {
     description: 'The time the company was created in your system.',
-    label: 'Created At',
+    label: 'Company Creation Time',
     type: 'datetime',
     required: false
   },
   plan: {
-    description: 'The name of the plan the company is on.',
+    description: 'The name of the plan you have associated with the company.',
     label: 'Company Plan',
     type: 'string',
     required: false
   },
   monthly_spend: {
-    description: 'How much revenue the company generates for your business.',
+    description: 'The monthly spend of the company, e.g. how much revenue the company generates for your business.',
     label: 'Monthly Spend',
     type: 'integer',
     required: false
   },
   size: {
     description: 'The number of employees in the company.',
-    label: 'Size',
+    label: 'Company Size',
     type: 'integer',
     required: false
   },
   website: {
     description: 'The URL for the company website.',
-    label: 'Website',
+    label: 'Company Website',
     type: 'string',
     required: false
   },
   industry: {
-    description: 'The industry of the company.',
+    description: 'The industry that the company operates in.',
     label: 'Industry',
     type: 'string',
     required: false
