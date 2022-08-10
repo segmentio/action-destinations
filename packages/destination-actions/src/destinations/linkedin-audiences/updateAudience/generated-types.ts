64484ffc48f09b5e294e5ac9f518d26a56cb870e
Brennan Gamwell <brennan@segment.com>
2022-08-10 13:38:27 -0700

    Generate types.

--- packages/destination-actions/src/destinations/linkedin-audiences/updateAudience/generated-types.ts
+++ packages/destination-actions/src/destinations/linkedin-audiences/updateAudience/generated-types.ts
@@ -1,3 +1,20 @@
 // Generated file. DO NOT MODIFY IT BY HAND.
 
-export interface Payload {}
+export interface Payload {
+  /**
+   * The name of the LinkedIn DMP Segment to send data to.
+   */
+  dmp_segment_name: string
+  /**
+   * Enable batching of requests to the DMP Segment.
+   */
+  enable_batching?: boolean
+  /**
+   * Whether to send a SHA-256 hash of users' email address to LinkedIn.
+   */
+  send_email_address?: boolean
+  /**
+   * Whether to send each user's Google Advertising ID (GAID) to LinkedIn. GAID is sometimes referred to as Android Advertising ID.
+   */
+  send_google_advertising_id?: boolean
+}
