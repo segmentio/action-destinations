28b406a24ba362731620ed377a6eb7e5f426aaaa
Zihe Jia <zihe.jia@mixpanel.com>
2022-08-18 11:06:38 -0700

    Add batching support to the Mixpanel Actions Destination

--- packages/destination-actions/src/destinations/mixpanel/trackEvent/index.ts
+++ packages/destination-actions/src/destinations/mixpanel/trackEvent/index.ts
@@ -1,9 +1,85 @@
-import { ActionDefinition, InvalidAuthenticationError } from '@segment/actions-core'
+import { ActionDefinition, InvalidAuthenticationError, RequestClient } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 import { MixpanelEvent } from './types'
 import { getApiServerUrl, getBrowser, getBrowserVersion, cheapGuid } from '../utils'
 import dayjs from '../../../lib/dayjs'
+
+
+const getEventFromPayload = (payload: Payload): MixpanelEvent => {
+  const datetime = payload.time
+  const time = datetime && dayjs.utc(datetime).isValid() ? dayjs.utc(datetime).valueOf() : Date.now()
+
+  const utm = payload.utm_properties || {}
+  let browser, browserVersion
+  if (payload.userAgent) {
+    browser = getBrowser(payload.userAgent, payload.device_manufacturer)
+    browserVersion = getBrowserVersion(payload.userAgent, payload.device_manufacturer)
+  }
+
+  const event: MixpanelEvent = {
+    event: payload.event,
+    properties: {
+      time: time,
+      ip: payload.ip,
+      id: payload.distinct_id,
+      distinct_id: payload.distinct_id,
+      $app_build_number: payload.app_build,
+      $app_version_string: payload.app_version,
+      $app_namespace: payload.app_namespace,
+      $app_name: payload.app_name,
+      $browser: browser,
+      $browser_version: browserVersion,
+      $bluetooth_enabled: payload.bluetooth,
+      $cellular_enabled: payload.cellular,
+      $carrier: payload.carrier,
+      $current_url: payload.url,
+      $device: payload.device_name,
+      $device_id: payload.device_id,
+      $device_type: payload.device_type,
+      $device_name: payload.device_name,
+      $group_id: payload.group_id,
+      $insert_id: cheapGuid(),
+      $ios_ifa: payload.idfa,
+      $lib_version: payload.library_version,
+      $locale: payload.language,
+      $manufacturer: payload.device_manufacturer,
+      $model: payload.device_model,
+      $os: payload.os_name,
+      $os_version: payload.os_version,
+      $referrer: payload.referrer,
+      $screen_height: payload.screen_height,
+      $screen_width: payload.screen_width,
+      $screen_density: payload.screen_density,
+      $source: 'segment',
+      $wifi_enabled: payload.wifi,
+      mp_country_code: payload.country,
+      mp_lib: payload.library_name && `Segment: ${payload.library_name}`,
+      utm_campaign: utm.utm_campaign,
+      utm_content: utm.utm_content,
+      utm_medium: utm.utm_medium,
+      utm_source: utm.utm_source,
+      utm_term: utm.utm_term,
+      ...payload.event_properties
+    }
+  }
+  return event;
+}
+
+const processData = async (request: RequestClient, settings: Settings, payload: Payload[]) => {
+  if (!settings.apiSecret) {
+    throw new InvalidAuthenticationError('Missing api secret')
+  }
+
+  const events = payload.map((value) => getEventFromPayload(value))
+  return request(`${getApiServerUrl(settings.apiRegion)}/import?strict=1`, {
+    method: 'post',
+    json: events,
+    headers: {
+      authorization: `Basic ${Buffer.from(`${settings.apiSecret}:`).toString('base64')}`
+    }
+  })
+}
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Track Event',
@@ -328,74 +404,13 @@ const action: ActionDefinition<Settings, Payload> = {
       }
     }
   },
-  perform: async (request, { payload, settings }) => {
-    const datetime = payload.time
-    const time = datetime && dayjs.utc(datetime).isValid() ? dayjs.utc(datetime).valueOf() : Date.now()
 
-    const utm = payload.utm_properties || {}
-    let browser, browserVersion
-    if (payload.userAgent) {
-      browser = getBrowser(payload.userAgent, payload.device_manufacturer)
-      browserVersion = getBrowserVersion(payload.userAgent, payload.device_manufacturer)
-    }
+  performBatch: async (request, { settings, payload }) => {
+    return processData(request, settings, payload)
+  },
 
-    const event: MixpanelEvent = {
-      event: payload.event,
-      properties: {
-        time: time,
-        ip: payload.ip,
-        id: payload.distinct_id,
-        distinct_id: payload.distinct_id,
-        $app_build_number: payload.app_build,
-        $app_version_string: payload.app_version,
-        $app_namespace: payload.app_namespace,
-        $app_name: payload.app_name,
-        $browser: browser,
-        $browser_version: browserVersion,
-        $bluetooth_enabled: payload.bluetooth,
-        $cellular_enabled: payload.cellular,
-        $carrier: payload.carrier,
-        $current_url: payload.url,
-        $device: payload.device_name,
-        $device_id: payload.device_id,
-        $device_type: payload.device_type,
-        $device_name: payload.device_name,
-        $group_id: payload.group_id,
-        $insert_id: cheapGuid(),
-        $ios_ifa: payload.idfa,
-        $lib_version: payload.library_version,
-        $locale: payload.language,
-        $manufacturer: payload.device_manufacturer,
-        $model: payload.device_model,
-        $os: payload.os_name,
-        $os_version: payload.os_version,
-        $referrer: payload.referrer,
-        $screen_height: payload.screen_height,
-        $screen_width: payload.screen_width,
-        $screen_density: payload.screen_density,
-        $source: 'segment',
-        $wifi_enabled: payload.wifi,
-        mp_country_code: payload.country,
-        mp_lib: payload.library_name && `Segment: ${payload.library_name}`,
-        utm_campaign: utm.utm_campaign,
-        utm_content: utm.utm_content,
-        utm_medium: utm.utm_medium,
-        utm_source: utm.utm_source,
-        utm_term: utm.utm_term,
-        ...payload.event_properties
-      }
-    }
-
-    if (!settings.apiSecret) {
-      throw new InvalidAuthenticationError('Missing api secret')
-    }
-    return request(`${getApiServerUrl(settings.apiRegion)}/import?strict=1`, {
-      method: 'post',
-      json: [event],
-      headers: {
-        authorization: `Basic ${Buffer.from(`${settings.apiSecret}:`).toString('base64')}`
-      }
-    })
+  perform: async (request, { settings, payload }) => {
+    return processData(request, settings, [payload])
   }
 }
 
