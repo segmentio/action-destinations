3057f71843c068dc80ee326dc3e935512d9749f9
Maryam Sharif <maryam.sharif@segment.com>
2022-08-23 16:57:52 -0700

    sends conversion tracking id in action

--- packages/destination-actions/src/destinations/google-enhanced-conversions/generated-types.ts
+++ packages/destination-actions/src/destinations/google-enhanced-conversions/generated-types.ts
@@ -2,7 +2,7 @@
 
 export interface Settings {
   /**
-   * You will find this information in the event snippet for your conversion action, for example `send_to: AW-CONVERSION_ID/AW-CONVERSION_LABEL`. In the sample snippet, AW-CONVERSION_ID stands for the conversion ID unique to your account. Enter the conversion Id, without the AW- prefix.
+   * You will find this information in the event snippet for your conversion action, for example `send_to: AW-CONVERSION_ID/AW-CONVERSION_LABEL`. In the sample snippet, AW-CONVERSION_ID stands for the conversion ID unique to your account. Enter the conversion Id, without the AW- prefix. This is required if you are using the Upload Enhanced Conversion (Legacy) Action which uses the Enhanced Conversions API.
    */
-  conversionTrackingId: string
+  conversionTrackingId?: string
 }
