d2bfa8d8fa6fe4ff825a32f2feacc24f3f9a5eb1
Pooya Jaferian <pooya.j@gmail.com>
2022-08-26 12:28:38 -0700

    adding tests for all of the actions

--- packages/browser-destinations/src/destinations/hubspot-web/trackCustomBehavioralEvent/__tests__/index.test.ts
+++ packages/browser-destinations/src/destinations/hubspot-web/trackCustomBehavioralEvent/__tests__/index.test.ts
@@ -1,6 +1,65 @@
 import { Analytics, Context } from '@segment/analytics-next'
-import { destination } from '../../index'
+import { Subscription } from 'src/lib/browser-destinations'
+import hubspotDestination, { destination } from '../../index'
+import { Hubspot } from '../../types'
 
-describe('HubspotWeb.trackCustomBehavioralEvent', () => {
-  // TODO: Test your action
+const subscriptions: Subscription[] = [
+  {
+    partnerAction: 'trackCustomBehavioralEvent',
+    name: 'Track Custom Behavioral Event',
+    enabled: true,
+    subscribe: 'type = "track"',
+    mapping: {
+      name: {
+        '@path': '$.event'
+      },
+      properties: {
+        '@path': '$.properties'
+      }
+    }
+  }
+]
+
+describe('Hubspot.trackCustomBehavioralEvent', () => {
+  const settings = {
+    portalId: '1234'
+  }
+
+  let mockHubspot: Hubspot
+  let trackCustomBehavioralEvent: any
+  beforeEach(async () => {
+    jest.restoreAllMocks()
+
+    const [trackCustomBehavioralEventPlugin] = await hubspotDestination({
+      ...settings,
+      subscriptions
+    })
+    trackCustomBehavioralEvent = trackCustomBehavioralEventPlugin
+
+    jest.spyOn(destination, 'initialize').mockImplementation(() => {
+      mockHubspot = {
+        push: jest.fn()
+      }
+      return Promise.resolve(mockHubspot)
+    })
+    await trackCustomBehavioralEvent.load(Context.system(), {} as Analytics)
+  })
+
+  test('maps custom traits correctly', async () => {
+    const context = new Context({
+      type: 'track',
+      event: 'purchased a 🍱',
+      properties: {
+        type: '🍣',
+        price: '$12.00',
+        currency: 'USD'
+      }
+    })
+    await trackCustomBehavioralEvent.track?.(context)
+
+    expect(mockHubspot.push).toHaveBeenCalledWith([
+      'trackCustomBehavioralEvent',
+      { name: 'purchased a 🍱', properties: { currency: 'USD', price: '$12.00', type: '🍣' } }
+    ])
+  })
 })
--- packages/browser-destinations/src/destinations/hubspot-web/upsertContact/__tests__/index.test.ts
+++ packages/browser-destinations/src/destinations/hubspot-web/upsertContact/__tests__/index.test.ts
@@ -1,6 +1,81 @@
 import { Analytics, Context } from '@segment/analytics-next'
-import { destination } from '../../index'
+import { Subscription } from 'src/lib/browser-destinations'
+import hubspotDestination, { destination } from '../../index'
+import { Hubspot } from '../../types'
 
-describe('HubspotWeb.upsertContact', () => {
-  // TODO: Test your action
+const subscriptions: Subscription[] = [
+  {
+    partnerAction: 'upsertContact',
+    name: 'Upsert Contact',
+    enabled: true,
+    subscribe: 'type = "identify"',
+    mapping: {
+      email: {
+        '@path': '$.traits.email'
+      },
+      custom_properties: {
+        '@path': '$.traits'
+      },
+      id: {
+        '@path': '$.userId'
+      }
+    }
+  }
+]
+
+describe('Hubspot.trackPageView', () => {
+  const settings = {
+    portalId: '1234'
+  }
+
+  let mockHubspot: Hubspot
+  let upsertContactEvent: any
+  beforeEach(async () => {
+    jest.restoreAllMocks()
+
+    const [upsertContactEventPlugin] = await hubspotDestination({
+      ...settings,
+      subscriptions
+    })
+    upsertContactEvent = upsertContactEventPlugin
+
+    jest.spyOn(destination, 'initialize').mockImplementation(() => {
+      mockHubspot = {
+        push: jest.fn()
+      }
+      return Promise.resolve(mockHubspot)
+    })
+    await upsertContactEvent.load(Context.system(), {} as Analytics)
+  })
+
+  test('does not call Hubspot if there is no email or id', async () => {
+    const context = new Context({
+      type: 'identify',
+      anonymousId: '👻',
+      traits: {
+        friendly: true
+      }
+    })
+
+    await upsertContactEvent.identify?.(context)
+    expect(mockHubspot.push).toHaveBeenCalledTimes(0)
+  })
+
+  test('Identifies the user to Hubspot using when both email and id are present', async () => {
+    const context = new Context({
+      type: 'identify',
+      userId: 'real_hubspot_tester',
+      traits: {
+        friendly: false,
+        email: 'real_hubspot_tester@jest_experts.com'
+      }
+    })
+
+    await upsertContactEvent.identify?.(context)
+    expect(mockHubspot.push).toHaveBeenCalledTimes(1)
+    expect(mockHubspot.push).toHaveBeenCalledWith([
+      'identify',
+      { email: 'real_hubspot_tester@jest_experts.com', id: 'real_hubspot_tester', friendly: false }
+    ])
+  })
 })
