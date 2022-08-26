24b25daca70a837f6a33e5aee3b07b45f2e72404
Pooya Jaferian <pooya.j@gmail.com>
2022-08-26 09:46:07 -0700

    tests: init method

--- packages/browser-destinations/src/destinations/hubspot-web/__tests__/index.test.ts
+++ packages/browser-destinations/src/destinations/hubspot-web/__tests__/index.test.ts
@@ -1,4 +1,54 @@
+import { Subscription } from '../../../lib/browser-destinations'
 import { Analytics, Context } from '@segment/analytics-next'
-import plugins, { destination } from '../index'
+import hubspotDestination, { destination } from '../index'
 
-describe('Hubspot', () => {})
+const subscriptions: Subscription[] = [
+  {
+    partnerAction: 'trackCustomBehavioralEvent',
+    name: 'Track Custom Behavioral Event',
+    enabled: true,
+    subscribe: 'type = "track"',
+    mapping: {
+      event_name: {
+        '@path': '$.event'
+      }
+    }
+  }
+]
+
+describe('Hubspot Web (Actions)', () => {
+  test('loads hubspot analytics with just a HubID', async () => {
+    const [event] = await hubspotDestination({
+      portalId: '12345',
+      subscriptions
+    })
+
+    jest.spyOn(destination, 'initialize')
+
+    await event.load(Context.system(), {} as Analytics)
+    expect(destination.initialize).toHaveBeenCalled()
+
+    const hsScript = window.document.querySelector('#hs-analytics') as HTMLScriptElement
+
+    expect(hsScript).toBeDefined()
+    expect(hsScript?.src).toContain('https://js.hs-analytics.net/analytics')
+  })
+
+  test('loads hubspot analytics with EU script', async () => {
+    const [event] = await hubspotDestination({
+      portalId: '12345',
+      enableEuropeanDataCenter: true,
+      subscriptions
+    })
+
+    jest.spyOn(destination, 'initialize')
+
+    await event.load(Context.system(), {} as Analytics)
+    expect(destination.initialize).toHaveBeenCalled()
+
+    const hsScript = window.document.querySelector('#hs-analytics') as HTMLScriptElement
+
+    expect(hsScript).toBeDefined()
+    expect(hsScript?.src).toContain('https://js-eu1.hs-analytics.net')
+  })
+})
