551b6a29f2bd7b8ba9fadf01a78f24b93794335e
shhegde <shhegde@twilio.com>
2022-08-25 18:12:53 -0700

    changing ope_alt_user_ids from array to string type for 1PlusX

--- packages/destination-actions/src/destinations/1plusx/sendPageview/index.ts
+++ packages/destination-actions/src/destinations/1plusx/sendPageview/index.ts
@@ -30,8 +30,8 @@ const action: ActionDefinition<Settings, Payload> = {
       label: 'Alternative User IDs',
       description:
         'Alternative user ids if there is more than one identifier available, each prefixed with the identifier type and separated by commas',
-      type: 'string',
-      multiple: true
+      type: 'string'
+      //multiple: true  --commenting this since Fox wants ope_alt_user_ids to be a string value
     },
     //Highly recommended to include
     ope_item_uri: {
