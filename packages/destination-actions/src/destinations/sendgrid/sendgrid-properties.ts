7a16a5c9d07397e2bf56916260b959db7639bdf1
Anand Jha <aanandjha@users.noreply.github.com>
2022-09-12 14:33:25 -0700

    Adding Sendgrid destination with upsert mapping for contacts or user profiles (#753)
    * MCSCALE-1107: adding Sendgrid_MarketingCampaign destination
    
    * MCSCALE-1107: adding support for batch update for Sendgrid_MarketingCampaign destination
    
    * MCSCALE-1107: adressing code review from Dan on using of using of JS functions instead of navie approach
    
    * MCSCALE-1107: adressing code review from Dan on using of using of JS functions instead of navie approach
    
    * Adding unit test for batch contact updates for sendgrid
    
    * Fixing Description and slug name for Sendgrid Marketing Campaigns
    
    * Fixing text description for various fields
    
    * Fixing text description for various fields
    
    * Fixing unit test
    
    * Fixing description for consistency based on Kiara's feedback
    
    * Fixing description for consistency based on Kiara's feedback
    
    * Add register command with sendgrid unique id
    
    Co-authored-by: Anand Jha <anand.jha@sendgrid.com>
    Co-authored-by: Logan Luque <lluque@twilio.com>

---
+++ packages/destination-actions/src/destinations/sendgrid/sendgrid-properties.ts
@@ -0,0 +1,22 @@
+import { InputField } from '@segment/actions-core'
+import { Payload } from './updateUserProfile/generated-types'
+
+export const customFields: InputField = {
+  label: 'Other Fields',
+  description: `
+  Additional fields to send to SendGrid. On the left-hand side, input the SendGrid Custom Fields Id. On the right-hand side, map the Segment field that contains the value.
+
+  Custom Fields must be predefined in your SendGrid account and you can retrieve corresponding Id using get all field definitions endpoint.
+
+  Reference: [Get All field definitions](https://docs.sendgrid.com/api-reference/custom-fields/get-all-field-definitions)
+  ---
+
+  `,
+  type: 'object',
+  defaultObjectUI: 'keyvalue'
+}
+
+export const convertPayload = (payload: Payload) => {
+  const { state, primary_email, enable_batching, customFields, ...rest } = payload
+  return { ...rest, state_province_region: state, email: primary_email, custom_fields: customFields }
+}
