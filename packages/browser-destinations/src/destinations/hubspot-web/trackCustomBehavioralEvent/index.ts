e462f59a655db03d8e8a369da6c6c7f6ade015e5
Pooya Jaferian <pooya.j@gmail.com>
2022-09-20 08:30:21 -0700

    Hubspot Web (#742)
    * bootstrap hubspot web
    
    * adding top level settings
    
    * adding the skeleton for all actions
    
    * lib initialization
    
    * first working version of all actions
    
    * EU SDK support
    
    * tests: init method
    
    * adding tests for all of the actions
    
    * only identify if either email or id exist
    
    * added an option for immediately flushing identify calls
    
    * improve the handling of nested objects in the traits
    
    * import type
    
    Co-authored-by: Brennan Gamwell <brennan@segment.com>
    
    * Fix text
    
    Co-authored-by: Brennan Gamwell <brennan@segment.com>
    
    * improve handling of CustomBehavioralEvent
    
    * typo
    
    * make email required for upsert contact
    
    * types
    
    * change the wording around contacts vs. visitors
    
    * types
    
    * better email detection
    
    * flatten nested traits
    
    * support for forms SDK
    
    * support flattening for CBE
    
    * registering hubspot-web
    
    * adding presets
    
    * Update packages/browser-destinations/src/destinations/hubspot-web/upsertContact/index.ts
    
    Co-authored-by: Brennan Gamwell <brennan@segment.com>
    
    * Update packages/browser-destinations/src/destinations/hubspot-web/upsertContact/index.ts
    
    Co-authored-by: Brennan Gamwell <brennan@segment.com>
    
    * Update packages/browser-destinations/src/destinations/hubspot-web/upsertContact/index.ts
    
    Co-authored-by: Brennan Gamwell <brennan@segment.com>
    
    * Update packages/browser-destinations/src/destinations/hubspot-web/upsertContact/index.ts
    
    Co-authored-by: Brennan Gamwell <brennan@segment.com>
    
    * Update packages/browser-destinations/src/destinations/hubspot-web/upsertContact/index.ts
    
    Co-authored-by: Brennan Gamwell <brennan@segment.com>
    
    * Update packages/browser-destinations/src/destinations/hubspot-web/upsertContact/index.ts
    
    Co-authored-by: Brennan Gamwell <brennan@segment.com>
    
    * regional forms SDK
    
    * cleanup/types
    
    * default mapping adjustment
    
    * better typing
    
    Co-authored-by: Brennan Gamwell <brennan@segment.com>

---
+++ packages/browser-destinations/src/destinations/hubspot-web/trackCustomBehavioralEvent/index.ts
@@ -0,0 +1,49 @@
+import type { BrowserActionDefinition } from '../../../lib/browser-destinations'
+import type { Settings } from '../generated-types'
+import type { Payload } from './generated-types'
+import type { Hubspot } from '../types'
+import { flatten } from '../utils/flatten'
+
+// Change from unknown to the partner SDK types
+const action: BrowserActionDefinition<Settings, Hubspot, Payload> = {
+  title: 'Track Custom Behavioral Event',
+  description:
+    'Using custom behavioral events, you can tie event completions to contacts records and populate event properties with metadata about the event.',
+  platform: 'web',
+  defaultSubscription: 'type = "track"',
+  fields: {
+    name: {
+      description: 'The event_id or internal name of the event that you created in HubSpot.',
+      label: 'Event Name',
+      type: 'string',
+      required: true,
+      default: {
+        '@path': '$.event'
+      }
+    },
+    properties: {
+      description: 'A list of key-value pairs, with one key-value pair per property.',
+      label: 'Event Properties',
+      type: 'object',
+      required: false,
+      default: {
+        '@path': '$.properties'
+      }
+    }
+  },
+  perform: (_hsq, event) => {
+    let { name, properties } = event.payload
+
+    if (event.settings.formatCustomBehavioralEventNames) {
+      name = `pe${event.settings.portalId}_${name.replace(/[\s.]+/g, '_').toLocaleLowerCase()}`
+    }
+
+    // for custom properties, we will:
+    // remove any non-primitives, replace spaces and dots with underscores, then lowercase
+    properties = properties && flatten(properties, '', [], (key) => key.replace(/[\s.]+/g, '_').toLocaleLowerCase())
+
+    _hsq.push(['trackCustomBehavioralEvent', { name, properties }])
+  }
+}
+
+export default action
