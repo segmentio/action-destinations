203a41077e654450e9898735c9b3d912937fd87e
Varadarajan V <vvijayaragavan@twilio.com>
2022-09-27 13:54:21 +0530

    squash and merge 'hubspot-cloud-actions' into tx-variables
    commit ea5bba6c02c7ff65e5816a44881259844f26cd56
    Author: Varadarajan V <vvijayaragavan@twilio.com>
    Date:   Tue Sep 27 12:59:33 2022 +0530
    
        fix: default subscription in company and contact
    
    commit 3f634dc140bb74746c2f122b8d0e65012efa0e5e
    Author: Varadarajan V <vvijayaragavan@twilio.com>
    Date:   Tue Sep 27 12:51:34 2022 +0530
    
        Scaffold send custom behavioral event
    
    commit dd9eeea279ee35f729486ec1a1d5788b23e7c527
    Author: Sayan Das <sayan.das@segment.com>
    Date:   Mon Sep 26 17:21:04 2022 +0530
    
        Added refreshToken and testAuthentication
    
    commit 4e004481122d89536f95d3c7fa37a4cec1f75668
    Author: Varadarajan V <vvijayaragavan@twilio.com>
    Date:   Fri Sep 23 16:34:20 2022 +0530
    
        Scaffold company action
    
    commit 8ea6783828c8cbedac53952606e800778a41331e
    Author: Varadarajan V <vvijayaragavan@twilio.com>
    Date:   Fri Sep 23 16:22:00 2022 +0530
    
        scaffold contact action
    
    commit 5463d5a04b527393b09c22717ced61742dc0793b
    Author: Varadarajan V <vvijayaragavan@twilio.com>
    Date:   Fri Sep 23 15:26:00 2022 +0530
    
        register hubspot action destination
    
    commit 90019dc9bef40884b7a1da01044180ae7bd99ed7
    Author: Varadarajan V <vvijayaragavan@twilio.com>
    Date:   Fri Sep 23 15:22:25 2022 +0530
    
        fix: case in hubspot actions name
    
    commit 2ee50e90b50252da88c7b36300bb02332d72c120
    Author: Varadarajan V <vvijayaragavan@twilio.com>
    Date:   Fri Sep 23 15:21:28 2022 +0530
    
        rename folder: hubspot-cloud -> hubspot
    
    commit c2a9ba13963fd8b43e16085d3615432fdeea5141
    Merge: 90f0f352 e462f59a
    Author: Varadarajan V <vvijayaragavan@twilio.com>
    Date:   Wed Sep 21 18:46:08 2022 +0530
    
        Merge branch 'main' into hubspot-cloud-actions
    
    commit 90f0f352b71b9c05c5dfefd39ccd6f3a9af0f208
    Author: Varadarajan V <vvijayaragavan@twilio.com>
    Date:   Wed Sep 14 15:55:12 2022 +0530
    
        Scaffold HubSpot cloud destination

---
+++ packages/destination-actions/src/destinations/hubspot/sendCustomBehavioralEvent/generated-types.ts
@@ -0,0 +1,30 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * The internal event name assigned by HubSpot. This can be found in your HubSpot account. Events must be predefined in HubSpot. Learn how to find the internal name in [HubSpot’s documentation](https://knowledge.hubspot.com/analytics-tools/create-custom-behavioral-events?_ga=2.219778269.578939721.1663963266-497800475.1660075188#define-the-api-call).
+   */
+  eventName: string
+  /**
+   * The time when this event occurred. If this isn't set, the current time will be used.
+   */
+  occuredAt?: string
+  /**
+   * The email of the contact associated with this event. This is required if no user token or object ID is provided.
+   */
+  email?: string
+  /**
+   * The user token (utk) of the contact associated with this event. This is required if no email or object ID is provided.
+   */
+  utk?: string
+  /**
+   * The ID of the object associated with this event. This can be the HubSpot contact ID, company ID, or ID of any other object. This is required if no email or user token is provided.
+   */
+  objectId?: string
+  /**
+   * Default or custom properties that describe the event. See more information in [HubSpot’s documentation](https://knowledge.hubspot.com/analytics-tools/create-custom-behavioral-events#add-and-manage-event-properties).
+   */
+  properties?: {
+    [k: string]: unknown
+  }
+}
