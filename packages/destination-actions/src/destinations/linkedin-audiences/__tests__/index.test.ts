7b39392ab70a7e1f2fd6ebc3c543b8f5e9f65305
Brennan Gamwell <brennan@segment.com>
2022-09-28 10:25:48 -0700

    [HGI-65] LinkedIn Audiences Updates (#775)
    * Fixes testAuthentication method.
    * Adds https agent to LinkedIn destination and actions-core.

--- packages/destination-actions/src/destinations/linkedin-audiences/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/linkedin-audiences/__tests__/index.test.ts
@@ -7,10 +7,14 @@ const testDestination = createTestIntegration(Definition)
 describe('Linkedin Audiences', () => {
   describe('testAuthentication', () => {
     it('should validate authentication inputs', async () => {
-      nock('https://api.linkedin.com/rest/adAccountsV2').post(/.*/).reply(200, {})
+      nock('https://api.linkedin.com/rest/adAccounts').get(/.*/).reply(200, {})
 
       const authData = {
-        ad_account_id: 'testId'
+        ad_account_id: 'testId',
+        oauth: {
+          access_token: '123',
+          refresh_token: '123'
+        }
       }
 
       await expect(testDestination.testAuthentication(authData)).resolves.not.toThrowError()
