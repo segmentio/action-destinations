6db3ed3bd358349a09668fc218aa1ed87f305eb9
Varadarajan V <vvijayaragavan@twilio.com>
2022-10-03 12:03:57 +0530

    Add tests for contact action

--- packages/destination-actions/src/destinations/hubspot/contact/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/hubspot/contact/__tests__/index.test.ts
@@ -5,8 +5,10 @@ import { HubSpotBaseURL } from '../../properties'
 
 const testDestination = createTestIntegration(Destination)
 
+beforeEach(() => nock.cleanAll())
+
 describe('Hubspot.contact', () => {
-  test('should create contact sucessfully', () => {
+  test('should create contact successfully and set contact id in transaction context', async () => {
     const testEmail = 'vep@beri.dz'
     const event = createTestEvent({
       type: 'identify',
@@ -54,10 +56,162 @@ describe('Hubspot.contact', () => {
         message: 'resource not found',
         correlationId: 'be56c5f3-5841-4661-b52f-65b3aacd0244'
       })
-    nock(HubSpotBaseURL).post('/crm/v3/objects/contacts', postExpectedPayload).reply(201, {})
 
-    return expect(
-      testDestination.testAction('contact', { mapping, useDefaultMappings: true, event })
+    nock(HubSpotBaseURL).post('/crm/v3/objects/contacts', postExpectedPayload).reply(201, {
+      id: '801',
+      properties: postExpectedPayload.properties
+    })
+
+    const transactionContext: Record<string, string> = {}
+    const setTransactionContext = (key: string, value: string) => (transactionContext[key] = value)
+
+    await expect(
+      testDestination.testAction('contact', {
+        mapping,
+        useDefaultMappings: true,
+        event,
+        transactionContext: { transaction: {}, setTransaction: setTransactionContext }
+      })
     ).resolves.not.toThrowError()
+
+    expect(transactionContext['contact_id']).toEqual('801')
+  })
+  test('should update contact successfully and set contact id in transaction context', async () => {
+    const testEmail = 'vep@beri.dz'
+    const event = createTestEvent({
+      type: 'identify',
+      traits: {
+        email: testEmail,
+        first_name: 'John',
+        last_name: 'Doe',
+        address: {
+          city: 'San Fransico'
+        },
+        graduation_date: 1664533942262,
+        lifecyclestage: 'subscriber'
+      }
+    })
+
+    const patchExpectedPayload = {
+      properties: {
+        firstname: 'John',
+        lastname: 'Doe',
+        city: 'San Fransico',
+        graduation_date: 1664533942262,
+        lifecyclestage: 'subscriber'
+      }
+    }
+
+    const mapping = {
+      properties: {
+        graduation_date: {
+          '@path': '$.traits.graduation_date'
+        },
+        lifecyclestage: {
+          '@path': '$.traits.lifecyclestage'
+        }
+      }
+    }
+
+    nock(HubSpotBaseURL)
+      .patch(`/crm/v3/objects/contacts/${testEmail}?idProperty=email`, patchExpectedPayload)
+      .reply(200, {
+        id: '801',
+        properties: {
+          lifecyclestage: 'subscriber'
+        }
+      })
+
+    const transactionContext: Record<string, string> = {}
+    const setTransactionContext = (key: string, value: string) => (transactionContext[key] = value)
+
+    await expect(
+      testDestination.testAction('contact', {
+        mapping,
+        useDefaultMappings: true,
+        event,
+        transactionContext: { transaction: {}, setTransaction: setTransactionContext }
+      })
+    ).resolves.not.toThrowError()
+
+    expect(transactionContext['contact_id']).toEqual('801')
+  })
+  test('should reset lifecyclestage and update if lifecyclestage is to be moved backwards', async () => {
+    const testEmail = 'vep@beri.dz'
+    const event = createTestEvent({
+      type: 'identify',
+      traits: {
+        email: testEmail,
+        first_name: 'John',
+        last_name: 'Doe',
+        address: {
+          city: 'San Fransico'
+        },
+        graduation_date: 1664533942262,
+        lifecyclestage: 'subscriber'
+      }
+    })
+
+    const patchExpectedPayload = {
+      properties: {
+        firstname: 'John',
+        lastname: 'Doe',
+        city: 'San Fransico',
+        graduation_date: 1664533942262,
+        lifecyclestage: 'subscriber'
+      }
+    }
+
+    const mapping = {
+      properties: {
+        graduation_date: {
+          '@path': '$.traits.graduation_date'
+        },
+        lifecyclestage: {
+          '@path': '$.traits.lifecyclestage'
+        }
+      }
+    }
+
+    nock(HubSpotBaseURL)
+      .patch(`/crm/v3/objects/contacts/${testEmail}?idProperty=email`, patchExpectedPayload)
+      .reply(200, {
+        id: '801',
+        properties: {
+          lifecyclestage: 'lead'
+        }
+      })
+
+    nock(HubSpotBaseURL)
+      .patch(`/crm/v3/objects/contacts/${testEmail}?idProperty=email`, { properties: { lifecyclestage: '' } })
+      .reply(200, {
+        id: '801',
+        properties: {
+          lifecyclestage: ''
+        }
+      })
+
+    nock(HubSpotBaseURL)
+      .patch(`/crm/v3/objects/contacts/${testEmail}?idProperty=email`, patchExpectedPayload)
+      .reply(200, {
+        id: '801',
+        properties: {
+          lifecyclestage: 'subscriber'
+        }
+      })
+
+    const transactionContext: Record<string, string> = {}
+    const setTransactionContext = (key: string, value: string) => (transactionContext[key] = value)
+
+    await expect(
+      testDestination.testAction('contact', {
+        mapping,
+        useDefaultMappings: true,
+        event,
+        transactionContext: { transaction: {}, setTransaction: setTransactionContext }
+      })
+    ).resolves.not.toThrowError()
+
+    expect(transactionContext['contact_id']).toEqual('801')
   })
 })
--- packages/destination-actions/src/destinations/hubspot/contact/__tests__/snapshot.test.ts
+++ packages/destination-actions/src/destinations/hubspot/contact/__tests__/snapshot.test.ts
@@ -2,30 +2,37 @@ import { createTestEvent, createTestIntegration } from '@segment/actions-core'
 import { generateTestData } from '../../../../lib/test-data'
 import destination from '../../index'
 import nock from 'nock'
+import { TransactionContext } from '@segment/actions-core/src/destination-kit'
 
 const testDestination = createTestIntegration(destination)
 const actionSlug = 'contact'
 const destinationSlug = 'Hubspot'
 const seedName = `${destinationSlug}#${actionSlug}`
 
+beforeEach(() => nock.cleanAll())
+
 describe(`Testing snapshot for ${destinationSlug}'s ${actionSlug} destination action:`, () => {
   it('required fields', async () => {
     const action = destination.actions[actionSlug]
     const [eventData, settingsData] = generateTestData(seedName, destination, action, true)
-    nock(/.*/).persist().get(/.*/).reply(200)
-    nock(/.*/).persist().post(/.*/).reply(200)
-    nock(/.*/).persist().put(/.*/).reply(200)
+
     nock(/.*/).persist().patch(/.*/).reply(200)
 
     const event = createTestEvent({
       properties: eventData
     })
 
+    const transactionContext: TransactionContext = {
+      transaction: {},
+      setTransaction: (key, value) => ({ [key]: value })
+    }
+
     const responses = await testDestination.testAction(actionSlug, {
       event: event,
       mapping: event.properties,
       settings: settingsData,
-      auth: undefined
+      auth: undefined,
+      transactionContext
     })
 
     const request = responses[0].request
@@ -46,20 +53,32 @@ describe(`Testing snapshot for ${destinationSlug}'s ${actionSlug} destination ac
     const action = destination.actions[actionSlug]
     const [eventData, settingsData] = generateTestData(seedName, destination, action, false)
 
-    nock(/.*/).persist().get(/.*/).reply(200)
     nock(/.*/).persist().post(/.*/).reply(200)
-    nock(/.*/).persist().put(/.*/).reply(200)
-    nock(/.*/).persist().patch(/.*/).reply(200)
+    nock(/.*/)
+      .persist()
+      .patch(/.*/)
+      .reply(200, {
+        id: '801',
+        properties: {
+          lifecyclestage: eventData.lifecyclestage.toLowerCase()
+        }
+      })
 
     const event = createTestEvent({
       properties: eventData
     })
 
+    const transactionContext: TransactionContext = {
+      transaction: {},
+      setTransaction: (key, value) => ({ [key]: value })
+    }
+
     const responses = await testDestination.testAction(actionSlug, {
       event: event,
       mapping: event.properties,
       settings: settingsData,
-      auth: undefined
+      auth: undefined,
+      transactionContext
     })
 
     const request = responses[0].request
