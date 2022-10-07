93df15b25dad82fc8b3174535521fe1b41b3ee5a
Brennan Gamwell <brennan@segment.com>
2022-10-07 13:21:29 -0700

    Validate oauthed user's credentials and access.

--- packages/destination-actions/src/destinations/linkedin-audiences/index.ts
+++ packages/destination-actions/src/destinations/linkedin-audiences/index.ts
@@ -11,6 +11,14 @@ interface RefreshTokenResponse {
   token_type: string
 }
 
+interface ProfileAPIResponse {
+  id: string
+}
+
+interface AdAccountUserResponse {
+  role: string
+}
+
 const destination: DestinationDefinition<Settings> = {
   // We  need to match `name` with the creationName in the db.
   // The name used in the UI is "LinkedIn Audiences".
@@ -29,14 +37,58 @@ const destination: DestinationDefinition<Settings> = {
       }
     },
     testAuthentication: async (request, { settings, auth }) => {
-      return await request(`https://api.linkedin.com/rest/adAccounts/${settings.ad_account_id}`, {
-        method: 'GET',
-        headers: {
-          'X-Restli-Protocol-Version': '2.0.0',
-          'LinkedIn-Version': LINKEDIN_API_VERSION,
-          authorization: `Bearer ${auth?.accessToken}`
+      if (!auth?.accessToken) {
+        throw new Error('Please authenticate via Oauth before updating other settings and/or enabling the destination.')
+      }
+
+      let firstRes
+
+      try {
+        // GET the current user's id from LinkedIn's profile API: https://learn.microsoft.com/en-us/linkedin/shared/integrations/people/profile-api?context=linkedin%2Fcompliance%2Fcontext&view=li-lms-unversioned&preserve-view=true#request
+        // We request `r_basicprofile` scope when a user oauths into LinkedIn, so we retrieve the "Basic Profile Fields": https://learn.microsoft.com/en-us/linkedin/shared/references/v2/profile/basic-profile
+        firstRes = await request<ProfileAPIResponse>(`https://api.linkedin.com/rest/me`, {
+          method: 'GET'
+        })
+      } catch (e: any) {
+        if (e.message === 'Unauthorized') {
+          throw new Error(
+            'Invalid LinkedIn Oauth access token. Please reauthenticate to retrieve a valid access token before updating other settings and/or enabling the destination.'
+          )
         }
-      })
+        throw e
+      }
+
+      const userId = firstRes?.data?.id
+
+      let secondRes
+
+      try {
+        // GET the current user's permissions for this specific Ad Account: https://learn.microsoft.com/en-us/linkedin/marketing/integrations/ads/account-structure/create-and-manage-account-users?view=li-lms-2022-09&tabs=http#get-ad-account-user
+        // If the user's role is VIEWER, then they won't have adequate permission to send batch updates to any DMP Segment audience in this LinkedIn account.
+        secondRes = await request<AdAccountUserResponse>(
+          `https://api.linkedin.com/rest/adAccountUsers/account=urn:li:sponsoredAccount:${settings.ad_account_id}&user=urn:li:person:${userId}`,
+          {
+            method: 'GET'
+          }
+        )
+      } catch (e: any) {
+        if (e.message === 'Not Found') {
+          throw new Error(
+            'Invalid LinkedIn Ad Account id. Please verify that the LinkedIn Ad Account exists and that you have access to it.'
+          )
+        }
+        throw e
+      }
+
+      const userRole = secondRes?.data.role
+
+      if (userRole === 'VIEWER') {
+        throw new Error(
+          "It looks like you don't have access to this LinkedIn Ad Account. Please reach out to a LinkedIn Ad Account Admin on your team to grant access."
+        )
+      }
+
+      return true
     },
     refreshAccessToken: async (request, { auth }) => {
       const res = await request<RefreshTokenResponse>('https://www.linkedin.com/oauth/v2/accessToken', {
