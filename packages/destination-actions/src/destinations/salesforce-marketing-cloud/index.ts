a166d9c999f5a7f610566e72b05ecf9c8bbaa6cf
rvadera12 <89420099+rvadera12@users.noreply.github.com>
2022-10-17 21:42:38 -0700

    Update packages/destination-actions/src/destinations/salesforce-marketing-cloud/index.ts
    Co-authored-by: kdaswani <49517136+kdaswani@users.noreply.github.com>

--- packages/destination-actions/src/destinations/salesforce-marketing-cloud/index.ts
+++ packages/destination-actions/src/destinations/salesforce-marketing-cloud/index.ts
@@ -16,7 +16,7 @@ const destination: DestinationDefinition<Settings> = {
       subdomain: {
         label: 'Subdomain',
         description:
-          'The unique subdomain Salesforce Marketing Cloud assigned to your account. Subdomains are tenant specific and should be a 28-character string starting with the letters "mc". Do not include the .rest.marketingcloudapis.com part of your subdomain URL. See more information on how to find your subdomain [here](https://developer.salesforce.com/docs/marketing/marketing-cloud/guide/your-subdomain-tenant-specific-endpoints.html)',
+          'The unique subdomain Salesforce Marketing Cloud assigned to your account. Subdomains are tenant specific and should be a 28-character string starting with the letters "mc". Do not include the .rest.marketingcloudapis.com part of your subdomain URL. See more information on how to find your subdomain [here](https://developer.salesforce.com/docs/marketing/marketing-cloud/guide/your-subdomain-tenant-specific-endpoints.html).',
         type: 'string',
         required: true
       },
