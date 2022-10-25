4cfb3cfa5ddd4ab1528901cc700a4fc12d4662eb
vikramkumar19 <vikram.kumar@segment.com>
2022-10-25 12:01:15 -0500

    Register Subscriptions destination

--- packages/destination-actions/src/destinations/index.ts
+++ packages/destination-actions/src/destinations/index.ts
@@ -61,6 +61,7 @@ register('61dc4e96894a6d7954cc6e45', './voyage')
 register('62ded0cf5753c743883ca0f3', './intercom')
 register('631a6f32946dd8197e9cab66', './sendgrid')
 register('632b1116e0cb83902f3fd717', './hubspot')
+register('635812628d1b29fd1e4efe16', './subscriptions')
 
 function register(id: MetadataId, destinationPath: string) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
