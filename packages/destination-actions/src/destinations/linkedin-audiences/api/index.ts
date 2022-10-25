f1a7c0b4cfca12a039c968410b9067d51be1598d
Brennan Gamwell <brennan@segment.com>
2022-10-24 17:31:06 -0700

    [LinkedIn Audiences] Destination Improvements (#817)
    * Move send_email and send_google_advertising_id from fields to types.
    * DRY up unit tests; re-generate snapshots; add and enhance code comments.
    * Encapsulate all LinkedIn requests in api client class.
    * Handle refresh token error response.

---
+++ packages/destination-actions/src/destinations/linkedin-audiences/api/index.ts
@@ -0,0 +1,72 @@
+import type { RequestClient, ModifiedResponse } from '@segment/actions-core'
+import type { Settings } from '../generated-types'
+import type { Payload } from '../updateAudience/generated-types'
+import { BASE_URL, LINKEDIN_SOURCE_PLATFORM } from '../constants'
+import type { ProfileAPIResponse, AdAccountUserResponse } from '../types'
+
+export class LinkedInAudiences {
+  request: RequestClient
+
+  constructor(request: RequestClient) {
+    this.request = request
+  }
+
+  async getProfile(): Promise<ModifiedResponse<ProfileAPIResponse>> {
+    return this.request(`${BASE_URL}/me`, {
+      method: 'GET'
+    })
+  }
+
+  async getAdAccountUserProfile(settings: Settings, userId: string): Promise<ModifiedResponse<AdAccountUserResponse>> {
+    return this.request(
+      `${BASE_URL}/adAccountUsers/account=urn:li:sponsoredAccount:${settings.ad_account_id}&user=urn:li:person:${userId}`,
+      {
+        method: 'GET'
+      }
+    )
+  }
+
+  async getDmpSegment(settings: Settings, payload: Payload): Promise<ModifiedResponse> {
+    return this.request(`${BASE_URL}/dmpSegments`, {
+      method: 'GET',
+      searchParams: {
+        q: 'account',
+        account: `urn:li:sponsoredAccount:${settings.ad_account_id}`,
+        sourceSegmentId: payload.source_segment_id || '',
+        sourcePlatform: LINKEDIN_SOURCE_PLATFORM
+      }
+    })
+  }
+
+  async createDmpSegment(settings: Settings, payload: Payload): Promise<ModifiedResponse> {
+    return this.request(`${BASE_URL}/dmpSegments`, {
+      method: 'POST',
+      json: {
+        name: payload.dmp_segment_name,
+        sourcePlatform: LINKEDIN_SOURCE_PLATFORM,
+        sourceSegmentId: payload.source_segment_id,
+        account: `urn:li:sponsoredAccount:${settings.ad_account_id}`,
+        accessPolicy: 'PRIVATE',
+        type: 'USER',
+        destinations: [
+          {
+            destination: 'LINKEDIN'
+          }
+        ]
+      }
+    })
+  }
+
+  async batchUpdate(dmpSegmentId: string, elements: Record<string, string>[]): Promise<ModifiedResponse> {
+    return this.request(`${BASE_URL}/dmpSegments/${dmpSegmentId}/users`, {
+      method: 'POST',
+      headers: {
+        'X-RestLi-Method': 'BATCH_CREATE'
+      },
+      json: {
+        elements
+      },
+      throwHttpErrors: false
+    })
+  }
+}
--- packages/destination-actions/src/destinations/linkedin-audiences/linkedin-properties.ts
+++
@@ -1,1 +0,0 @@
-export const LINKEDIN_API_VERSION = '202207'
