3bab9963cccbc2c84e796ace1b31d33441616b1b
kdaswani <49517136+kdaswani@users.noreply.github.com>
2022-11-11 11:28:26 -0800

    Update descriptions for Google Ads Upload Adjustment

--- packages/destination-actions/src/destinations/google-enhanced-conversions/uploadConversionAdjustment/index.ts
+++ packages/destination-actions/src/destinations/google-enhanced-conversions/uploadConversionAdjustment/index.ts
@@ -40,7 +40,7 @@ const action: ActionDefinition<Settings, Payload> = {
     order_id: {
       label: 'Order ID',
       description:
-        'The order ID of the conversion to be adjusted. If the conversion was reported with an order ID specified, that order ID must be used as the identifier here. Required for ENHANCEMENT adjustments.',
+        'The order ID of the conversion to be adjusted. If the conversion was reported with an order ID specified, that order ID must be used as the identifier here.',
       type: 'string',
       default: {
         '@if': {
@@ -53,13 +53,13 @@ const action: ActionDefinition<Settings, Payload> = {
     gclid: {
       label: 'GCLID',
       description:
-        'Google click ID associated with the original conversion for this adjustment. This is used for the GCLID Date Time Pair. Required for non-ENHANCEMENT adjustments. If adjustment is ENHANCEMENT, this value is optional but may be set in addition to the order ID.',
+        'Google click ID associated with the original conversion for this adjustment. This is used for the GCLID Date Time Pair.',
       type: 'string'
     },
     conversion_timestamp: {
       label: 'Conversion Timestamp',
       description:
-        'The date time at which the original conversion for this adjustment occurred. The timezone must be specified. The format is "yyyy-mm-dd hh:mm:ss+|-hh:mm", e.g. "2019-01-01 12:32:45-08:00". This is used for the GCLID Date Time Pair. Required for non-ENHANCEMENT adjustments. If adjustment is ENHANCEMENT, this value is optional but may be set in addition to the order ID.',
+        'The date time at which the original conversion for this adjustment occurred. The timezone must be specified. The format is "yyyy-mm-dd hh:mm:ss+|-hh:mm", e.g. "2019-01-01 12:32:45-08:00". This is used for the GCLID Date Time Pair.',
       type: 'string'
     },
     restatement_value: {
