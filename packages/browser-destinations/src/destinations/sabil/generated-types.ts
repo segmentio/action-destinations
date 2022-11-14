3d4da9a2b6ecacd2f151e59d20495fd2b4623da9
ahmed-classtra <57930525+ahmed-classtra@users.noreply.github.com>
2022-11-14 14:06:06 -0500

    Add Sabil (Web) Destination Action (#871)
    Co-authored-by: Ahmed Saleh <amd.saleh99@gmail.com>

---
+++ packages/browser-destinations/src/destinations/sabil/generated-types.ts
@@ -0,0 +1,8 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Settings {
+  /**
+   * Your project API client ID. You can find it in your Sabil [dashboard](https://dashboard.sabil.io/api_keys)
+   */
+  client_id: string
+}
