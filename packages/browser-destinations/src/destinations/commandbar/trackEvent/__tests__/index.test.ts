83ed85a9d2dd1de0051f08471aa6a7fbb16b1539
Thomas Kainrad <thomas@tkainrad.dev>
2022-11-14 10:36:43 +0100

    Refactor test for trackEvent

--- packages/browser-destinations/src/destinations/commandbar/trackEvent/__tests__/index.test.ts
+++ packages/browser-destinations/src/destinations/commandbar/trackEvent/__tests__/index.test.ts
@@ -1,33 +1,54 @@
 import { Analytics, Context } from '@segment/analytics-next'
+import { Subscription } from 'src/lib/browser-destinations'
 import commandBarDestination, { destination } from '../../index'
 
+const subscriptions: Subscription[] = [
+  {
+    partnerAction: 'trackEvent',
+    name: 'Track Event',
+    enabled: true,
+    subscribe: 'type = "track"',
+    mapping: {
+      event_name: {
+        '@path': '$.event'
+      },
+      event_metadata: {
+        '@path': '$.properties'
+      }
+    }
+  }
+]
+
 describe('Commandbar.trackEvent', () => {
-  it('Sends events to CommandBar', async () => {
-    const [identifyUserPlugin] = await commandBarDestination({
-      orgId: '05f077f2',
-      subscriptions: [
-        {
-          partnerAction: 'trackEvent',
-          name: 'Track Event',
-          enabled: true,
-          subscribe: 'type = "track"',
-          mapping: {
-            event_name: {
-              '@path': '$.event'
-            },
-            event_metadata: {
-              '@path': '$.properties'
-            }
-          }
-        }
-      ]
+  const settings = {
+    orgId: 'xxxxxxxx'
+  }
+  let mockCommandBarTrackEvent: jest.Mock<any, any>
+
+  let plugin: any
+
+  beforeEach(async () => {
+    const [commandBarPlugin] = await commandBarDestination({
+      ...settings,
+      subscriptions
     })
 
-    destination.actions.trackEvent.perform = jest.fn()
-    const trackSpy = jest.spyOn(destination.actions.trackEvent, 'perform')
-    await identifyUserPlugin.load(Context.system(), {} as Analytics)
+    plugin = commandBarPlugin
 
-    await identifyUserPlugin.track?.(
+    mockCommandBarTrackEvent = jest.fn()
+    jest.spyOn(destination, 'initialize').mockImplementation(() => {
+      const mockedWithTrack = {
+        boot: jest.fn(),
+        addMetadataBatch: jest.fn(),
+        trackEvent: mockCommandBarTrackEvent
+      }
+      return Promise.resolve(mockedWithTrack)
+    })
+    await plugin.load(Context.system(), {} as Analytics)
+  })
+
+  it('Sends events to CommandBar', async () => {
+    await plugin.track?.(
       new Context({
         type: 'track',
         event: 'example-event',
@@ -37,16 +58,6 @@ describe('Commandbar.trackEvent', () => {
       })
     )
 
-    expect(trackSpy).toHaveBeenCalledWith(
-      expect.anything(),
-      expect.objectContaining({
-        payload: {
-          event_name: 'example-event',
-          event_metadata: {
-            foo: 'bar'
-          }
-        }
-      })
-    )
+    expect(mockCommandBarTrackEvent).toHaveBeenCalledWith('example-event', { foo: 'bar' })
   })
 })
