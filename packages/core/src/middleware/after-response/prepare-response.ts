13bb53cbe62c17c7b166f88d31461108831f5bbc
Valerie Ernst <valerie.ernst@segment.com>
2022-11-14 16:00:44 -0800

    Add patch back after testing

--- packages/core/src/middleware/after-response/prepare-response.ts
+++ packages/core/src/middleware/after-response/prepare-response.ts
@@ -6,25 +6,22 @@ const prepareResponse: AfterResponseHook = async (_request, _options, response) 
 
   const modifiedResponse = response as ModifiedResponse
 
-  // let content: string
-  // if (_options.skipResponseCloning) {
-  //   // Skip cloning the response to avoid a Node crash in case the response payload is larger than 16KB
-  //   // TODO STRATCONN-1396: Move all action-destinations to follow this code path instead of cloning the response
-  //   content = await response.text()
-  // } else {
-  // stat before and after the response.clone() to see how frequently we are hitting this issue:
-  // https://segment.atlassian.net/browse/ACT-242
-  // exclude tags for now - we can add them if they're needed
-  statsClient?.incr('before-response-clone', 1)
-  // Clone the response before reading the body to avoid
-  // `TypeError: body used already` elsewhere
-  const clone = response.clone()
-  const content = await clone.text()
-  if (content) {
+  let content: string
+  if (_options.skipResponseCloning) {
+    // Skip cloning the response to avoid a Node crash in case the response payload is larger than 16KB
+    // TODO STRATCONN-1396: Move all action-destinations to follow this code path instead of cloning the response
+    content = await response.text()
+  } else {
+    // stat before and after the response.clone() to see how frequently we are hitting this issue:
+    // https://segment.atlassian.net/browse/ACT-242
+    // exclude tags for now - we can add them if they're needed
+    statsClient?.incr('before-response-clone', 1)
+    // Clone the response before reading the body to avoid
+    // `TypeError: body used already` elsewhere
+    const clone = response.clone()
+    content = await clone.text()
     statsClient?.incr('after-response-clone', 1)
   }
-
-  // }
 
   const agent: NormalizedOptions['agent'] = _options.agent
   if (agent) {
