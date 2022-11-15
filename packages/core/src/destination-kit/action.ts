f0912cd6e8f23983662b463563325322a0e42cfc
valerieernst <valerieernst@users.noreply.github.com>
2022-11-15 12:43:49 -0800

    Add statsContext to requestClient and stat before and after response.clone() (#874)
    * Add statsContext to requestClient and stat before and after clone
    
    * Remove patch fix to test unhappy path
    
    * Move stat to after clone.text()
    
    * add conditional to after stat
    
    * Add patch back after testing
    
    Co-authored-by: Valerie Ernst <valerie.ernst@segment.com>

--- packages/core/src/destination-kit/action.ts
+++ packages/core/src/destination-kit/action.ts
@@ -237,7 +237,8 @@ export class Action<Settings, Payload extends JSONLikeObject> extends EventEmitt
     // TODO turn `extendRequest` into a beforeRequest hook
     const options = this.extendRequest?.(data) ?? {}
     return createRequestClient(options, {
-      afterResponse: [this.afterResponse.bind(this)]
+      afterResponse: [this.afterResponse.bind(this)],
+      statsContext: data.statsContext
     })
   }
 
