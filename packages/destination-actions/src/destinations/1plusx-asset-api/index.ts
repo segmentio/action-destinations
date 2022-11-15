4b8a3a5b27bc93b261f0269d87a0000c452f351f
EA <27460815+longstoryshort@users.noreply.github.com>
2022-11-15 11:02:09 -0800

    Added testAuthentication, updated base64 encoding

--- packages/destination-actions/src/destinations/1plusx-asset-api/index.ts
+++ packages/destination-actions/src/destinations/1plusx-asset-api/index.ts
@@ -34,19 +34,34 @@ const destination: DestinationDefinition<Settings> = {
         required: true
       }
     },
-
-    refreshAccessToken: async (request, { settings }) => {
-      console.log('refreshAccessToken start')
-      // Refresh access token using client_id and client_secret provided in the Settings
+    testAuthentication: async (request, { settings }) => {
       const res = await request<RefreshTokenResponse>('https://ui.1plusx.io/api/auth/token', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: 'Basic ' + btoa(settings.client_id + ':' + settings.client_secret)
+          Authorization: `Basic ${Buffer.from(settings.client_id + ':' + settings.client_secret).toString('base64')}`
         },
         body: '{"grant_type":"client_credentials"}'
       })
-      console.log('refresh token')
+      if (res.status == 200) {
+        return { accessToken: res.data.access_token }
+      } else {
+        throw new Error(res.status + res.statusText)
+      }
+    },
+    refreshAccessToken: async (request, { settings }) => {
+      console.log('refreshAccessToken start')
+      // Refresh access token using client_id and client_secret provided in the Settings
+
+      const res = await request<RefreshTokenResponse>('https://ui.1plusx.io/api/auth/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${Buffer.from(settings.client_id + ':' + settings.client_secret).toString('base64')}`
+        },
+        body: '{"grant_type":"client_credentials"}'
+      })
+      console.log('refreshed token')
       return { accessToken: res.data.access_token }
     }
   },
