7c0b0f3ce6a7d9c4b6328c916bc224460e26fa20
Gaurav Kochar <gaurav.kochar@segment.com>
2022-11-28 17:52:43 +0530

    working on creating braze cohort action destination, added authentication

---
+++ packages/destination-actions/src/destinations/braze-cohorts/index.ts
@@ -0,0 +1,58 @@
+import type { DestinationDefinition } from '@segment/actions-core'
+import type { Settings } from './generated-types'
+
+import syncAudiences from './syncAudiences'
+
+const destination: DestinationDefinition<Settings> = {
+  name: 'Braze Cohorts',
+  slug: 'actions-braze-cohorts',
+  mode: 'cloud',
+  authentication: {
+    scheme: 'custom',
+    fields: {
+      client_secret: {
+        label: 'Client Secret key',
+        description: 'Created under Developer Console in the Braze Dashboard.',
+        type: 'password',
+        required: true
+      },
+      endpoint: {
+        label: 'REST Endpoint',
+        description: 'Your Braze REST endpoint. [See more details](https://www.braze.com/docs/api/basics/#endpoints)',
+        type: 'string',
+        format: 'uri',
+        choices: [
+          { label: 'US-01	(https://dashboard-01.braze.com)', value: 'https://rest.iad-01.braze.com' },
+          { label: 'US-02	(https://dashboard-02.braze.com)', value: 'https://rest.iad-02.braze.com' },
+          { label: 'US-03	(https://dashboard-03.braze.com)', value: 'https://rest.iad-03.braze.com' },
+          { label: 'US-04	(https://dashboard-04.braze.com)', value: 'https://rest.iad-04.braze.com' },
+          { label: 'US-05	(https://dashboard-05.braze.com)', value: 'https://rest.iad-05.braze.com' },
+          { label: 'US-06	(https://dashboard-06.braze.com)', value: 'https://rest.iad-06.braze.com' },
+          { label: 'US-08	(https://dashboard-08.braze.com)', value: 'https://rest.iad-08.braze.com' },
+          { label: 'EU-01	(https://dashboard-01.braze.eu)', value: 'https://rest.fra-01.braze.eu' },
+          { label: 'EU-02	(https://dashboard-02.braze.eu)', value: 'https://rest.fra-02.braze.eu' }
+        ],
+        default: 'https://rest.iad-01.braze.com',
+        required: true
+      }
+    },
+    testAuthentication: (request, { settings }) => {
+      console.log('when will it comme here ***********************')
+      const partnerName = 'segment'
+      return request(`${settings.endpoint}/partners/${partnerName}/cohorts/users`, {
+        method: 'post',
+        json: {
+          client_secret: settings.client_secret,
+          partner_api_key: 'partner_api-key',
+          cohort_id: 'will_add_in_constant',
+          cohort_changes: []
+        }
+      })
+    }
+  },
+  actions: {
+    syncAudiences
+  }
+}
+
+export default destination
---
+++ packages/destination-actions/src/destinations/braze-cohorts/syncAudiences/generated-types.ts
@@ -0,0 +1,41 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * The unique user identifier
+   */
+  external_id?: string
+  /**
+   * A user alias object. See [the docs](https://www.braze.com/docs/api/objects_filters/user_alias_object/).
+   */
+  user_alias?: {
+    alias_name?: string
+    alias_label?: string
+  }
+  /**
+   * The unique device Identifier
+   */
+  device_id?: string
+  /**
+   * The Cohort Identifier
+   */
+  cohort_id?: string
+  /**
+   * The name of Cohort
+   */
+  name?: string
+  /**
+   * Enable batching of requests to the Braze cohorts.
+   */
+  enable_batching?: boolean
+  /**
+   * The `audience_key` of the Engage audience you want to sync to LinkedIn. This value must be a hard-coded string variable, e.g. `personas_test_audience`, in order for batching to work properly.
+   */
+  personas_audience_key: string
+  /**
+   * Properties of the event
+   */
+  event_properties: {
+    [k: string]: unknown
+  }
+}
