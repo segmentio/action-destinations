dd87031f9b1e338db057d6c72fb0028d66cc33d2
Jonas Reif <reif.jonas@gmx.de>
2022-12-01 13:32:25 +0100

    add support for test_event_code feature

--- packages/destination-actions/src/destinations/facebook-conversions-api/__tests__/pageView.test.ts
+++ packages/destination-actions/src/destinations/facebook-conversions-api/__tests__/pageView.test.ts
@@ -4,11 +4,17 @@ import Destination from '../index'
 import { API_VERSION } from '../constants'
 
 const testDestination = createTestIntegration(Destination)
-
 const settings = {
   pixelId: '123321',
+  testEventCode: '',
   token: process.env.TOKEN
 }
+const settingsWithTestEventCode = {
+  pixelId: '123321',
+  testEventCode: '1234567890',
+  token: process.env.TOKEN
+}
+
 describe('FacebookConversionsApi', () => {
   describe('PageView', () => {
     it('should handle a basic event', async () => {
@@ -136,6 +142,38 @@ describe('FacebookConversionsApi', () => {
           }
         })
       ).rejects.toThrowError("The root value is missing the required field 'user_data'.")
+    })
+
+    it('should send test_event_code if present in settings', async () => {
+      nock(`https://graph.facebook.com/v${API_VERSION}/${settingsWithTestEventCode.pixelId}`)
+        .post(`/events`)
+        .reply(201, {})
+
+      const event = createTestEvent({
+        type: 'page',
+        userId: 'abc123',
+        timestamp: '1631210020',
+        messageId: 'test',
+        properties: {
+          timestamp: 1631210000,
+          action_source: 'email',
+          email: 'nicholas.aguilar@segment.com'
+        }
+      })
+
+      const responses = await testDestination.testAction('pageView', {
+        event,
+        settings: settingsWithTestEventCode,
+        useDefaultMappings: true,
+        mapping: { action_source: { '@path': '$.properties.action_source' } }
+      })
+
+      expect(responses.length).toBe(1)
+      expect(responses[0].status).toBe(201)
+
+      expect(responses[0].options.body).toMatchInlineSnapshot(
+        `"{\\"data\\":[{\\"event_name\\":\\"PageView\\",\\"event_time\\":\\"1631210020\\",\\"action_source\\":\\"email\\",\\"event_source_url\\":\\"https://segment.com/academy/\\",\\"event_id\\":\\"test\\",\\"user_data\\":{\\"external_id\\":\\"6ca13d52ca70c883e0f0bb101e425a89e8624de51db2d2392593af6a84118090\\",\\"client_ip_address\\":\\"8.8.8.8\\",\\"client_user_agent\\":\\"Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1\\"}}],\\"test_event_code\\":\\"1234567890\\"}"`
+      )
     })
   })
 })
--- packages/destination-actions/src/destinations/facebook-conversions-api/generated-types.ts
+++ packages/destination-actions/src/destinations/facebook-conversions-api/generated-types.ts
@@ -5,4 +5,8 @@ export interface Settings {
    * Your Facebook Pixel ID
    */
   pixelId: string
+  /**
+   * Use this field to specify that events should be test events rather than actual traffic. You can find your Test Event Code in your Facebook Events Manager under the "Test events" tab. You'll want to remove your Test Event Code when sending real traffic through this integration.
+   */
+  testEventCode?: string
 }
