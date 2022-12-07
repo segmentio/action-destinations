215d09cd9cc2ac354ff845be463f7198d8d59854
Gaurav Kochar <gaurav.kochar@segment.com>
2022-12-07 22:26:07 +0530

    client_secret is madatory

--- packages/destination-actions/src/destinations/braze-cohorts/generated-types.ts
+++ packages/destination-actions/src/destinations/braze-cohorts/generated-types.ts
@@ -4,7 +4,7 @@ export interface Settings {
   /**
    * Data Import Key for the client whose cohort this belongs to. Also known as customer key.
    */
-  client_secret?: string
+  client_secret: string
   /**
    * Your Braze REST endpoint. [See more details](https://www.braze.com/docs/api/basics/#endpoints)
    */
