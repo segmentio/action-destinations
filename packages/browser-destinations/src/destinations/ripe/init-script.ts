77374ed7aea8b1c2f5d7e509a7b39186d8e29947
Simon <simpajj@users.noreply.github.com>
2022-12-08 01:59:40 +0100

    Add Ripe destination (#921)

---
+++ packages/browser-destinations/src/destinations/ripe/init-script.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+// @ts-nocheck
+export function initScript() {
+  if (window.Ripe) {
+    return
+  }
+
+  window.Ripe = []
+  ;['alias', 'group', 'identify', 'init', 'page', 'setIds', 'track'].forEach(function (method) {
+    window.Ripe[method] = function () {
+      let args = Array.from(arguments)
+      args.unshift(method)
+      window.Ripe.push(args)
+      return window.Ripe
+    }
+  })
+}
