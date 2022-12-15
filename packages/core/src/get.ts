ee862b483f69d49f50274250b3ea1e7a74caf942
Dan Lasky <dan.lasky@segment.com>
2022-12-15 14:03:52 -0600

    explanatory text + new link

--- packages/core/src/get.ts
+++ packages/core/src/get.ts
@@ -14,15 +14,17 @@ export function get<T = unknown>(
   // Not defined
   if (path === null || path == undefined) return undefined
 
-  // Check if path is string or array. Regex : ensure that we do not have '.' and brackets.
-  // Regex explained: https://regexr.com/58j0k
+  // Check if path is string or array. Regex : splits the path into valid array of path chunks
+  // we support an extra edge case that lodash does not around brackets
+  // a['b'] = ['a','b']
+  // a[b] = ['a[b]'] //brackets without numeric index or a string are considered part of the key
+  // Regex explained: https://regexr.com/74m2m
   const pathArray = Array.isArray(path)
     ? path
     : path
         .split(/\[(?="|'|\d)|\.|(?<="|'|\d)]+/g)
         .filter((f) => f)
         .map((s) => s.replace(/'|"/g, ''))
-  // const pathArray = Array.isArray(path) ? path : (path.match(/([^[.\]])+/g) as string[])
 
   // Find value if exist return otherwise return undefined value
   return pathArray.reduce((prevObj, key) => prevObj && prevObj[key], obj)
