55887ae603708d3c37c3af942168503cb447118f
Denis Egorushkin <denis.egorushkin@saleswingsapp.com>
2022-12-18 14:21:04 +0400

    Auth tests & track event tests

--- packages/destination-actions/src/destinations/saleswings/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/saleswings/__tests__/index.test.ts
@@ -11,5 +11,11 @@ describe('Saleswings', () => {
       nock(apiBaseUrl).get('/project/account').matchHeader('authorization', 'Bearer myApiKey').reply(200, {})
       await expect(testDestination.testAuthentication({ apiKey: 'myApiKey' })).resolves.not.toThrowError()
     })
+
+    it('should reject invalid API key', async () => {
+      nock(apiBaseUrl).get('/project/account').matchHeader('authorization', 'Bearer myApiKey').reply(200, {})
+      nock(apiBaseUrl).get('/project/account').reply(401, {})
+      await expect(testDestination.testAuthentication({ apiKey: 'invalidApiKey' })).rejects.toThrowError()
+    })
   })
 })
--- packages/destination-actions/src/destinations/saleswings/submitEvent/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/saleswings/submitEvent/__tests__/index.test.ts
@@ -1,17 +1,71 @@
-import { createTestEvent as createTestEvent, createTestIntegration } from '@segment/actions-core'
+import {
+  createTestEvent as createTestEvent,
+  createTestIntegration,
+  JSONObject,
+  SegmentEvent
+} from '@segment/actions-core'
 import Destination from '../../index'
 import nock from 'nock'
 import { apiBaseUrl } from '../../api'
 
-const testDestination = createTestIntegration(Destination)
-
-const settings = { apiKey: 'TEST_API_KEY' }
-
 describe('SalesWings', () => {
   describe('.submitEvent', () => {
-    test('should submit event on Track event', async () => {
-      nock(apiBaseUrl).post('/events').reply(200, {})
+    it('should submit event on Track event', async () => {
+      const event = createTestEvent({
+        type: 'track',
+        event: 'User Registered',
+        properties: {
+          plan: 'Pro Annual',
+          accountType: 'Facebook'
+        },
+        context: {
+          userAgent,
+          page: {
+            url: 'https://example.com',
+            referrer: 'https://example.com/other'
+          }
+        }
+      })
+      const request = await testAction(event)
+      expect(request).toMatchObject({
+        type: 'tracking',
+        leadRefs: [
+          { type: 'client-id', value: event.userId },
+          { type: 'client-id', value: event.anonymousId }
+        ],
+        kind: 'Track',
+        data: 'User Registered',
+        url: 'https://example.com',
+        referrerUrl: 'https://example.com/other',
+        userAgent,
+        timestamp: expectedTs(event.timestamp),
+        values: {
+          plan: 'Pro Annual',
+          accountType: 'Facebook'
+        }
+      })
+    })
 
+    it('should submit event on Track event with all optional fields omitted', async () => {
+      const event = createTestEvent({
+        type: 'track',
+        event: 'User Registered'
+      })
+      const request = await testAction(event)
+      expect(request).toMatchObject({
+        type: 'tracking',
+        leadRefs: [
+          { type: 'client-id', value: event.userId },
+          { type: 'client-id', value: event.anonymousId }
+        ],
+        kind: 'Track',
+        data: 'User Registered',
+        timestamp: expectedTs(event.timestamp),
+        values: {}
+      })
+    })
+
+    it('should use custom event property mapping', async () => {
       const event = createTestEvent({
         type: 'track',
         event: 'User Registered',
@@ -20,27 +74,106 @@ describe('SalesWings', () => {
           accountType: 'Facebook'
         }
       })
-
-      const responses = await testDestination.testAction('submitEvent', { event, settings, useDefaultMappings: true })
-      expect(responses[0].request.headers.get('Authorization')).toBe(`Bearer ${settings.apiKey}`)
+      const customEventPropertyMapping = {
+        'Some Irrelevant Event': 'foo',
+        'User Registered': 'accountType'
+      }
+      const mapping = { customEventPropertyMapping, ...minRequiredMapping }
+      const request = await testAction(event, mapping)
+      expect(request).toMatchObject({
+        type: 'tracking',
+        leadRefs: [
+          { type: 'client-id', value: event.userId },
+          { type: 'client-id', value: event.anonymousId }
+        ],
+        kind: 'User Registered',
+        data: 'Facebook',
+        timestamp: expectedTs(event.timestamp),
+        values: {
+          plan: 'Pro Annual',
+          accountType: 'Facebook'
+        }
+      })
     })
 
-    test('should skip Track event if name not specified', async () => {})
+    it('should not skip an event with userId only', async () => {
+      const event = createTestEvent({
+        type: 'track',
+        event: 'User Registered',
+        anonymousId: undefined
+      })
+      const request = await testAction(event)
+      expect(request).toMatchObject({
+        type: 'tracking',
+        leadRefs: [{ type: 'client-id', value: event.userId }],
+        kind: 'Track',
+        data: 'User Registered',
+        timestamp: expectedTs(event.timestamp),
+        values: {}
+      })
+    })
 
-    test('should submit event on Page event', async () => {})
+    it('should not skip an event with anonymousID only', async () => {
+      const event = createTestEvent({
+        type: 'track',
+        event: 'User Registered',
+        userId: undefined
+      })
+      const request = await testAction(event)
+      expect(request).toMatchObject({
+        type: 'tracking',
+        leadRefs: [{ type: 'client-id', value: event.anonymousId }],
+        kind: 'Track',
+        data: 'User Registered',
+        timestamp: expectedTs(event.timestamp),
+        values: {}
+      })
+    })
 
-    test('should skip Page event if url not specified', async () => {})
+    it('should submit event on Page event', async () => {})
 
-    test('should submit event on Identify event', async () => {})
+    it('should skip Page event if url not specified', async () => {})
 
-    test('should skip Identify event if email not specified', async () => {})
+    it('should submit event on Identify event', async () => {})
 
-    test('should submit event on Screen event', async () => {})
+    it('should skip Identify event if email not specified', async () => {})
 
-    test('should skip Screen event if name not specified', async () => {})
+    it('should submit event on Screen event', async () => {})
 
-    test('should use custom event property mapping', async () => {})
+    it('should skip Screen event if name not specified', async () => {})
 
-    test('should skip an event is neither userID nor anonymousID are specified', async () => {})
+    it('should submit event batch', async () => {})
   })
 })
+
+const testDestination = createTestIntegration(Destination)
+
+const testAction = async (event: SegmentEvent, mapping: JSONObject | undefined = undefined): Promise<any> => {
+  const settings = { apiKey: 'TEST_API_KEY' }
+  nock(apiBaseUrl).post('/events').reply(200, {})
+  const input = { event, settings, mapping, useDefaultMappings: mapping === undefined }
+  const responses = await testDestination.testAction('submitEvent', input)
+  expect(responses.length).toBe(1)
+  const request = responses[0].request
+  expect(request.headers.get('Authorization')).toBe(`Bearer ${settings.apiKey}`)
+  const rawBody = await request.text()
+  return JSON.parse(rawBody)
+}
+
+const expectedTs = (segmentEventTs: string | Date | undefined): number => {
+  if (segmentEventTs === undefined) throw new Error('Unexpected state: test event created without a timestamp')
+  else if (typeof segmentEventTs === 'string') return Date.parse(segmentEventTs)
+  else return segmentEventTs.valueOf()
+}
+
+const userAgent =
+  '"Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+
+const minRequiredMapping = {
+  type: { '@path': '$.type' },
+  userId: { '@path': '$.userId' },
+  anonymousId: { '@path': '$.anonymousId' },
+  eventName: { '@path': '$.event' },
+  properties: { '@path': '$.properties' },
+  timestamp: { '@path': '$.timestamp' }
+}
