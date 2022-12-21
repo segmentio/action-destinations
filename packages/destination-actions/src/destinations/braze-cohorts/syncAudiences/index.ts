270d024849d3bc0ae93224fa1307d5b07b4390a0
Gaurav Kochar <gaurav.kochar@segment.com>
2022-12-21 19:08:47 +0530

    worked on PR review comments and tested it

--- packages/destination-actions/src/destinations/braze-cohorts/syncAudiences/index.ts
+++ packages/destination-actions/src/destinations/braze-cohorts/syncAudiences/index.ts
@@ -2,8 +2,8 @@ import { ActionDefinition, RequestClient, IntegrationError } from '@segment/acti
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 import { SyncAudiences } from '../api'
-import { CohortChanges } from '../cohortChanges'
-import { Logger, StateContext } from '@segment/actions-core/src/destination-kit'
+import { CohortChanges } from '../braze-cohorts-types'
+import { StateContext } from '@segment/actions-core/src/destination-kit'
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Sync Audience',
@@ -102,48 +102,69 @@ const action: ActionDefinition<Settings, Payload> = {
       }
     }
   },
-  perform: async (request, { settings, payload, logger, stateContext }) => {
-    return processPayload(request, settings, [payload], logger, stateContext)
+  perform: async (request, { settings, payload, stateContext }) => {
+    return processPayload(request, settings, [payload], stateContext)
   },
-  performBatch: async (request, { settings, payload, logger, stateContext }) => {
-    return processPayload(request, settings, payload, logger, stateContext)
+  performBatch: async (request, { settings, payload, stateContext }) => {
+    return processPayload(request, settings, payload, stateContext)
   }
 }
 async function processPayload(
   request: RequestClient,
   settings: Settings,
   payloads: Payload[],
-  logger?: Logger,
   stateContext?: StateContext
 ) {
   validate(payloads)
   const syncAudiencesApiClient: SyncAudiences = new SyncAudiences(request, settings)
   const { cohort_name, cohort_id } = payloads[0]
-  logger?.info?.('Added New Log', payloads[0].time)
-  logger?.info?.('Testing State Context', stateContext?.getRequestContext?.('cohort_name'))
+  const cohortChanges: Array<CohortChanges> = []
+  payloads.push({
+    ...payloads[0],
+    external_id: 'external_gaurav_kochar020',
+    event_properties: {
+      abc: false
+    }
+  })
+  payloads.push({
+    ...payloads[0],
+    external_id: 'external_gaurav_kochar021',
+    event_properties: {
+      abc: false
+    }
+  })
+  payloads.push({
+    ...payloads[0],
+    external_id: 'external_gaurav_kochar',
+    event_properties: {
+      abc: true
+    }
+  })
 
   if (stateContext?.getRequestContext?.('cohort_name') != cohort_name) {
     await syncAudiencesApiClient.createCohort(settings, payloads[0])
-    stateContext?.setResponseContext?.(`cohort_name`, cohort_name, { minute: 2 })
+    stateContext?.setResponseContext?.(`cohort_name`, cohort_name, { hour: 1 })
   }
   const { addUsers, removeUsers } = extractUsers(payloads)
-  const users = {
-    addUsers,
-    removeUsers,
-    hasAddUsers: hasUsersToAddOrRemove(addUsers),
-    hasRemoveUsers: hasUsersToAddOrRemove(removeUsers)
-  }
+  const hasAddUsers = hasUsersToAddOrRemove(addUsers)
+  const hasRemoveUsers = hasUsersToAddOrRemove(removeUsers)
 
   // We should never hit this condition because at least an user_id or device_id
   // or user_alias is required in each payload, but if we do, returning early
   // rather than hitting Cohort's API (with no data) is more efficient.
   // The monoservice will interpret this early return as a 200.
 
-  if (!users.hasAddUsers && !users.hasRemoveUsers) {
+  if (!hasAddUsers && !hasRemoveUsers) {
     return
   }
+  if (hasAddUsers) {
+    cohortChanges.push(addUsers)
+  }
+  if (hasRemoveUsers) {
+    cohortChanges.push(removeUsers)
+  }
 
-  const res = await syncAudiencesApiClient.batchUpdate(settings, cohort_id, users)
+  const res = await syncAudiencesApiClient.batchUpdate(settings, cohort_id, cohortChanges)
   return res
 }
 
@@ -171,11 +192,11 @@ function extractUsers(payloads: Payload[]) {
     const user = userEnteredOrRemoved ? addUsers : removeUsers
 
     if (external_id) {
-      user?.user_ids.push(external_id)
+      user?.user_ids?.push(external_id)
     } else if (device_id) {
-      user?.device_ids.push(device_id)
+      user?.device_ids?.push(device_id)
     } else if (user_alias) {
-      user?.aliases.push(user_alias)
+      user?.aliases?.push(user_alias)
     }
   })
 
