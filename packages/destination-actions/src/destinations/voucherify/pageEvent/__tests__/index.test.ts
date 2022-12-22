cf73b2914d1672b5bf5ba42b1ec02f242543c28a
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2022-12-22 17:42:56 +0100

    Delete test folder for now

--- packages/destination-actions/src/destinations/voucherify/pageEvent/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-// import nock from 'nock'
-// import { createTestEvent, createTestIntegration } from '@segment/actions-core'
-// import Destination from '../../index'
-
-// const testDestination = createTestIntegration(Destination)
-
-// describe('Voucherify.pageEvent', () => {
-//   // TODO: Test your action
-// })
--- packages/destination-actions/src/destinations/voucherify/trackEvent/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-// import nock from 'nock'
-// import { createTestEvent, createTestIntegration } from '@segment/actions-core'
-// import Destination from '../../index'
-
-// const testDestination = createTestIntegration(Destination)
-
-// describe('Voucherify.trackEvent', () => {
-//   // TODO: Test your action
-// })
