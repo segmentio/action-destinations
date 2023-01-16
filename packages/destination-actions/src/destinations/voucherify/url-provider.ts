14f2ef6834aebbac46765bd9d1550d44222c4500
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2023-01-16 17:20:33 +0100

    Reduce the getVoucherifyEndpointURL function

--- packages/destination-actions/src/destinations/voucherify/url-provider.ts
+++ packages/destination-actions/src/destinations/voucherify/url-provider.ts
@@ -1,12 +1,9 @@
 import type { Settings } from './generated-types'
 
 export const getVoucherifyEndpointURL = (settings: Settings, eventType: string) => {
-  let voucherifyRequestURL: string
-
-  if (settings.customURL) {
-    voucherifyRequestURL = `${settings.customURL}/segmentio/${eventType}-processing`
-  } else {
+  if (!settings.customURL) {
     throw new Error('URL not provided.')
   }
-  return voucherifyRequestURL
+
+  return `${settings.customURL}/segmentio/${eventType}-processing`
 }
