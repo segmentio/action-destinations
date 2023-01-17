967454fa47090c3ecada40000cb8162201c66d94
Wesley Walser <wesley.walser@algolia.com>
2023-01-18 02:33:19 +1100

    test: write auth schema test

--- packages/destination-actions/src/destinations/algolia-insights/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/algolia-insights/__tests__/index.test.ts
@@ -1,17 +1,19 @@
 import nock from 'nock'
-import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import { createTestIntegration } from '@segment/actions-core'
 import Definition from '../index'
+import { BaseAlgoliaInsightsURL } from '../algolia-insight-api'
 
 const testDestination = createTestIntegration(Definition)
 
 describe('Algolia Insights', () => {
   describe('testAuthentication', () => {
     it('should validate authentication inputs', async () => {
-      nock('https://your.destination.endpoint').get('*').reply(200, {})
-
-      // This should match your authentication.fields
-      const authData = {}
-
+      nock(BaseAlgoliaInsightsURL).post('*').reply(200, {})
+      const authData = {
+        appId: 'algolia-application-id',
+        apiKey: 'algolia-api-key'
+      }
+      // there is no testAuthentication function for this destination so this just validates the authData schema
       await expect(testDestination.testAuthentication(authData)).resolves.not.toThrowError()
     })
   })
