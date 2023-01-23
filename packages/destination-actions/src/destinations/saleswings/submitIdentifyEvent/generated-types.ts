b955be0b843f9537880a806a96a81558d07306dc
Denis Egorushkin <denis.egorushkin@saleswingsapp.com>
2023-01-23 19:37:40 +0400

    Update geenrated types

--- packages/destination-actions/src/destinations/saleswings/submitIdentifyEvent/generated-types.ts
+++ packages/destination-actions/src/destinations/saleswings/submitIdentifyEvent/generated-types.ts
@@ -2,6 +2,14 @@
 
 export interface Payload {
   /**
+   * Type of the SalesWings custom event (a custom event is visualized in SalesWings cockpit and SalesForce Lead Intent View as "[[Kind]] Data").
+   */
+  kind: string
+  /**
+   * String description of the SalesWings custom event payload (a custom event is visualized in SalesWings cockpit and SalesForce Lead Intent View as "[[Kind]] Data").
+   */
+  data: string
+  /**
    * Permanent identifier of a Segment user the event is attributed to.
    */
   userId?: string
@@ -30,15 +38,7 @@ export interface Payload {
    */
   timestamp?: string | number
   /**
-   * Type of the SalesWings custom event (event is visualized in SalesWings cockpit / SalesForce Lead Intent View as "[kind] data")
-   */
-  kind: string
-  /**
-   * String description of the SalesWings custom event payload (event is visualized in SalesWings cockpit / SalesForce Lead Intent View as "[kind] data")
-   */
-  data: string
-  /**
-   * Custom attribute properties associated with the SalesWings custom event
+   * Custom attribute values associated with the SalesWings custom event.
    */
   values?: {
     [k: string]: unknown
