8906c2d8b1362edace69b6b2543b447561ce1301
Dan Lasky <dan.lasky@segment.com>
2023-01-25 16:22:17 -0500

    split tests by functionality

--- packages/core/src/__tests__/get.test.ts
+++ packages/core/src/__tests__/get.test.ts
@@ -21,6 +21,8 @@ const fixtures: Record<string, unknown> = {
   '': obj,
   a: obj.a,
   'a.b': obj.a.b,
+  "['a'].b": obj.a.b,
+  '["a"].b': obj.a.b,
   'a.b.c': obj.a.b.c,
   'a.b.d': obj.a.b.d,
   'a.b.e': obj.a.b.e,
@@ -28,12 +30,10 @@ const fixtures: Record<string, unknown> = {
   'a.b.f[0].g': obj.a.b.f[0].g,
   'a.h': obj.a.h,
   'a.b.x': undefined,
+  '[txt] non': true,
+  '[txt] nest.inner': true,
   u: undefined
 }
-
-/** note that this test is basically a duplicate of the get.test.ts file,
- * only with the tests that safari can't handle due to its lack of lookbehind (ES2018)
- * grouping removed so it passes in webkit */
 
 describe('get', () => {
   for (const path of Object.keys(fixtures)) {
