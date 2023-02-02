b3ac55c4c210a7e9f2d6912a927b26a6c4d6a254
Beatrice Parfait <beatrice.parfait@algolia.com>
2023-02-02 12:41:26 +1100

    create productClick presets

--- packages/destination-actions/src/destinations/algolia-insights/productClickedEvents/index.ts
+++ packages/destination-actions/src/destinations/algolia-insights/productClickedEvents/index.ts
@@ -1,7 +1,56 @@
 import type { ActionDefinition } from '@segment/actions-core'
+import { Subscription } from '@segment/actions-core'
+import { defaultValues } from '@segment/actions-core'
 import { AlgoliaBehaviourURL, AlgoliaProductClickedEvent } from '../algolia-insight-api'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
+
+/** used in the quick setup */
+export const productClickPresets: Subscription[] = [
+  // and is $ meant to represent something specific?
+  {
+    name: 'Product ID',
+    subscribe: 'type = "track" and event = "Product Clicked"',
+    partnerAction: 'properties',
+    mapping: defaultValues({ '@path': '$.properties.product_id' })
+  },
+  {
+    name: 'Index',
+    subscribe: 'type = "track" and event = "Product Clicked"',
+    partnerAction: 'properties',
+    mapping: defaultValues({ '@path': '$.properties.search_index' })
+  },
+  {
+    name: 'Query ID',
+    subscribe: 'type = "track" and event = "Product Clicked"',
+    partnerAction: 'properties',
+    mapping: defaultValues({ '@path': '$.properties.query_id' })
+  },
+  {
+    name: 'Position',
+    subscribe: 'type = "track" and event = "Product Clicked"',
+    partnerAction: 'properties',
+    mapping: defaultValues({ '@path': '$.properties.position' })
+  },
+  {
+    name: 'Anonymous ID',
+    subscribe: 'type = "track" and event = "Product Clicked"',
+    partnerAction: 'anonymousId',
+    mapping: defaultValues({ '@path': '$.anonymousId' })
+  },
+  {
+    name: 'User ID',
+    subscribe: 'type = "track" and event = "Product Clicked"',
+    partnerAction: 'userId',
+    mapping: defaultValues({ '@path': '$.userId' })
+  },
+  {
+    name: 'timestamp',
+    subscribe: 'type = "track" and event = "Product Clicked"',
+    partnerAction: 'timestamp',
+    mapping: defaultValues({ '@path': '$.timestamp' })
+  }
+]
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Product Clicked Events',
@@ -47,7 +96,7 @@ const action: ActionDefinition<Settings, Payload> = {
       label: 'Anonymous ID',
       description: "The user's anonymous id.",
       type: 'string',
-      required: true,
+      required: false,
       default: { '@path': '$.anonymousId' }
     },
     userID: {
