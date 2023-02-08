cf0808f3bfd036d7f79be74633e0753eec4c83ce
Wasi Haider <whaider@twilio.com>
2023-02-08 13:08:34 -0800

    Try to fix script

--- scripts/test-browser.sh
+++ scripts/test-browser.sh
@@ -3,6 +3,6 @@ NODE_VERSION="$(node --version)";
 NODE_VERSION_MAJOR="${NODE_VERSION:1}"; # strip the "v"  prefix
 NODE_VERSION_MAJOR="${NODE_VERSION_MAJOR%%.*}"; #get everything before the first dot
 if [ "$NODE_VERSION_MAJOR" -ge "18" ]; then
-echo lerna run build:karma --stream && NODE_OPTIONS=--openssl-legacy-provider karma start;
+lerna run build:karma --stream && NODE_OPTIONS=--openssl-legacy-provider karma start;
 else lerna run build:karma --stream && karma start;
 fi
