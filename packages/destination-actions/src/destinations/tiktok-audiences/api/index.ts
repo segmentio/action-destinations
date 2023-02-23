9fd30341ec651093aeb6885cce7bbf8dd67c1174
Nick Aguilar <nicholas.aguilar@segment.com>
2023-02-23 12:21:36 -0800

    Added more comments

--- packages/destination-actions/src/destinations/tiktok-audiences/api/index.ts
+++ packages/destination-actions/src/destinations/tiktok-audiences/api/index.ts
@@ -66,6 +66,16 @@ export class TikTokAudiences {
     })
   }
 
+  /**
+   * Returns a list of possible advertisers for the user to select from.
+   * Items selected here will be saved to the user mapping, and so every advertiser ID is
+   * first "stringified" to prevent conversion to a number.
+   * @param advertiser_ids The list of advertiser IDs to fetch names for. This is a list of strings,
+   * all of which are purely numeric.
+   * @returns A list of ("id_<advertiser_id>", "name") pairs, where "id_<advertiser_id>" is the stringified
+   * advertiser ID, and "name" is the advertiser's name. If an error occurs we fallback to returning the
+   * list of advertiser IDs we have saved as settings.
+   */
   fetchAdvertisers = async (advertiser_ids: string[]): Promise<DynamicFieldResponse> => {
     if (!advertiser_ids.length) {
       return {
