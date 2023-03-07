b61e31200ce21e7aeadb51bab869820899bdba8f
Gaurav Kochar <gaurav.kochar@segment.com>
2023-03-07 19:44:02 +0530

    worked on report conversion event

---
+++ packages/destination-actions/src/destinations/pinterest-conversions/pinterest-capi-custom-data.ts
@@ -0,0 +1,84 @@
+import { InputField } from '@segment/actions-core/src/destination-kit/types'
+// import { Payload } from './reportConversionEvent/generated-types'
+
+// Implementation of Facebook user data object
+// https://developers.facebook.com/docs/marketing-api/conversions-api/parameters/customer-information-parameters
+export const custom_data_field: InputField = {
+  label: 'Custom Data',
+  description: 'Object containing customer information data.',
+  type: 'object',
+  properties: {
+    currency: {
+      label: 'Currency',
+      description: 'ISO-4217 currency code. If not provided, it will default to the currency set for the ad account.',
+      type: 'string',
+      default: {
+        '@path': '$.properties.currency'
+      }
+    },
+    value: {
+      label: 'Value',
+      description:
+        'Total value of the event. E.g. if there are multiple items in a checkout event, value should be the total price of all items',
+      type: 'number',
+      default: {
+        '@if': {
+          exists: { '@path': '$.properties.price' },
+          then: { '@path': '$.properties.price' },
+          else: { '@path': '$.properties.value' }
+        }
+      }
+    },
+    content_ids: {
+      label: 'Content IDs',
+      description: 'Product IDs as an array of strings',
+      type: 'string',
+      multiple: true
+    },
+    contents: {
+      label: 'Gender',
+      description: 'Gender in lowercase. Either f or m.',
+      type: 'object',
+      multiple: true,
+      properties: {
+        item_price: {
+          label: 'Price',
+          type: 'number',
+          description: 'The price of the Item'
+        },
+        quantity: {
+          label: 'quantity',
+          type: 'integer',
+          description: 'The number of items purchased'
+        }
+      }
+    },
+    num_items: {
+      label: 'Number of Items',
+      description: 'Total number of products in the event. ',
+      type: 'string'
+    },
+    order_id: {
+      label: 'Order ID',
+      description: 'Order ID',
+      type: 'string',
+      default: {
+        '@path': '$.properties.order_id'
+      }
+    },
+    search_string: {
+      label: 'Search string',
+      description: 'Search string related to the conversion event.',
+      type: 'string',
+      default: {
+        '@path': '$.properties.query'
+      }
+    },
+    opt_out_type: {
+      label: 'Opt Out Type',
+      description:
+        'opt_out_type is the field where we accept opt outs for your users’ privacy preference.  It can handle multiple values with commas separated.',
+      type: 'string'
+    }
+  }
+}
