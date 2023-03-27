6fd931d41ada1e5e96b76d4c6c105ac2169ae8e1
A Murphy <amanda.murphy@heapanalytics.com>
2023-03-27 14:22:12 -0700

    [HEAP-41596]: Add Identity and traits to track event (#1127)
    * Add identity to track call
    
    * fix types
    
    * add anonymous id to track event
    
    * update the tests
    
    * update the docs
    
    * fix spacing

--- packages/browser-destinations/src/destinations/heap/trackEvent/__tests__/index.test.ts
+++ packages/browser-destinations/src/destinations/heap/trackEvent/__tests__/index.test.ts
@@ -9,19 +9,28 @@ import {
 import { HEAP_SEGMENT_BROWSER_LIBRARY_NAME } from '../../constants'
 
 describe('#trackEvent', () => {
-  const createHeapDestinationAndSpy = async (): Promise<[Plugin, jest.SpyInstance]> => {
+  let event: Plugin
+  let heapTrackSpy: jest.SpyInstance
+  let addUserPropertiesSpy: jest.SpyInstance
+  let identifySpy: jest.SpyInstance
+
+  beforeAll(async () => {
     mockHeapJsHttpRequest()
     window.heap = createMockedHeapJsSdk()
 
-    const [event] = await heapDestination({ appId: HEAP_TEST_ENV_ID, subscriptions: [trackEventSubscription] })
+    event = (await heapDestination({ appId: HEAP_TEST_ENV_ID, subscriptions: [trackEventSubscription] }))[0]
 
     await event.load(Context.system(), {} as Analytics)
-    const heapTrackSpy = jest.spyOn(window.heap, 'track')
+    heapTrackSpy = jest.spyOn(window.heap, 'track')
+    addUserPropertiesSpy = jest.spyOn(window.heap, 'addUserProperties')
+    identifySpy = jest.spyOn(window.heap, 'identify')
+  })
 
-    return [event, heapTrackSpy]
-  }
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
   it('sends events to heap', async () => {
-    const [event, heapTrackSpy] = await createHeapDestinationAndSpy()
     await event.track?.(
       new Context({
         type: 'track',
@@ -36,10 +45,11 @@ describe('#trackEvent', () => {
       banana: '📞',
       segment_library: HEAP_SEGMENT_BROWSER_LIBRARY_NAME
     })
+    expect(addUserPropertiesSpy).toHaveBeenCalledTimes(0)
+    expect(identifySpy).toHaveBeenCalledTimes(0)
   })
 
   it('should send segment_library property if no other properties were provided', async () => {
-    const [event, heapTrackSpy] = await createHeapDestinationAndSpy()
     await event.track?.(
       new Context({
         type: 'track',
@@ -50,23 +60,58 @@ describe('#trackEvent', () => {
     expect(heapTrackSpy).toHaveBeenCalledWith('hello!', {
       segment_library: HEAP_SEGMENT_BROWSER_LIBRARY_NAME
     })
+    expect(addUserPropertiesSpy).toHaveBeenCalledTimes(0)
+    expect(identifySpy).toHaveBeenCalledTimes(0)
   })
 
   it('should not override segment_library property value if provided by user', async () => {
-    const [event, heapTrackSpy] = await createHeapDestinationAndSpy()
     const segmentLibraryValue = 'user-provided-value'
+    const userId = 'TEST_ID77'
     await event.track?.(
       new Context({
         type: 'track',
         name: 'hello!',
         properties: {
           segment_library: segmentLibraryValue
-        }
+        },
+        userId
       })
     )
 
     expect(heapTrackSpy).toHaveBeenCalledWith('hello!', {
       segment_library: HEAP_SEGMENT_BROWSER_LIBRARY_NAME
     })
+    expect(identifySpy).toHaveBeenCalledWith(userId)
+    expect(addUserPropertiesSpy).toHaveBeenCalledTimes(0)
+  })
+
+  it('should add traits', async () => {
+    const segmentLibraryValue = 'test123'
+    const anonymous_id = 'ANON1'
+    const name = 'Grace Hopper'
+    await event.track?.(
+      new Context({
+        type: 'track',
+        name: 'hello!',
+        properties: {
+          segment_library: segmentLibraryValue
+        },
+        context: {
+          traits: {
+            name
+          }
+        },
+        anonymousId: anonymous_id
+      })
+    )
+
+    expect(heapTrackSpy).toHaveBeenCalledWith('hello!', {
+      segment_library: HEAP_SEGMENT_BROWSER_LIBRARY_NAME
+    })
+    expect(addUserPropertiesSpy).toHaveBeenCalledWith({
+      anonymous_id,
+      name
+    })
+    expect(identifySpy).toHaveBeenCalledTimes(0)
   })
 })
