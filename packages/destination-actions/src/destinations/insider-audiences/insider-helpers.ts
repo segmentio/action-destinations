3f82943d6c192fe1df703fd77d9b20c58b78806c
Emre Isik <emreisik@yandex.com>
2023-03-31 15:44:25 +0300

    SD-86746 - Authentication

--- packages/destination-actions/src/destinations/insider-audiences/insider-helpers.ts
+++ packages/destination-actions/src/destinations/insider-audiences/insider-helpers.ts
@@ -2,6 +2,7 @@ import { Payload } from './insiderAudiences/generated-types'
 
 export const API_BASE = 'https://unification.useinsider.com/api/'
 export const UPSERT_ENDPOINT = 'user/v1/upsert'
+export const DELETE_ATTRIBUTE_ENDPOINT = 'user/v1/attribute/delete'
 
 export function computedTraitsPayloadForIdentifyCall(data: Payload) {
   const identifiers = getIdentifiers(data)
@@ -17,7 +18,53 @@ export function computedTraitsPayloadForIdentifyCall(data: Payload) {
   }
 }
 
+export function computedAudiencesPayloadForIdentifyCall(data: Payload) {
+  const identifiers = getIdentifiers(data)
+  const attributes = getAttributesForAudiences(data)
+
+  return {
+    users: [
+      {
+        identifiers,
+        attributes
+      }
+    ]
+  }
+}
+
 export function computedTraitsPayloadForTrackCall(data: Payload) {
+  const identifiers = getIdentifiers(data)
+  const events = getEvents(data)
+
+  return {
+    users: [
+      {
+        identifiers,
+        events
+      }
+    ]
+  }
+}
+
+export function deleteAttributePartial(data: Payload) {
+  const identifiers = getIdentifiers(data)
+  const computationKey = data.context.personas.computation_key
+
+  return {
+    users: [
+      {
+        identifiers,
+        custom: {
+          partial: {
+            segment_audience_name: [computationKey]
+          }
+        }
+      }
+    ]
+  }
+}
+
+export function computedAudiencePayloadForTrackCall(data: Payload) {
   const identifiers = getIdentifiers(data)
   const events = getEvents(data)
 
@@ -53,6 +100,17 @@ const getIdentifiers = function (data: Payload) {
   }
 
   return identifiers
+}
+
+const getAttributesForAudiences = function (data: Payload) {
+  const computationKey = data.context.personas.computation_key
+  const attributes = {
+    custom: {
+      segment_audience_name: [computationKey]
+    }
+  }
+
+  return attributes
 }
 
 const getAttributes = function (data: Payload) {
