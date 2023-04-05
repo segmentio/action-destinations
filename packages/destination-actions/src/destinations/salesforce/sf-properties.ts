2cab5d54baa1b87958e7df59ece27fb3657cff5d
Nick Aguilar <nicholas.aguilar@segment.com>
2023-04-05 11:54:58 -0700

    Adds action level unit tests and updates descriptions

--- packages/destination-actions/src/destinations/salesforce/sf-properties.ts
+++ packages/destination-actions/src/destinations/salesforce/sf-properties.ts
@@ -4,7 +4,7 @@ import { IntegrationError } from '@segment/actions-core'
 export const operation: InputField = {
   label: 'Operation',
   description:
-    'The Salesforce operation performed. The available operations are Create, Update or Upsert records in Salesforce.',
+    'The Salesforce operation performed. The available operations are Create, Delete, Update or Upsert records in Salesforce.',
   type: 'string',
   required: true,
   choices: [
@@ -65,11 +65,11 @@ export const recordMatcherOperator: InputField = {
 
 export const traits: InputField = {
   label: 'Record Matchers',
-  description: `The fields used to find Salesforce records for updates. **This is required if the operation is Update or Upsert.**
+  description: `The fields used to find Salesforce records for updates. **This is required if the operation is Delete, Update or Upsert.**
 
   Any field can function as a matcher, including Record ID, External IDs, standard fields and custom fields. On the left-hand side, input the Salesforce field API name. On the right-hand side, map the Segment field that contains the value.  
   
-  If multiple records are found, no updates will be made. **Please use fields that result in unique records.**
+  If multiple records are found, no changes will be made. **Please use fields that result in unique records.**
   
   ---
 
