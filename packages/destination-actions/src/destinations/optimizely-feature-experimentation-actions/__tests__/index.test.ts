cb7f50f893b0658dc260cb2012a997f34f6ef940
Harsh Vardhan <harsh.vardhan@segment.com>
2023-04-11 22:36:39 +0530

    Added unit test for delete request

--- packages/destination-actions/src/destinations/optimizely-feature-experimentation-actions/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/optimizely-feature-experimentation-actions/__tests__/index.test.ts
@@ -44,5 +44,29 @@ describe('Optimizely Feature Experimentation (actions)', () => {
         expect(resp.data).toMatchObject({})
       }
     })
+
+    it('should throw error if access token not provided in settings and getting delete request', async () => {
+      nock('https://api.optimizely.com/v2/subject-access-requests', {}).post('').reply(400, {})
+
+      const event = createTestEvent({
+        type: 'delete',
+        userId: 'sloth@segment.com',
+        traits: {
+          data_type: 'visitor',
+          identifier: 12345566,
+          identifier_type: 'dcp_id',
+          request_type: 'delete'
+        }
+      })
+
+      if (testDestination.onDelete) {
+        await expect(
+          testDestination.onDelete(event, {
+            accountId: '12345566',
+            dataFileUrl: 'https://cdn.example.com/dataFile.json'
+          })
+        ).rejects.toThrowError(`Access Token is required for user deletion`)
+      }
+    })
   })
 })
