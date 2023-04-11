d8e86396a8b028e84dff9d1f9e39a1a9b607b45c
seeratawan01 <seeratsdsking@gmail.com>
2023-04-11 11:05:13 +0500

    feat: usermaven track event

---
+++ packages/destination-actions/src/destinations/usermaven/trackEvent/generated-types.ts
@@ -0,0 +1,18 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * The user ID.
+   */
+  userId: string
+  /**
+   * The event type.
+   */
+  eventType: string
+  /**
+   * The event attributes.
+   */
+  eventAttributes?: {
+    [k: string]: unknown
+  }
+}
---
+++ packages/destination-actions/src/destinations/usermaven/trackEvent/index.ts
@@ -0,0 +1,49 @@
+import type { ActionDefinition } from '@segment/actions-core'
+import type { Settings } from '../generated-types'
+import type { Payload } from './generated-types'
+import { trackEventRequestParams } from '../request-params'
+
+const action: ActionDefinition<Settings, Payload> = {
+  title: 'Track Event',
+  description: '',
+  fields: {
+    userId: {
+      label: 'User ID',
+      description: 'The user ID.',
+      type: 'string',
+      required: true,
+      default: {
+        '@path': '$.userId'
+      }
+    },
+    eventType: {
+      label: 'Event Type',
+      description: 'The event type.',
+      type: 'string',
+      required: true,
+      default: {
+        '@path': '$.event'
+      }
+    },
+    eventAttributes: {
+      label: 'Event Attributes',
+      description: 'The event attributes.',
+      type: 'object',
+      required: false,
+      default: {
+        '@path': '$.attributes'
+      }
+    }
+  },
+  perform: (request, { payload, settings }) => {
+    const { url, options } = trackEventRequestParams(
+      settings,
+      payload.userId,
+      payload.eventType,
+      payload.eventAttributes
+    )
+    return request(url, options)
+  }
+}
+
+export default action
