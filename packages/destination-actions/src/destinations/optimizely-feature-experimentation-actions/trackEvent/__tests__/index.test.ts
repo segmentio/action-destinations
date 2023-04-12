9cdae9c16b2d781e3d1005fe6fbe411d01a5b45d
Harsh Vardhan <harsh.vardhan@segment.com>
2023-04-12 17:09:16 +0530

    Added new test case for bot filtering in optimizely

--- packages/destination-actions/src/destinations/optimizely-feature-experimentation-actions/trackEvent/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/optimizely-feature-experimentation-actions/trackEvent/__tests__/index.test.ts
@@ -5,7 +5,7 @@ import { dataFile } from '../mock-dataFile'
 
 const testDestination = createTestIntegration(Destination)
 
-describe('OptimizelyFullStack.trackEvent', () => {
+describe('OptimizelyFeatureExperimentation.trackEvent', () => {
   it('should send event successfully', async () => {
     const settings = {
       accountId: '12345566',
@@ -50,4 +50,27 @@ describe('OptimizelyFullStack.trackEvent', () => {
         testDestination.testAction('trackEvent', { event, settings, useDefaultMappings: true })
       ).rejects.toThrowError(`Event with name ${event.event} is not defined`)
     })
+  it('should be able to send a basic track with bot filtering enabled', async () => {
+    const settings = {
+      accountId: '12345566',
+      dataFileUrl: 'https://cdn.example.com/dataFile.json'
+    }
+    nock(settings.dataFileUrl).get('').reply(200, dataFile)
+    dataFile.botFiltering = true
+    nock('https://logx.optimizely.com/v1/events').post('').reply(200)
+    const event = createTestEvent({
+      event: 'Product List Clicked',
+      properties: {
+        revenue: 1000
+      },
+      context: {
+        traits: {
+          test: 'test'
+        }
+      }
+    })
+    await expect(
+      testDestination.testAction('trackEvent', { event, settings, useDefaultMappings: true })
+    ).resolves.not.toThrowError()
+  })
 })
