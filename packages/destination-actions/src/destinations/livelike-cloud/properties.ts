3234a91f5de94f2474e289106bf4ef021b322073
Gautam <gautam@livelike.com>
2023-04-12 14:38:04 +0530

    reverted api url

--- packages/destination-actions/src/destinations/livelike-cloud/properties.ts
+++ packages/destination-actions/src/destinations/livelike-cloud/properties.ts
@@ -1,2 +1,1 @@
-// export const apiBaseUrl = 'https://cf-blast-iconic.livelikecdn.com/api/v1'
-export const apiBaseUrl = 'https://9dad-2401-4900-1c30-19a-6d0b-d0bd-aabb-7e64.ngrok.io/api/v1'
+export const apiBaseUrl = 'https://cf-blast.livelikecdn.com/api/v1'
