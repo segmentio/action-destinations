a0af15fe65789f2c293f75c66da8a4d54bfbf066
Harsh Joshi <harsh.joshi@segment.com>
2023-04-19 12:08:30 +0530

    Throwing payload validation error for invalid headers

--- packages/destination-actions/src/destinations/webhook/send/index.ts
+++ packages/destination-actions/src/destinations/webhook/send/index.ts
@@ -1,4 +1,4 @@
-import { ActionDefinition } from '@segment/actions-core'
+import { ActionDefinition, PayloadValidationError } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 
@@ -48,21 +48,30 @@ const action: ActionDefinition<Settings, Payload> = {
     }
   },
   perform: (request, { payload }) => {
-    return request(payload.url, {
-      method: payload.method as RequestMethod,
-      headers: payload.headers as Record<string, string>,
-      json: payload.data
-    })
+    try {
+      return request(payload.url, {
+        method: payload.method as RequestMethod,
+        headers: payload.headers as Record<string, string>,
+        json: payload.data
+      })
+    } catch (error) {
+      if (error instanceof TypeError) throw new PayloadValidationError(error.message)
+      throw error
+    }
   },
   performBatch: (request, { payload }) => {
     // Expect these to be the same across the payloads
     const { url, method, headers } = payload[0]
-
-    return request(url, {
-      method: method as RequestMethod,
-      headers: headers as Record<string, string>,
-      json: payload.map(({ data }) => data)
-    })
+    try {
+      return request(url, {
+        method: method as RequestMethod,
+        headers: headers as Record<string, string>,
+        json: payload.map(({ data }) => data)
+      })
+    } catch (error) {
+      if (error instanceof TypeError) throw new PayloadValidationError(error.message)
+      throw error
+    }
   }
 }
 
