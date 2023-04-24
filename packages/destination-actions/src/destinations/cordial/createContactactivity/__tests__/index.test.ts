507ae19fc9b143bd0789376d83b6ea2b908fbe82
Noah Cooper <ncooper@twilio.com>
2023-04-24 11:12:22 -0400

    Remove linting changes

--- packages/destination-actions/src/destinations/cordial/createContactactivity/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/cordial/createContactactivity/__tests__/index.test.ts
@@ -1,29 +1,29 @@
-import nock from 'nock'
+  import nock from 'nock'
 import { createTestEvent, createTestIntegration } from '@segment/actions-core'
 import Destination from '../../index'
 
 const testDestination = createTestIntegration(Destination)
 
 describe('Cordial.createContactactivity', () => {
-it('should work with default mappings', async () => {
-  nock(/api.cordial.io/).post('/api/segment/createContactactivity').reply(200, {})
-  const event = createTestEvent()
+  it('should work with default mappings', async () => {
+    nock(/api.cordial.io/).post('/api/segment/createContactactivity').reply(200, {})
+    const event = createTestEvent()
 
-  const mapping = {
-    userIdentities: {'channels.email.address': 'contact@example.com'}
-  }
+    const mapping = {
+      userIdentities: {'channels.email.address': 'contact@example.com'}
+    }
 
-  const settings = {
-    apiKey: 'cordialApiKey',
-    endpoint: 'https://api.cordial.io' as const,
-    segmentIdKey: 'segment_id'
-  }
+    const settings = {
+      apiKey: 'cordialApiKey',
+      endpoint: 'https://api.cordial.io' as const,
+      segmentIdKey: 'segment_id'
+    }
 
-  await testDestination.testAction('createContactactivity', {
-    event,
-    mapping,
-    settings,
-    useDefaultMappings: true
+    await testDestination.testAction('createContactactivity', {
+      event,
+      mapping,
+      settings,
+      useDefaultMappings: true
+    })
   })
-})
 })
