ee9e2a1eebcd08af976029c5b5bdde30b30aa744
Robert Hall <rhall@twilio.com>
2023-04-27 13:18:34 -0500

    temporarily remove warnings from scaffold

--- packages/destination-actions/src/destinations/liveramp-audiences/index.ts
+++ packages/destination-actions/src/destinations/liveramp-audiences/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { DestinationDefinition } from '@segment/actions-core'
 import type { Settings } from './generated-types'
 
@@ -9,14 +10,14 @@ const destination: DestinationDefinition<Settings> = {
   authentication: {
     scheme: 'custom',
     fields: {},
-    testAuthentication: (request) => {
+    testAuthentication: (_) => {
       // Return a request that tests/validates the user's credentials.
       // If you do not have a way to validate the authentication fields safely,
       // you can remove the `testAuthentication` function, though discouraged.
     }
   },
 
-  onDelete: async (request, { settings, payload }) => {
+  onDelete: async (_) => {
     // Return a request that performs a GDPR delete for the provided Segment userId or anonymousId
     // provided in the payload. If your destination does not support GDPR deletion you should not
     // implement this function and should remove it completely.
