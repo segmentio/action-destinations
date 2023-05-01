30a32e2442b53a991a18779f61e449a5583613a2
rvadera12 <89420099+rvadera12@users.noreply.github.com>
2023-05-01 10:15:48 -0700

    Revert Event Name Description

--- packages/destination-actions/src/destinations/pinterest-conversions/reportConversionEvent/index.ts
+++ packages/destination-actions/src/destinations/pinterest-conversions/reportConversionEvent/index.ts
@@ -15,7 +15,7 @@ const action: ActionDefinition<Settings, Payload> = {
     event_name: {
       label: 'Event Name',
       description:
-        'Report events directly Pinterest. Data shared can power Pinterest solutions that will help evaluate ads effectiveness and improve content, targeting, and placement of future ads.',
+        'The conversion event type. For custom events, you must use the predefined event name "custom". Please refer to the possible event types in [Pinterest API docs](https://developers.pinterest.com/docs/api/v5/#operation/events/create).',
       type: 'string',
       required: true,
       choices: [
