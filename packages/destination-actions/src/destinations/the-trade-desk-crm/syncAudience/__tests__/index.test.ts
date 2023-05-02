4c67c50d955b0c5dc32e3b89609942d2ae8c006a
Maryam Sharif <maryam.sharif@segment.com>
2023-05-02 16:27:57 -0700

    Add test case

--- packages/destination-actions/src/destinations/the-trade-desk-crm/syncAudience/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/the-trade-desk-crm/syncAudience/__tests__/index.test.ts
@@ -1,9 +1,52 @@
-// import nock from 'nock'
-// import { createTestEvent, createTestIntegration } from '@segment/actions-core'
-// import Destination from '../../index'
+import nock from 'nock'
+import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import Destination from '../../index'
 
-// const testDestination = createTestIntegration(Destination)
+const testDestination = createTestIntegration(Destination)
 
-// describe('TheTradeDeskCrm.syncAudience', () => {
-//   // TODO: Test your action
-// })
+const event = createTestEvent({
+  event: 'Audience Entered',
+  type: 'track',
+  properties: {
+    audience_key: 'personas_test_audience'
+  },
+  context: {
+    device: {
+      advertisingId: '123'
+    },
+    traits: {
+      email: 'testing@testing.com'
+    }
+  }
+})
+
+describe('TheTradeDeskCrm.syncAudience', () => {
+  it('should succeed if an exisitng CRM Segment is found', async () => {
+    // nock get CRMs
+    nock(`https://api.thetradedesk.com/v3/crmdata/segment/advertiser_id`)
+      .get(/.*/)
+      .reply(200, { Segments: [{ SegmentName: 'test_audience', CrmDataId: '123' }] })
+
+    nock(`https://api.thetradedesk.com/v3/crmdata/segment/advertiser_id/123`)
+      .post(/.*/, { PiiType: 'Email', MergeMode: 'Replace' })
+      .reply(200, { Url: 'https://api.thetradedesk.com/drop' })
+
+    nock(`https://api.thetradedesk.com/drop`).put(/.*/).reply(200)
+
+    await expect(
+      testDestination.testAction('syncAudience', {
+        event,
+        settings: {
+          advertiser_id: 'advertiser_id',
+          auth_token: 'test_token'
+        },
+        useDefaultMappings: true,
+        mapping: {
+          name: 'test_audience',
+          region: 'US',
+          pii_type: 'Email'
+        }
+      })
+    ).resolves.not.toThrowError()
+  })
+})
