53da959db82a2f6133f6e73671d517092eac7b0b
Nick Aguilar <nicholas.aguilar@segment.com>
2023-05-08 14:59:53 -0700

    Initial scaffold commit; includes endpoints for create and update audiences

---
+++ packages/destination-actions/src/destinations/facebook-custom-audiences/syncRetl/index.ts
@@ -0,0 +1,18 @@
+import type { ActionDefinition } from '@segment/actions-core'
+import type { Settings } from '../generated-types'
+import type { Payload } from './generated-types'
+
+const action: ActionDefinition<Settings, Payload> = {
+  title: 'Sync Reverse ETL',
+  description: '',
+  fields: {},
+  perform: (request, data) => {
+    // Make your partner api request here!
+    // return request('https://example.com', {
+    //   method: 'post',
+    //   json: data.payload
+    // })
+  }
+}
+
+export default action
