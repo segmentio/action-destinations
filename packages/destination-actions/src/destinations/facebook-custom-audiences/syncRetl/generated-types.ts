18a1ed1068d441fd11128f962bd42094c69f2bcc
Nick Aguilar <nicholas.aguilar@segment.com>
2023-05-08 16:31:06 -0700

    Basic action fields for testing; trigger create/update audience on perform

--- packages/destination-actions/src/destinations/facebook-custom-audiences/syncRetl/generated-types.ts
+++ packages/destination-actions/src/destinations/facebook-custom-audiences/syncRetl/generated-types.ts
@@ -1,3 +1,36 @@
 // Generated file. DO NOT MODIFY IT BY HAND.
 
-export interface Payload {}
+export interface Payload {
+  /**
+   * The method to use
+   */
+  method: string
+  /**
+   * The account ID to use
+   */
+  accountId: string
+  /**
+   * The access token to use
+   */
+  accessToken: string
+  /**
+   * The name to use when creating
+   */
+  createName?: string
+  /**
+   * The description to use when creating
+   */
+  createDescription?: string
+  /**
+   * The ID to use when updating
+   */
+  updateId?: string
+  /**
+   * The schema to use when updating
+   */
+  updateSchema?: string
+  /**
+   * The email to use when updating
+   */
+  email?: string
+}
