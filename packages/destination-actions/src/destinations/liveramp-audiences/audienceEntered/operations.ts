b26c408f2b3c8871c7f41078a2b86f9b53a24377
Robert Hall <rhall@twilio.com>
2023-05-10 12:01:03 -0500

    add support for arrays

--- packages/destination-actions/src/destinations/liveramp-audiences/audienceEntered/operations.ts
+++ packages/destination-actions/src/destinations/liveramp-audiences/audienceEntered/operations.ts
@@ -3,7 +3,7 @@ import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 import { uploadS3, validateS3 } from '../s3'
 
-async function processData(request: RequestClient, settings: Settings, payload: Payload) {
+async function processData(request: RequestClient, settings: Settings, payloads: Payload[]) {
   // TODO: Add support for SFTP
   if (settings.upload_mode == 'S3') {
     validateS3(settings)
@@ -15,12 +15,12 @@ async function processData(request: RequestClient, settings: Settings, payload: 
   // row format: liveramp_audience_key[1],identifier_data[0..n]
   const headers = ['audience_key']
   const rows = []
-  if (payload.identifier_data) {
-    for (const identifier of Object.getOwnPropertyNames(payload.identifier_data)) {
+  if (payloads[0].identifier_data) {
+    for (const identifier of Object.getOwnPropertyNames(payloads[0].identifier_data)) {
       headers.push(identifier)
     }
   }
-  rows.push(headers.join(payload.delimiter))
+  rows.push(headers.join(payloads[0].delimiter))
 
   // Surround all values with quotation marks
   // https://docs.liveramp.com/connect/en/formatting-file-data.html#idm45998667347936
@@ -30,19 +30,21 @@ async function processData(request: RequestClient, settings: Settings, payload: 
     return `"${value.replace('"', '""')}"`
   }
 
-  const row = []
-  row.push(payload.audience_key)
-  if (payload.identifier_data) {
-    for (const identifier of Object.getOwnPropertyNames(payload.identifier_data)) {
-      row.push(payload.identifier_data[identifier] as string)
+  for (const payload of payloads) {
+    const row = []
+    row.push(payload.audience_key)
+    if (payload.identifier_data) {
+      for (const identifier of Object.getOwnPropertyNames(payload.identifier_data)) {
+        row.push(payload.identifier_data[identifier] as string)
+      }
     }
+    rows.push(row.map(enquoteValue).join(payload.delimiter))
   }
-  rows.push(row.map(enquoteValue).join(payload.delimiter))
   const fileContent = rows.join('\n')
 
   // TODO: verify multiple emails are handled
 
-  const filename = `${payload.audience_name}_PII_${payload.received_at}.csv`
+  const filename = `${payloads[0].audience_name}_PII_${payloads[0].received_at}.csv`
 
   if (settings.upload_mode == 'S3') {
     return await uploadS3(settings, filename, fileContent, request)
