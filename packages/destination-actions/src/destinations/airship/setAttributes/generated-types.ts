cd05b0d04538e397c7f51fe44f5db8b67cef40b4
John Greene <jumpinggrendel@gmail.com>
2023-05-15 16:07:49 -0700

    use attribute presets

--- packages/destination-actions/src/destinations/airship/setAttributes/generated-types.ts
+++ packages/destination-actions/src/destinations/airship/setAttributes/generated-types.ts
@@ -10,9 +10,101 @@ export interface Payload {
    */
   occurred: string | number
   /**
-   * User Identify Traits
+   * User Attributes. Attributes should exist in Airship in order to be set, including the predifined ones defaulted here.
    */
   attributes?: {
+    /**
+     * Title (Mr, Mrs, Professor, ...
+     */
+    title?: string
+    /**
+     * The user's first name.
+     */
+    first_name?: string
+    /**
+     * The user's last name.
+     */
+    last_name?: string
+    /**
+     * The user's full name.
+     */
+    full_name?: string
+    /**
+     * The user's gender.
+     */
+    gender?: string
+    /**
+     * The user's zipcode.
+     */
+    zipcode?: number
+    /**
+     * The user's city.
+     */
+    city?: string
+    /**
+     * The user's region.
+     */
+    region?: string
+    /**
+     * The user's country.
+     */
+    country?: string
+    /**
+     * The user's birthdate.
+     */
+    birthdate?: string | number
+    /**
+     * The user's age in years.
+     */
+    age?: number
+    /**
+     * The user's mobile phone number.
+     */
+    mobile_phone?: number
+    /**
+     * The user's home phone number.
+     */
+    home_phone?: number
+    /**
+     * The user's work phone number.
+     */
+    work_phone?: number
+    /**
+     * The user's loyalty tier.
+     */
+    loyalty_tier?: string
+    /**
+     * The user's company name.
+     */
+    company?: string
+    /**
+     * The user's username.
+     */
+    username?: string
+    /**
+     * The user's account creation date.
+     */
+    account_creation?: string | number
+    /**
+     * The user's email address.
+     */
+    email?: string
+    /**
+     * The user's altitude.
+     */
+    altitude?: number
+    /**
+     * The user's latitude.
+     */
+    latitude?: number
+    /**
+     * The user's longitude.
+     */
+    longitude?: number
+    /**
+     * The user's advertising ID.
+     */
+    advertising_id?: string
     [k: string]: unknown
   }
 }
--- packages/destination-actions/src/destinations/airship/setAttributes/index.ts
+++ packages/destination-actions/src/destinations/airship/setAttributes/index.ts
@@ -28,10 +28,152 @@ const action: ActionDefinition<Settings, Payload> = {
     },
     attributes: {
       label: 'Attributes',
-      description: 'User Identify Traits',
+      description:
+        'User Attributes. Attributes should exist in Airship in order to be set, including the predifined ones defaulted here.',
       type: 'object',
+      additionalProperties: true,
+      defaultObjectUI: 'keyvalue',
+      properties: {
+        title: {
+          label: 'Title',
+          type: 'string',
+          description: 'Title (Mr, Mrs, Professor, ...'
+        },
+        first_name: {
+          label: 'First Name',
+          type: 'string',
+          description: "The user's first name."
+        },
+        last_name: {
+          label: 'Last Name',
+          type: 'string',
+          description: "The user's last name."
+        },
+        full_name: {
+          label: 'Full Name',
+          type: 'string',
+          description: "The user's full name."
+        },
+        gender: {
+          label: 'Gender',
+          type: 'string',
+          description: "The user's gender."
+        },
+        zipcode: {
+          label: 'Zipcode',
+          type: 'integer',
+          description: "The user's zipcode."
+        },
+        city: {
+          label: 'City',
+          type: 'string',
+          description: "The user's city."
+        },
+        region: {
+          label: 'Region',
+          type: 'string',
+          description: "The user's region."
+        },
+        country: {
+          label: 'Country',
+          type: 'string',
+          description: "The user's country."
+        },
+        birthdate: {
+          label: 'Birthdate',
+          type: 'datetime',
+          description: "The user's birthdate."
+        },
+        age: {
+          label: 'Age',
+          type: 'integer',
+          description: "The user's age in years."
+        },
+        mobile_phone: {
+          label: 'Mobile Phone Number',
+          type: 'integer',
+          description: "The user's mobile phone number."
+        },
+        home_phone: {
+          label: 'Home Phone Number',
+          type: 'integer',
+          description: "The user's home phone number."
+        },
+        work_phone: {
+          label: 'Work Phone Number',
+          type: 'integer',
+          description: "The user's work phone number."
+        },
+        loyalty_tier: {
+          label: 'Loyalty Tier',
+          type: 'string',
+          description: "The user's loyalty tier."
+        },
+        company: {
+          label: 'Company Name',
+          type: 'string',
+          description: "The user's company name."
+        },
+        username: {
+          label: 'Username',
+          type: 'string',
+          description: "The user's username."
+        },
+        account_creation: {
+          label: 'Account Creation Date',
+          type: 'datetime',
+          description: "The user's account creation date."
+        },
+        email: {
+          label: 'Email Address',
+          type: 'string',
+          description: "The user's email address."
+        },
+        altitude: {
+          label: 'Altitude',
+          type: 'integer',
+          description: "The user's altitude."
+        },
+        latitude: {
+          label: 'Latitude',
+          type: 'number',
+          description: "The user's latitude."
+        },
+        longitude: {
+          label: 'Longitude',
+          type: 'number',
+          description: "The user's longitude."
+        },
+        advertising_id: {
+          label: 'Advertising ID (IDFA/AAID)',
+          type: 'string',
+          description: "The user's advertising ID."
+        }
+      },
       default: {
-        '@path': '$.traits'
+        title: { '@path': '$.traits.title' },
+        first_name: { '@path': '$.traits.first_name' },
+        last_name: { '@path': '$.traits.last_name' },
+        full_name: { '@path': '$.traits.full_name' },
+        gender: { '@path': '$.traits.gender' },
+        zipcode: { '@path': '$.traits.zipcode' },
+        city: { '@path': '$.traits.city' },
+        region: { '@path': '$.traits.region' },
+        country: { '@path': '$.traits.country' },
+        birthdate: { '@path': '$.traits.birthdate' },
+        age: { '@path': '$.traits.age' },
+        mobile_phone: { '@path': '$.traits.mobile_phone' },
+        home_phone: { '@path': '$.traits.home_phone' },
+        work_phone: { '@path': '$.traits.work_phone' },
+        loyalty_tier: { '@path': '$.traits.loyalty_tier' },
+        company: { '@path': '$.traits.company' },
+        username: { '@path': '$.traits.username' },
+        account_creation: { '@path': '$.traits.account_creation' },
+        email: { '@path': '$.traits.email' },
+        altitude: { '@path': '$.traits.altitude' },
+        latitude: { '@path': '$.traits.latitude' },
+        longitude: { '@path': '$.traits.longitude' },
+        advertising_id: { '@path': '$.traits.advertising_id' }
       }
     }
   },
