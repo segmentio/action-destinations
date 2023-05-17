c122ac2894050542bde07972767a8d46e138d4e7
Innovative-GauravKochar <117165746+Innovative-GauravKochar@users.noreply.github.com>
2023-05-17 21:43:18 +0530

    StratConn-2388 | Removed token from settings and logic to get engage_space dynamically for segment-profile  (#1268)
    * Removing Dynamic Fields from segment Profiles for engage_space as we are getting it in gateway-api using control-plane service now.
    
    * updated label
    
    * removed dynamicFields engage_space
    
    * updated label
    
    ---------
    
    Co-authored-by: Gaurav Kochar <gaurav.kochar@segment.com>

--- packages/destination-actions/src/destinations/segment-profiles/generated-types.ts
+++ packages/destination-actions/src/destinations/segment-profiles/generated-types.ts
@@ -2,10 +2,6 @@
 
 export interface Settings {
   /**
-   * The Segment Public API requires that you have an authentication token before you send requests. [This document](https://docs.segmentapis.com/tag/Getting-Started#section/Get-an-API-token) explains how to setup a token.
-   */
-  segment_papi_token: string
-  /**
    * The region to send your data.
    */
   endpoint: string
