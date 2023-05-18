1c5b8662e661ec46a2f449ac5143a975491dbca8
Sayan Das <sayan.das@segment.com>
2023-05-19 00:10:24 +0530

    Minor Fixes

--- packages/destination-actions/src/destinations/segment-profiles/properties.ts
+++ packages/destination-actions/src/destinations/segment-profiles/properties.ts
@@ -2,31 +2,28 @@ interface SegmentEndpoint {
   label: string
   url: string
   cdn: string
-  papi: string
 }
-const SEG_ENDPOINT: { [key: string]: SegmentEndpoint } = {
+
+export const SEGMENT_ENDPOINTS: { [key: string]: SegmentEndpoint } = {
   north_america: {
     label: 'North America',
     url: 'https://api.segment.io/v1',
-    cdn: 'https://cdn.segment.com/v1',
-    papi: 'https://api.segmentapis.com'
+    cdn: 'https://cdn.segment.com/v1'
   },
   europe: {
     label: 'Europe',
     url: 'https://events.eu1.segmentapis.com/v1',
-    cdn: 'https://cdn.segment.com/v1',
-    papi: 'https://eu1.api.segmentapis.com'
+    cdn: 'https://cdn.segment.com/v1'
   }
+  // Special scenario: The following lines enables Staging configuration in Settings
+  // and should be un-commented while running a push in Production environment
+  // stage: {
+  //   label: 'Staging',
+  //   url: 'https://api.segment.build/v1',
+  //   cdn: 'https://cdn.segment.build/v1'
+  // }
 }
-if (process.env.NODE_ENV != 'production') {
-  SEG_ENDPOINT.stage = {
-    label: 'Staging',
-    url: 'https://api.segment.build/v1',
-    cdn: 'https://cdn.segment.build/v1',
-    papi: 'https://api.segmentapis.build'
-  }
-}
-export const SEGMENT_ENDPOINTS = SEG_ENDPOINT
+
 export const DEFAULT_SEGMENT_ENDPOINT = 'north_america'
 
 export const PAGINATION_COUNT = 10
