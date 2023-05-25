67f94ebf1ac5670ef8a2a908ccd1c789d06b750a
ThabetIbrahim <thabet.ig@gmail.com>
2023-05-25 16:04:46 +0300

    Update packages/browser-destinations/src/destinations/userpilot/identifyUser/index.ts
    Co-authored-by: Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>

--- packages/browser-destinations/src/destinations/userpilot/identifyUser/index.ts
+++ packages/browser-destinations/src/destinations/userpilot/identifyUser/index.ts
@@ -6,7 +6,7 @@ import type { Userpilot } from '../types'
 const action: BrowserActionDefinition<Settings, Userpilot, Payload> = {
   title: 'Identify User',
   description:
-    "Create or update a user entity in Userpilot; it's mandatory to identify a user prior to invoking other methods such as track, page, or group. You can learn more by visiting the [Userpilot documentation](https://docs.userpilot.com/article/23-identify-users-track-custom-events).",
+    "Create or update a user entity in Userpilot; it's mandatory to identify a user by calling identify() prior to invoking other methods such as track(), page(), or group(). You can learn more by visiting the [Userpilot documentation](https://docs.userpilot.com/article/23-identify-users-track-custom-events).",
   platform: 'web',
   fields: {
     userId: {
