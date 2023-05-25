a93698e4d9f136d4633db4f590c7f73e3851ab05
ThabetIbrahim <thabet.ig@gmail.com>
2023-05-25 16:04:26 +0300

    Update packages/browser-destinations/src/destinations/userpilot/trackEvent/index.ts
    Co-authored-by: Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>

--- packages/browser-destinations/src/destinations/userpilot/trackEvent/index.ts
+++ packages/browser-destinations/src/destinations/userpilot/trackEvent/index.ts
@@ -6,7 +6,7 @@ import type { Userpilot } from '../types'
 const action: BrowserActionDefinition<Settings, Userpilot, Payload> = {
   title: 'Track Event',
   description:
-    'Send an event to Userpilot. You can learn more by visiting the [Userpilot documentation](https://docs.userpilot.com/article/23-identify-users-track-custom-events).',
+    'Send an event to Userpilot. it's mandatory to identify a user by calling identify() prior to invoking other methods such as track(). You can learn more by visiting the [Userpilot documentation](https://docs.userpilot.com/article/23-identify-users-track-custom-events).',
   platform: 'web',
   fields: {
     name: {
