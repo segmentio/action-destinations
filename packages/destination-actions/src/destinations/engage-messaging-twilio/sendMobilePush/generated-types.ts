5825a1f0937597c810c720149653b033ee2322f3
alfrimpong <afrimpong@twilio.com>
2023-05-25 09:28:23 -0500

    deploy

--- packages/destination-actions/src/destinations/engage-messaging-twilio/sendMobilePush/generated-types.ts
+++ packages/destination-actions/src/destinations/engage-messaging-twilio/sendMobilePush/generated-types.ts
@@ -9,6 +9,9 @@ export interface Payload {
    * The Push Service Sid to send the push notification from.
    */
   from: string
+  /**
+   * Customizations for notification deliverability
+   */
   customizations?: {
     /**
      * The title to be displayed for your notification
