a1c26f665ac5118aff02d9f1336d8074f93dcdba
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2023-06-07 15:18:18 +0100

    updating tests

--- packages/destination-actions/src/destinations/tiktok-offline-conversions/index.ts
+++ packages/destination-actions/src/destinations/tiktok-offline-conversions/index.ts
@@ -4,7 +4,7 @@ import trackPaymentOfflineConversion from './trackPaymentOfflineConversion'
 import trackNonPaymentOfflineConversion from './trackNonPaymentOfflineConversion'
 
 const destination: DestinationDefinition<Settings> = {
-  name: 'Tiktok Offline Conversions (Actions)',
+  name: 'Tiktok Offline Conversions',
   slug: 'actions-tiktok-offline-conversions',
   mode: 'cloud',
 
