2c74545d433c2a5872e98d8f86dc6b660d45e9e3
jon-listrak <134103521+jon-listrak@users.noreply.github.com>
2023-07-06 20:31:02 +0000

    started working on test for profile field updates.

--- packages/destination-actions/src/destinations/listrak/updateContactProfileFields/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/listrak/updateContactProfileFields/__tests__/index.test.ts
@@ -5,5 +5,60 @@ import Destination from '../../index'
 const testDestination = createTestIntegration(Destination)
 
 describe('Listrak.updateContactProfileFields', () => {
+  afterEach(function () {
+    if (!nock.isDone()) {
+      logger.error(new Error('Not all nock interceptors were used!'))
+      nock.cleanAll()
+    }
+  })
+
   // TODO: Test your action
+  it('No Auth Token updates contact profile fields', async () => {
+    nock('https://auth.listrak.com')
+      .post('/OAuth2/Token', 'client_id=clientId1&client_secret=clientSecret1&grant_type=client_credentials')
+      .matchHeader('Content-Type', 'application/x-www-form-urlencoded')
+      .reply(200, {
+        access_token: 'token',
+        token_type: 'Bearer',
+        expires_in: 900
+      })
+
+    nock('https://api.listrak.com/email/v1')
+      .post('/List/123/Contact/SegmentationField', [
+        {
+          emailAddress: 'test.email@test.com',
+          segmentationFieldValues: [
+            {
+              segmentationFieldId: null,
+              value: null
+            }
+          ]
+        }
+      ])
+      .matchHeader('Content-Type', 'application/json')
+      .matchHeader('Authorization', `Bearer token`)
+      .reply(201, {
+        status: 201,
+        resourceId: ''
+      })
+
+    const settings = {
+      client_id: 'clientId1',
+      client_secret: 'clientSecret1'
+    }
+
+    const event = createTestEvent({
+      traits: {
+        email: 'test.email@test.com'
+      }
+    })
+
+    await expect(
+      testDestination.testAction('updateContactProfileFields', {
+        event,
+        settings,
+        useDefaultMappings: true
+      })
+    ).resolves.not.toThrowError()
+  })
 })
