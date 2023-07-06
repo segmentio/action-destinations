7abe529e54b1e88c0485784e42ff9eb965a97dbe
gary-hopkins-listrak <133998371+gary-hopkins-listrak@users.noreply.github.com>
2023-07-06 19:39:45 +0000

    cleanup test file

--- packages/destination-actions/src/destinations/listrak/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/listrak/__tests__/index.test.ts
@@ -4,73 +4,55 @@ import Definition from '../index'
 
 const testDestination = createTestIntegration(Definition)
 
-describe('Listrak', () => {
-  describe('testAuthentication', () => {
-    it('should pass client id and secret to auth endpoint and verify access token received', async () => {
-      nock('https://auth.listrak.com')
-        .post('/OAuth2/Token', 'client_id=clientId1&client_secret=clientSecret1&grant_type=client_credentials')
-        .matchHeader("Content-Type", "application/x-www-form-urlencoded")
-        .reply(200, {
-          "access_token": "token",
-          "token_type": "Bearer",
-          "expires_in": 900
-        });
+describe('testAuthentication', () => {
+  it('should pass client id and secret to auth endpoint and verify access token received', async () => {
+    nock('https://auth.listrak.com')
+      .post('/OAuth2/Token', 'client_id=clientId1&client_secret=clientSecret1&grant_type=client_credentials')
+      .matchHeader("Content-Type", "application/x-www-form-urlencoded")
+      .reply(200, {
+        "access_token": "token",
+        "token_type": "Bearer",
+        "expires_in": 900
+      });
 
-        
-      await expect(testDestination.testAuthentication({
-        client_id: "clientId1",
-        client_secret: "clientSecret1"
-      })).resolves.not.toThrowError();
-    })
+      
+    await expect(testDestination.testAuthentication({
+      client_id: "clientId1",
+      client_secret: "clientSecret1"
+    })).resolves.not.toThrowError();
   })
-
-  const testCases: any[] = [ 
-      {
-        name: 'empty response body',
-        body: undefined,
-        status_code: 200
-      },
-      {
-        name: 'no token returned',
-        body: {
-          access_token: ""
-        },
-        status_code: 200
-      },
-      {
-        name: 'non 200 status code',
-        body: { message: "Unexpected error" },
-        status_code: 500
-      }
-    ];
-  testCases.forEach((element: any) => {
-    it(`Should throw exception if ${element.name}`, async () => {
-      nock('https://auth.listrak.com')
-        .post('/OAuth2/Token', 'client_id=clientId1&client_secret=clientSecret1&grant_type=client_credentials')
-        .matchHeader("Content-Type", "application/x-www-form-urlencoded")
-        .reply(element.status_code, element.body);
-
-        
-      await expect(testDestination.testAuthentication({
-        client_id: "clientId1",
-        client_secret: "clientSecret1"
-      })).rejects.toThrowError();
-    })  
-  });
-
 })
 
-/*
-const profile = {
-  name: 'John',
-  age: 25
-};
+const testCases: any[] = [ 
+    {
+      name: 'empty response body',
+      body: undefined,
+      status_code: 200
+    },
+    {
+      name: 'no token returned',
+      body: {
+        access_token: ""
+      },
+      status_code: 200
+    },
+    {
+      name: 'non 200 status code',
+      body: { message: "Unexpected error" },
+      status_code: 500
+    }
+  ];
+testCases.forEach((element: any) => {
+  it(`Should throw exception if ${element.name}`, async () => {
+    nock('https://auth.listrak.com')
+      .post('/OAuth2/Token', 'client_id=clientId1&client_secret=clientSecret1&grant_type=client_credentials')
+      .matchHeader("Content-Type", "application/x-www-form-urlencoded")
+      .reply(element.status_code, element.body);
 
-const scope = nock('https://mydomainname.local')
-  .post('/api/send-profile', profile)
-  .reply(200, {status:200});
-
-request.post('https://mydomainname.local/api/send-profile', {json: {name: 'John', age: 25}}).on('response', function(request) {
-  console.log(request.statusCode); // 200
+      
+    await expect(testDestination.testAuthentication({
+      client_id: "clientId1",
+      client_secret: "clientSecret1"
+    })).rejects.toThrowError();
+  })  
 });
-*/
