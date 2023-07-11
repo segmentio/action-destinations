1b185cb6112f4850dbb9a5a49ebbd669e0bccabe
jon-listrak <134103521+jon-listrak@users.noreply.github.com>
2023-07-11 17:10:38 +0000

    Use single quotes

--- packages/destination-actions/src/destinations/listrak/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/listrak/__tests__/index.test.ts
@@ -6,7 +6,6 @@ import { clearToken } from '../listrak'
 const testDestination = createTestIntegration(Definition)
 
 describe('testAuthentication', () => {
-
   beforeEach(() => {
     clearToken()
     nock.cleanAll()
@@ -15,51 +14,53 @@ describe('testAuthentication', () => {
   it('Should pass client id and secret to auth endpoint and verify access token received', async () => {
     nock('https://auth.listrak.com')
       .post('/OAuth2/Token', 'client_id=clientId1&client_secret=clientSecret1&grant_type=client_credentials')
-      .matchHeader("Content-Type", "application/x-www-form-urlencoded")
+      .matchHeader('Content-Type', 'application/x-www-form-urlencoded')
       .reply(200, {
-        "access_token": "token",
-        "token_type": "Bearer",
-        "expires_in": 900
-      });
+        access_token: 'token',
+        token_type: 'Bearer',
+        expires_in: 900
+      })
 
-      
-    await expect(testDestination.testAuthentication({
-      client_id: "clientId1",
-      client_secret: "clientSecret1"
-    })).resolves.not.toThrowError();
+    await expect(
+      testDestination.testAuthentication({
+        client_id: 'clientId1',
+        client_secret: 'clientSecret1'
+      })
+    ).resolves.not.toThrowError()
   })
 
-  const testCases: any[] = [ 
-      {
-        name: 'empty response body',
-        body: undefined,
-        status_code: 200
+  const testCases: any[] = [
+    {
+      name: 'empty response body',
+      body: undefined,
+      status_code: 200
+    },
+    {
+      name: 'no token returned',
+      body: {
+        access_token: ''
       },
-      {
-        name: 'no token returned',
-        body: {
-          access_token: ""
-        },
-        status_code: 200
-      },
-      {
-        name: 'non 200 status code',
-        body: { message: "Unexpected error" },
-        status_code: 500
-      }
-    ];
+      status_code: 200
+    },
+    {
+      name: 'non 200 status code',
+      body: { message: 'Unexpected error' },
+      status_code: 500
+    }
+  ]
   testCases.forEach((element: any) => {
     it(`Should throw exception if ${element.name}`, async () => {
       nock('https://auth.listrak.com')
         .post('/OAuth2/Token', 'client_id=clientId1&client_secret=clientSecret1&grant_type=client_credentials')
-        .matchHeader("Content-Type", "application/x-www-form-urlencoded")
-        .reply(element.status_code, element.body);
+        .matchHeader('Content-Type', 'application/x-www-form-urlencoded')
+        .reply(element.status_code, element.body)
 
-        
-      await expect(testDestination.testAuthentication({
-        client_id: "clientId1",
-        client_secret: "clientSecret1"
-      })).rejects.toThrowError();
-    })  
-  });
+      await expect(
+        testDestination.testAuthentication({
+          client_id: 'clientId1',
+          client_secret: 'clientSecret1'
+        })
+      ).rejects.toThrowError()
+    })
+  })
 })
