5cb89d37dc1e87aa1fbc482999dc538891231680
Philipp Munin <pmunin@twilio.com>
2023-07-18 23:25:49 -0400

    moving operationTracking to parent level to share it between sendgrid and twilio

--- packages/destination-actions/src/destinations/engage/twilio/utils/IntegrationErrorWrapper.ts
+++ packages/destination-actions/src/destinations/engage/twilio/utils/IntegrationErrorWrapper.ts
@@ -1,5 +1,5 @@
 import { IntegrationError } from '@segment/actions-core'
-import { OperationErrorHandler, TryCatchFinallyContext, TryCatchFinallyHook } from '../operationTracking'
+import { OperationErrorHandler, TryCatchFinallyContext, TryCatchFinallyHook } from '../../operationTracking'
 import { TwilioApiError } from './types'
 
 export type IntegrationErrorWrapperContext<TContext extends TryCatchFinallyContext = TryCatchFinallyContext> =
--- packages/destination-actions/src/destinations/engage/twilio/utils/track.ts
+++ packages/destination-actions/src/destinations/engage/twilio/utils/track.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ContextFromDecorator, OperationDecorator } from '../operationTracking'
+import { ContextFromDecorator, OperationDecorator } from '../../operationTracking'
 import { MessageStats } from './MessageStats'
 import { MessageLogger } from './MessageLogger'
 import { IntegrationErrorWrapper } from './IntegrationErrorWrapper'
