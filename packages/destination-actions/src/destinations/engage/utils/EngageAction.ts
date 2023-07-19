ce1e22c5b97dafb69931449c3fa66dae446e3785
Philipp Munin <pmunin@twilio.com>
2023-07-19 10:23:23 -0400

    moving engage/operationTracking under engage/utils/

---
+++ packages/destination-actions/src/destinations/engage/utils/EngageAction.ts
@@ -0,0 +1,16 @@
+import { RequestClient } from '@segment/actions-core/create-request-client'
+import { ExecuteInput } from '@segment/actions-core/destination-kit'
+import { MaybePromise } from '@segment/actions-core/destination-kittypes'
+
+export abstract class EngageAction<TSettings, TPayload, TReturn = any> {
+  constructor(
+    protected readonly requestClient: RequestClient,
+    protected readonly executeInput: ExecuteInput<TSettings, TPayload>
+  ) {}
+
+  perform() {
+    return this.doPerform()
+  }
+
+  abstract doPerform(): MaybePromise<TReturn>
+}
---
+++ packages/destination-actions/src/destinations/engage/utils/index.ts
@@ -0,0 +1,2 @@
+export * from './EngageAction'
+export * from './operationTracking'
