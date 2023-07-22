12200296ac577ccf7c6a30bc0c42b3cf146b306b
Philipp Munin <pmunin@twilio.com>
2023-07-21 21:57:57 -0400

    fixing response error type

--- packages/destination-actions/src/destinations/engage/utils/ResponseError.ts
+++ packages/destination-actions/src/destinations/engage/utils/ResponseError.ts
@@ -1,22 +1,21 @@
 export interface ResponseError extends Error {
   response: {
     data: {
-      code: number
+      code: string
       message: string
       more_info: string
       status: number
     }
     headers?: Response['headers']
   }
-  code?: number
+  code?: string
   status?: number
-  statusCode?: number
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getErrorStatusCode(error: any) {
   const respError = error as ResponseError
-  const status = respError?.response?.data?.status || respError.status || respError?.statusCode
+  const status = respError?.response?.data?.status || respError.status
   const code = respError?.response?.data?.code || respError.code
   return { status, code }
 }
