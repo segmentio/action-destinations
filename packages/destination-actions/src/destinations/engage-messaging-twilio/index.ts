c955019eefd1949accc5898938c646d8cc541be7
Philipp Munin <pmunin@twilio.com>
2023-07-24 10:41:23 -0400

    adding proxy modules for sendgrid and twilio under old paths that are registered in integrations

---
+++ packages/destination-actions/src/destinations/engage-messaging-twilio/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Since this is path is also registered in integrations: https://github.com/segmentio/integrations/blob/7779ada5bff3ab27212f02b8dca6d169552c9aca/integrations/index.js#L256
+ * I have to use this proxy module under this path to avoid the following error happening in runtime error after deployment:
+ * RetryableError: Could not load version 3.187.1-CHANNELS-657.1 of actions-personas-messaging-twilio, retrying
+    at ActionVersionManager.importAndRegisterDestination (/usr/src/integrations/createActionDestination/actionVersionManager.js:177:19)
+    at ActionVersionManager.retrieveIntegration (/usr/src/integrations/createActionDestination/actionVersionManager.js:130:18)
+
+ */
+export * from '../engage/twilio'
