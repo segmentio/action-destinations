4cb9d57eef822caf0c31553b1a40b725c420e769
Peter DeMartini <peter.demartini@segment.com>
2023-07-24 10:33:04 -0700

    add some performance improvements to generateFile

--- packages/destination-actions/src/destinations/liveramp-audiences/operations.ts
+++ packages/destination-actions/src/destinations/liveramp-audiences/operations.ts
@@ -6,8 +6,7 @@ Generates the LiveRamp ingestion file. Expected format:
 liveramp_audience_key[1],identifier_data[0..n]
 */
 function generateFile(payloads: Payload[]) {
-  const rows = []
-  const headers = ['audience_key']
+  const headers: string[] = ['audience_key']
 
   // Prepare header row
   if (payloads[0].identifier_data) {
@@ -21,31 +20,35 @@ function generateFile(payloads: Payload[]) {
       headers.push(identifier)
     }
   }
-  rows.push(headers.join(payloads[0].delimiter))
 
+  let rows = Buffer.from(headers.join(payloads[0].delimiter) + '\n')
+
+  const hashOrCache = createHashOrCache()
   // Prepare data rows
-  for (const payload of payloads) {
-    const row = []
-    row.push(payload.audience_key)
+  for (let i = 0; i < payloads.length; i++) {
+    const payload = payloads[i]
+    const row: string[] = [enquoteIdentifier(payload.audience_key)]
     if (payload.identifier_data) {
-      for (const identifier of Object.getOwnPropertyNames(payload.identifier_data)) {
-        row.push(payload.identifier_data[identifier] as string)
+      for (const key in payload.identifier_data) {
+        if (Object.prototype.hasOwnProperty.call(payload.identifier_data, key)) {
+          row.push(enquoteIdentifier(String(payload.identifier_data[key])))
+        }
       }
     }
 
     if (payload.unhashed_identifier_data) {
-      for (const identifier of Object.getOwnPropertyNames(payload.unhashed_identifier_data)) {
-        row.push(hash(payload.unhashed_identifier_data[identifier] as string))
+      for (const key in payload.unhashed_identifier_data) {
+        if (Object.prototype.hasOwnProperty.call(payload.unhashed_identifier_data, key)) {
+          row.push(`"${hashOrCache(String(payload.unhashed_identifier_data[key]))}"`)
+        }
       }
     }
-    rows.push(row.map(enquoteIdentifier).join(payload.delimiter))
+    rows = Buffer.concat([rows, Buffer.from(row.join(payload.delimiter) + (i + 1 === payloads.length) ? '' : '\n')])
   }
 
   // TODO: verify multiple emails are handled
   const filename = payloads[0].filename
-  const fileContent = Buffer.from(rows.join('\n'))
-
-  return { filename, fileContent }
+  return { filename, fileContent: rows }
 }
 
 /*
@@ -57,7 +60,20 @@ function generateFile(payloads: Payload[]) {
   LCD TV,50" -> "LCD TV,50"""
 */
 function enquoteIdentifier(identifier: string) {
-  return `"${identifier.replace(/"/g, '""')}"`
+  return `"${String(identifier).replace(/"/g, '""')}"`
+}
+
+// createHashOrCache will cache the unhashed_identifier_data
+// to reduce CPU time spent generating sha256
+function createHashOrCache() {
+  const cache = new Map<string, string>()
+  return function hashOrCache(value: string): string {
+    if (!cache.has(value)) {
+      cache.set(value, hash(value))
+    }
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return cache.get(value)!
+  }
 }
 
 const hash = (value: string): string => {
