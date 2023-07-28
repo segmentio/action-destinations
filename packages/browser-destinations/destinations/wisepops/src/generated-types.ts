edbe7c11a5ffb21ea31a9faee55c3932957f17be
Harsh Vardhan <harsh.vardhan@segment.com>
2023-07-28 17:00:46 +0530

    Removed unwanted changes

--- packages/browser-destinations/destinations/wisepops/src/generated-types.ts
+++ packages/browser-destinations/destinations/wisepops/src/generated-types.ts
@@ -2,7 +2,7 @@
 
 export interface Settings {
   /**
-   * The identifier of your Wisepops' website. You can find it in [your setup code on Wisepops](https://id.wisepops.com/r/id/workspaces/_workspaceId_/settings/setup-code).
+   * The identifier of your Wisepops' website. You can find it in [your setup code on Wisepops](https://app.wisepops.com/f/settings/websites).
    */
   websiteId: string
 }
