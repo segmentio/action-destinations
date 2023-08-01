a491847cfc6d015d771f6d6301070658f12ca94a
Marín Alcaraz <marin.alcarazcordova@segment.com>
2023-08-01 10:33:17 -0700

    [STRATCONN-2929] Add createAudience to Facebook Custom Audiences (#1398)

--- packages/destination-actions/src/destinations/facebook-custom-audiences/index.ts
+++ packages/destination-actions/src/destinations/facebook-custom-audiences/index.ts
@@ -1,11 +1,14 @@
-import type { DestinationDefinition } from '@segment/actions-core'
-import type { Settings } from './generated-types'
+import type { AudienceDestinationDefinition } from '@segment/actions-core'
+import { IntegrationError } from '@segment/actions-core'
+import type { Settings, AudienceSettings } from './generated-types'
 
-import syncEngage from './syncEngage'
+import add from './add'
 
-import syncRetl from './syncRetl'
+import remove from './remove'
 
-const destination: DestinationDefinition<Settings> = {
+export const FACEBOOK_API_VERSION = 'v17.0'
+
+const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
   name: 'Facebook Custom Audiences (Actions)',
   slug: 'actions-facebook-custom-audiences',
   mode: 'cloud',
@@ -24,9 +27,82 @@ const destination: DestinationDefinition<Settings> = {
       return { accessToken: 'TODO: Implement this' }
     }
   },
+  extendRequest({ auth }) {
+    return {
+      headers: {
+        authorization: `Bearer ${auth?.accessToken}`
+      }
+    }
+  },
+  audienceFields: {
+    adAccountId: {
+      type: 'string',
+      label: 'Advertiser Account ID',
+      description: 'Your advertiser account id. Read [more](https://www.facebook.com/business/help/1492627900875762).',
+      required: true
+    },
+    audienceDescription: {
+      type: 'string',
+      label: 'Description',
+      description: 'A brief description about your audience.',
+      required: true
+    }
+  },
+  audienceConfig: {
+    mode: {
+      type: 'synced',
+      full_audience_sync: false
+    },
+    async createAudience(request, createAudienceInput) {
+      const externalIdKey = 'id'
+
+      const audienceName = createAudienceInput.audienceName
+      const adAccountId = createAudienceInput.audienceSettings?.adAccountId
+      const audienceDescription = createAudienceInput.audienceSettings?.audienceDescription
+
+      if (!audienceName) {
+        throw new IntegrationError('Missing audience name value', 'MISSING_REQUIRED_FIELD', 400)
+      }
+
+      if (!adAccountId) {
+        throw new IntegrationError('Missing ad account ID value', 'MISSING_REQUIRED_FIELD', 400)
+      }
+
+      const createAudienceUrl = `https://graph.facebook.com/${FACEBOOK_API_VERSION}/act_${adAccountId}/customaudiences`
+      const payload = {
+        name: audienceName,
+        description: audienceDescription || '',
+        subtype: 'CUSTOM',
+        customer_file_source: 'BOTH_USER_AND_PARTNER_PROVIDED'
+      }
+
+      const response = await request(createAudienceUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams(payload)
+      })
+
+      const r = await response.json()
+      if (!r[externalIdKey]) {
+        throw new IntegrationError('Invalid response from create audience request', 'INVALID_RESPONSE', 400)
+      }
+
+      return {
+        externalId: r[externalIdKey]
+      }
+    },
+    async getAudience(_request, _getAudienceInput) {
+      // TODO: Implement this method
+      return {
+        externalId: '42'
+      }
+    }
+  },
   actions: {
-    syncEngage,
-    syncRetl
+    add,
+    remove
   }
 }
 
