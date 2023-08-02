fc30bcec664f60e4336c7aa5daa132043db1b0d3
Gaurav Kochar <gaurav.kochar@segment.com>
2023-08-02 19:07:11 +0530

    hashing comment

--- packages/destination-actions/src/destinations/facebook-conversions-api/fb-capi-user-data.ts
+++ packages/destination-actions/src/destinations/facebook-conversions-api/fb-capi-user-data.ts
@@ -245,7 +245,7 @@ export const hash_user_data = (payload: UserData): Object => {
     st: hash(payload.user_data?.state),
     zp: hash(payload.user_data?.zip),
     country: hash(payload.user_data?.country),
-    external_id: hash_and_normalised(payload.user_data?.externalId),
+    external_id: hash_and_normalised(payload.user_data?.externalId), // Hashing this is recommended but not required
     client_ip_address: payload.user_data?.client_ip_address,
     client_user_agent: payload.user_data?.client_user_agent,
     fbc: payload.user_data?.fbc,
