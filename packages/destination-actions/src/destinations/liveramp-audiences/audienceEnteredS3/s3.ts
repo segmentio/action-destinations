ad06b718caa3091156620f7ab56ee9058d12c736
maryamsharif <99763167+maryamsharif@users.noreply.github.com>
2023-08-09 09:54:43 -0700

    [Liveramp] Split into 2 actions (#1448)
    * Split into 2 actions
    
    * bring down the settings to the mappings level
    
    ---------
    
    Co-authored-by: Robert Hall <rhall@twilio.com>

--- packages/destination-actions/src/destinations/liveramp-audiences/audienceEnteredS3/s3.ts
+++ packages/destination-actions/src/destinations/liveramp-audiences/audienceEnteredS3/s3.ts
@@ -1,40 +1,40 @@
 import generateS3RequestOptions from '../../../lib/AWS/s3'
 import { InvalidAuthenticationError, ModifiedResponse, RequestOptions } from '@segment/actions-core'
-import { Settings } from '../generated-types'
+import { Payload } from './generated-types'
 
-function validateS3(settings: Settings) {
-  if (!settings.s3_aws_access_key) {
+function validateS3(payload: Payload) {
+  if (!payload.s3_aws_access_key) {
     throw new InvalidAuthenticationError('Selected S3 upload mode, but missing AWS Access Key')
   }
 
-  if (!settings.s3_aws_secret_key) {
+  if (!payload.s3_aws_secret_key) {
     throw new InvalidAuthenticationError('Selected S3 upload mode, but missing AWS Secret Key')
   }
 
-  if (!settings.s3_aws_bucket_name) {
+  if (!payload.s3_aws_bucket_name) {
     throw new InvalidAuthenticationError('Selected S3 upload mode, but missing AWS S3 bucket name')
   }
 
-  if (!settings.s3_aws_region) {
+  if (!payload.s3_aws_region) {
     throw new InvalidAuthenticationError('Selected S3 upload mode, but missing AWS Region')
   }
 }
 
 async function uploadS3(
-  settings: Settings,
+  payload: Payload,
   filename: string,
   fileContent: Buffer,
   request: <Data = unknown>(url: string, options?: RequestOptions) => Promise<ModifiedResponse<Data>>
 ) {
   const method = 'PUT'
   const opts = await generateS3RequestOptions(
-    settings.s3_aws_bucket_name as string,
-    settings.s3_aws_region as string,
+    payload.s3_aws_bucket_name as string,
+    payload.s3_aws_region as string,
     filename,
     method,
     fileContent,
-    settings.s3_aws_access_key as string,
-    settings.s3_aws_secret_key as string
+    payload.s3_aws_access_key as string,
+    payload.s3_aws_secret_key as string
   )
   if (!opts.headers || !opts.method || !opts.host || !opts.path) {
     throw new InvalidAuthenticationError('Unable to generate signature header for AWS S3 request.')
