7785749cec382600327e8a47286d095ae26bba92
ajaysikri <64368898+ajaysikri@users.noreply.github.com>
2023-08-09 12:06:42 -0400

    Create generated-types.ts

---
+++ packages/browser-destinations/destinations/cdpresolution/src/generated-types.ts
@@ -0,0 +1,12 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Settings {
+  /**
+   * Identity resolution endpoint. [CDP Resolution documentation](https://www.cdpresolution.com/docs/)
+   */
+  endpoint: string
+  /**
+   * Client identifier provided by CDP Resolution [CDP Resultion Account](https://www.cdpresolution.com/accountsettings)
+   */
+  ClientIdentifier: string
+}
