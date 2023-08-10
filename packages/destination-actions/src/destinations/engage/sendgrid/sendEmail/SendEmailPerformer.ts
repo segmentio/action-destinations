a813be314e63ed65ad01e23d2f637dbf3e5a278c
vikramkumar19 <vikram.kumar@segment.com>
2023-08-10 16:46:39 -0500

    Email optout model

--- packages/destination-actions/src/destinations/engage/sendgrid/sendEmail/SendEmailPerformer.ts
+++ packages/destination-actions/src/destinations/engage/sendgrid/sendEmail/SendEmailPerformer.ts
@@ -37,8 +37,16 @@ export class SendEmailPerformer extends MessageSendPerformer<Settings, Payload> 
       this.currentOperation?.logs.push('Bypassing subscription')
       return true
     }
-
-    const isSubscribed = super.isExternalIdSubscribed(extId)
+    const send_opt_out = this.payload.sendBasedOnOptOut !== undefined && this.payload.sendBasedOnOptOut
+    let isSubscribed
+    if (send_opt_out) {
+      this.currentOperation?.logs.push('OptOut subscription')
+      console.log('send_opt_out', send_opt_out)
+      isSubscribed = super.isExternalIdSubscribedOptOutModel(extId)
+    } else {
+      isSubscribed = super.isExternalIdSubscribed(extId)
+    }
+    console.log('isSubscribed', isSubscribed)
     if (!isSubscribed) return isSubscribed // may be undefined => as invalid
 
     if (!this.payload.groupId) return isSubscribed
