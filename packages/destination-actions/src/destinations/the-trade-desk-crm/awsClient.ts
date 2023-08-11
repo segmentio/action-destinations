0548ba89dd0052ba6c923117703124da3b0932bc
Sayan Das <sayan.das@segment.com>
2023-08-11 12:05:21 +0530

    MVP1 Updates

--- packages/destination-actions/src/destinations/the-trade-desk-crm/awsClient.ts
+++ packages/destination-actions/src/destinations/the-trade-desk-crm/awsClient.ts
@@ -1,5 +1,5 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
-import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs'
+import { SQSClient, SendMessageCommand, MessageAttributeValue } from '@aws-sdk/client-sqs'
 import { v4 as uuidv4 } from '@lukeed/uuid'
 import { Readable } from 'stream'
 
@@ -16,13 +16,12 @@ interface SendToAWSRequest {
     RetentionEnabled?: boolean
   }
 }
-interface SQSMessageBody {
-  EventUUID: string
+
+interface TTDEventPayload {
   TDDAuthToken: string
   AdvertiserId: string
-  CrmDataId: string
   SegmentName: string
-  S3FilePath: string
+  CrmDataId: string
   RequeueCount: number
   DropReferenceId?: string
   DropOptions: {
@@ -33,9 +32,21 @@ interface SQSMessageBody {
   }
 }
 
+interface IntegrationsOutboundSQSMessageAttributes {
+  ActionSlug: MessageAttributeValue
+  EventUUID: MessageAttributeValue
+  [key: string]: MessageAttributeValue
+}
+
+interface SQSMessageBody {
+  ActionSlug: string
+  S3FilePath: string
+  DestinationPayload: TTDEventPayload
+}
+
 const NODE_ENV = process.env['NODE_ENV'] || `stage`
 const AWS_REGION = process.env['AWS_REGION'] || `us-west-2`
-const AWS_ACCOUNT_ID = parseARN(process.env['AWS_ROLE_ARN'])
+const AWS_ACCOUNT_ID = parseARN(process.env['AWS_ROLE_ARN']) || `1234567890`
 
 const ACTION_SLUG = `actions-the-trade-desk-crm`
 
@@ -45,28 +56,28 @@ const S3ClientInstance = new S3Client({ region: AWS_REGION })
 const SQSClientInstance = new SQSClient({ region: AWS_REGION })
 
 const S3_BUCKET_NAME = `integrations-outbound-event-store-${NODE_ENV}-${AWS_REGION}`
-const SQS_QUEUE_NAME = `integrations-outbound-event-queue-${NODE_ENV}-${AWS_REGION}.fifo`
+const SQS_QUEUE_NAME = `integrations-outbound-event-aggregation-queue-${NODE_ENV}-${AWS_REGION}.fifo`
 const SQS_QUEUE_URL = `https://sqs.${AWS_REGION}.amazonaws.com/${AWS_ACCOUNT_ID}/${SQS_QUEUE_NAME}`
 
 export const sendEventToAWS = async (request: SendToAWSRequest) => {
   // Compute file path and message dedupe id
   // Each advertiser and segment can eventually have multiple data drops, we use uuid create unique files
   const uuidValue = uuidv4()
-  const S3FilePath = `${ACTION_SLUG}/outbound/${request.CrmDataId}-${uuidValue}.txt`
-  const MessageDeduplicationId = `${request.CrmDataId}-${uuidValue}`
+  const S3FilePath = `${ACTION_SLUG}/${request.AdvertiserId}-${request.CrmDataId}-${uuidValue}.txt`
+  const MessageDeduplicationId = `${request.AdvertiserId}-${request.CrmDataId}-${uuidValue}`
 
   // Create Queue Message Payload
-  // Store almost all of the S3Payload in the SQS message, this prevents fetching and parsing
-  // payload from S3 before concurrent operations in the same CRM Data Segment is complete
   const SQSPayload: SQSMessageBody = {
-    EventUUID: uuidValue,
-    TDDAuthToken: request.TDDAuthToken,
-    AdvertiserId: request.AdvertiserId,
-    CrmDataId: request.CrmDataId,
-    SegmentName: request.SegmentName,
-    DropOptions: request.DropOptions,
-    RequeueCount: 0,
-    S3FilePath
+    ActionSlug: ACTION_SLUG,
+    S3FilePath,
+    DestinationPayload: {
+      TDDAuthToken: request.TDDAuthToken,
+      AdvertiserId: request.AdvertiserId,
+      SegmentName: request.SegmentName,
+      CrmDataId: request.CrmDataId,
+      DropOptions: request.DropOptions,
+      RequeueCount: 0
+    }
   }
 
   // Create a Readable Stream from the payload to be uploaded to S3
@@ -86,13 +97,27 @@ export const sendEventToAWS = async (request: SendToAWSRequest) => {
     })
   )
 
+  // Add Metadata to the SQS Message
+  // This would enable us to route messages appropriate lambda functions without even parsing the message body
+  const MessageAttributes: IntegrationsOutboundSQSMessageAttributes = {
+    ActionSlug: {
+      DataType: 'String',
+      StringValue: ACTION_SLUG
+    },
+    EventUUID: {
+      DataType: 'String',
+      StringValue: uuidValue
+    }
+  }
+
   // Queue the file in SQS
   const SQSResponse = await SQSClientInstance.send(
     new SendMessageCommand({
       QueueUrl: SQS_QUEUE_URL,
       MessageBody: JSON.stringify(SQSPayload),
       MessageGroupId: request.CrmDataId,
-      MessageDeduplicationId
+      MessageDeduplicationId,
+      MessageAttributes
     })
   )
 
@@ -104,7 +129,8 @@ export const sendEventToAWS = async (request: SendToAWSRequest) => {
 
 function parseARN(awsARN: string | undefined) {
   if (awsARN === undefined) {
-    throw new Error(`AWS_ROLE_ARN environment variable is possibly undefined`)
+    // throw new Error(`AWS_ROLE_ARN environment variable is possibly undefined`)
+    return ''
   }
 
   const regexParse = /arn:aws:iam[:]+(\d+):role\/[\da-z-.]+/.exec(awsARN)
@@ -112,6 +138,7 @@ function parseARN(awsARN: string | undefined) {
   if (regexParse && regexParse[1]) {
     return regexParse[1]
   } else {
-    throw new Error(`AWS_ROLE_ARN environment variable is possibly malformed`)
+    // throw new Error(`AWS_ROLE_ARN environment variable is possibly malformed`)
+    return ''
   }
 }
