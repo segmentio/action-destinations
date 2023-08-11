cad1af187dfd3c352d145739dd4411d0f3e16328
Sayan Das <sayan.das@segment.com>
2023-08-11 12:05:20 +0530

    Refactor Code

--- packages/destination-actions/src/destinations/the-trade-desk-crm/awsClient.ts
+++ packages/destination-actions/src/destinations/the-trade-desk-crm/awsClient.ts
@@ -1,14 +1,35 @@
-import { RequestOptions } from '@segment/actions-core'
-import { S3Client, PutObjectCommand, PutObjectRequest } from '@aws-sdk/client-s3'
-import { SQSClient, SendMessageCommand, SendMessageRequest } from '@aws-sdk/client-sqs'
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs'
 import { v4 as uuidv4 } from '@lukeed/uuid'
 import { Readable } from 'stream'
 
-interface ControlledBatchOutboundEventPayload {
-  audienceId: string
-  request: {
-    url: string
-    requestOptions: RequestOptions
+interface SendToAWSRequest {
+  TDDAuthToken: string
+  AdvertiserId: string
+  CrmDataId: string
+  SegmentName: string
+  UsersFormatted: string
+  DropOptions: {
+    PiiType: string
+    MergeMode: string
+    TtlInMinutes?: number
+    RetentionEnabled?: boolean
+  }
+}
+interface SQSMessageBody {
+  EventUUID: string
+  TDDAuthToken: string
+  AdvertiserId: string
+  CrmDataId: string
+  SegmentName: string
+  S3FilePath: string
+  RequeueCount: number
+  DropReferenceId?: string
+  DropOptions: {
+    PiiType: string
+    MergeMode: string
+    TtlInMinutes?: number
+    RetentionEnabled?: boolean
   }
 }
 
@@ -21,67 +42,59 @@ const ACTION_SLUG = `actions-the-trade-desk-crm`
 // Note: Clients are created in global scope to prevent client creation on every request
 // Region and Segment Environment values are unique per Integrations Monoservice instance and will never change
 const S3ClientInstance = new S3Client({ region: AWS_REGION })
-const SQSClientInstance = new SQSClient({ region: AWS_REGION, logger: console })
+const SQSClientInstance = new SQSClient({ region: AWS_REGION })
 
 const S3_BUCKET_NAME = `integrations-outbound-event-store-${NODE_ENV}-${AWS_REGION}`
 const SQS_QUEUE_NAME = `integrations-outbound-event-queue-${NODE_ENV}-${AWS_REGION}.fifo`
-const SQS_QUEUE_URL = `https://sqs.${AWS_REGION}.amazonaws.com/3${AWS_ACCOUNT_ID}/${SQS_QUEUE_NAME}`
+const SQS_QUEUE_URL = `https://sqs.${AWS_REGION}.amazonaws.com/${AWS_ACCOUNT_ID}/${SQS_QUEUE_NAME}`
 
-export const sendEventToAWS = async (endpoint: string, users: string, audienceId: string) => {
+export const sendEventToAWS = async (request: SendToAWSRequest) => {
   // Compute file path and message dedupe id
+  // Each advertiser and segment can eventually have multiple data drops, we use uuid create unique files
   const uuidValue = uuidv4()
-  const s3FilePath = `${ACTION_SLUG}/outbound/${audienceId}-${uuidValue}.json`
-  const messageDedupeId = `${audienceId}-${uuidValue}`
-
-  // Create Batch Request Payload
-  const S3Payload: ControlledBatchOutboundEventPayload = {
-    audienceId,
-    request: {
-      url: endpoint,
-      requestOptions: {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'text/plain'
-        },
-        body: users
-      }
-    }
-  }
+  const S3FilePath = `${ACTION_SLUG}/outbound/${request.CrmDataId}-${uuidValue}.txt`
+  const MessageDeduplicationId = `${request.CrmDataId}-${uuidValue}`
 
   // Create Queue Message Payload
-  const SQSPayload = {
-    audienceId,
-    s3FilePath
+  // Store almost all of the S3Payload in the SQS message, this prevents fetching and parsing
+  // payload from S3 before concurrent operations in the same CRM Data Segment is complete
+  const SQSPayload: SQSMessageBody = {
+    EventUUID: uuidValue,
+    TDDAuthToken: request.TDDAuthToken,
+    AdvertiserId: request.AdvertiserId,
+    CrmDataId: request.CrmDataId,
+    SegmentName: request.SegmentName,
+    DropOptions: request.DropOptions,
+    RequeueCount: 0,
+    S3FilePath
   }
 
   // Create a Readable Stream from the payload to be uploaded to S3
   const S3PayloadData = new Readable()
-  S3PayloadData.push(JSON.stringify(S3Payload))
+  S3PayloadData.push(request.UsersFormatted)
   S3PayloadData.push(null)
 
-  // Construct a PUT request for S3
-  const S3PutObjectRequest: PutObjectRequest = {
-    Bucket: S3_BUCKET_NAME,
-    Key: s3FilePath,
-    Body: S3PayloadData,
-    ContentType: 'application/json',
-    // ContentLength Issue: https://stackoverflow.com/questions/68332633/aws-s3-node-js-sdk-notimplemented-error-with-multer
-    ContentLength: S3PayloadData.readableLength
-  }
-
-  // Construct Send Message request for SQS
-  const SQSSendMessageRequest: SendMessageRequest = {
-    QueueUrl: SQS_QUEUE_URL,
-    MessageBody: JSON.stringify(SQSPayload),
-    MessageDeduplicationId: messageDedupeId,
-    MessageGroupId: audienceId
-  }
-
   // Upload the file to the S3 bucket
-  const S3Response = await S3ClientInstance.send(new PutObjectCommand(S3PutObjectRequest))
+  const S3Response = await S3ClientInstance.send(
+    new PutObjectCommand({
+      Bucket: S3_BUCKET_NAME,
+      Key: S3FilePath,
+      Body: S3PayloadData,
+      ContentType: 'text/plain',
+      // ContentLength Issue: https://stackoverflow.com/questions/68332633/aws-s3-node-js-sdk-notimplemented-error-with-multer
+      ContentLength: S3PayloadData.readableLength
+    })
+  )
 
   // Queue the file in SQS
-  const SQSResponse = await SQSClientInstance.send(new SendMessageCommand(SQSSendMessageRequest))
+  const SQSResponse = await SQSClientInstance.send(
+    new SendMessageCommand({
+      QueueUrl: SQS_QUEUE_URL,
+      MessageBody: JSON.stringify(SQSPayload),
+      MessageGroupId: request.CrmDataId,
+      MessageDeduplicationId
+    })
+  )
 
   return {
     S3Response,
--- packages/destination-actions/src/destinations/the-trade-desk-crm/functions.ts
+++ packages/destination-actions/src/destinations/the-trade-desk-crm/functions.ts
@@ -46,16 +46,20 @@ export async function processPayload(request: RequestClient, settings: Settings,
   const crmID = await getCRMInfo(request, settings, payloads[0])
 
   // Get user emails from the payloads
-  const users = extractUsers(payloads)
-
-  // Generate a Data Drop Endpoint
-  const dataDropEndpoint = await getDropEndpoint(request, settings, payloads[0], crmID)
-
-  // Send users to the Data Drop Endpoint
-  // return sendCRMData(request, dataDropEndpoint, users)
+  const usersFormatted = extractUsers(payloads)
 
   // Send request to AWS to be processed
-  const AWSOperationStatus = await sendEventToAWS(dataDropEndpoint, users, 'test-audience-id')
+  const AWSOperationStatus = await sendEventToAWS({
+    TDDAuthToken: settings.auth_token,
+    AdvertiserId: settings.advertiser_id,
+    CrmDataId: crmID,
+    SegmentName: payloads[0].name,
+    UsersFormatted: usersFormatted,
+    DropOptions: {
+      PiiType: payloads[0].pii_type,
+      MergeMode: 'Replace'
+    }
+  })
 
   return AWSOperationStatus
 }
@@ -93,8 +97,8 @@ async function getAllDataSegments(request: RequestClient, settings: Settings) {
   return allDataSegments
 }
 
-async function getCRMInfo(request: RequestClient, settings: Settings, payload: Payload) {
-  let segmentId
+async function getCRMInfo(request: RequestClient, settings: Settings, payload: Payload): Promise<string> {
+  let segmentId: string
   const segments = await getAllDataSegments(request, settings)
   const segmentExists = segments.filter(function (segment) {
     if (segment.SegmentName == payload.name) {
@@ -105,14 +109,11 @@ async function getCRMInfo(request: RequestClient, settings: Settings, payload: P
   // More than 1 audience returned matches name
   if (segmentExists.length > 1) {
     throw new IntegrationError('Multiple audiences found with the same name', 'INVALID_SETTINGS', 400)
-  }
-
-  if (segmentExists.length == 1) {
+  } else if (segmentExists.length == 1) {
     segmentId = segmentExists[0].CrmDataId
-  }
-  // If an audience does not exist, we will create it. In V1, we will send a single batch
-  // of full audience syncs every 24 hours to eliminate the risk of a race condition.
-  else {
+  } else {
+    // If an audience does not exist, we will create it. In V1, we will send a single batch
+    // of full audience syncs every 24 hours to eliminate the risk of a race condition.
     const response: ModifiedResponse<CREATE_API_RESPONSE> = await request(`${BASE_URL}/crmdata/segment`, {
       method: 'POST',
       headers: {
@@ -129,25 +130,6 @@ async function getCRMInfo(request: RequestClient, settings: Settings, payload: P
   }
 
   return segmentId
-}
-
-async function getDropEndpoint(request: RequestClient, settings: Settings, payload: Payload, crmId: string) {
-  const response: ModifiedResponse<DROP_ENDPOINT_API_RESPONSE> = await request(
-    `${BASE_URL}/crmdata/segment/${settings.advertiser_id}/${crmId}`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'TTD-Auth': settings.auth_token
-      },
-      json: {
-        PiiType: payload.pii_type,
-        MergeMode: 'Replace'
-      }
-    }
-  )
-
-  return response.data.Url
 }
 
 function extractUsers(payloads: Payload[]): string {
@@ -170,18 +152,8 @@ function extractUsers(payloads: Payload[]): string {
   return users
 }
 
-// async function sendCRMData(request: RequestClient, endpoint: string, users: string) {
-//   return await request(endpoint, {
-//     method: 'PUT',
-//     headers: {
-//       'Content-Type': 'text/plain'
-//     },
-//     body: users
-//   })
-// }
-
 // More info about email normalization: https://api.thetradedesk.com/v3/portal/data/doc/DataPiiNormalization#email-normalize
-const normalizeEmail = (email: string) => {
+function normalizeEmail(email: string) {
   // Remove all of the leading and trailing whitespace and convert to lowercase
   email = email.trim().toLowerCase()
 
