472096d2abdc72fc2b3d67913d0fb98daafa96a2
kwlandry-acoustic <kip.landry@acoustic.com>
2023-08-12 22:39:20 -0400

    remove extraneous fields
    Signed-off-by: kwlandry-acoustic <kip.landry@acoustic.com>

--- packages/destination-actions/src/destinations/fullstory/generated-types.ts
+++
@@ -1,8 +0,0 @@
-// Generated file. DO NOT MODIFY IT BY HAND.
-
-export interface Settings {
-  /**
-   * [FullStory API key](https://help.fullstory.com/hc/en-us/articles/360052021773-Managing-API-Keys)
-   */
-  apiKey: string
-}
