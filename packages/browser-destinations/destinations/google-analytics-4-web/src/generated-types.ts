0bcdf8c3708a2038587109a44da8bde7470e9ef9
Ankit Gupta <ankit.gupta@segment.com>
2023-08-22 14:48:31 +0530

    change in generate-type file

--- packages/browser-destinations/destinations/google-analytics-4-web/src/generated-types.ts
+++ packages/browser-destinations/destinations/google-analytics-4-web/src/generated-types.ts
@@ -56,5 +56,5 @@ export interface Settings {
   /**
    * The stream ID associated with the web stream. Found in the Google Analytics UI under: Admin > Data Streams > Web > Stream ID.
    */
-  streamID: string
+  streamID?: string
 }
