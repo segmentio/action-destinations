8b8345f3978c748fdab470f3383b2b7c580085ea
Sayan Das <109198085+sayan-das-in@users.noreply.github.com>
2023-08-24 15:14:45 +0530

    TTD: Removed Merge Mode field (#1523)

--- packages/destination-actions/src/destinations/the-trade-desk-crm/functions.ts
+++ packages/destination-actions/src/destinations/the-trade-desk-crm/functions.ts
@@ -83,7 +83,7 @@ export async function processPayload(input: ProcessPayloadInput) {
       UsersFormatted: usersFormatted,
       DropOptions: {
         PiiType: input.payloads[0].pii_type,
-        MergeMode: input.payloads[0].merge_mode || 'Replace'
+        MergeMode: 'Replace'
       }
     })
   }
@@ -216,7 +216,7 @@ async function getCRMDataDropEndpoint(request: RequestClient, settings: Settings
       },
       json: {
         PiiType: payload.pii_type,
-        MergeMode: payload.merge_mode || 'Replace'
+        MergeMode: 'Replace'
       }
     }
   )
