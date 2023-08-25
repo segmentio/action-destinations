114aede4a444a916104d7fc5233660a29d4773ba
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2023-08-25 12:16:46 +0200

    new Acoustic integration

---
+++ packages/destination-actions/src/destinations/acoustic-s3tc/generated-types.ts
@@ -0,0 +1,32 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Settings {
+  /**
+   * Prefix to all Stored File Names
+   */
+  fileNamePrefix?: string
+  /**
+   * Write permission to the S3 bucket.
+   */
+  s3_access_key?: string
+  /**
+   * Write permission to the S3 bucket.
+   */
+  s3_secret?: string
+  /**
+   * An Access Point created as access to the S3 bucket.
+   */
+  s3_bucket?: string
+  /**
+   * See S3 definition, should be eg: us-east-1, us-east-2
+   */
+  s3_region?: string
+  /**
+   *
+   *
+   * Last-Modified: 08.22.2023 09.14.43
+   *
+   *
+   */
+  version?: string
+}
---
+++ packages/destination-actions/src/destinations/acoustic-s3tc/receiveEvents/index.ts
@@ -0,0 +1,117 @@
+import { ActionDefinition, IntegrationError, InvalidAuthenticationError } from '@segment/actions-core'
+import { Settings } from '../generated-types'
+import { Payload } from '../receiveEvents/generated-types'
+import get from 'lodash/get'
+import { addUpdateEvents } from './eventprocessing'
+import generateS3RequestOptions from '../../../lib/AWS/s3'
+import { validateSettings } from './preCheck'
+
+const action: ActionDefinition<Settings, Payload> = {
+  title: 'Receive Events',
+  description:
+    'Send Segment identify() and track() events to Acoustic Connect. At least one of the following optional fields should be populated: Key-Value pairs, Arrays, Context, Properties, Traits.',
+  fields: {
+    key_value_pairs: {
+      label: 'Key-Value pairs',
+      description: '(optional) Map simple Key-Value pairs',
+      type: 'object'
+    },
+    array_data: {
+      label: 'Arrays',
+      description:
+        '(optional) If the data needed is in an array, use this section to Map Array data into useable attributes',
+      type: 'object',
+      multiple: true,
+      additionalProperties: true
+    },
+    context: {
+      label: 'Context',
+      description:
+        '(optional) If the data is present in a Context section, use this to map the attributes of a Context Section',
+      type: 'object'
+    },
+    properties: {
+      label: 'Properties',
+      description:
+        '(optional) If the data is present in a Properties section, use this to map the attributes of a Properties Section',
+      type: 'object'
+    },
+    traits: {
+      label: 'Traits',
+      description:
+        '(optional) If the data is present in a Traits section, use this to map the attributes of a Traits Section',
+      type: 'object'
+    },
+    email: {
+      label: 'Email',
+      description: 'Do Not Modify - Email is required',
+      type: 'string',
+      format: 'email',
+      required: true,
+      default: {
+        '@path': '$.email'
+      }
+    },
+    type: {
+      label: 'Type',
+      description: 'The type of event. e.g. track or identify. This field is required',
+      type: 'string',
+      required: true,
+      default: {
+        '@path': '$.type'
+      }
+    },
+    timestamp: {
+      label: 'Timestamp',
+      description: 'The timestamp for when the event took place. This field is required',
+      type: 'datetime',
+      required: true,
+      default: {
+        '@path': '$.timestamp'
+      }
+    }
+  },
+  perform: async (request, { settings, payload }) => {
+    const email = get(payload, 'email', '')
+
+    if (!email) {
+      throw new IntegrationError('Email Not Found, invalid Event received.', 'INVALID_EVENT_HAS_NO_EMAIL', 400)
+    }
+
+    if (!payload.context && !payload.traits && !payload.properties)
+      throw new IntegrationError(
+        'No mapped data provided, must use at least one of the mapping fields to define the data to be sent to Acoustic.',
+        'INVALID_NO_DATA_MAPPED',
+        400
+      )
+
+    validateSettings(settings)
+
+    //Parse Event-Payload into an Update
+    const csvRows = addUpdateEvents(payload, email)
+
+    //Set File Store Name
+    const fileName = settings.fileNamePrefix + `${new Date().toISOString().replace(/(\.|-|:)/g, '_')}` + '.csv'
+
+    const method = 'PUT'
+    const opts = await generateS3RequestOptions(
+      settings.s3_bucket as string,
+      settings.s3_region as string,
+      fileName,
+      method,
+      csvRows,
+      settings.s3_access_key as string,
+      settings.s3_secret as string
+    )
+    if (!opts.headers || !opts.method || !opts.host || !opts.path) {
+      throw new InvalidAuthenticationError('Unable to generate signature header for AWS S3 request.')
+    }
+
+    return await request(`https://${opts.host}/${opts.path}`, {
+      headers: opts.headers as Record<string, string>,
+      method,
+      body: opts.body
+    })
+  }
+}
+export default action
