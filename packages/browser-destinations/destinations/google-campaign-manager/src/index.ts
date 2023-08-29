5b6743e6036c11ca82ad8d69f92fbe13cd457df9
Robert Hall <rhall@twilio.com>
2023-08-29 14:48:18 -0500

    draft

--- packages/browser-destinations/destinations/google-campaign-manager/src/index.ts
+++ packages/browser-destinations/destinations/google-campaign-manager/src/index.ts
@@ -9,12 +9,53 @@ export const destination: BrowserDestinationDefinition<Settings, unknown> = {
   mode: 'device',
 
   settings: {
-    // Add any Segment destination settings required here
+    floodlightConfigID: {
+      description: 'This field identifies the Floodlight configuration the tag is associated with.',
+      label: 'Floodlight Config ID',
+      type: 'string',
+      required: true,
+      default: 'DC-'
+    },
+    activityGroupTagString: {
+      description: 'This field identifies the Floodlight activity group the tag is associated with.',
+      label: 'Activity Group Tag',
+      type: 'string',
+      required: false
+    },
+    activityTagString: {
+      description: 'This identifies the Floodlight activity the tag is associated with.',
+      label: 'Activity Tag',
+      type: 'string',
+      required: false
+    },
+    allowAdPersonalizationSignals: {
+      description: 'This allows you disable to the usage of the data for personalized ads.',
+      label: 'Activity Tag',
+      type: 'boolean',
+      required: false,
+      default: false
+    }
   },
 
-  initialize: async (_, deps) => {
-    await deps.loadScript('<path_to_partner_script>')
-    // initialize client code here
+  //TODO: allowCustomScripts, variables, send_to
+  // TODO: counter vs sales?
+
+  initialize: async ({ settings }, deps) => {
+    const config = {
+      allow_ad_personalization_signals: settings.allowAdPersonalizationSignals
+    }
+
+    window.dataLayer = window.dataLayer || []
+    window.gtag = function () {
+      // eslint-disable-next-line prefer-rest-params
+      window.dataLayer.push(arguments)
+    }
+
+    window.gtag('js', new Date())
+    window.gtag('config', settings.floodlightConfigID, config)
+    const script = `https://www.googletagmanager.com/gtag/js?id=${settings.floodlightConfigID}`
+    await deps.loadScript(script)
+    return window.gtag
   },
 
   actions: {}
