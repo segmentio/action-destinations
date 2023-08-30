1f442ef8e4597aa31ba59ef4db0f4ea5d056775f
Robert Hall <rhall@twilio.com>
2023-08-30 13:58:16 -0500

    scaffold counter mappings

--- packages/browser-destinations/destinations/google-campaign-manager/src/counterActivity/index.ts
+++ packages/browser-destinations/destinations/google-campaign-manager/src/counterActivity/index.ts
@@ -2,12 +2,59 @@ import type { BrowserActionDefinition } from '@segment/browser-destination-runti
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 
-// Change from unknown to the partner SDK types
-const action: BrowserActionDefinition<Settings, unknown, Payload> = {
+// TODO: reasonable default paths
+
+const action: BrowserActionDefinition<Settings, Function, Payload> = {
   title: 'Counter Activity',
-  description: '',
+  description: 'Counter tags are used to count the number of conversions.',
   platform: 'web',
-  fields: {},
+  fields: {
+    activityGroupTagString: {
+      label: 'Activity Group Tag String',
+      description:
+        'An identifier for the Floodlight activity group associated with this activity, which appears as a parameter in your tags. This value is case sensitive.',
+      type: 'string',
+      required: true
+    },
+    activityTagString: {
+      label: 'Activity Tag String',
+      description:
+        'An identifier for your Floodlight activity, which appears as a parameter in your tags. This value is case sensitive.',
+      type: 'string',
+      required: true
+    },
+    enableDynamicTags: {
+      label: 'Enable Dynamic Tags',
+      type: 'boolean',
+      description:
+        'In Campaign Manager, go to Floodlight -> Configuration, under Tags, if **Dynamic** is selected, select **True**'
+    },
+    countingMethod: {
+      label: 'Counting Method',
+      type: 'string',
+      description: 'Specifies how conversions will be counted for this Floodlight activity.',
+      choices: ['standard', 'unique', 'per_session'],
+      required: true
+    },
+    sessionId: {
+      label: 'Session ID',
+      description:
+        'Use this field to insert a unique session ID if you’re using counter tags with a per session counting methodology. The session ID tells Campaign Manager 360 to count only one event per session on your site.',
+      type: 'string'
+    },
+    uVariables: {
+      label: 'U Variables',
+      description:
+        'Custom Floodlight variables enable you to capture information beyond the basics (visits and revenue) that you can collect with standard parameters in your tags.',
+      type: 'object'
+    },
+    dcCustomParams: {
+      label: 'Custom Parameters',
+      description:
+        'You can insert custom data into event snippets with the dc_custom_params field. This field accepts any values you want to pass to Google Marketing Platform.',
+      type: 'object'
+    }
+  },
   perform: (_client) => {
     // Invoke Partner SDK here
   }
