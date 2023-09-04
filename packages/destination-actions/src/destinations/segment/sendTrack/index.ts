2dc6f1537ec12efa619187c8b401de5d841f4184
Varadarajan V <109586712+varadarajan-tw@users.noreply.github.com>
2023-09-04 21:09:16 +0530

    [STRATCONN-2850] [Segment] Return transformed payload from all actions (#1545)
    * [STRATCONN-2850] [Segment] Return transformed payload from all actions
    
    * Update properties.ts

--- packages/destination-actions/src/destinations/segment/sendTrack/index.ts
+++ packages/destination-actions/src/destinations/segment/sendTrack/index.ts
@@ -48,7 +48,7 @@ const action: ActionDefinition<Settings, Payload> = {
     group_id,
     properties
   },
-  perform: (request, { payload, settings }) => {
+  perform: (request, { payload, settings, features }) => {
     if (!payload.anonymous_id && !payload.user_id) {
       throw MissingUserOrAnonymousIdThrowableError
     }
@@ -81,6 +81,11 @@ const action: ActionDefinition<Settings, Payload> = {
     // Throw an error if endpoint is not defined or invalid
     if (!settings.endpoint || !(settings.endpoint in SEGMENT_ENDPOINTS)) {
       throw InvalidEndpointSelectedThrowableError
+    }
+
+    if (features && features['actions-segment-tapi-internal']) {
+      const payload = { ...trackPayload, type: 'track' }
+      return { batch: [payload] }
     }
 
     const selectedSegmentEndpoint = SEGMENT_ENDPOINTS[settings.endpoint].url
