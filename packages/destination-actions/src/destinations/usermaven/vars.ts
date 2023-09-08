804ef3126015a6954f54ac7d919930bc167c2ee0
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2023-09-08 16:43:06 +0100

    using lodash random

--- packages/destination-actions/src/destinations/usermaven/vars.ts
+++ packages/destination-actions/src/destinations/usermaven/vars.ts
@@ -1,11 +1,5 @@
+import random from 'lodash/random'
+
 export const generateId = () => {
-  const length = 10
-  let result = ''
-
-  for (let i = 0; i < length; i++) {
-    const randomNumber = Math.floor(Math.random() * 10) // Generates a random digit between 0 and 9
-    result += randomNumber.toString() // Convert the digit to a string and append it to the result
-  }
-
-  return result
+  return random(1000000000, 9999999999)
 }
