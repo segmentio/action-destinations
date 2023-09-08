2a35e4710ae094bbdfb301f5e00650712b286718
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2023-09-08 10:14:16 +0100

    fixing flakey test

--- packages/destination-actions/src/destinations/usermaven/vars.ts
+++ packages/destination-actions/src/destinations/usermaven/vars.ts
@@ -1,1 +1,7 @@
-export const generateId = () => Math.random().toString(36).substring(2, 12)
+export const generateId = () => {
+  let randomString = Math.random().toString(36).substring(2, 12)
+  while (randomString.length < 10) {
+    randomString += '0'
+  }
+  return randomString.substring(0, 10)
+}
