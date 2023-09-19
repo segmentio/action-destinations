1922989fa7e2683e8ef93335608fff24b8fbefef
Swaty Gupta <58588123+Swaty-G@users.noreply.github.com>
2023-09-19 07:01:41 -0700

    CONMAN-615(Develop new action sendSubscription for destination SegmentProfiles to get channel subscription data into engage) (#1521)
    * create a scaffold for action sendSubscription
    
    * basic skeleton--not working
    
    * working changes
    
    * remove token
    
    * working changes, tested with groups also
    
    * adding unit tests
    
    * working changes with groups
    
    * removing comments from segment properties
    
    * fix build break-update snap test
    
    * removing snap test to fix build break
    
    * making subscription groups dynamic
    
    * minor changes
    
    * adding field validations,tapi call and sending retl flag
    
    * added push tokens working changes
    
    * adding tests
    
    * add tests
    
    * refactoring
    
    * add more tests
    
    * modifying tests to fix build
    
    * fix snapshot tests
    
    * Changes to validations
    
    * removing unnecessary validations
    
    * updating tests
    
    * Update snapshot.test.ts
    
    * modifying test to fix build failure
    
    * fix tests with console-2
    
    * fix tests with console-3
    
    * fix tests with console-3
    
    * Update snapshot.test.ts
    
    * fix snap tests at destination level
    
    * remove console logs from snapshot action test
    
    ---------
    
    Co-authored-by: vikramkumar19 <vikram.kumar@segment.com>

--- packages/destination-actions/src/destinations/segment-profiles/errors.ts
+++ packages/destination-actions/src/destinations/segment-profiles/errors.ts
@@ -19,3 +19,41 @@ export const MissingUserOrAnonymousIdThrowableError = new PayloadValidationError
 export const InvalidEndpointSelectedThrowableError = new PayloadValidationError(
   'A valid endpoint must be selected. Please check your Segment settings.'
 )
+
+export const MissingExternalIdsThrowableError = new PayloadValidationError(
+  'Either `Email` or `Phone` or `Android_Push_Token` or `Ios_Push_Token` must be defined.'
+)
+
+export const MissingEmailIfEmailSubscriptionIsPresentThrowableError = new PayloadValidationError(
+  'A valid `Email` must be defined for `Email_Subscription`.'
+)
+
+export const MissingPhoneIfSmsOrWhatsappSubscriptionIsPresentThrowableError = new PayloadValidationError(
+  'A valid `Phone` must be defined for `Sms_Subscription` or `Whatsapp_Subscription`.'
+)
+
+export const MissingSubscriptionStatusesThrowableError = new PayloadValidationError(
+  'If any of `Email`, `Phone`, `Android_Push_Token` or `Ios_Push_Token` is present, `Email_Subscription` or `Sms_Subscription` or `Whatsapp_Subscription` or `Ios_Push_Subscription` or `Android_Push_Subscription` must be defined.'
+)
+
+export const MissingEmailSubscriptionIfSubscriptionGroupsIsPresentThrowableError = new PayloadValidationError(
+  '`Email_Subscription` must be defined if Subscription Groups are present.'
+)
+
+export const InvalidSubscriptionStatusThrowableError = new PayloadValidationError(
+  'Invalid Subscription Status in `Email_Subscription` or `Sms_Subscription` or `Whatsapp_Subscription` or `Android_Push_Subscription` or `Ios_Push_Subscription`.'
+)
+
+export const InvalidGroupSubscriptionStatusThrowableError = new PayloadValidationError(
+  'Invalid Subscription Status for subscription groups.'
+)
+
+export const MissingAndroidPushTokenIfAndroidPushSubscriptionIsPresentThrowableError = new PayloadValidationError(
+  'A valid `Android_Push_Token` must be defined for `Android_Push_Subscription`.'
+)
+
+export const MissingIosPushTokenIfIosPushSubscriptionIsPresentThrowableError = new PayloadValidationError(
+  'A valid `Ios_Push_Token` must be defined for `Ios_Push_Subscription`.'
+)
+
+export const InvalidSubsriptionStatusThrowableError = new PayloadValidationError('Invalid Subscription Status.')
