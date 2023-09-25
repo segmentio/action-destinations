0fcdf42f992c527d9322ebf6af2a30add44bee5b
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2023-09-25 19:17:43 +0100

    removing files which were added by accident

--- packages/destination-actions/src/destinations/movable-ink/productAdded/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-import nock from 'nock'
-import { createTestEvent, createTestIntegration } from '@segment/actions-core'
-import Destination from '../../index'
-
-const testDestination = createTestIntegration(Destination)
-
-describe('MovableInk.productAdded', () => {
-  // TODO: Test your action
-})
