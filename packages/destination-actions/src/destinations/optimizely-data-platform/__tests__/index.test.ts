9d7e19ae0a074f2c26375341d34f122ed4b7a798
Romani Ramzi <102735170+Romani-Ramzi@users.noreply.github.com>
2023-09-26 13:21:35 +0100

    Optimizely events (#1586)
    * Update README.md
    
    * commit events & identify
    
    * refactor
    
    * updated code
    
    * more changes
    
    * update api url and test
    
    * changes to fields
    
    * updating actions
    
    * fixing up tests and labels
    
    * fixing gest
    
    * renaming events going to ODP
    
    * fixing tests
    
    ---------
    
    Co-authored-by: Sam Pontefract <103249462+ISapoI@users.noreply.github.com>
    Co-authored-by: joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>

---
+++ packages/destination-actions/src/destinations/optimizely-data-platform/__tests__/index.test.ts
@@ -0,0 +1,22 @@
+import nock from 'nock'
+import { createTestIntegration } from '@segment/actions-core'
+import Definition from '../index'
+
+const testDestination = createTestIntegration(Definition)
+
+describe('Optimizely Data Platform', () => {
+  describe('testAuthentication', () => {
+
+    it('should validate authentication inputs', async () => {
+      nock('https://function.zaius.app/twilio_segment').post('/auth').reply(200)
+
+      // This should match your authentication.fields
+      const authData = {
+        apiKey: 'test',
+        region: 'US'
+      }
+
+      await expect(testDestination.testAuthentication(authData)).resolves.not.toThrowError()
+    })
+  })
+})
