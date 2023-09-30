6084d119ef56cd15e52eb804a7f088eb4a898e48
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2023-09-29 17:06:53 -0700

    Success and failure cases for `YahooAudiences.updateSegment`.

--- packages/destination-actions/src/destinations/yahoo-audiences/updateSegment/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/yahoo-audiences/updateSegment/__tests__/index.test.ts
@@ -5,40 +5,80 @@ import Destination from '../../index'
 const testDestination = createTestIntegration(Destination)
 
 describe('YahooAudiences.updateSegment', () => {
-  it('should update yahoo subtaxonomy', async () => {
-    // Given
-    const event = createTestEvent({
-      context: {
-        device: {
-          type: 'mobile',
-          advertisingId: '123'
-        },
-        personas: {
-          computation_id: '123',
-          computation_key: '234',
-          computation_class: 'audience'
+  describe('Success cases', () => {
+    it('Success - should update yahoo subtaxonomy', async () => {
+      // Given
+      const event = createTestEvent({
+        context: {
+          device: {
+            type: 'mobile',
+            advertisingId: '123'
+          },
+          personas: {
+            computation_id: '123',
+            computation_key: '234',
+            computation_class: 'audience'
+          }
         }
-      }
+      })
+
+      nock(`https://dataxonline.yahoo.com`).post('/online/audience/').reply(200)
+
+      // When
+      const responses = await testDestination.testAction('updateSegment', {
+        event,
+        mapping: {},
+        useDefaultMappings: true,
+        settings: {
+          engage_space_id: '123',
+          mdm_id: '234',
+          taxonomy_client_key: '345',
+          taxonomy_client_secret: '456',
+          customer_desc: 'Spacely Sprockets'
+        }
+      })
+
+      // Then
+      expect(responses.length).toBe(1)
+      expect(responses[0].status).toBe(200)
     })
+  })
 
-    // When
-    nock(`https://dataxonline.yahoo.com`).post('/online/audience/').reply(200)
+  describe('Failure cases', () => {
+    it('Failure - Unauthorized', async () => {
+      // Given
+      const event = createTestEvent({
+        context: {
+          device: {
+            type: 'mobile',
+            advertisingId: '123'
+          },
+          personas: {
+            computation_id: '123',
+            computation_key: '234',
+            computation_class: 'audience'
+          }
+        }
+      })
 
-    // Then
-    const responses = await testDestination.testAction('updateSegment', {
-      event,
-      mapping: {},
-      useDefaultMappings: true,
-      settings: {
-        engage_space_id: '123',
-        mdm_id: '234',
-        taxonomy_client_key: '345',
-        taxonomy_client_secret: '456',
-        customer_desc: 'Spacely Sprockets'
-      }
+      // When
+      nock(`https://dataxonline.yahoo.com`).post('/online/audience/').reply(401)
+      const response = await testDestination.testAction('updateSegment', {
+        event,
+        mapping: {},
+        useDefaultMappings: true,
+        settings: {
+          engage_space_id: '123',
+          mdm_id: '234',
+          taxonomy_client_key: '345',
+          taxonomy_client_secret: '456',
+          customer_desc: 'Spacely Sprockets'
+        }
+      })
+
+      // Then
+      expect(response).toHaveLength(1)
+      expect(response[0].status).toBe(401)
     })
-
-    expect(responses.length).toBe(1)
-    expect(responses[0].status).toBe(200)
   })
 })
