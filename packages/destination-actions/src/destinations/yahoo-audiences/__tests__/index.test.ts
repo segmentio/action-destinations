70eeb43c3a395f24773529e5b693ab113dd2cb6a
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2023-10-02 12:58:29 -0700

    Fixing unit tests.

--- packages/destination-actions/src/destinations/yahoo-audiences/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/yahoo-audiences/__tests__/index.test.ts
@@ -3,7 +3,7 @@ import { IntegrationError, createTestIntegration } from '@segment/actions-core'
 
 import Destination from '../index'
 
-const AUDIENCE_ID = 'aud_12345' // References audienceSettings.audience_id
+const AUDIENCE_ID = 'aud_123456789012345678901234567' // References audienceSettings.audience_id
 const AUDIENCE_KEY = 'sneakers_buyers' // References audienceSettings.audience_key
 const ENGAGE_SPACE_ID = 'acme_corp_engage_space' // References settings.engage_space_id
 const MDM_ID = 'mdm 123' // References settings.mdm_id
@@ -36,12 +36,12 @@ describe('Yahoo Audiences', () => {
 
     describe('Success cases', () => {
       it('It should create the audience successfully', async () => {
-        nock('https://datax.yahooapis.com').put('/v1/taxonomy/append/345').reply(202, {
+        nock('https://datax.yahooapis.com').put(`/v1/taxonomy/append/${ENGAGE_SPACE_ID}`).reply(202, {
           anything: '123'
         })
 
         const result = await testDestination.createAudience(createAudienceInput)
-        expect(result.externalId).toBe('123')
+        expect(result.externalId).toBe(AUDIENCE_ID)
       })
     })
     describe('Failure cases', () => {
@@ -62,7 +62,7 @@ describe('Yahoo Audiences', () => {
       it('should throw an error when engage_space_id setting is missing', async () => {
         createAudienceInput.settings.engage_space_id = ''
         createAudienceInput.audienceSettings.audience_key = 'sneakeres_buyers'
-        createAudienceInput.audienceSettings.audience_id = 'aud_12345'
+        createAudienceInput.audienceSettings.audience_id = 'aud_123456789012345678901234567'
         await expect(testDestination.createAudience(createAudienceInput)).rejects.toThrowError(IntegrationError)
       })
     })
