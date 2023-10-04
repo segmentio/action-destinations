0da0c60570d15909486eb8865f4c9ae04bfd6983
Ankit Gupta <ankit.gupta@segment.com>
2023-10-04 11:12:18 +0530

    Changed description of conversion action field

--- packages/destination-actions/src/destinations/google-enhanced-conversions/uploadClickConversion/index.ts
+++ packages/destination-actions/src/destinations/google-enhanced-conversions/uploadClickConversion/index.ts
@@ -25,8 +25,7 @@ const action: ActionDefinition<Settings, Payload> = {
   fields: {
     conversion_action: {
       label: 'Conversion Action ID',
-      description:
-        'The ID of the conversion action associated with this conversion. To find the Conversion Action ID, click on your conversion in Google Ads and get the value for `ctId` in the URL. For example, if the URL is `https://ads.google.com/aw/conversions/detail?ocid=00000000&ctId=570000000`, your Conversion Action ID is `570000000`.',
+      description: 'The ID of the conversion action associated with this conversion.',
       type: 'number',
       required: true,
       dynamic: true
