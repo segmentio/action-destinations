15ec5bab39ba350e6fbc10a697c7224337ba366a
namirali <n.amirali@gmail.com>
2023-10-04 18:20:03 +0300

    [UserMotion] test utils

---
+++ packages/browser-destinations/destinations/usermotion/src/testIUtils.ts
@@ -0,0 +1,28 @@
+import { UserMotion } from './types'
+import { Subscription } from '@segment/browser-destination-runtime/types'
+import nock from 'nock'
+
+export const TEST_API_KEY = '0000000000000001'
+
+export const mockedSdk = (): UserMotion => {
+  return {
+    track: jest.fn(),
+    identify: jest.fn(),
+    pageview: jest.fn(),
+    group: jest.fn()
+  }
+}
+export const identifySubscription: Subscription = {
+  partnerAction: 'identify',
+  name: 'Identify User',
+  enabled: true,
+  subscribe: 'type = "identify"',
+  mapping: {
+    userId: { '@path': '$.userId' },
+    traits: { '@path': '$.traits' }
+  }
+}
+
+export const mockJsHttpRequest = (): void => {
+  nock('https://api.usermotion.com').get(`/js/${TEST_API_KEY}.js`).reply(200, {})
+}
