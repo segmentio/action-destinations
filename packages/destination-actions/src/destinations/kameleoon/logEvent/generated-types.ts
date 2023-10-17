c134b53e7f1151973589a01d697cd2f2a5012d2d
akalyuzhnyi <115689020+akalyuzhnyi@users.noreply.github.com>
2023-10-17 17:58:06 +0300

    Action Destination for Kameleoon (#1644)
    * init kameleoon destination
    
    * fixing timestamp test issue
    
    * fix after review
    
    * added description and updated tests
    
    * updated tests and added additional checking for apikey
    
    ---------
    
    Co-authored-by: joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>

---
+++ packages/destination-actions/src/destinations/kameleoon/logEvent/generated-types.ts
@@ -0,0 +1,36 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * The event name
+   */
+  event?: string
+  /**
+   * The type of the event
+   */
+  type: string
+  /**
+   * Additional event Properties or user Traits to send with the event
+   */
+  properties?: {
+    [k: string]: unknown
+  }
+  /**
+   * Kameleoon Visitor Code - a unique identifier for the user
+   */
+  kameleoonVisitorCode?: string
+  /**
+   * The timestamp of the event
+   */
+  timestamp: string
+  /**
+   * Context properties to send with the event
+   */
+  context?: {
+    [k: string]: unknown
+  }
+  /**
+   * The Segment messageId
+   */
+  messageId: string
+}
