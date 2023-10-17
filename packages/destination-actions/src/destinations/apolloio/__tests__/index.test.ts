e2fa61a3f374bfab77cc6bffa4a568cebef7096e
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2023-10-17 15:18:52 +0100

    adding apollo

---
+++ packages/destination-actions/src/destinations/apolloio/__tests__/index.test.ts
@@ -0,0 +1,20 @@
+import nock from 'nock'
+import { createTestIntegration } from '@segment/actions-core'
+import Definition from '../index'
+
+const testDestination = createTestIntegration(Definition)
+
+const VALID_SETTINGS = {
+  apiToken: 'YOUR_API_KEY_HERE'
+}
+const MOCK_TOKEN_RESPONSE = { healthy: true, is_logged_in: true }
+
+describe('Apolloio', () => {
+  describe('testAuthentication', () => {
+    it('should validate authentication inputs', async () => {
+      nock('https://apollo.io/').get(/.*/).reply(200, MOCK_TOKEN_RESPONSE)
+      const settings = VALID_SETTINGS
+      await expect(testDestination.testAuthentication(settings)).resolves.not.toThrowError()
+    })
+  })
+})
---
+++ packages/destination-actions/src/destinations/apolloio/track/__tests__/index.test.ts
@@ -0,0 +1,33 @@
+import nock from 'nock'
+import { createTestIntegration } from '@segment/actions-core'
+import Destination from '../../index'
+
+
+const testDestination = createTestIntegration(Destination)
+
+describe('Apolloio.track', () => {
+  it('should work', async () => {
+    nock('https://apollo.io/').post(/.*/).reply(200, {})
+
+    const responses = await testDestination.testAction('track', {
+      mapping: {
+        anonymousId: '#ZEj!sso)8ImNKQTntip',
+        event: 'click',
+        ipAddress: '127.0.0.1',
+        timestamp: '2111-12-22T04:37:50.244Z',
+        userId: 'SampleUser123',
+        page: {}
+      },
+      settings: { apiToken: 'api-key' }
+    })
+
+    expect(responses.length).toBe(1)
+    expect(responses[0].status).toBe(200)
+    expect(responses[0].data).toMatchObject({})
+    expect(responses[0].options.body).toContain('anonymousId')
+    expect(responses[0].options.body).toContain('2111-12-22T04:37:50.244Z')
+    expect(responses[0].options.body).toContain('anonymousId')
+    expect(responses[0].options.body).toContain('event')
+    expect(responses[0].options.body).toContain('click')
+  })
+})
