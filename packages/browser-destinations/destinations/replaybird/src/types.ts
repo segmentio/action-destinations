91194d618199fba9a5ddcad166f54a10b6e444cb
prakashatatus <prakash@atatus.com>
2023-10-17 12:25:36 +0530

    renamed siteKey to apiKey in types.ts file

--- packages/browser-destinations/destinations/replaybird/src/types.ts
+++ packages/browser-destinations/destinations/replaybird/src/types.ts
@@ -5,5 +5,5 @@ type EventProperties = {
 export type ReplayBird = {
   capture: (eventName: string, eventProperties: EventProperties, library?: string) => void
   identify: (identity: string, props: any) => void
-  init: (siteKey: string, props: any) => void
+  init: (apiKey: string, props: any) => void
 }
