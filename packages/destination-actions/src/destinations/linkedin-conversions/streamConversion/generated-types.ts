d47743d87ed244b7e324b82afb1240e00895869c
Nick Aguilar <nicholas.aguilar@segment.com>
2023-10-17 19:01:18 -0700

    Generated types for linkedin hook

--- packages/destination-actions/src/destinations/linkedin-conversions/streamConversion/generated-types.ts
+++ packages/destination-actions/src/destinations/linkedin-conversions/streamConversion/generated-types.ts
@@ -1,17 +1,39 @@
 // Generated file. DO NOT MODIFY IT BY HAND.
 
 export interface Payload {}
-
+// Generated inputs for the on-subscription-save hook. DO NOT MODIFY IT BY HAND.
 
 export interface SubscriptionSaveInputs {
-        name: string
-        conversionType: string
-        account: string
-        attribution_type: string
+  /**
+   * The name of the conversion rule.
+   */
+  name: string
+  /**
+   * The type of conversion rule.
+   */
+  conversionType: string
+  /**
+   * The account to associate this conversion rule with.
+   */
+  account: string
+  /**
+   * The attribution type for the conversion rule.
+   */
+  attribution_type: string
 }
+// Generated outputs for the on-subscription-save hook. DO NOT MODIFY IT BY HAND.
 
 export interface SubscriptionSaveOutputs {
-        id: string
-        name: string
-        conversionType: string
-    }
+  /**
+   * The ID of the conversion rule.
+   */
+  id: string
+  /**
+   * The name of the conversion rule.
+   */
+  name: string
+  /**
+   * The type of conversion rule.
+   */
+  conversionType: string
+}
