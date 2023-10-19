0956dd5a099b0d5e24ac18fb03dd475e2ce7a4f2
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2023-10-19 15:58:13 +0100

    fixing more tests

--- packages/destination-actions/src/destinations/apolloio/track/__tests__/snapshot.test.ts
+++ packages/destination-actions/src/destinations/apolloio/track/__tests__/snapshot.test.ts
@@ -1,29 +1,33 @@
 import { createTestEvent, createTestIntegration } from '@segment/actions-core'
-import { generateTestData } from '../../../../lib/test-data'
 import destination from '../../index'
 import nock from 'nock'
 
 const testDestination = createTestIntegration(destination)
 const actionSlug = 'track'
 const destinationSlug = 'actions-apolloio'
-const seedName = `${destinationSlug}#${actionSlug}`
 
 describe(`Testing snapshot for ${destinationSlug}'s ${actionSlug} destination action:`, () => {
   it('required fields', async () => {
-    const action = destination.actions[actionSlug]
-    const [eventData] = generateTestData(seedName, destination, action, true)
-
     nock(/.*/).persist().get(/.*/).reply(200)
     nock(/.*/).persist().post(/.*/).reply(200)
     nock(/.*/).persist().put(/.*/).reply(200)
 
     const event = createTestEvent({
-      properties: eventData
+      type: 'track',
+      event: 'Test Event',
+      userId: 'user1234',
+      timestamp: '2023-07-29T00:00:00.000Z',
+      context: {
+        page: {
+          search: 'search_query'
+        },
+        ip: '111.222.333.444'
+      }
     })
 
     const responses = await testDestination.testAction(actionSlug, {
       event: event,
-      mapping: event.properties,
+      useDefaultMappings: true,
       settings: {
         apiToken: 'test'
       },
@@ -45,20 +49,37 @@ describe(`Testing snapshot for ${destinationSlug}'s ${actionSlug} destination ac
   })
 
   it('all fields', async () => {
-    const action = destination.actions[actionSlug]
-    const [eventData] = generateTestData(seedName, destination, action, false)
-
     nock(/.*/).persist().get(/.*/).reply(200)
     nock(/.*/).persist().post(/.*/).reply(200)
     nock(/.*/).persist().put(/.*/).reply(200)
 
     const event = createTestEvent({
-      properties: eventData
+      type: 'track',
+      event: 'Test Event',
+      userId: 'user1234',
+      anonymousId: '72d7bed1-4f42-4f2f-8955-72677340546b',
+      timestamp: '2023-07-29T00:00:00.000Z',
+      properties: {
+        product_id: 'pid_1'
+      },
+      context: {
+        page: {
+          search: 'search_query'
+        },
+        ip: '111.222.333.444',
+        campaign: {
+          name: 'campaign_name',
+          term: 'campaign_term',
+          source: 'campaign_source',
+          medium: 'campaign_medium',
+          content: 'campaign_content'
+        }
+      }
     })
 
     const responses = await testDestination.testAction(actionSlug, {
       event: event,
-      mapping: event.properties,
+      useDefaultMappings: true,
       settings: {
         apiToken: 'test'
       },
