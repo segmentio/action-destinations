c3d9a17537f539b5494d10cf5d0d8205c61dc865
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2023-10-19 11:34:16 +0100

    more tests

--- packages/destination-actions/src/destinations/movable-ink/conversion/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/movable-ink/conversion/__tests__/index.test.ts
@@ -1,9 +1,123 @@
 import nock from 'nock'
 import { createTestEvent, createTestIntegration } from '@segment/actions-core'
 import Destination from '../../index'
+import { Settings } from '../../generated-types'
 
 const testDestination = createTestIntegration(Destination)
+const actionSlug = 'conversion'
+
+const settings: Settings = {
+    movable_ink_url: 'https://www.test.com',
+    username: 'test',
+    password: 'test'
+}
+
+const event = createTestEvent({
+  type: 'track',
+  event: 'Order Completed',
+  userId: 'user1234',
+  anonymousId: '72d7bed1-4f42-4f2f-8955-72677340546b',
+  timestamp: '2022-03-30T17:24:58Z',
+  properties: {
+    order_id: 'order_1',
+    revenue: 100,
+    products: [
+      {product_id: 'pid_1'}
+    ]
+  }
+})
+
+const eventNoOrderId = createTestEvent({
+  type: 'track',
+  event: 'Category Viewed',
+  userId: 'user1234',
+  anonymousId: '72d7bed1-4f42-4f2f-8955-72677340546b',
+  timestamp: '2022-03-30T17:24:58Z',
+  properties: {
+    revenue: 100,
+    products: [
+      {product_id: 'pid_1'}
+    ]
+  }
+})
+
+const eventNoProducts = createTestEvent({
+  type: 'track',
+  event: 'Category Viewed',
+  userId: 'user1234',
+  anonymousId: '72d7bed1-4f42-4f2f-8955-72677340546b',
+  timestamp: '2022-03-30T17:24:58Z',
+  properties: {
+    order_id: 'order_1',
+    revenue: 100    
+  }
+})
+
+const eventNoRevenue = createTestEvent({
+  type: 'track',
+  event: 'Category Viewed',
+  userId: 'user1234',
+  anonymousId: '72d7bed1-4f42-4f2f-8955-72677340546b',
+  timestamp: '2022-03-30T17:24:58Z',
+  properties: {
+    order_id: 'order_1',
+    products: [
+      {product_id: 'pid_1'}
+    ]  
+  }
+})
 
 describe('MovableInk.conversion', () => {
-  // TODO: Test your action
+  it('should send event to Movable Ink if revenue and products and order_id provided', async () => {
+    nock(settings.movable_ink_url as string).post(/.*/).reply(200)
+
+    const responses = await testDestination.testAction(actionSlug, {
+      event,
+      settings: settings,
+      useDefaultMappings: true
+    })
+
+    expect(responses.length).toBe(1)
+    expect(responses[0].status).toBe(200)
+    expect(responses[0].options.json).toMatchObject(
+      {
+        event_name: 'Conversion',
+        user_id: 'user1234', 
+        anonymous_id: '72d7bed1-4f42-4f2f-8955-72677340546b', 
+        timestamp: '2022-03-30T17:24:58Z',
+        products: [{id: 'pid_1'}],
+        revenue: 100
+      }
+    )
+  })
+
+  it('should throw an error if revenue missing', async () => {
+    await expect(
+      testDestination.testAction(actionSlug, {
+        event: eventNoOrderId,
+        useDefaultMappings: true,
+        settings: settings
+      })
+    ).rejects.toThrowError("The root value is missing the required field 'order_id'.")
+  })
+
+  it('should throw an error if products missing', async () => {
+    await expect(
+      testDestination.testAction(actionSlug, {
+        event: eventNoProducts,
+        useDefaultMappings: true,
+        settings: settings
+      })
+    ).rejects.toThrowError("The root value is missing the required field 'products'.")
+  })
+
+  it('should throw an error if revenue missing', async () => {
+    await expect(
+      testDestination.testAction(actionSlug, {
+        event: eventNoRevenue,
+        useDefaultMappings: true,
+        settings: settings
+      })
+    ).rejects.toThrowError("The root value is missing the required field 'revenue'.")
+  })
 })
--- packages/destination-actions/src/destinations/movable-ink/search/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/movable-ink/search/__tests__/index.test.ts
@@ -11,11 +11,6 @@ const settings: Settings = {
     password: 'test'
 }
 
-const settingsNoMovableInkURL: Settings = {
-  username: 'test',
-  password: 'test'
-}
-
 const event = createTestEvent({
   type: 'track',
   event: 'Search',
@@ -47,43 +42,6 @@ describe('MovableInk.search', () => {
       event,
       settings: settings,
       useDefaultMappings: true
-    })
-
-    expect(responses.length).toBe(1)
-    expect(responses[0].status).toBe(200)
-    expect(responses[0].options.json).toMatchObject(
-      {
-        event_name: 'Search',
-        user_id: 'user1234', 
-        anonymous_id: '72d7bed1-4f42-4f2f-8955-72677340546b', 
-        timestamp: '2022-03-30T17:24:58Z',
-        query: 'transformer toys',
-        url: 'https://www.transformertoys.com'
-      }
-    )
-  })
-
-  it('should send search event to Movable Ink if settings url not provided but if properties.url provided', async () => {
-    nock(settings.movable_ink_url as string).post(/.*/).reply(200)
-
-    const responses = await testDestination.testAction(actionSlug, {
-      event: {
-        ...event, 
-        properties: {
-          ...event.properties, 
-          movable_ink_url: 'https://www.test.com'
-        }
-      },
-      settings: settingsNoMovableInkURL,
-      mapping: {
-        movable_ink_url: { '@path': '$.properties.movable_ink_url'},
-        timestamp:  { '@path': '$.timestamp'},
-        query: { '@path': '$.properties.query' },
-        anonymous_id: { '@path': '$.anonymousId' },
-        user_id: { '@path': '$.userId' },
-        query_url: { '@path': '$.properties.url' }
-      },
-      useDefaultMappings: false
     })
 
     expect(responses.length).toBe(1)
