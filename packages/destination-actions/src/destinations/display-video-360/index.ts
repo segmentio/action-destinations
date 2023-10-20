c9f58df345a6018b28246149d38871d248d7dad4
Nick Aguilar <nicholas.aguilar@segment.com>
2023-10-20 15:24:34 -0700

    OAuth 2 scheme for DV360

--- packages/destination-actions/src/destinations/display-video-360/index.ts
+++ packages/destination-actions/src/destinations/display-video-360/index.ts
@@ -9,6 +9,17 @@ const destination: DestinationDefinition<Settings> = {
   name: 'Display and Video 360 (Actions)',
   slug: 'actions-display-video-360',
   mode: 'cloud',
+  authentication: {
+    scheme: 'oauth2',
+    fields: {}
+  },
+  extendRequest({ auth }) {
+    return {
+      headers: {
+        authorization: `Bearer ${auth?.accessToken}`
+      }
+    }
+  },
   actions: {
     addToAudience,
     removeFromAudience
