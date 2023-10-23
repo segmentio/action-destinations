39b10035d3870988aacb6ab82f55e3e45dfe7749
Elena Parshina <elena.parshina@segment.com>
2023-10-23 10:45:59 -0700

    Yahoo: refactored action field types

--- packages/destination-actions/src/destinations/yahoo-audiences/updateSegment/generated-types.ts
+++ packages/destination-actions/src/destinations/yahoo-audiences/updateSegment/generated-types.ts
@@ -20,14 +20,6 @@ export interface Payload {
    */
   segment_computation_action: string
   /**
-   * Enable batching of requests
-   */
-  enable_batching?: boolean
-  /**
-   * Maximum number of events to include in each batch. Actual batch sizes may be lower.
-   */
-  batch_size?: number
-  /**
    * Email address of a user
    */
   email?: string
