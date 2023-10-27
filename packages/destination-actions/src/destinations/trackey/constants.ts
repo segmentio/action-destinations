73e526aecb807146691dcc1d9734fab121689da0
Javier Morales de Vera <javierMorales9@gmail.com>
2023-10-27 12:59:18 +0200

    fix issue

--- packages/destination-actions/src/destinations/trackey/constants.ts
+++ packages/destination-actions/src/destinations/trackey/constants.ts
@@ -1,2 +1,2 @@
 const base = 'https://app.trackey.io'
-export const baseUrl = base + 'public-api/integrations/segment/webhook'
+export const baseUrl = base + '/public-api/integrations/segment/webhook'
