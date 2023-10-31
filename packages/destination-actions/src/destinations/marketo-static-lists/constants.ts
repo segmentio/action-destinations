852f4d159d5a281616be42c79684bf3103ab88a7
Maryam Sharif <maryam.sharif@segment.com>
2023-10-31 16:52:53 -0700

    Cleanup and address comments

--- packages/destination-actions/src/destinations/marketo-static-lists/constants.ts
+++ packages/destination-actions/src/destinations/marketo-static-lists/constants.ts
@@ -1,11 +1,17 @@
 import { RequestClient } from '@segment/actions-core/*'
 import { Settings } from './generated-types'
 
+const API_VERSION = 'v1'
+const OAUTH_ENDPOINT = 'identity/oauth/token'
+export const GET_FOLDER_ENDPOINT = `/rest/asset/${API_VERSION}/folder/byName.json?name=folderName`
+export const CREATE_LIST_ENDPOINT = `/rest/asset/${API_VERSION}/staticLists.json?folder=folderId&name=listName`
+export const GET_LIST_ENDPOINT = `/rest/asset/${API_VERSION}/staticList/listId.json`
+
 export interface RefreshTokenResponse {
   access_token: string
 }
 
-export interface MarketoResonse {
+export interface MarketoResponse {
   success: boolean
   errors: [
     {
@@ -22,7 +28,7 @@ export interface MarketoResonse {
 }
 
 export async function getAccessToken(request: RequestClient, settings: Settings) {
-  const res = await request<RefreshTokenResponse>(`${settings.api_endpoint}/identity/oauth/token`, {
+  const res = await request<RefreshTokenResponse>(`${settings.api_endpoint}/${OAUTH_ENDPOINT}`, {
     method: 'POST',
     body: new URLSearchParams({
       client_id: settings.client_id,
--- packages/destination-actions/src/destinations/marketo-static-lists/index.ts
+++ packages/destination-actions/src/destinations/marketo-static-lists/index.ts
@@ -4,7 +4,13 @@ import type { Settings } from './generated-types'
 
 import addToList from './addToList'
 import removeFromList from './removeFromList'
-import { MarketoResonse, getAccessToken } from './constants'
+import {
+  MarketoResponse,
+  getAccessToken,
+  GET_FOLDER_ENDPOINT,
+  GET_LIST_ENDPOINT,
+  CREATE_LIST_ENDPOINT
+} from './constants'
 
 const destination: AudienceDestinationDefinition<Settings> = {
   name: 'Marketo Static Lists (Actions)',
@@ -57,91 +63,93 @@ const destination: AudienceDestinationDefinition<Settings> = {
       full_audience_sync: false // If true, we send the entire audience. If false, we just send the delta.
     },
     async createAudience(request, createAudienceInput) {
-      const audience_name = createAudienceInput.audienceName
+      const audienceName = createAudienceInput.audienceName
       const folder = createAudienceInput.settings.folder_name
       const endpoint = createAudienceInput.settings.api_endpoint
       const statsClient = createAudienceInput?.statsContext?.statsClient
       const statsTags = createAudienceInput?.statsContext?.tags
 
-      if (!audience_name) {
+      if (!audienceName) {
         throw new IntegrationError('Missing audience name value', 'MISSING_REQUIRED_FIELD', 400)
       }
 
       // Get access token
-      const access_token = await getAccessToken(request, createAudienceInput.settings)
+      const accessToken = await getAccessToken(request, createAudienceInput.settings)
+
+      const getFolderUrl = endpoint + GET_FOLDER_ENDPOINT.replace('folderName', encodeURIComponent(folder))
 
       // Get folder ID by name
-      const get_folder_response = await request<MarketoResonse>(
-        `${endpoint}/rest/asset/v1/folder/byName.json?name=${encodeURIComponent(folder)}`,
-        {
-          method: 'GET',
-          headers: {
-            authorization: `Bearer ${access_token}`
-          }
+      const getFolderResponse = await request<MarketoResponse>(getFolderUrl, {
+        method: 'GET',
+        headers: {
+          authorization: `Bearer ${accessToken}`
         }
-      )
+      })
 
       // Since the API will return 200 we need to parse the response to see if it failed.
-      if (!get_folder_response.data.success && get_folder_response.data.errors) {
+      if (!getFolderResponse.data.success && getFolderResponse.data.errors) {
         statsClient?.incr('createAudience.error', 1, statsTags)
-        throw new IntegrationError(`${get_folder_response.data.errors[0].message}`, 'INVALID_RESPONSE', 400)
+        throw new IntegrationError(`${getFolderResponse.data.errors[0].message}`, 'INVALID_RESPONSE', 400)
       }
 
-      if (!get_folder_response.data.result) {
+      if (!getFolderResponse.data.result) {
         statsClient?.incr('createAudience.error', 1, statsTags)
         throw new IntegrationError(`A folder with the name ${folder} not found`, 'INVALID_REQUEST_DATA', 400)
       }
 
-      const folder_id = get_folder_response.data.result[0].id
+      const folderId = getFolderResponse.data.result[0].id.toString()
+
+      const createListUrl =
+        endpoint +
+        CREATE_LIST_ENDPOINT.replace('folderId', folderId).replace('listName', encodeURIComponent(audienceName))
 
       // Create list in given folder
-      const create_list_response = await request<MarketoResonse>(
-        `${endpoint}/rest/asset/v1/staticLists.json?folder=${folder_id}&name=${encodeURIComponent(audience_name)}`,
-        {
-          method: 'POST',
-          headers: {
-            authorization: `Bearer ${access_token}`
-          }
+      const createListResponse = await request<MarketoResponse>(createListUrl, {
+        method: 'POST',
+        headers: {
+          authorization: `Bearer ${accessToken}`
         }
-      )
+      })
 
-      if (!create_list_response.data.success || create_list_response.data.errors || !create_list_response.data.result) {
+      if (!createListResponse.data.success && createListResponse.data.errors) {
         statsClient?.incr('createAudience.error', 1, statsTags)
-        throw new IntegrationError(`${create_list_response.data.errors[0].message}`, 'INVALID_RESPONSE', 400)
+        throw new IntegrationError(`${createListResponse.data.errors[0].message}`, 'INVALID_RESPONSE', 400)
       }
 
-      const external_id = create_list_response.data.result[0].id.toString()
+      const externalId = createListResponse.data.result[0].id.toString()
       statsClient?.incr('createAudience.success', 1, statsTags)
 
       return {
-        externalId: external_id
+        externalId: externalId
       }
     },
     async getAudience(request, getAudienceInput) {
       const endpoint = getAudienceInput.settings.api_endpoint
-      const list_id = getAudienceInput.externalId
+      const listId = getAudienceInput.externalId
       const statsClient = getAudienceInput?.statsContext?.statsClient
       const statsTags = getAudienceInput?.statsContext?.tags
 
       // Get access token
-      const access_token = await getAccessToken(request, getAudienceInput.settings)
+      const accessToken = await getAccessToken(request, getAudienceInput.settings)
 
-      const response = await request<MarketoResonse>(`${endpoint}/rest/asset/v1/staticList/${list_id}.json`, {
+      const getListUrl = endpoint + GET_LIST_ENDPOINT.replace('listId', listId)
+
+      const getListResponse = await request<MarketoResponse>(getListUrl, {
         method: 'GET',
         headers: {
-          authorization: `Bearer ${access_token}`
+          authorization: `Bearer ${accessToken}`
         }
       })
 
-      if (!response.data.success || response.data.errors || !response.data.result) {
+      if (!getListResponse.data.success && getListResponse.data.errors) {
         statsClient?.incr('getAudience.error', 1, statsTags)
-        throw new IntegrationError(`${response.data.errors[0].message}`, 'INVALID_RESPONSE', 400)
+        throw new IntegrationError(`${getListResponse.data.errors[0].message}`, 'INVALID_RESPONSE', 400)
       }
 
       statsClient?.incr('getAudience.success', 1, statsTags)
 
       return {
-        externalId: list_id
+        externalId: listId
       }
     }
   },
