b3c039b6206db77fe8daca25bd36267de1f4a7b0
Andrew Byrd <andrewbyrd@hey.com>
2023-11-15 08:21:01 -0500

    APP-86828 Pendo browser destination (#1683)
    * APP-86828 update pendo browser destination
    
    * APP-86828 set fields to readOnly, will need to test if this works as expected
    
    * APP-86828 change default region to value from label
    
    * APP-86828 update region labels and add jp
    
    * APP-86828 add CNAME configuration support
    
    * APP-86828 add parentAccount field for group()
    
    * APP-86828 remove flushNow call and update identify unit test

--- packages/browser-destinations/destinations/pendo-web-actions/src/identify/__tests__/index.test.ts
+++ packages/browser-destinations/destinations/pendo-web-actions/src/identify/__tests__/index.test.ts
@@ -15,9 +15,6 @@ const subscriptions: Subscription[] = [
       },
       visitorData: {
         '@path': '$.traits'
-      },
-      accountId: {
-        '@path': '$.context.group_id'
       }
     }
   }
@@ -26,7 +23,6 @@ const subscriptions: Subscription[] = [
 describe('Pendo.identify', () => {
   const settings = {
     apiKey: 'abc123',
-    setVisitorIdOnLoad: 'disabled',
     region: 'io'
   }
 
@@ -46,7 +42,8 @@ describe('Pendo.identify', () => {
         initialize: jest.fn(),
         isReady: jest.fn(),
         track: jest.fn(),
-        identify: jest.fn()
+        identify: jest.fn(),
+        flushNow: jest.fn()
       }
       return Promise.resolve(mockPendo)
     })
@@ -59,15 +56,11 @@ describe('Pendo.identify', () => {
       userId: 'testUserId',
       traits: {
         first_name: 'Jimbo'
-      },
-      context: {
-        group_id: 'company_id_1'
       }
     })
     await identifyAction.identify?.(context)
 
     expect(mockPendo.identify).toHaveBeenCalledWith({
-      account: { id: 'company_id_1' },
       visitor: { first_name: 'Jimbo', id: 'testUserId' }
     })
   })
--- packages/browser-destinations/destinations/pendo-web-actions/src/index.ts
+++ packages/browser-destinations/destinations/pendo-web-actions/src/index.ts
@@ -2,7 +2,8 @@ import type { Settings } from './generated-types'
 import type { BrowserDestinationDefinition } from '@segment/browser-destination-runtime/types'
 import { browserDestination } from '@segment/browser-destination-runtime/shim'
 import { loadPendo } from './loadScript'
-import { InitializeData, PendoSDK } from './types'
+import { PendoOptions, PendoSDK } from './types'
+import { ID } from '@segment/analytics-next'
 
 import identify from './identify'
 import track from './track'
@@ -28,90 +29,68 @@ export const destination: BrowserDestinationDefinition<Settings, PendoSDK> = {
       type: 'string',
       required: true
     },
-    accountId: {
-      label: 'Set Pendo Account ID on Load',
-      description:
-        'Segment can set the Pendo Account ID upon page load. This can be overridden via the Account ID field in the Send Identify/Group Actions',
-      type: 'string',
-      required: false
-    },
-    parentAccountId: {
-      label: 'Set Pendo Parent Account ID on Load',
-      description:
-        'Segment can set the Pendo Parent Account ID upon page load. This can be overridden via the Parent Account ID field in the Send Identify/Group Actions. Note: Contact Pendo to request enablement of Parent Account feature.',
-      type: 'string',
-      required: false
-    },
     region: {
       label: 'Region',
       type: 'string',
-      description: "The Pendo Region you'd like to send data to",
+      description: 'The region for your Pendo subscription.',
       required: true,
-      default: 'io',
+      default: 'https://cdn.pendo.io',
       choices: [
-        { value: 'io', label: 'io' },
-        { value: 'eu', label: 'eu' }
+        { value: 'https://cdn.pendo.io', label: 'US (default)' },
+        { value: 'https://cdn.eu.pendo.io', label: 'EU' },
+        { value: 'https://us1.cdn.pendo.io', label: 'US restricted' },
+        { value: 'https://cdn.jpn.pendo.io', label: 'Japan' }
       ]
     },
-    setVisitorIdOnLoad: {
-      label: 'Set Vistor ID on Load',
+    cnameContentHost: {
+      label: 'Optional CNAME content host',
       description:
-        'Segment can set the Pendo Visitor ID upon page load to either the Segment userId or anonymousId. This can be overridden via the Visitor ID field in the Send Identify/Group Actions',
+        "If you are using Pendo's CNAME feature, this will update your Pendo install snippet with your content host.",
       type: 'string',
-      default: 'disabled',
-      choices: [
-        { value: 'disabled', label: 'Do not set Visitor ID on load' },
-        { value: 'userIdOnly', label: 'Set Visitor ID to userId on load' },
-        { value: 'userIdOrAnonymousId', label: 'Set Visitor ID to userId or anonymousId on load' },
-        { value: 'anonymousIdOnly', label: 'Set Visitor ID to anonymousId on load' }
-      ],
-      required: true
+      required: false
     }
   },
 
   initialize: async ({ settings, analytics }, deps) => {
-    loadPendo(settings.apiKey, settings.region)
+    if (settings.cnameContentHost && !/^https?:/.exec(settings.cnameContentHost) && settings.cnameContentHost.length) {
+      settings.cnameContentHost = 'https://' + settings.cnameContentHost
+    }
+
+    loadPendo(settings.apiKey, settings.region, settings.cnameContentHost)
 
     await deps.resolveWhen(() => window.pendo != null, 100)
 
-    const initialData: InitializeData = {}
+    let visitorId: ID = null
+    let accountId: ID = null
 
-    if (settings.setVisitorIdOnLoad) {
-      let vistorId: string | null = null
-
-      switch (settings.setVisitorIdOnLoad) {
-        case 'disabled':
-          vistorId = null
-          break
-        case 'userIdOnly':
-          vistorId = analytics.user().id() ?? null
-          break
-        case 'userIdOrAnonymousId':
-          vistorId = analytics.user().id() ?? analytics.user().anonymousId() ?? null
-          break
-        case 'anonymousIdOnly':
-          vistorId = analytics.user().anonymousId() ?? null
-          break
-      }
-
-      if (vistorId) {
-        initialData.visitor = {
-          id: vistorId
-        }
-      }
-    }
-    if (settings.accountId) {
-      initialData.account = {
-        id: settings.accountId
-      }
-    }
-    if (settings.parentAccountId) {
-      initialData.parentAccount = {
-        id: settings.parentAccountId
-      }
+    if (analytics.user().id()) {
+      visitorId = analytics.user().id()
+    } else if (analytics.user().anonymousId()) {
+      // Append Pendo anonymous visitor tag
+      // https://github.com/segmentio/analytics.js-integrations/blob/master/integrations/pendo/lib/index.js#L114
+      visitorId = '_PENDO_T_' + analytics.user().anonymousId()
     }
 
-    window.pendo.initialize(initialData)
+    if (analytics.group().id()) {
+      accountId = analytics.group().id()
+    }
+
+    const options: PendoOptions = {
+      visitor: {
+        ...analytics.user().traits(),
+        id: visitorId
+      },
+      ...(accountId
+        ? {
+            account: {
+              ...analytics.group().traits(),
+              id: accountId
+            }
+          }
+        : {})
+    }
+
+    window.pendo.initialize(options)
 
     return window.pendo
   },
