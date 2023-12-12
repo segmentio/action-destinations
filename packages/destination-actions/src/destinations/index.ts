25fd6a1c0b4b83a51673b5565e4fb54846df6d33
Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
2023-12-12 19:31:47 +0100

    Registering responsys in staging

--- packages/destination-actions/src/destinations/index.ts
+++ packages/destination-actions/src/destinations/index.ts
@@ -141,7 +141,7 @@ register('6537b55db9e94b2e110c9cf9', './movable-ink')
 register('6537b5da8f27fd20713a5ba8', './usermotion')
 register('6554dc58634812f080d83a23', './canvas')
 register('656f2474a919b7e6e4900265', './gleap')
-register('6578a19fbd1201d21f035156', './responsys')
+register('6578a6aad6619f2cba205727', './responsys')
 
 
 function register(id: MetadataId, destinationPath: string) {
