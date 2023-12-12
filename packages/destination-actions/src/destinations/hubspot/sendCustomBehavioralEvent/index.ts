e8f15ae0f92d00016aa68aa185761a17a3446686
Gaurav Kochar <gaurav.kochar@segment.com>
2023-12-12 15:58:40 +0530

    quick fix

--- packages/destination-actions/src/destinations/hubspot/sendCustomBehavioralEvent/index.ts
+++ packages/destination-actions/src/destinations/hubspot/sendCustomBehavioralEvent/index.ts
@@ -67,9 +67,9 @@ const action: ActionDefinition<Settings, Payload> = {
       defaultObjectUI: 'keyvalue:only'
     }
   },
-  perform: (request, { payload, settings }) => {
-    const eventName = transformEventName(payload.eventName);
-    
+  perform: (request, { payload, settings, statsContext }) => {
+    const eventName = transformEventName(payload.eventName)
+
     const event: CustomBehavioralEvent = {
       eventName: eventName,
       occurredAt: payload.occurredAt,
