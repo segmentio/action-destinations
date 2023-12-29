a73dc6d318673753c6394b16f407137f4f171fe0
Harsh Vardhan <harsh.vardhan@segment.com>
2023-12-29 13:05:25 +0530

    Register amazon ads

--- packages/destination-actions/src/destinations/index.ts
+++ packages/destination-actions/src/destinations/index.ts
@@ -141,6 +141,7 @@ register('6537b55db9e94b2e110c9cf9', './movable-ink')
 register('6537b5da8f27fd20713a5ba8', './usermotion')
 register('6554dc58634812f080d83a23', './canvas')
 register('656f2474a919b7e6e4900265', './gleap')
+register('658c0e76cec9f7cf46533eed', './amazon-ads')
 
 function register(id: MetadataId, destinationPath: string) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
