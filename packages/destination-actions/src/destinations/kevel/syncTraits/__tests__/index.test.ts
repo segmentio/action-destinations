3cc39e7d55721863d50116e0b215a13ddc21e866
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-01-02 15:45:53 +0100

    adding unit tests

--- packages/destination-actions/src/destinations/kevel/syncTraits/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/kevel/syncTraits/__tests__/index.test.ts
@@ -4,6 +4,47 @@ import Destination from '../../index'
 
 const testDestination = createTestIntegration(Destination)
 
+const goodIdentifyEvent = createTestEvent({
+  type: 'identify',
+  userId: 'uid1',
+
+  traits: {
+    first_name: 'Billy',
+    last_name: 'Bob'
+  }
+})
+
 describe('Kevel.syncTraits', () => {
-  // TODO: Test your action
+  it('should fetch and merge traits, and then not throw an error - track', async () => {
+    const userId = 'uid1'
+    const networkId1 = 'networkId1'
+    const baseUrl = `https://e-${networkId1}.adzerk.net/udb/${networkId1}`
+
+    const allTraits = {
+      first_name: 'Billy',
+      last_name: 'Bob',
+      age: 2
+    }
+
+    nock(baseUrl)
+      .get(`/read?userKey=${userId}`)
+      .reply(200, {
+        custom: {
+          age: 24
+        }
+      })
+
+    nock(baseUrl).post(`/customProperties?userKey=${userId}`, allTraits).reply(200)
+
+    await expect(
+      testDestination.testAction('syncTraits', {
+        event: goodIdentifyEvent,
+        settings: {
+          networkId: networkId1,
+          apiKey: 'apiKey1'
+        },
+        useDefaultMappings: true
+      })
+    ).resolves.not.toThrowError()
+  })
 })
