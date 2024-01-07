75aa1480566e928e239b21b452f834c61c27c47f
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-01-07 14:52:18 +0100

    adding test

--- packages/browser-destinations/destinations/algolia-plugins/src/__tests__/index.test.ts
+++ packages/browser-destinations/destinations/algolia-plugins/src/__tests__/index.test.ts
@@ -1,4 +1,46 @@
-import { Analytics, Context } from '@segment/analytics-next'
-import plugins, { destination } from '../index'
+import { Analytics, Context, Plugin } from '@segment/analytics-next'
+import { Subscription } from '@segment/browser-destination-runtime/types'
+import browserPluginsDestination from '../'
+import { queryIdIntegrationFieldName } from '../utils'
 
-describe('Algolia Plugins', () => {})
+const example: Subscription[] = [
+  {
+    partnerAction: 'algoliaPlugin',
+    name: 'Algolia Plugin',
+    enabled: true,
+    subscribe: 'type = "track"',
+    mapping: {}
+  }
+]
+
+let browserActions: Plugin[]
+let algoliaPlugin: Plugin
+let ajs: Analytics
+
+beforeEach(async () => {
+  browserActions = await browserPluginsDestination({ subscriptions: example })
+  algoliaPlugin = browserActions[0]
+
+  ajs = new Analytics({
+    writeKey: 'w_123'
+  })
+})
+
+describe('ajs-integration', () => {
+  test('updates the original event with an Algolia query ID', async () => {
+    await algoliaPlugin.load(Context.system(), ajs)
+
+    const ctx = new Context({
+      type: 'track',
+      event: 'Test Event',
+      properties: {
+        greeting: 'Yo!'
+      }
+    })
+
+    const updatedCtx = await algoliaPlugin.track?.(ctx)
+
+    const algoliaIntegrationsObj = updatedCtx?.event?.integrations['Algolia Insights (Actions)']
+    expect(algoliaIntegrationsObj[queryIdIntegrationFieldName]).toEqual('test_data')
+  })
+})
