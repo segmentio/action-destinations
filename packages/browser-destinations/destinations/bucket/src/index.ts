c2c349b36571d450bf21168f8cfd26f2660a72d4
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-01-08 14:07:11 +0100

    adding change to bucket web integration from PR 1777

--- packages/browser-destinations/destinations/bucket/src/index.ts
+++ packages/browser-destinations/destinations/bucket/src/index.ts
@@ -60,10 +60,21 @@ export const destination: BrowserDestinationDefinition<Settings, Bucket> = {
   },
 
   initialize: async ({ settings, analytics }, deps) => {
-    await deps.loadScript('https://cdn.jsdelivr.net/npm/@bucketco/tracking-sdk@2')
+    const {
+      // @ts-expect-error versionSettings is not part of the settings object but they are injected by Analytics 2.0, making Braze SDK raise a warning when we initialize it.
+      versionSettings,
+      // @ts-expect-error same as above.
+      subscriptions,
+
+      trackingKey,
+      // @ts-expect-error Code-only SDK version override. Can be set via analytics.load() integrations overrides
+      sdkVersion = '2',
+      ...options
+    } = settings
+    await deps.loadScript(`https://cdn.jsdelivr.net/npm/@bucketco/tracking-sdk@${sdkVersion}`)
     await deps.resolveWhen(() => window.bucket != undefined, 100)
 
-    window.bucket.init(settings.trackingKey)
+    window.bucket.init(settings.trackingKey, options)
 
     // If the analytics client already has a logged in user from a
     // previous session or page, consider the user logged in.
