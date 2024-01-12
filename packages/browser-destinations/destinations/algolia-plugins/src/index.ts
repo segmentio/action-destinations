61ff1b83c5f32b21ee29c7c70cd41bdbf47c570b
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-01-12 14:54:38 +0000

    tested queryID code

--- packages/browser-destinations/destinations/algolia-plugins/src/index.ts
+++ packages/browser-destinations/destinations/algolia-plugins/src/index.ts
@@ -2,7 +2,7 @@ import type { Settings } from './generated-types'
 import type { BrowserDestinationDefinition } from '@segment/browser-destination-runtime/types'
 import { browserDestination } from '@segment/browser-destination-runtime/shim'
 import { UniversalStorage } from '@segment/analytics-next'
-import { storageFallback, storageQueryIdKey, getQueryID } from './utils'
+import { storageFallback, storageQueryIdKey, queryIdQueryStringNameDefault } from './utils'
 
 import algoliaPlugin from './algoliaPlugin'
 
@@ -11,11 +11,23 @@ export const destination: BrowserDestinationDefinition<Settings, {}> = {
   name: 'Algolia Plugins',
   slug: 'actions-algolia-plugins',
   mode: 'device',
-  settings: {},
-  initialize: async ({ analytics }) => {
+  settings: {
+    queryIdQueryStringName: {
+      label: 'QueryID QueryString Name',
+      description: 'QueryString name you use for when storing the Algolia QueryID in a page URL.',
+      type: 'string',
+      default: queryIdQueryStringNameDefault,
+      required: false
+    }
+  },
+  initialize: async ({ analytics, settings }) => {
     const storage = (analytics.storage as UniversalStorage<Record<string, string>>) ?? storageFallback
 
-    const queryId: string | null = getQueryID()
+    const urlParams = new URLSearchParams(window.location.search)
+
+    const queryId: string | null =
+      urlParams.get(settings.queryIdQueryStringName ?? queryIdQueryStringNameDefault) || null
+
     if (queryId) {
       storage.set(storageQueryIdKey, queryId)
     }
