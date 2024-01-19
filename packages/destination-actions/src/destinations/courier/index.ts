80e1b2d5d61f031b78ab8f3d0cdd716d5ed664ee
Bryan Weber <bryan@courier.com>
2024-01-19 11:54:13 -0800

    removing GDPR delete and testAuth, will implement later

--- packages/destination-actions/src/destinations/courier/index.ts
+++ packages/destination-actions/src/destinations/courier/index.ts
@@ -34,22 +34,8 @@ const destination: DestinationDefinition<Settings> = {
         ],
         required: true
       }
-    },
-    testAuthentication: (request) => {
-      // Return a request that tests/validates the user's credentials.
-      // If you do not have a way to validate the authentication fields safely,
-      // you can remove the `testAuthentication` function, though discouraged.
-      return true
     }
   },
-
-  onDelete: async (request, { settings, payload }) => {
-    // Return a request that performs a GDPR delete for the provided Segment userId or anonymousId
-    // provided in the payload. If your destination does not support GDPR deletion you should not
-    // implement this function and should remove it completely.
-    return true
-  },
-
   actions: {
     postToCourier
   }
