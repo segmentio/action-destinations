28d8fdd5b0a0a0a8d8e4a257763a780cf8b79bf7
Bryan Weber <bryan@courier.com>
2024-01-19 11:33:04 -0800

    throwing new PayloadValidation Error

--- packages/destination-actions/src/destinations/courier/postToCourier/index.ts
+++ packages/destination-actions/src/destinations/courier/postToCourier/index.ts
@@ -1,4 +1,5 @@
 import type { ActionDefinition } from '@segment/actions-core'
+import { PayloadValidationError } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 
@@ -6,18 +7,22 @@ const action: ActionDefinition<Settings, Payload> = {
   title: 'Forward to Courier',
   description: 'Forward track, group and identify events to Courier',
   fields: {},
-  // defaultSubscription: 'type = "track" or type = "group" or type = "identify"',
+  defaultSubscription: `type = "track" or type = "identify" or type = "group"`,
   perform: (request, { settings, payload }) => {
     const domain = `https://api.${settings.region === 'EU' ? 'eu.' : ''}courier.com`
     const headers = {
       Authorization: `Bearer ${settings.apiKey}`,
       'Content-Type': 'application/json'
     }
-    return request(`${domain}/inbound/segment`, {
-      method: 'POST',
-      headers,
-      json: payload
-    })
+    if (!['track', 'identify', 'group'].includes(payload['type'])) {
+      throw new PayloadValidationError(`Invalid event type: ${payload['type']}`)
+    } else {
+      return request(`${domain}/inbound/segment`, {
+        method: 'POST',
+        headers,
+        json: payload
+      })
+    }
   }
 }
 
