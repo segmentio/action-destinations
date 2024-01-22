266ed5e3997befa82e4ddc4ad7d7c2356d5dcad3
Ankit Gupta <139338151+AnkitSegment@users.noreply.github.com>
2024-01-22 17:02:28 +0530

    [STRATCONN] [GA4] added user properties in all event (#1807)
    * added user properties in all event
    
    * added user_properties in ga4 web
    
    * Change in customEvent GA4

--- packages/browser-destinations/destinations/google-analytics-4-web/src/viewItemList/index.ts
+++ packages/browser-destinations/destinations/google-analytics-4-web/src/viewItemList/index.ts
@@ -3,7 +3,6 @@ import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 
 import { user_properties, params, user_id, items_multi_products, item_list_name, item_list_id } from '../ga4-properties'
-import { updateUser } from '../ga4-functions'
 
 const action: BrowserActionDefinition<Settings, Function, Payload> = {
   title: 'View Item List',
@@ -22,12 +21,12 @@ const action: BrowserActionDefinition<Settings, Function, Payload> = {
     params: params
   },
   perform: (gtag, { payload }) => {
-    updateUser(payload.user_id, payload.user_properties, gtag)
-
     gtag('event', 'view_item_list', {
       item_list_id: payload.item_list_id,
       item_list_name: payload.item_list_name,
       items: payload.items,
+      user_id: payload.user_id ?? undefined,
+      user_properties: payload.user_properties,
       ...payload.params
     })
   }
