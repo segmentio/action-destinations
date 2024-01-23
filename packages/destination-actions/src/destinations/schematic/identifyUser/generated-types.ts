085c782ce86b04f427551b2619b8aedb20ebf8e4
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-01-23 14:49:59 +0000

    adding schematic Destination

---
+++ packages/destination-actions/src/destinations/schematic/identifyUser/generated-types.ts
@@ -0,0 +1,50 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * Key-value pairs associated with a company (e.g. organization_id: 123456)
+   */
+  company_keys?: {
+    /**
+     * Segment groupId
+     */
+    groupId?: string
+    /**
+     * Organization ID
+     */
+    organization_id?: string
+  }
+  /**
+   * Name of company
+   */
+  company_name?: string
+  /**
+   * Properties associated with company
+   */
+  company_traits?: {
+    [k: string]: unknown
+  }
+  /**
+   * Key-value pairs associated with a user (e.g. email: example@example.com)
+   */
+  user_keys: {
+    /**
+     * Email address
+     */
+    email_address?: string
+    /**
+     * Segment userId
+     */
+    userId?: string
+  }
+  /**
+   * User's full name
+   */
+  user_name?: string
+  /**
+   * Properties associated with user
+   */
+  user_traits?: {
+    [k: string]: unknown
+  }
+}
