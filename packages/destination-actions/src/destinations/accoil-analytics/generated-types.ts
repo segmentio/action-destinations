a10a4c2e70a794f78eec87aebb8ffdf1b6c1d9ff
Jeremiah L Kwarteng <jlkwarteng@gmail.com>
2024-01-28 22:30:12 +0000

    Generated Types

--- packages/destination-actions/src/destinations/accoil-analytics/generated-types.ts
+++ packages/destination-actions/src/destinations/accoil-analytics/generated-types.ts
@@ -1,3 +1,8 @@
 // Generated file. DO NOT MODIFY IT BY HAND.
 
-export interface Settings {}
+export interface Settings {
+  /**
+   * Your Accoil.com API Key. This key requires Write permissions.
+   */
+  api_key: string
+}
--- packages/destination-actions/src/destinations/accoil-analytics/index.ts
+++ packages/destination-actions/src/destinations/accoil-analytics/index.ts
@@ -5,24 +5,42 @@ const destination: DestinationDefinition<Settings> = {
   name: 'Accoil Analytics',
   slug: 'actions-accoil-analytics',
   mode: 'cloud',
-
   authentication: {
     scheme: 'custom',
-    fields: {}
-    // testAuthentication: (request) => {
-    //   // Return a request that tests/validates the user's credentials.
-    //   // If you do not have a way to validate the authentication fields safely,
-    //   // you can remove the `testAuthentication` function, though discouraged.
-    // }
+    fields: {
+      api_key: {
+        label: 'API Key',
+        description: 'Your Accoil.com API Key. This key requires Write permissions.',
+        type: 'password',
+        required: true
+      }
+    },
+    testAuthentication: async (request, { settings }) => {
+      try {
+        return await request(`https://in.accoil.com/segment`, {
+          method: 'get',
+          headers: {
+            'x-api-token': settings.api_key
+          }
+        })
+      } catch (e: any) {
+        // const error = e as AggregationsAuthError
+        if (e.response.data) {
+          const { message } = e.response.data
+          console.log('THIS IS ERROR', message)
+          // throw new InvalidAuthenticationError(message)
+        }
+        // throw new InvalidAuthenticationError('Error Validating Credentials')
+      }
+    }
+
+    // onDelete: async (request, { settings, payload }) => {
+    //   // Return a request that performs a GDPR delete for the provided Segment userId or anonymousId
+    //   // provided in the payload. If your destination does not support GDPR deletion you should not
+    //   // implement this function and should remove it completely.
+
+    // },
   },
-
-  // onDelete: async (request, { settings, payload }) => {
-  //   // Return a request that performs a GDPR delete for the provided Segment userId or anonymousId
-  //   // provided in the payload. If your destination does not support GDPR deletion you should not
-  //   // implement this function and should remove it completely.
-
-  // },
-
   actions: {}
 }
 
