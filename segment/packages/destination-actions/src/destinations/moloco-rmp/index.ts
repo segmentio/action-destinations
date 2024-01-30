2b6c4bd770e7d7aec21312692e18e6c48fc759a9
heonjang.lee <heonjang.lee@moloco.com>
2024-01-30 14:49:31 -0800

    Replace the default auth with the implemented object

--- segment/packages/destination-actions/src/destinations/moloco-rmp/index.ts
+++ segment/packages/destination-actions/src/destinations/moloco-rmp/index.ts
@@ -1,20 +1,13 @@
 import type { DestinationDefinition } from '@segment/actions-core'
 import type { Settings } from './generated-types'
+import { authentication } from './common/auth'
 
 const destination: DestinationDefinition<Settings> = {
   name: 'Moloco Rmp',
   slug: 'actions-moloco-rmp',
   mode: 'cloud',
 
-  authentication: {
-    scheme: 'custom',
-    fields: {},
-    testAuthentication: (request) => {
-      // Return a request that tests/validates the user's credentials.
-      // If you do not have a way to validate the authentication fields safely,
-      // you can remove the `testAuthentication` function, though discouraged.
-    }
-  },
+  authentication: authentication,
 
   onDelete: async (request, { settings, payload }) => {
     // Return a request that performs a GDPR delete for the provided Segment userId or anonymousId
