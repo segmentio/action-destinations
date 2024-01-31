ecd06275ffc765d9760dd1efdbe90101c1a26dc7
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-01-31 18:36:24 +0000

    adding generated types

--- packages/destination-actions/src/destinations/responsys/sendCustomTraits/generated-types.ts
+++ packages/destination-actions/src/destinations/responsys/sendCustomTraits/generated-types.ts
@@ -2,49 +2,19 @@
 
 export interface Payload {
   /**
-   * The user's email address
+   * <TODO>>
    */
-  email?: string
-  /**
-   * Responsys Customer ID
-   */
-  customer_id?: string
-  /**
-   * The Engage Audience's Key. This field is only used when syncing a Engage Audience to Responsys.
-   */
-  engage_audience_key?: string
-  /**
-   * Record data that represents Field Names and corresponding values for the recipient.
-   */
-  userData?: {
+  userData: {
+    /**
+     * The user's email address
+     */
+    EMAIL_ADDRESS_?: string
+    /**
+     * Responsys Customer ID.
+     */
+    CUSTOMER_ID_?: string
     [k: string]: unknown
   }
-  /**
-   * Hidden field: Used to capture Audience value from properties or traits
-   */
-  properties_or_traits: {
-    [k: string]: unknown
-  }
-  /**
-   * Not needed for Segment Destination, thus hidden: The Map Template in Responsys that can be used to map Field Names of the Profile List to Column Names.
-   */
-  mapTemplateName: string
-  /**
-   * Indicates what should be done for records where a match is not found.
-   */
-  insertOnNoMatch: boolean
-  /**
-   * First match column for determining whether an insert or update should occur.
-   */
-  matchColumnName1: string
-  /**
-   * Second match column for determining whether an insert or update should occur.
-   */
-  matchColumnName2?: string
-  /**
-   * Controls how the existing record should be updated. Defaults to Replace All.
-   */
-  updateOnMatch: string
   /**
    * Once enabled, Segment will collect events into batches of 200 before sending to Responsys.
    */
