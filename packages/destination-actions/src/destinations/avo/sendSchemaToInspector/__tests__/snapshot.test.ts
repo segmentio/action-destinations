1e23b385cd93e68f59f9459e64f3596f759a9067
bjornj12 <bjorn@avo.sh>
2024-02-02 12:04:59 +0000

    test to make sure we extract appVersion from a property if set in settings

--- packages/destination-actions/src/destinations/avo/sendSchemaToInspector/__tests__/snapshot.test.ts
+++ packages/destination-actions/src/destinations/avo/sendSchemaToInspector/__tests__/snapshot.test.ts
@@ -72,4 +72,40 @@ describe(`Testing snapshot for ${destinationSlug}'s ${actionSlug} destination ac
       expect(rawBody).toMatchSnapshot()
     }
   })
+
+  it('expect app Version to be extracted from property when set in settings', async () => {
+    const action = destination.actions[actionSlug]
+    const [eventData, settingsData] = generateTestData(seedName, destination, action, false)
+
+    nock(/.*/).persist().get(/.*/).reply(200)
+    nock(/.*/).persist().post(/.*/).reply(200)
+    nock(/.*/).persist().put(/.*/).reply(200)
+
+    const event = createTestEvent({
+      properties: {
+        ...eventData,
+        appVersion: '2.0.3'
+      }
+    })
+
+    settingsData.appVersionPropertyName = 'appVersion'
+
+    const responses = await testDestination.testAction(actionSlug, {
+      event: event,
+      mapping: event.properties,
+      settings: settingsData,
+      auth: undefined
+    })
+
+    const request = responses[0].request
+    const rawBody = await request.text()
+
+    try {
+      const json = JSON.parse(rawBody)
+      expect(json).toMatchSnapshot()
+      return
+    } catch (err) {
+      expect(rawBody).toMatchSnapshot()
+    }
+  })
 })
