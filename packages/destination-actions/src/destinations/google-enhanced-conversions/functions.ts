7ce68590f7e26ecff3b1d75378f91c8b0ad8aa14
Gaurav Kochar <gaurav.kochar@segment.com>
2024-02-06 12:44:27 +0530

    remove unnecessary changes

--- packages/destination-actions/src/destinations/google-enhanced-conversions/functions.ts
+++ packages/destination-actions/src/destinations/google-enhanced-conversions/functions.ts
@@ -18,7 +18,7 @@ import { StatsContext } from '@segment/actions-core/destination-kit'
 import { Features } from '@segment/actions-core/mapping-kit'
 import { fullFormats } from 'ajv-formats/dist/formats'
 
-export const API_VERSION = 'v14'
+export const API_VERSION = 'v15'
 export const CANARY_API_VERSION = 'v15'
 export const FLAGON_NAME = 'google-enhanced-canary-version'
 
