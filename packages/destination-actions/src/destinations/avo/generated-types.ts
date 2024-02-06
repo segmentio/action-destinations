be90748bd163239c4f924fc896b28fb594b0b3d1
Björn Ólafur Jóhannsson <bjornolafur@gmail.com>
2024-02-06 13:40:31 +0000

    Avo inspector destination (#1850)
    * avo inspector destination
    
    * define perform and performBatch as async
    
    * make env enum so its always valid
    
    * simplify our logic a bit by using the segment config to get the properties we need from context
    
    * update snapshots
    
    * rename destination to avo
    
    * rename action to sendSchemaToInspector
    
    * fix action name in tests so they pass
    
    * small polish
    
    * allowing optional field to be set to extract appVersion from a property in properties
    
    * refactor removeDuplicates to handle EventProperty object instead of any
    
    * test to make sure we extract appVersion from a property if set in settings
    
    * Update packages/destination-actions/src/destinations/avo/sendSchemaToInspector/index.ts
    
    Co-authored-by: Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
    
    * Update packages/destination-actions/src/destinations/avo/index.ts
    
    Co-authored-by: Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
    
    * just default to getting values from context, dont try to go into properties
    
    ---------
    
    Co-authored-by: Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>

---
+++ packages/destination-actions/src/destinations/avo/generated-types.ts
@@ -0,0 +1,16 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Settings {
+  /**
+   * Avo Inspector API Key
+   */
+  apiKey: string
+  /**
+   * Avo Inspector Environment
+   */
+  env: string
+  /**
+   * Optionally set which property represents the app version in your events. If not set, the app version will be taken from the $event.app.version
+   */
+  appVersionPropertyName?: string
+}
