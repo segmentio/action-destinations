e1bca4af705b50264ae3f73dab8b99a7b4a989a1
Ankit Gupta <ankit.gupta@segment.com>
2024-02-08 15:39:44 +0530

    code refactored

--- packages/destination-actions/src/destinations/criteo-audiences/addUserToAudience/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/criteo-audiences/addUserToAudience/__tests__/index.test.ts
@@ -1,101 +1,179 @@
-import { ActionDefinition } from '@segment/actions-core'
-import { getContactListId, patchContactList, hash } from '../criteo-audiences'
-import type { Operation, ClientCredentials } from '../criteo-audiences'
-import type { Settings } from '../generated-types'
-import type { Payload } from './generated-types'
-import type { RequestClient } from '@segment/actions-core'
+import nock from 'nock'
+import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import Destination from '../../index'
 
-const getOperationFromPayload = async (
-  request: RequestClient,
-  advertiser_id: string,
-  payload: Payload[],
-  credentials: ClientCredentials
-): Promise<Operation> => {
-  const add_user_list: string[] = []
-  let audience_key = ''
+const testDestination = createTestIntegration(Destination)
 
-  /*
-  The logic below assumes that all events within the batch will have the same audience_key
-  Customers should connect single audience to each instance of Criteo audience
-  */
+const VALID_ADVERTISER_ID = '12345'
+const INVALID_ADVERTISER_ID = 'abcd'
+const AUDIENCE_KEY = "weekly_active_shoppers_viewed_product_within_7_days"
 
-  for (const event of payload) {
-    let email = undefined
-
-    if (!audience_key && event.audience_key)
-      audience_key = event.audience_key
-
-    email = event.hash_emails ? hash(event.email) : event.email
-    if (email) add_user_list.push(email)
-  }
-
-  const contactlist_id = await getContactListId(request, advertiser_id, audience_key, credentials)
-  const operation: Operation = {
-    operation_type: "add",
-    contactlist_id: contactlist_id,
-    user_list: add_user_list,
-  }
-  return operation;
-}
-
-const processPayload = async (
-  request: RequestClient,
-  settings: Settings,
-  payload: Payload[]
-): Promise<Response> => {
-  const credentials: ClientCredentials = {
-    client_id: settings.client_id,
-    client_secret: settings.client_secret
-  }
-  const operation: Operation = await getOperationFromPayload(request, settings.advertiser_id, payload, credentials);
-  return await patchContactList(request, operation, credentials)
-}
-
-const action: ActionDefinition<Settings, Payload> = {
-  title: 'Add users to Audience',
-  description: 'Add users from Criteo audience by connecting to Criteo API',
-  defaultSubscription: 'type = "track" and event = "Audience Entered"',
-  fields: {
-    audience_key: {
-      label: 'Audience key',
-      description: "Unique name for personas audience",
-      type: 'string',
-      default: {
-        '@path': '$.properties.audience_key'
-      }
-    },
-    event: {
-      label: 'Event name',
-      description: "Event for audience entering or exiting",
-      type: 'string',
-      default: {
-        '@path': '$.event'
-      }
-    },
-    email: {
-      label: 'Email',
-      description: "The user's email",
-      type: 'string',
-      format: 'email',
-      default: {
-        '@path': '$.context.traits.email'
-      }
-    },
-    hash_emails: {
-      label: 'Hash Emails',
-      description: "Hash emails before sending them to Criteo (may lower your audience's match rate). If deactivated, emails will be sent unhashed to Criteo's API and will be hashed upon reception at Criteo's server.",
-      type: 'boolean',
-      default: false,
-      required: false
-    },
+const event = createTestEvent({
+  traits: {
+    email: "test.email@test.com"
   },
-  perform: async (request, { settings, payload }) => {
-    return await processPayload(request, settings, [payload])
-  },
-
-  performBatch: async (request, { settings, payload }) => {
-    return await processPayload(request, settings, payload)
-
+  properties: {
+    "audience_key": AUDIENCE_KEY,
   }
+})
+
+const MOCK_TOKEN_RESPONSE = {
+  "access_token": "token",
+  "token_type": "Bearer",
+  "refresh_token": null,
+  "expires_in": 900
 }
-export default action
+
+const VALID_SETTINGS = {
+  client_id: "client_id",
+  client_secret: "client_id",
+  advertiser_id: VALID_ADVERTISER_ID
+}
+
+const ADVERTISER_AUDIENCES = {
+  "meta": {
+    "totalItems": 1,
+    "limit": 0,
+    "offset": 0
+  },
+  "data": [
+    {
+      "id": "1234",
+      "attributes": {
+        "advertiserId": VALID_ADVERTISER_ID,
+        "name": AUDIENCE_KEY
+      }
+    }
+  ]
+}
+
+const ADVERTISER_AUDIENCES_KEY_DOES_NOT_EXIST = {
+  "meta": {
+    "totalItems": 1,
+    "limit": 0,
+    "offset": 0
+  },
+  "data": [
+    {
+      "id": "1234",
+      "attributes": {
+        "advertiserId": VALID_ADVERTISER_ID,
+        "name": "Other audience name"
+      }
+    }
+  ]
+}
+
+const AUDIENCE_CREATION_RESPONSE = {
+  "data": [
+    {
+      "attributes": {
+        "name": AUDIENCE_KEY,
+        "description": AUDIENCE_KEY,
+        "type": "ContactList",
+        "advertiserId": VALID_ADVERTISER_ID,
+        "contactList": {
+          "file": null,
+          "isFromPublicApi": true
+        }
+      },
+      "id": "5678",
+      "type": "AudienceSegment"
+    }
+  ],
+  "errors": [],
+  "warnings": []
+}
+
+const DUPLICATE_AUDIENCE_ERROR = {
+  "errors": [
+    {
+      "type": "validation",
+      "code": "name-must-be-unique",
+      "title": "Segment name must be unique",
+      "detail": "Another Segment exists with the name: ABCD"
+    }
+  ]
+}
+
+describe('addUserToAudience', () => {
+  it('should throw error if no access to the audiences of the advertiser', async () => {
+    const settings = VALID_SETTINGS;
+    nock('https://api.criteo.com').persist().post('/oauth2/token').reply(200, MOCK_TOKEN_RESPONSE)
+    nock('https://api.criteo.com').post(/^\/\d{4}-\d{2}\/marketing-solutions\/audience-segments\/search$/).query(true).reply(403)
+    await expect(
+      testDestination.testAction('addUserToAudience', {
+        event,
+        settings,
+        useDefaultMappings: true
+      })
+    ).rejects.toThrowError("Forbidden")
+  })
+
+  it('should throw error if advertiser ID is not a number', async () => {
+    nock('https://api.criteo.com').persist().post('/oauth2/token').reply(200, MOCK_TOKEN_RESPONSE)
+
+    const settings = {
+      client_id: "client_id",
+      client_secret: "client_secret",
+      advertiser_id: INVALID_ADVERTISER_ID
+    }
+
+    await expect(
+      testDestination.testAction('addUserToAudience', {
+        event,
+        settings,
+        useDefaultMappings: true
+      })
+    ).rejects.toThrowError('The Advertiser ID should be a number')
+  })
+
+  it('should not throw an error if the audience creation and the patch requests succeed', async () => {
+    const settings = VALID_SETTINGS;
+    nock('https://api.criteo.com').persist().post('/oauth2/token').reply(200, MOCK_TOKEN_RESPONSE)
+    nock('https://api.criteo.com').post(/^\/\d{4}-\d{2}\/marketing-solutions\/audience-segments\/search$/).query(true).reply(200, ADVERTISER_AUDIENCES_KEY_DOES_NOT_EXIST)
+    // The audience key is not present in the list of the advertiser's audiences so a new audience needs to be created
+    nock('https://api.criteo.com').post(/^\/\d{4}-\d{2}\/marketing-solutions\/audience-segments\/create$/).reply(200, AUDIENCE_CREATION_RESPONSE)
+    nock('https://api.criteo.com').patch(/^\/\d{4}-\d{2}\/marketing-solutions\/audience-segments\/\d+\/contact-list$/).reply(200)
+
+    await expect(
+      testDestination.testAction('addUserToAudience', {
+        event,
+        settings,
+        useDefaultMappings: true
+      })
+    ).resolves.not.toThrowError()
+  })
+
+  it('should not throw an error if the audience already exists and the patch requests succeeds', async () => {
+    const settings = VALID_SETTINGS;
+    nock('https://api.criteo.com').persist().post('/oauth2/token').reply(200, MOCK_TOKEN_RESPONSE)
+    nock('https://api.criteo.com').post(/^\/\d{4}-\d{2}\/marketing-solutions\/audience-segments\/search$/).query(true).reply(200, ADVERTISER_AUDIENCES)
+    nock('https://api.criteo.com').patch(/^\/\d{4}-\d{2}\/marketing-solutions\/audience-segments\/\d+\/contact-list$/).reply(200)
+
+    await expect(
+      testDestination.testAction('addUserToAudience', {
+        event,
+        settings,
+        useDefaultMappings: true
+      })
+    ).resolves.not.toThrowError()
+  })
+
+  it('should not throw an error in case of concurrent audience creation attempt', async () => {
+    const settings = VALID_SETTINGS;
+    nock('https://api.criteo.com').persist().post('/oauth2/token').reply(200, MOCK_TOKEN_RESPONSE)
+    nock('https://api.criteo.com').post(/^\/\d{4}-\d{2}\/marketing-solutions\/audience-segments\/search$/).query(true).reply(200, ADVERTISER_AUDIENCES_KEY_DOES_NOT_EXIST)
+    nock('https://api.criteo.com').post(/^\/\d{4}-\d{2}\/marketing-solutions\/audience-segments\/create$/).reply(400, DUPLICATE_AUDIENCE_ERROR)
+    nock('https://api.criteo.com').post(/^\/\d{4}-\d{2}\/marketing-solutions\/audience-segments\/search$/).query(true).reply(200, ADVERTISER_AUDIENCES)
+    nock('https://api.criteo.com').patch(/^\/\d{4}-\d{2}\/marketing-solutions\/audience-segments\/\d+\/contact-list$/).reply(200)
+
+    await expect(
+      testDestination.testAction('addUserToAudience', {
+        event,
+        settings,
+        useDefaultMappings: true
+      })
+    ).resolves.not.toThrowError()
+  })
+})
