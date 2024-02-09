1fbb3d67c6c59e4f8e0eb6e1ce1cc374d0a23954
Nick Aguilar <nicholas.aguilar@segment.com>
2024-02-09 13:28:27 -0800

    Removes try/catch around perform block requests in an attempt to properly throw errors when they occur. Motivated by 401 errors being returned as 200 in stage testing
    Attempt 2 to get errors thrown correctly - use try/catch but return IntegrationError

--- packages/destination-actions/src/destinations/linkedin-conversions/streamConversion/index.ts
+++ packages/destination-actions/src/destinations/linkedin-conversions/streamConversion/index.ts
@@ -1,9 +1,10 @@
 import type { ActionDefinition } from '@segment/actions-core'
-import { PayloadValidationError } from '@segment/actions-core'
+import { IntegrationError, PayloadValidationError } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import { LinkedInConversions } from '../api'
 import { SUPPORTED_ID_TYPE } from '../constants'
 import type { Payload, HookBundle } from './generated-types'
+import { LinkedInError } from '../types'
 
 const action: ActionDefinition<Settings, Payload, undefined, HookBundle> = {
   title: 'Stream Conversion Event',
@@ -235,7 +236,12 @@ const action: ActionDefinition<Settings, Payload, undefined, HookBundle> = {
       await linkedinApiClient.bulkAssociateCampaignToConversion(payload.campaignId)
       return linkedinApiClient.streamConversionEvent(payload, conversionTime)
     } catch (error) {
-      return error
+      const asLinkedInError = error as LinkedInError
+      throw new IntegrationError(
+        asLinkedInError.response.data.message ?? 'Unknown Error',
+        asLinkedInError.response.data.code ?? 'UNKNOWN',
+        asLinkedInError.response.data.status ?? 500
+      )
     }
   }
 }
