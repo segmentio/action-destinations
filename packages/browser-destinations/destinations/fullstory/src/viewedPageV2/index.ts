8eb72ba616e7ce21d95eb483135e31e73c043ebe
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-02-09 13:36:51 +0000

    adding more files

---
+++ packages/browser-destinations/destinations/fullstory/src/viewedPageV2/index.ts
@@ -0,0 +1,62 @@
+import type { BrowserActionDefinition } from '@segment/browser-destination-runtime/types'
+import type { Settings } from '../generated-types'
+import type { Payload } from './generated-types'
+import type { FS } from '../types'
+import { segmentEventSource } from '..'
+
+const action: BrowserActionDefinition<Settings, FS, Payload> = {
+  title: 'Viewed Page V2',
+  description: 'Sets page properties events',
+  defaultSubscription: 'type = "page"',
+  platform: 'web',
+  fields: {
+    pageName: {
+      type: 'string',
+      required: false,
+      description: 'The name of the page that was viewed.',
+      label: 'Page Name',
+      default: {
+        '@if': {
+          exists: { '@path': '$.category' },
+          then: { '@path': '$.category' },
+          else: { '@path': '$.name' }
+        }
+      }
+    },
+    properties: {
+      type: 'object',
+      required: false,
+      description: 'The properties of the page that was viewed.',
+      label: 'Properties',
+      default: {
+        '@path': '$.properties'
+      }
+    }
+  },
+  perform: (FS, event) => {
+    if (event.payload.pageName) {
+      FS(
+        'setProperties',
+        {
+          type: 'page',
+          properties: {
+            pageName: event.payload.pageName,
+            ...event.payload.properties
+          }
+        },
+        segmentEventSource
+      )
+    } else if (event.payload.properties) {
+      FS(
+        'setProperties',
+        {
+          type: 'page',
+          properties: event.payload.properties
+        },
+        segmentEventSource
+      )
+    }
+  }
+}
+
+export default action
