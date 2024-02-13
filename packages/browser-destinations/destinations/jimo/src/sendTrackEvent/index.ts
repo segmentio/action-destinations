29cdbcadd6a9c829119ae1bd646006b6faebb17b
sdubrez <22425976+imsamdez@users.noreply.github.com>
2024-02-13 14:39:47 +0100

    feat(Jimo/Actions): sendTrackEvent

---
+++ packages/browser-destinations/destinations/jimo/src/sendTrackEvent/index.ts
@@ -0,0 +1,96 @@
+import type { BrowserActionDefinition } from '@segment/browser-destination-runtime/types'
+import { JimoSDK } from 'src/types'
+import type { Settings } from '../generated-types'
+import { Payload } from './generated-types'
+
+const action: BrowserActionDefinition<Settings, JimoSDK, Payload> = {
+  title: 'Send Track Event',
+  description: 'Submit an event to Jimo',
+  defaultSubscription: 'type = "track"',
+  platform: 'web',
+  fields: {
+    messageId: {
+      description: 'The internal id of the message.',
+      label: 'Message Id',
+      type: 'string',
+      required: true,
+      default: {
+        '@path': '$.messageId'
+      }
+    },
+    timestamp: {
+      description: 'The timestamp of the event.',
+      label: 'Timestamp',
+      type: 'string',
+      required: true,
+      default: {
+        '@path': '$.timestamp'
+      }
+    },
+    receivedAt: {
+      description: 'The reception date of the event.',
+      label: 'Received at',
+      type: 'string',
+      required: true,
+      default: {
+        '@path': '$.receivedAt'
+      }
+    },
+    event_name: {
+      description: 'The name of the event.',
+      label: 'Event Name',
+      type: 'string',
+      required: true,
+      default: {
+        '@path': '$.event'
+      }
+    },
+    userId: {
+      description: 'A unique identifier for the user.',
+      label: 'User ID',
+      type: 'string',
+      required: false,
+      default: {
+        '@path': '$.userId'
+      }
+    },
+    anonymousId: {
+      description: 'An anonymous identifier for the user.',
+      label: 'Anonymous ID',
+      type: 'string',
+      required: false,
+      default: {
+        '@path': '$.anonymousId'
+      }
+    },
+    properties: {
+      description: 'Information associated with the event',
+      label: 'Event Properties',
+      type: 'object',
+      required: false,
+      default: {
+        '@path': '$.properties'
+      }
+    }
+  },
+  perform: (jimo, { payload }) => {
+    const {
+      event_name,
+      userId,
+      anonymousId,
+      timestamp,
+      /* receivedAt, (for some reason, receivedAt is undefined) */
+      messageId,
+      properties
+    } = payload
+    const receivedAt = timestamp
+
+    jimo.push([
+      'do',
+      'segmentio:track',
+      [{ event: event_name, userId, anonymousId, messageId, timestamp, receivedAt, properties }]
+    ])
+  }
+}
+
+export default action
