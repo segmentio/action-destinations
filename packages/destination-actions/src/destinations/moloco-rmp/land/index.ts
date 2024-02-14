aaf51d33cdd622f9b98ac5659d6a5d63e05388f0
heonjang.lee <heonjang.lee@moloco.com>
2024-02-13 21:43:16 -0800

    Implement details

--- packages/destination-actions/src/destinations/moloco-rmp/land/index.ts
+++ packages/destination-actions/src/destinations/moloco-rmp/land/index.ts
@@ -1,17 +1,24 @@
 import type { ActionDefinition } from '@segment/actions-core'
+import { EventType, MolocoEvent } from '../common/event'
+import { MolocoAPIClient } from '../common/request-client'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 
+const event = new MolocoEvent(EventType.Land, {
+  requireItems: false,
+  requireSearchQuery: false,
+  requirePageId: true,
+  requireReferrerPageId: true
+})
+
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Land',
-  description: '',
-  fields: {},
+  description: 'Represents a user visiting the client’s website from an external source (ex. Google Shopping)',
+  fields: event.getFields(),
   perform: (request, data) => {
-    // Make your partner api request here!
-    // return request('https://example.com', {
-    //   method: 'post',
-    //   json: data.payload
-    // })
+    const client = new MolocoAPIClient(request, data.settings)
+    const body = event.buildBody(data.payload)
+    return client.sendEvent(body)
   }
 }
 
