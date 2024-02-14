b2b8d54040fbc0ab5652fbe2342223025a276381
heonjang.lee <heonjang.lee@moloco.com>
2024-02-13 20:52:15 -0800

    Implement details

--- segment/packages/destination-actions/src/destinations/moloco-rmp/pageView/index.ts
+++ segment/packages/destination-actions/src/destinations/moloco-rmp/pageView/index.ts
@@ -1,17 +1,26 @@
 import type { ActionDefinition } from '@segment/actions-core'
+import { EventType, MolocoEvent } from '../common/event'
+import { MolocoAPIClient } from '../common/request-client'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 
+const event = new MolocoEvent(EventType.PageView, {
+  requireItems: false,
+  requirePageId: true,
+  requireReferrerPageId: false,
+  requireShippingCharge: false
+})
+
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Page View',
-  description: '',
-  fields: {},
+  description: 
+    'Represents a user viewing a certain page that is pertinent to sequence-based ML model training'
+    + ' (Ex. a user browsing sneakers)',
+  fields: event.getFields(),
   perform: (request, data) => {
-    // Make your partner api request here!
-    // return request('https://example.com', {
-    //   method: 'post',
-    //   json: data.payload
-    // })
+    const client = new MolocoAPIClient(request, data.settings)
+    const body = event.buildBody(data.payload)
+    return client.sendEvent(body)
   }
 }
 
