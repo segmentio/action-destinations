ca40d422194f2d9feb114809663a89747a33b343
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2024-02-13 16:23:30 -0800

    Updating authentication test.

--- packages/destination-actions/src/destinations/kafka/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/kafka/__tests__/index.test.ts
@@ -1,5 +1,5 @@
 import nock from 'nock'
-import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import { createTestIntegration } from '@segment/actions-core'
 import Definition from '../index'
 
 const testDestination = createTestIntegration(Definition)
@@ -10,7 +10,13 @@ describe('Kafka', () => {
       nock('https://your.destination.endpoint').get('*').reply(200, {})
 
       // This should match your authentication.fields
-      const authData = {}
+      const authData = {
+        brokers: 'localhost:9092',
+        saslAuthenticationMechanism: 'plain',
+        topic: 'test-topic',
+        username: 'username',
+        password: 'password'
+      }
 
       await expect(testDestination.testAuthentication(authData)).resolves.not.toThrowError()
     })
