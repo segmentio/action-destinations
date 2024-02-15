768769294b4fc1b4bf76943aeaa53296f3097d03
heonjang.lee <heonjang.lee@moloco.com>
2024-02-15 13:40:18 -0800

    Implement Purchase

--- segment/packages/destination-actions/src/destinations/moloco-rmp/purchase/index.ts
+++ segment/packages/destination-actions/src/destinations/moloco-rmp/purchase/index.ts
@@ -1,17 +1,24 @@
 import type { ActionDefinition } from '@segment/actions-core'
+import { EventType, MolocoEvent } from '../common/event'
+import { MolocoAPIClient } from '../common/request-client'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 
+const event = new MolocoEvent(EventType.Purchase, {
+  requireItems: true,
+  requireRevenue: true,
+  requirePageId: false,
+  requireShippingCharge: false
+})
+
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Purchase',
-  description: '',
-  fields: {},
+  description: 'Represents a user purchasing an item',
+  fields: event.getFields(),
   perform: (request, data) => {
-    // Make your partner api request here!
-    // return request('https://example.com', {
-    //   method: 'post',
-    //   json: data.payload
-    // })
+    const client = new MolocoAPIClient(request, data.settings)
+    const body = event.buildBody(data.payload)
+    return client.sendEvent(body)
   }
 }
 
