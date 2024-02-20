41b09e694291d3525a2b067c1873d40b4ddd7821
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2024-02-20 10:44:48 -0300

    Stubbing unit tests and mocks.

--- packages/destination-actions/src/destinations/kafka/send/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/kafka/send/__tests__/index.test.ts
@@ -1,9 +1,23 @@
-import nock from 'nock'
 import { createTestEvent, createTestIntegration } from '@segment/actions-core'
 import Destination from '../../index'
 
 const testDestination = createTestIntegration(Destination)
 
 describe('Kafka.send', () => {
-  // TODO: Test your action
+  it.skip('works with default mappings', async () => {
+    const event = createTestEvent({
+      type: 'track',
+      event: 'Test Event',
+      properties: {
+        email: 'test@iterable.com'
+      }
+    })
+
+    const responses = await testDestination.testAction('send', {
+      event,
+      useDefaultMappings: true
+    })
+
+    expect(responses[0].status).toBe(200)
+  })
 })
