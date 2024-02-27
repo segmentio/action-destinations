f0c36481776d6b2f3575aea82feea47f5be5a461
Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-02-27 09:50:05 +0000

    Algolia changes requested by Partner (#1891)

--- packages/destination-actions/src/destinations/algolia-insights/productAddedEvents/index.ts
+++ packages/destination-actions/src/destinations/algolia-insights/productAddedEvents/index.ts
@@ -45,7 +45,7 @@ export const productAddedEvents: ActionDefinition<Settings, Payload> = {
       type: 'string',
       required: true,
       description: 'The ID associated with the user.',
-      label: 'userToken',
+      label: 'User Token',
       default: {
         '@if': {
           exists: { '@path': '$.userId' },
@@ -58,11 +58,11 @@ export const productAddedEvents: ActionDefinition<Settings, Payload> = {
       type: 'string',
       required: false,
       description: 'The timestamp of the event.',
-      label: 'timestamp',
+      label: 'Timestamp',
       default: { '@path': '$.timestamp' }
     },
     extraProperties: {
-      label: 'extraProperties',
+      label: 'Extra Properties',
       required: false,
       description:
         'Additional fields for this event. This field may be useful for Algolia Insights fields which are not mapped in Segment.',
