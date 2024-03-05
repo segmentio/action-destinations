a77f4a7fe632509c5c19f9b6aa36a1c6045269ec
Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-03-05 13:11:18 +0000

    basic SSL config options (#1908)

--- packages/destination-actions/src/destinations/kafka/generated-types.ts
+++ packages/destination-actions/src/destinations/kafka/generated-types.ts
@@ -2,27 +2,35 @@
 
 export interface Settings {
   /**
-   * The brokers for your Kafka instance, in the format of `host:port`. Accepts a comma delimited string.
+   * The brokers for your Kafka instance, in the format of `host:port`. E.g. localhost:9092. Accepts a comma delimited string.
    */
   brokers: string
   /**
-   * The SASL Authentication Mechanism for your Kafka instance.
+   * The Authentication Mechanism for your Kafka instance.
    */
   mechanism: string
   /**
-   * The client ID for your Kafka instance. Defaults to "segment-actions-kafka-producer".
+   * The client ID for your Kafka instance. Defaults to 'segment-actions-kafka-producer'.
    */
   clientId: string
   /**
-   * The username for your Kafka instance.
+   * The username for your Kafka instance. If using AWS IAM Authentication this should be your AWS Access Key ID.
    */
   username: string
   /**
-   * The password for your Kafka instance.
+   * The password for your Kafka instance. If using AWS IAM Authentication this should be your AWS Secret Key.
    */
   password: string
   /**
-   * The partitioner type for your Kafka instance. Defaults to "Default Partitioner".
+   * The partitioner type for your Kafka instance. Defaults to 'Default Partitioner'.
    */
   partitionerType: string
+  /**
+   * The aws:userid of the AWS IAM identity. Required if 'SASL Authentication Mechanism' field is set to 'AWS IAM'.
+   */
+  authorizationIdentity?: string
+  /**
+   * Indicates the type of SSL to be used.
+   */
+  ssl: string
 }
--- packages/destination-actions/src/destinations/kafka/index.ts
+++ packages/destination-actions/src/destinations/kafka/index.ts
@@ -14,44 +14,47 @@ const destination: DestinationDefinition<Settings> = {
       brokers: {
         label: 'Brokers',
         description:
-          'The brokers for your Kafka instance, in the format of `host:port`. Accepts a comma delimited string.',
+          'The brokers for your Kafka instance, in the format of `host:port`. E.g. localhost:9092. Accepts a comma delimited string.',
         type: 'string',
         required: true
       },
       mechanism: {
         label: 'SASL Authentication Mechanism',
-        description: 'The SASL Authentication Mechanism for your Kafka instance.',
+        description: 'The Authentication Mechanism for your Kafka instance.',
         type: 'string',
         required: true,
         choices: [
           { label: 'Plain', value: 'plain' },
           { label: 'SCRAM/SHA-256', value: 'scram-sha-256' },
-          { label: 'SCRAM/SHA-512', value: 'scram-sha-512' }
+          { label: 'SCRAM/SHA-512', value: 'scram-sha-512' },
+          { label: 'AWS IAM', value: 'aws' }
         ],
         default: 'plain'
       },
       clientId: {
         label: 'Client ID',
-        description: 'The client ID for your Kafka instance. Defaults to "segment-actions-kafka-producer".',
+        description: "The client ID for your Kafka instance. Defaults to 'segment-actions-kafka-producer'.",
         type: 'string',
         required: true,
         default: 'segment-actions-kafka-producer'
       },
       username: {
-        label: 'Username',
-        description: 'The username for your Kafka instance.',
+        label: 'Username or IAM Access Key ID',
+        description:
+          'The username for your Kafka instance. If using AWS IAM Authentication this should be your AWS Access Key ID.',
         type: 'string',
         required: true
       },
       password: {
-        label: 'Password',
-        description: 'The password for your Kafka instance.',
+        label: 'Password or IAM Secret Key',
+        description:
+          'The password for your Kafka instance. If using AWS IAM Authentication this should be your AWS Secret Key.',
         type: 'password',
         required: true
       },
       partitionerType: {
         label: 'Partitioner Type',
-        description: 'The partitioner type for your Kafka instance. Defaults to "Default Partitioner".',
+        description: "The partitioner type for your Kafka instance. Defaults to 'Default Partitioner'.",
         type: 'string',
         required: true,
         choices: [
@@ -59,6 +62,25 @@ const destination: DestinationDefinition<Settings> = {
           { label: 'Legacy Partitioner', value: 'LegacyPartitioner' }
         ],
         default: 'DefaultPartitioner'
+      },
+      authorizationIdentity: {
+        label: 'AWS Authorization Identify',
+        description:
+          "The aws:userid of the AWS IAM identity. Required if 'SASL Authentication Mechanism' field is set to 'AWS IAM'.",
+        type: 'string',
+        required: false,
+        default: ''
+      },
+      ssl: {
+        label: 'SSL Configuration Options',
+        description: 'Indicates the type of SSL to be used.',
+        type: 'string',
+        required: true,
+        choices: [
+          { label: 'No SSL Encryption', value: 'none' },
+          { label: 'Default SSL Encryption', value: 'default' }
+        ],
+        default: 'default'
       }
     }
   },
