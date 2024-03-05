78c2b0a41cac8530a40d57f792638e1e5482c016
Harsh Joshi <harsh.joshi@segment.com>
2024-03-05 08:41:02 +0530

    Add error handling in add profile to list

--- packages/destination-actions/src/destinations/klaviyo/addProfileToList/index.ts
+++ packages/destination-actions/src/destinations/klaviyo/addProfileToList/index.ts
@@ -1,8 +1,9 @@
-import { ActionDefinition, PayloadValidationError } from '@segment/actions-core'
+import { ActionDefinition, IntegrationError, PayloadValidationError } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import { Payload } from './generated-types'
 import { createProfile, addProfileToList, createImportJobPayload, sendImportJobRequest } from '../functions'
 import { email, external_id, list_id, enable_batching, batch_size } from '../properties'
+import { KlaviyoAPIError } from '../types'
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Add Profile To List',
@@ -20,8 +21,21 @@ const action: ActionDefinition<Settings, Payload> = {
     if (!email && !external_id) {
       throw new PayloadValidationError('One of Email or External Id is required')
     }
-    const profileId = await createProfile(request, email, external_id)
-    return await addProfileToList(request, profileId, list_id)
+
+    try {
+      const profileId = await createProfile(request, email, external_id)
+      return await addProfileToList(request, profileId, list_id)
+    } catch (error: unknown) {
+      const apiError = error as { errors: KlaviyoAPIError }
+
+      if (apiError.errors && Array.isArray(apiError.errors)) {
+        const detailedError = apiError.errors[0]
+        const errorMessage = detailedError.detail || 'An error occurred while processing the request.'
+        throw new IntegrationError(errorMessage, detailedError.code || 'UNEXPECTED_ERROR', detailedError.status || 500)
+      } else {
+        throw error
+      }
+    }
   },
   performBatch: async (request, { payload }) => {
     // Filtering out profiles that do not contain either an email or external_id.
