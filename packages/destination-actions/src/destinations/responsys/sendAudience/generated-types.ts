815135f9a3c73dab4c78e7a3f913a00a6877010e
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-03-12 12:19:57 +0000

    adding retry logic

--- packages/destination-actions/src/destinations/responsys/sendAudience/generated-types.ts
+++ packages/destination-actions/src/destinations/responsys/sendAudience/generated-types.ts
@@ -37,4 +37,8 @@ export interface Payload {
    * Maximum number of events to include in each batch. Actual batch sizes may be lower.
    */
   batch_size?: number
+  /**
+   * The timestamp of when the event occurred.
+   */
+  timestamp: string | number
 }
