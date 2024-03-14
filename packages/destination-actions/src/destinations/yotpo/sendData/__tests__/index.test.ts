4b549aa5109e1121799531e91968f72fe8e70015
Joe Ayoub <joe.ayoub@segment.com>
2024-03-14 13:20:24 +0000

    adding yotpo for auth testing

---
+++ packages/destination-actions/src/destinations/yotpo/sendData/__tests__/index.test.ts
@@ -0,0 +1,12 @@
+// import nock from 'nock'
+// import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+// import Destination from '../../index'
+//
+// const testDestination = createTestIntegration(Destination)
+
+describe('Yotpo.sendData', () => {
+  // make this test pass
+  it('should pass', async () => {
+    expect(true).toBe(true)
+  })
+})
