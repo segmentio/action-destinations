7d690ffe69766f1e6ac0be79e2058bff5822614e
Maryam Sharif <maryam.sharif@segment.com>
2024-03-18 13:05:47 -0700

    generate types

--- packages/destination-actions/src/destinations/marketo-static-lists/addToList/generated-types.ts
+++ packages/destination-actions/src/destinations/marketo-static-lists/addToList/generated-types.ts
@@ -6,7 +6,7 @@ export interface Payload {
    */
   external_id?: string
   /**
-   * The lead field to use for deduplication and filtering. This field must be apart of the field(s) you are sending to Marketo.
+   * The lead field to use for deduplication and filtering. This field must be apart of the Lead Info Fields below.
    */
   lookup_field: string
   /**
@@ -17,18 +17,6 @@ export interface Payload {
      * The user's email address to send to Marketo.
      */
     email?: string
-    /**
-     * The user's first name.
-     */
-    firstName?: string
-    /**
-     * The user's last name.
-     */
-    lastName?: string
-    /**
-     * The user's phone number.
-     */
-    phone?: string
     [k: string]: unknown
   }
   /**
