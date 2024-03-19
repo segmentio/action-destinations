58ab9ffa32f1424132f3841af16a8636943e235f
Joe Ayoub <joe.ayoub@segment.com>
2024-03-19 10:03:08 +0000

    adding generated types for yotpo

--- packages/destination-actions/src/destinations/yotpo/sendData/generated-types.ts
+++ packages/destination-actions/src/destinations/yotpo/sendData/generated-types.ts
@@ -1,3 +1,10 @@
 // Generated file. DO NOT MODIFY IT BY HAND.
 
-export interface Payload {}
+export interface Payload {
+  /**
+   * The data to send to Yotpo
+   */
+  data?: {
+    [k: string]: unknown
+  }
+}
