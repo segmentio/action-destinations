dc32cd78161fd274ecb480028b2df177ab1c7194
David Bordoley <dbordoley@snapchat.com>
2024-03-20 14:01:48 -0400

    refactor input fields

--- packages/destination-actions/src/destinations/snap-conversions-api/reportConversionEvent/index.ts
+++ packages/destination-actions/src/destinations/snap-conversions-api/reportConversionEvent/index.ts
@@ -1,72 +1,14 @@
 import { ActionDefinition } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
-import {
-  products,
-  event_type,
-  event_conversion_type,
-  event_tag,
-  timestamp,
-  email,
-  mobile_ad_id,
-  uuid_c1,
-  idfv,
-  phone_number,
-  user_agent,
-  ip_address,
-  item_category,
-  brands,
-  item_ids,
-  description,
-  number_items,
-  price,
-  currency,
-  transaction_id,
-  level,
-  client_dedup_id,
-  search_string,
-  page_url,
-  sign_up_method,
-  device_model,
-  os_version,
-  click_id
-} from '../snap-capi-properties'
+import snap_capi_input_fields_v2 from './snap-capi-input-fields-v2'
 import { performSnapCAPIv3 as perform } from './snap-capi-v3'
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Report Conversion Event',
   description:
     'Report events directly to Snapchat. Data shared can power Snap solutions such as custom audience targeting, campaign optimization, Dynamic Ads, and more.',
-  fields: {
-    products: products,
-    event_type: { ...event_type, required: true },
-    event_conversion_type: { ...event_conversion_type, required: true },
-    event_tag: event_tag,
-    timestamp: { ...timestamp, required: true },
-    email: email,
-    mobile_ad_id: mobile_ad_id,
-    uuid_c1: uuid_c1,
-    idfv: idfv,
-    phone_number: phone_number,
-    user_agent: user_agent,
-    ip_address: ip_address,
-    item_category: item_category,
-    brands: brands,
-    item_ids: item_ids,
-    description: description,
-    number_items: number_items,
-    price: price,
-    currency: currency,
-    transaction_id: transaction_id,
-    level: level,
-    client_dedup_id: client_dedup_id,
-    search_string: search_string,
-    page_url: page_url,
-    sign_up_method: sign_up_method,
-    os_version: os_version,
-    device_model: device_model,
-    click_id: click_id
-  },
+  fields: snap_capi_input_fields_v2,
   perform
 }
 
