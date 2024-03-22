9f305dbe530b85d4e1ee046bae8e9296a43fd683
Dan <dlasky@users.noreply.github.com>
2024-03-22 16:33:56 -0400

    fixed + tests

--- packages/core/src/mapping-kit/flatten.ts
+++ packages/core/src/mapping-kit/flatten.ts
@@ -1,18 +1,16 @@
-import { JSONLikeObject } from '../json-object'
-import { isObject } from '../real-type-of'
+import { JSONLike, JSONLikeObject } from '../json-object'
+import { isArray, isObject } from '../real-type-of'
 
-export const flattenObject = (obj: JSONLikeObject, prefix = '', separator = '.'): JSONLikeObject => {
-  const newObj: JSONLikeObject = {}
-
-  for (const key of Object.keys(obj)) {
-    const value = obj[key]
-
-    if (isObject(value)) {
-      Object.assign(newObj, flattenObject(value, `${prefix}${key}${separator}`, separator))
-    } else {
-      newObj[`${prefix}${key}`] = value
-    }
+export const flattenObject = (input: JSONLike, prefix = '', separator = '.'): JSONLikeObject => {
+  //input may be a primitive value, object, or array
+  if (isObject(input) || isArray(input)) {
+    return Object.entries(input).reduce((acc, [key, value]) => {
+      const newKey = prefix ? `${prefix}${separator}${key}` : key
+      return {
+        ...acc,
+        ...flattenObject(value, newKey, separator)
+      }
+    }, {})
   }
-
-  return newObj
+  return { [prefix]: input }
 }
