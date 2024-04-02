abb11ac8963f43dc17f3315761bc6db7dd2da8b6
Varadarajan V <vvijayaragavan@twilio.com>
2024-04-02 20:32:35 +0530

    Add workflow and release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD);
-if [[ $branch != "main" ]];
+sha=$(git rev-parse HEAD);
+
+if [[ $branch != "staging" ]];
 then
-  echo "You must be on the main branch to release"
+  echo "You must be on the staging branch to release"
   exit
 fi;
 
-
 git pull --ff-only
 echo "Running lerna version minor..."
-lerna version minor --no-private -y
+lerna version prerelease --allow-branch $(git branch --show-current) --preid $(git branch --show-current) --no-private --yes
+
+# Generate and add release tag
+if ! n=$(git rev-list --count $sha~ --grep "Publish" --since="00:00"); then
+    echo 'failed to calculate tag'
+    exit 1
+fi
+case "$n" in
+    0) suffix="" ;; # first commit of the day gets no suffix
+    *) suffix=".$n" ;; # subsequent commits get a suffix, starting with .1
+esac
+
+tag=$(printf release-$(date '+%Y-%m-%d%%s') $suffix)
+echo "Tagging release with $tag"
+git tag -a $tag -m "Release $tag"
+git push origin $tag
