76b2290a90c737ae4ad56ccf20ffa9942c58c8fe
Varadarajan V <vvijayaragavan@twilio.com>
2024-04-02 22:03:12 +0530

    updates to postversion

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-set -e 
+set -e
 sha=$(git rev-parse HEAD);
 branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD);
 
 if [[ $branch != "main" ]];
 then
   echo "Skipping release tag generation for non-main branch"
+  exit 0
 fi;
 
 # Generate and push release tag. Release tag format: release-YYYY-MM-DD[.N] e.g. release-2024-01-01
 if ! n=$(git rev-list --count $sha~ --grep "Publish" --since="00:00"); then
-    echo 'Failed to find compute release tag. Please manually tag the release with git tag -a release-YYYY-MM-DD -m "Release release-YYYY-MM-DD" and push the tag with git push origin release-YYYY-MM-DD'
+    echo 'Failed to find compute release tag. Exiting.'
+    exit 1
 else 
     case "$n" in
         0) suffix="" ;; # first commit of the day gets no suffix
