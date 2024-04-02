5fab5ba72a6d1bbccb7c21512689eec114ccbbe5
Varadarajan V <vvijayaragavan@twilio.com>
2024-04-02 21:27:15 +0530

    refactor script

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,22 +9,20 @@ then
   exit
 fi;
 
+# Generate and push release tag. Release tag format: release-YYYY-MM-DD[.N] e.g. release-2024-01-01
+if ! n=$(git rev-list --count $sha~ --grep "Publish" --since="00:00"); then
+    echo 'Failed to find compute release tag. Please manually tag the release with git tag -a release-YYYY-MM-DD -m "Release release-YYYY-MM-DD" and push the tag with git push origin release-YYYY-MM-DD'
+else 
+    case "$n" in
+        0) suffix="" ;; # first commit of the day gets no suffix
+        *) suffix=".$n" ;; # subsequent commits get a suffix, starting with .1
+    esac
+
+    tag=$(printf release-$(date '+%Y-%m-%d%%s') $suffix)
+    echo "Tagging $sha with $tag"
+    git tag -a $tag -m "Release $tag"
+fi
+
 git pull --ff-only
 echo "Running lerna version minor..."
 lerna version minor --no-private -y
-
-# Generate and push release tag. Release tag format: release-YYYY-MM-DD[.N] e.g. release-2024-01-01
-if ! n=$(git rev-list --count $sha~ --grep "Publish" --since="00:00"); then
-    echo 'failed to calculate tag'
-    exit 1
-fi
-
-case "$n" in
-    0) suffix="" ;; # first commit of the day gets no suffix
-    *) suffix=".$n" ;; # subsequent commits get a suffix, starting with .1
-esac
-
-tag=$(printf release-$(date '+%Y-%m-%d%%s') $suffix)
-echo "Tagging $sha with $tag"
-git tag -a $tag -m "Release $tag"
-git push origin $tag
