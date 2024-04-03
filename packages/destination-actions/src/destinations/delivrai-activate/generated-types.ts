4abcd245505dd47b9c1f128c7e6fde7522c0aec1
unknown <sunil.kumar@portqii.com>
2024-04-03 22:30:27 +0530

    added delivrai activate

---
+++ packages/destination-actions/src/destinations/delivrai-activate/generated-types.ts
@@ -0,0 +1,6 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Settings {
+  __segment_internal_engage_force_full_sync: boolean
+  __segment_internal_engage_batch_sync: boolean
+}
---
+++ packages/destination-actions/src/destinations/delivrai-activate/properties.ts
@@ -0,0 +1,7 @@
+export const ACTION_SLUG = 'actions-delivrai-activate'
+
+export const DELIVRAI_LEGACY_FLOW_FLAG_NAME = 'actions-delivrai-activate-legacy-flow'
+export const DELIVRAI_MIN_RECORD_COUNT = 25
+
+export const DELIVRAI_SFTP_SERVER = 'files.delivrai.com'
+export const DELIVRAI_SFTP_PORT = 22
