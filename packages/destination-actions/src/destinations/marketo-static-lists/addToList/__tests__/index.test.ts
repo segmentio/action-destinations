9623e033368a3bcb1f6f63e1b8745835c2125102
Maryam Sharif <maryam.sharif@segment.com>
2024-04-03 16:56:10 -0700

    Add unit tests

--- packages/destination-actions/src/destinations/marketo-static-lists/addToList/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/marketo-static-lists/addToList/__tests__/index.test.ts
@@ -28,6 +28,38 @@ const event = createTestEvent({
   }
 })
 
+const audienceName = 'The Best Test Audience'
+const folderName = 'Test Folder'
+const clientId = 'test_client_id'
+const clientSecret = 'test_client_secret'
+const apiEndpoint = 'https://123-ABC-456.mktorest.com'
+
+const hookInputNew = {
+  settings: {
+    folder_name: folderName,
+    client_id: clientId,
+    client_secret: clientSecret,
+    api_endpoint: apiEndpoint
+  },
+  hookInputs: {
+    list_name: audienceName
+  },
+  payload: {}
+}
+
+const hookInputExisting = {
+  settings: {
+    folder_name: folderName,
+    client_id: clientId,
+    client_secret: clientSecret,
+    api_endpoint: apiEndpoint
+  },
+  hookInputs: {
+    list_id: '782'
+  },
+  payload: {}
+}
+
 describe('MarketoStaticLists.addToList', () => {
   it('should succeed if response from Marketo is successful', async () => {
     const bulkImport = API_ENDPOINT + BULK_IMPORT_ENDPOINT.replace('externalId', EXTERNAL_AUDIENCE_ID)
@@ -65,5 +97,96 @@ describe('MarketoStaticLists.addToList', () => {
         useDefaultMappings: true
       })
     ).rejects.toThrow('Static list not found')
+  })
+
+  it('create a new list with hook', async () => {
+    nock(
+      `${apiEndpoint}/identity/oauth/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
+    )
+      .post(/.*/)
+      .reply(200, {
+        access_token: 'access_token'
+      })
+
+    nock(`${apiEndpoint}/rest/asset/v1/folder/byName.json?name=${encodeURIComponent(folderName)}`)
+      .get(/.*/)
+      .reply(200, {
+        success: true,
+        result: [
+          {
+            name: folderName,
+            id: 12
+          }
+        ]
+      })
+
+    nock(`${apiEndpoint}/rest/asset/v1/staticLists.json?folder=12&name=${encodeURIComponent(audienceName)}`)
+      .post(/.*/)
+      .reply(200, {
+        success: true,
+        result: [
+          {
+            name: audienceName,
+            id: 782
+          }
+        ]
+      })
+
+    const r = await testDestination.actions.addToList.executeHook('retlOnMappingSave', hookInputNew)
+
+    expect(r.savedData).toMatchObject({
+      id: '782',
+      name: 'The Best Test Audience'
+    })
+    expect(r.successMessage).toMatchInlineSnapshot(`"List 782 created successfully!"`)
+  })
+
+  it('verify the existing list', async () => {
+    nock(
+      `${apiEndpoint}/identity/oauth/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
+    )
+      .post(/.*/)
+      .reply(200, {
+        access_token: 'access_token'
+      })
+    nock(`${apiEndpoint}/rest/asset/v1/staticList/782.json`)
+      .get(/.*/)
+      .reply(200, {
+        success: true,
+        result: [
+          {
+            name: folderName,
+            id: 782
+          }
+        ]
+      })
+
+    const r = await testDestination.actions.addToList.executeHook('retlOnMappingSave', hookInputExisting)
+
+    expect(r.savedData).toMatchObject({
+      id: '782',
+      name: 'Test Folder'
+    })
+    expect(r.successMessage).toMatchInlineSnapshot(`"Using existing list 782."`)
+  })
+
+  it('fail if list id does not exist', async () => {
+    nock(
+      `${apiEndpoint}/identity/oauth/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
+    )
+      .post(/.*/)
+      .reply(200, {
+        access_token: 'access_token'
+      })
+    nock(`${apiEndpoint}/rest/asset/v1/staticList/782.json`)
+      .get(/.*/)
+      .reply(200, {
+        success: false,
+        errors: [{ code: 1013, message: 'Static list not found' }]
+      })
+
+    const r = await testDestination.actions.addToList.executeHook('retlOnMappingSave', hookInputExisting)
+
+    expect(r).toMatchObject({ error: { code: 'LIST_ID_VERIFICATION_FAILURE', message: 'Static list not found' } })
   })
 })
