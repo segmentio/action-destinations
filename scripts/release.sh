cdab6e3c56cf0f9bcb4e80b997f660dcfbd9fcaa
Varadarajan V <109586712+varadarajan-tw@users.noreply.github.com>
2024-04-03 20:30:16 +0530

    [STRATCONN-3673] - Extract release to a separate job in publish workflow (#1963)
    This PR makes couple of changes to fix:
    
    To avoid using GH PAT token with write access to Github repo, I have moved the release tag computation to thepostversion.sh script. This script will be executed via postversion hook - only for main branch.
    Extract the release steps to separate job that depends on the build-and-publish job to ensure we don't use GH_PAT token. GH_PAT token contains permissions only to pull data from ctl-plane-js-client.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD);
+
 if [[ $branch != "main" ]];
 then
   echo "You must be on the main branch to release"
   exit
 fi;
-
 
 git pull --ff-only
 echo "Running lerna version minor..."
