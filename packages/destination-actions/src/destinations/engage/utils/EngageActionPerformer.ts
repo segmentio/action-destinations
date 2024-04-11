5f1ca30ad35a354b028e8f82716669d61a15a25e
Cesar Ayuso <cesar.ayuso@segment.com>
2024-04-11 16:57:01 -0700

    remove status code +

--- packages/destination-actions/src/destinations/engage/utils/EngageActionPerformer.ts
+++ packages/destination-actions/src/destinations/engage/utils/EngageActionPerformer.ts
@@ -114,7 +114,7 @@ export abstract class EngageActionPerformer<TSettings = any, TPayload = any, TRe
           msgLowercare?.includes('timedout') ||
           msgLowercare?.includes('exceeded the deadline') ||
           errorDetails.code?.toLowerCase().includes('etimedout')
-        // dont inlcude status code
+        // dont include code
         if (isTimeoutErrorInOnCatch) {
           op.error = new RetryableError('Timeout error throwing Retryable, retrying...')
         }
