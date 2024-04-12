c61856b8e3c0c0f354aa48e3c2f391a15383993a
Cesar Ayuso <cesar.ayuso@segment.com>
2024-04-12 13:39:01 -0700

    dummy commit + +

--- packages/destination-actions/src/destinations/engage/utils/EngageActionPerformer.ts
+++ packages/destination-actions/src/destinations/engage/utils/EngageActionPerformer.ts
@@ -78,7 +78,7 @@ export abstract class EngageActionPerformer<TSettings = any, TPayload = any, TRe
           const errorCode = errorDetails.code ?? respError.code ?? 'etimedout'
           respError.code = errorCode
           errorDetails.code = errorCode
-          // retry if timeout error, needed for integrations
+          // needed for integrations to retry
           respError.retry = true
         }
 
