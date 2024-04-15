994a90ec545706bc0e3639037d75d2d5db37b36d
Marín Alcaraz <marin.alcarazcordova@segment.com>
2024-04-15 16:23:59 -0700

    cherry-pick 234c9094cbdd05963bc67ea92fe141f8065a4901

--- packages/browser-destinations/destinations/tiktok-pixel/src/generated-types.ts
+++ packages/browser-destinations/destinations/tiktok-pixel/src/generated-types.ts
@@ -6,7 +6,11 @@ export interface Settings {
    */
   pixelCode: string
   /**
-   * Important! Changing this setting may block data collection to Segment if not done correctly. Select "true" to use an existing TikTok Pixel which is already installed on your website. The Pixel MUST be installed on your website when this is set to "true" or all data collection to Segment may fail.
+   * In order to help facilitate advertiser's compliance with the right to opt-out of sale and sharing of personal data under certain U.S. state privacy laws, TikTok offers a Limited Data Use ("LDU") feature. For more information, please refer to TikTok's [documentation page](https://business-api.tiktok.com/portal/docs?id=1770092377990145).
+   */
+  ldu?: boolean
+  /**
+   * Deprecated. Please do not provide any value.
    */
   useExistingPixel?: boolean
 }
