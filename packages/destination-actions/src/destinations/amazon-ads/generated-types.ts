b0f7f1d3567ec4917f482a0567283979a1cf2cd1
Harsh Vardhan <harsh.vardhan@segment.com>
2024-04-15 13:39:47 +0530

    Added the advertiserId field in audienceSetting

--- packages/destination-actions/src/destinations/amazon-ads/generated-types.ts
+++ packages/destination-actions/src/destinations/amazon-ads/generated-types.ts
@@ -5,10 +5,6 @@ export interface Settings {
    * Region for API Endpoint, either NA, EU, FE.
    */
   region: string
-  /**
-   * Advertiser Id
-   */
-  advertiserId: string
 }
 // Generated file. DO NOT MODIFY IT BY HAND.
 
@@ -37,4 +33,8 @@ export interface AudienceSettings {
    * Time-to-live in seconds. The amount of time the record is associated with the audience.
    */
   ttl?: number
+  /**
+   * Advertiser Id
+   */
+  advertiserId: string
 }
