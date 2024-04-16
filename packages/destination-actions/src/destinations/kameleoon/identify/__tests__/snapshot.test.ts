afa43e277366d14ffea9a3893286865f87b645ca
Joe Ayoub <joe.ayoub@segment.com>
2024-04-16 15:11:54 +0100

    fixing kameleoon timestamp failing tests

--- packages/destination-actions/src/destinations/kameleoon/identify/__tests__/snapshot.test.ts
+++ packages/destination-actions/src/destinations/kameleoon/identify/__tests__/snapshot.test.ts
@@ -11,7 +11,10 @@ const seedName = `${destinationSlug}#${actionSlug}`
 describe(`Testing snapshot for ${destinationSlug}'s ${actionSlug} destination action:`, () => {
   it('required fields', async () => {
     const action = destination.actions[actionSlug]
-    const [eventData, settingsData] = generateTestData(seedName, destination, action, true)
+    let [eventData, settingsData] = generateTestData(seedName, destination, action, true)
+
+    eventData = {...eventData, timestamp: new Date(Date.UTC(2024, 0, 3, 0, 0, 0)).toISOString()}
+    settingsData = {...settingsData}
 
     nock(/.*/).persist().get(/.*/).reply(200)
     nock(/.*/).persist().post(/.*/).reply(200)
@@ -44,7 +47,10 @@ describe(`Testing snapshot for ${destinationSlug}'s ${actionSlug} destination ac
 
   it('all fields', async () => {
     const action = destination.actions[actionSlug]
-    const [eventData, settingsData] = generateTestData(seedName, destination, action, false)
+    let [eventData, settingsData] = generateTestData(seedName, destination, action, false)
+
+    eventData = {...eventData, timestamp: new Date(Date.UTC(2024, 0, 3, 0, 0, 0)).toISOString()}
+    settingsData = {...settingsData}
 
     nock(/.*/).persist().get(/.*/).reply(200)
     nock(/.*/).persist().post(/.*/).reply(200)
