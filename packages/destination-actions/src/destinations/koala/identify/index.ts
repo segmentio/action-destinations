03ba064145ab394b7a86e50306a0e764f9f760db
Matthew Shwery <matt@getkoala.com>
2024-04-22 11:00:10 -0300

    add source options to koala identifies

--- packages/destination-actions/src/destinations/koala/identify/index.ts
+++ packages/destination-actions/src/destinations/koala/identify/index.ts
@@ -79,6 +79,7 @@ const action: ActionDefinition<Settings, Payload> = {
         identifies: [
           {
             type: 'identify',
+            source: 'segment-cloud',
             ...data.payload
           }
         ]
