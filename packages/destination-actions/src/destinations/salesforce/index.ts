92ceb2b6e2ef0660399809af4c703ec8086c95fd
Nick Aguilar <nicholas.aguilar@segment.com>
2024-04-22 20:14:01 -0700

    Implementation of a flow where the conventional OAuth refreshToken flow is attempted and the username+password flow is tried if that fails

--- packages/destination-actions/src/destinations/salesforce/index.ts
+++ packages/destination-actions/src/destinations/salesforce/index.ts
@@ -7,10 +7,7 @@ import opportunity from './opportunity'
 import customObject from './customObject'
 import contact from './contact'
 import account from './account'
-
-interface RefreshTokenResponse {
-  access_token: string
-}
+import Salesforce from './sf-operations'
 
 const destination: DestinationDefinition<Settings> = {
   name: 'Salesforce (Actions)',
@@ -27,6 +24,16 @@ const destination: DestinationDefinition<Settings> = {
         type: 'string',
         required: true
       },
+      username: {
+        label: 'Username',
+        description: 'Your Salesforce username',
+        type: 'string'
+      },
+      auth_password: {
+        label: 'Password',
+        description: 'Your Salesforce password',
+        type: 'password'
+      },
       isSandbox: {
         label: 'Sandbox Instance',
         description:
@@ -36,18 +43,16 @@ const destination: DestinationDefinition<Settings> = {
       }
     },
     refreshAccessToken: async (request, { auth, settings }) => {
-      // Return a request that refreshes the access_token if the API supports it
       const baseUrl = settings.isSandbox ? 'https://test.salesforce.com' : 'https://login.salesforce.com'
-      const res = await request<RefreshTokenResponse>(`${baseUrl}/services/oauth2/token`, {
-        method: 'POST',
-        body: new URLSearchParams({
-          refresh_token: auth.refreshToken,
-          client_id: auth.clientId,
-          client_secret: auth.clientSecret,
-          grant_type: 'refresh_token'
-        })
-      })
-      return { accessToken: res.data.access_token }
+      const sfClient = new Salesforce(baseUrl, request)
+
+      try {
+        return await sfClient.authenticateWithRefreshToken(auth)
+      } catch (error) {
+        if (settings?.username && settings?.auth_password) {
+          return await sfClient.authenticateWithPassword(auth, settings.username, settings.auth_password)
+        }
+      }
     }
   },
   extendRequest({ auth }) {
