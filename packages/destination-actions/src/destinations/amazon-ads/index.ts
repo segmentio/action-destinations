f5a066e694f2614dede1b5937d251f5aacb43bcd
Gaurav Kochar <gaurav.kochar@segment.com>
2024-05-02 16:15:52 +0530

    Fixing External Id issue , replace with quoted string

--- packages/destination-actions/src/destinations/amazon-ads/index.ts
+++ packages/destination-actions/src/destinations/amazon-ads/index.ts
@@ -220,8 +220,10 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
           }
         })
 
-        const r = await response.json()
-        const externalId = r?.audienceId
+        const res = await response.text()
+        //Replace the Big Int number with quoted String
+        const resString = res.replace(/"audienceId":(\d+)/, '"audienceId":"$1"')
+        const externalId = JSON.parse(resString)['audienceId']
 
         if (!externalId) {
           statsClient?.incr(`${statsName}.error`, 1, statsTags)
@@ -268,9 +270,10 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
         const response = await request(`${endpoint}/amc/audiences/metadata/${audience_id}`, {
           method: 'GET'
         })
-
-        const r = await response.json()
-        const externalId = r?.audienceId
+        const res = await response.text()
+        //Replace the Big Int number with quoted String
+        const resString = res.replace(/"audienceId":(\d+)/, '"audienceId":"$1"')
+        const externalId = JSON.parse(resString)['audienceId']
 
         if (externalId != getAudienceInput.externalId) {
           statsClient?.incr(`${statsName}.error`, 1, statsTags)
