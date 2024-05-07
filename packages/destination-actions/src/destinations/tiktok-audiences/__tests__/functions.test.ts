9bb6f529fce291ddd4e200a5dc08e474d5fd3d49
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2024-05-07 05:23:39 -0700

    Also not double-hashing the phone for TikTok Audiences (#1770)
    * Also not double-hashing the phone for TikTok Audiences
    
    * Just renaming the variable.

--- packages/destination-actions/src/destinations/tiktok-audiences/__tests__/functions.test.ts
+++ packages/destination-actions/src/destinations/tiktok-audiences/__tests__/functions.test.ts
@@ -23,5 +23,29 @@ describe('TikTok Audiences Functions', () => {
       const result: any[][] = extractUsers([payload])
       expect(result[0][0].id).toEqual('77bc071241f37b4736df28c0c1cb0a99163d1050696134325b99246b2183d408')
     })
+
+    it('Should hash phone number when phone is in a plain format', () => {
+      const payload = {
+        phone: '+1 (555) 555-5555',
+        send_phone: true,
+        audience_id: '1234567890',
+        advertising_id: '12345'
+      }
+
+      const result: any[][] = extractUsers([payload])
+      expect(result[0][0].id).toEqual('e5aaca26e304714083dccf6d2dbc16466e9cde94ca54feefa3dca412e2eeb74e')
+    })
+
+    it('Should NOT hash phone number when phone is already hashed', () => {
+      const payload = {
+        phone: 'e5aaca26e304714083dccf6d2dbc16466e9cde94ca54feefa3dca412e2eeb74e',
+        send_phone: true,
+        audience_id: '1234567890',
+        advertising_id: '12345'
+      }
+
+      const result: any[][] = extractUsers([payload])
+      expect(result[0][0].id).toEqual('e5aaca26e304714083dccf6d2dbc16466e9cde94ca54feefa3dca412e2eeb74e')
+    })
   })
 })
