714774b3d4a7a317a174b9a15fab55bd9fe89e1b
alfrimpong <afrimpong@twilio.com>
2024-05-21 10:45:25 -0500

    chore: sync with main

---
+++ packages/destination-actions/src/destinations/contentstack/constants.ts
@@ -0,0 +1,3 @@
+export const PERSONALIZE_API_BASE_URL = 'https://personalization-api.contentstack.com'
+export const PERSONALIZE_EDGE_API_URL = 'https://personalization-edge.contentstack.com'
+export const ACCESS_TOKEN_URL = 'https://developerhub-api.contentstack.com/apps/token'
