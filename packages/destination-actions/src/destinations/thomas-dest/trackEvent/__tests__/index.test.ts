56f7d09c561267f9e8bd3dba16a8edf62bec3406
Thomas Gilbert <thomas.gilbert@segment.com>
2024-05-29 13:16:44 -0400

    remove test dest

--- packages/destination-actions/src/destinations/thomas-dest/trackEvent/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-// import nock from 'nock'
-// import { createTestEvent, createTestIntegration } from '@segment/actions-core'
-// import Destination from '../../index'
-
-// const testDestination = createTestIntegration(Destination)
-
-// describe('ThomasDest.trackEvent', () => {
-//   // TODO: Test your action
-// })
