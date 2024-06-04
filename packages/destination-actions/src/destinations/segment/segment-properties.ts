7ebadd0573be6cd2a31a2fb4dfb482ae6cbbf761
Chenxiang Zhang <chenxiang.zhang@segment.com>
2024-06-04 17:02:44 -0400

    consent not hidden

--- packages/destination-actions/src/destinations/segment/segment-properties.ts
+++ packages/destination-actions/src/destinations/segment/segment-properties.ts
@@ -365,6 +365,5 @@ export const consent: InputField = {
   description: 'Segment event consent category preferences.',
   type: 'object',
   default: { '@path': '$.context.consent' },
-  additionalProperties: true,
-  unsafe_hidden: true
+  additionalProperties: true
 }
