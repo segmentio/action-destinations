2b3bd37c1e750f0bf7aa25713897891ea0e0e243
Ricky Zhong <ricky.zhong@stackadapt.com>
2024-06-19 10:11:42 -0400

    nit

--- packages/destination-actions/src/destinations/stackadapt-audiences/index.ts
+++ packages/destination-actions/src/destinations/stackadapt-audiences/index.ts
@@ -6,7 +6,7 @@ import postMessage from './postMessage'
 // TODO: change to production
 export const domain = 'https://sandbox.stackadapt.com/public/graphql'
 const destination: DestinationDefinition<Settings> = {
-  name: 'Stackadapt Audiences',
+  name: 'StackAdapt Audiences',
   slug: 'actions-stackadapt-audiences',
   mode: 'cloud',
 
@@ -21,9 +21,34 @@ const destination: DestinationDefinition<Settings> = {
       }
     },
     testAuthentication: async (request) => {
-      const res = await request(domain)
+      const scopeQuery = `query {
+        tokenInfo {
+          name
+          expiresAt
+          createdAt
+          scopesByAdvertiser {
+            nodes {
+              advertiser {
+                name
+              }
+              scopes
+            }
+          }
+        }
+      }`
+
+      const res = await request(domain, {
+        body: JSON.stringify({ query: scopeQuery })
+      })
       if (res.status !== 200) {
         throw new Error(res.status + res.statusText)
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      const scopes: string[] = JSON.parse(res.content)?.data?.tokenInfo?.scopesByAdvertiser?.nodes?.flatMap(
+        (node: { scopes: string[] }) => node.scopes
+      )
+      if (!scopes.includes('WRITE')) {
+        throw new Error('Please verify your GQL Token or contact support')
       }
     }
   },
