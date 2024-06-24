5f5d2945448c9a4ad30b5a89f168d233bb47e7fd
Alice Mackel <alice.mackel@stackadapt.com>
2024-06-24 05:03:52 -0400

    Feature(IDE-2258): Perform and perform batch functions for Segment audience destination

---
+++ packages/destination-actions/src/destinations/stackadapt-audiences/forwardProfile/__tests__/index.test.ts
@@ -0,0 +1,116 @@
+import nock from 'nock'
+import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import Definition from '../../index'
+import { SegmentEvent } from '@segment/actions-core/*'
+
+const testDestination = createTestIntegration(Definition)
+const mockGqlKey = 'test-graphql-key'
+
+const gqlHostUrl = 'https://sandbox.stackadapt.com'
+const gqlPath = '/public/graphql'
+const mockEmail = 'admin@stackadapt.com'
+const mockUserId = 'user-id'
+const mockEmail2 = 'email2@stackadapt.com'
+const mockUserId2 = 'user-id2'
+
+const defaultEventPayload: Partial<SegmentEvent> = {
+  userId: mockUserId,
+  type: 'identify',
+  traits: {
+    email: mockEmail,
+    first_time_buyer: true
+  },
+  context: {
+    personas: {
+      computation_class: 'audience',
+      computation_key: 'first_time_buyer'
+    }
+  }
+}
+
+const batchEventPayload: Partial<SegmentEvent> = {
+  userId: mockUserId2,
+  type: 'identify',
+  traits: {
+    email: mockEmail2
+  }
+}
+
+describe('forwardProfile', () => {
+  it('should translate audience entry/exit into GQL format', async () => {
+    let requestBody
+    nock(gqlHostUrl)
+      .post(gqlPath, (body) => {
+        requestBody = body
+        return body
+      })
+      .reply(200, { data: { success: true } })
+    const event = createTestEvent(defaultEventPayload)
+    const responses = await testDestination.testAction('forwardProfile', {
+      event,
+      useDefaultMappings: true,
+      settings: { apiKey: mockGqlKey }
+    })
+    expect(responses.length).toBe(1)
+    expect(responses[0].status).toBe(200)
+    expect(responses[0].request.headers).toMatchInlineSnapshot(`
+      Headers {
+        Symbol(map): Object {
+          "authorization": Array [
+            "Bearer test-graphql-key",
+          ],
+          "content-type": Array [
+            "application/json",
+          ],
+          "user-agent": Array [
+            "Segment (Actions)",
+          ],
+        },
+      }
+    `)
+    expect(requestBody).toMatchInlineSnapshot(`
+      Object {
+        "query": "mutation {
+            upsertProfiles(
+              subAdvertiserId: 1,
+              externalProvider: \\"Segment\\",
+              profiles: [{userId:\\"user-id\\",listId:\\"first_time_buyer\\",action:\\"enter\\",email:\\"admin@stackadapt.com\\"}]
+            ) {
+              success
+            }
+          }",
+      }
+    `)
+  })
+
+  it('should batch multiple profile events into a single request', async () => {
+    let requestBody
+    nock(gqlHostUrl)
+      .post(gqlPath, (body) => {
+        requestBody = body
+        return body
+      })
+      .reply(200, { data: { success: true } })
+    const events = [createTestEvent(defaultEventPayload), createTestEvent(batchEventPayload)]
+    const responses = await testDestination.testBatchAction('forwardProfile', {
+      events,
+      useDefaultMappings: true,
+      settings: { apiKey: mockGqlKey }
+    })
+    expect(responses.length).toBe(1)
+    expect(responses[0].status).toBe(200)
+    expect(requestBody).toMatchInlineSnapshot(`
+      Object {
+        "query": "mutation {
+            upsertProfiles(
+              subAdvertiserId: 1,
+              externalProvider: \\"Segment\\",
+              profiles: [{userId:\\"user-id\\",listId:\\"first_time_buyer\\",action:\\"enter\\",email:\\"admin@stackadapt.com\\"},{userId:\\"user-id2\\",email:\\"email2@stackadapt.com\\"}]
+            ) {
+              success
+            }
+          }",
+      }
+    `)
+  })
+})
---
+++ packages/destination-actions/src/destinations/stackadapt-audiences/forwardProfile/functions.ts
@@ -0,0 +1,35 @@
+import { RequestClient } from '@segment/actions-core'
+import { Payload } from './generated-types'
+
+export async function performForwardProfiles(request: RequestClient, events: Payload[]) {
+  // TODO: switch to production endpoint
+  const endpoint = 'https://sandbox.stackadapt.com/public/graphql'
+  const profileUpdates = events.map((event) => {
+    const profile: Record<string, string | number | undefined> = {
+      userId: event.user_id
+    }
+    if (event.segment_computation_class === 'audience' && event.traits && event.segment_computation_key) {
+      // This is an audience enter/exit event, there should be a boolean flag in the traits indicating if the user entered or exited the audience
+      // We need to translate it into an enter or exit action as expected by the profile upsert GraphQL mutation
+      const audienceKey = event.segment_computation_key
+      profile.listId = audienceKey
+      profile.action = event.traits[audienceKey] ? 'enter' : 'exit'
+      delete event.traits[audienceKey] // Don't need to include the boolean flag in the GQL payload
+    }
+    return { ...profile, ...event.traits }
+  })
+
+  // TODO: Add setting for advertiser ID and replace hardcoded value with it
+  const mutation = `mutation {
+      upsertProfiles(
+        subAdvertiserId: 1,
+        externalProvider: "Segment",
+        profiles: ${JSON.stringify(profileUpdates).replace(/"([^"]+)":/g, '$1:') /* Remove quotes around keys */}
+      ) {
+        success
+      }
+    }`
+  return await request(endpoint, {
+    body: JSON.stringify({ query: mutation })
+  })
+}
