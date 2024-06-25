0a5192ca3a122986751e2bf11682e7d0b3220c4e
Gaurav Kochar <gaurav.kochar@segment.com>
2024-06-25 15:10:44 +0530

    commented code

--- packages/destination-actions/src/destinations/yahoo-audiences/generated-types.ts
+++ packages/destination-actions/src/destinations/yahoo-audiences/generated-types.ts
@@ -21,17 +21,4 @@ export interface AudienceSettings {
    * Placeholder field to allow the audience to be created. Do not change this
    */
   placeholder?: boolean
-  /**
-   * The monetary value for a conversion. This is an object with shape: {"currencyCode": USD", "amount": "100"}
-   */
-  personas: {
-    /**
-     * ISO format
-     */
-    computation_id: string
-    /**
-     * Value of the conversion in decimal string. Can be dynamically set up or have a fixed value.
-     */
-    computation_key: string
-  }
 }
