899be59447e6c8d27817e6f4c32b61c8cf3011f8
Pooya Jaferian <pooya.j@gmail.com>
2024-07-02 14:29:46 -0700

    adding code for test destination

---
+++ packages/destination-actions/src/destinations/mappings-two-test/index.ts
@@ -0,0 +1,32 @@
+import type { DestinationDefinition } from '@segment/actions-core'
+import type { Settings } from './generated-types'
+
+import sendEvent from './sendEvent'
+
+const destination: DestinationDefinition<Settings> = {
+  name: 'Mappings Two Test',
+  slug: 'actions-mappings-two-test',
+  mode: 'cloud',
+
+  authentication: {
+    scheme: 'custom',
+    fields: {
+      apiKey: {
+        label: 'API Key',
+        description:
+          'Your Mappings Two Test API Key. This is a Segment production write key that you can find in your Segment settings. All the actions in this destination will send payloads as Segment events to this source.',
+        type: 'string',
+        required: true
+      }
+    },
+    testAuthentication: (request, { settings }) => {
+      return request(`https://cdn.segment.com/v1/projects/${settings.apiKey}/settings`)
+    }
+  },
+
+  actions: {
+    sendEvent
+  }
+}
+
+export default destination
