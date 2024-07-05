b0bf09a1c9ae2a4909e66be68f7a6cf9168efcaf
Varadarajan V <109586712+varadarajan-tw@users.noreply.github.com>
2024-07-05 17:43:57 +0530

    Introduce new release flow

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,10 +3,10 @@ branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD);
 
 if [[ $branch != "main" && $branch != "release" ]];
 then
-  echo "You must be on the main or relesase branch to release"
+  echo "You must be on the main or release branch to release"
   exit
 fi;
 
 git pull --ff-only
 echo "Running lerna version minor..."
-lerna version minor --no-private -y
+lerna version minor --no-private -y --no-git-tag-version
