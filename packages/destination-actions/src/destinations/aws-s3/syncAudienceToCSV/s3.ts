eb6e49f72b2600d18fbceb0c0ce5b41618e98669
Joe Ayoub <joe.ayoub@segment.com>
2024-07-10 19:08:29 +0200

    refactor

---
+++ packages/destination-actions/src/destinations/aws-s3/syncAudienceToCSV/s3.ts
@@ -0,0 +1,128 @@
+import { InvalidAuthenticationError } from '@segment/actions-core'
+import { AudienceSettings, Settings } from '../generated-types'
+import { STSClient, AssumeRoleCommand } from '@aws-sdk/client-sts'
+//Define the Interface for testing
+interface Credentials {
+  accessKeyId: string
+  secretAccessKey: string
+  sessionToken: string
+}
+// Assume role and get temporary credentials
+let roleArn: string,
+  roleSessionName: string,
+  region = ''
+
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+
+import { v4 as uuidv4 } from '@lukeed/uuid'
+
+// Assume role and get temporary credentials
+const assumeRole = async (roleArn: string, roleSessionName: string): Promise<Credentials> => {
+  const stsClient = new STSClient({ region })
+  const command = new AssumeRoleCommand({
+    RoleArn: roleArn,
+    RoleSessionName: roleSessionName
+  })
+
+  const response = await stsClient.send(command)
+  if (!response.Credentials) {
+    throw new Error('Failed to assume role and get temporary credentials')
+  }
+
+  return {
+    accessKeyId: response.Credentials.AccessKeyId!,
+    secretAccessKey: response.Credentials.SecretAccessKey!,
+    sessionToken: response.Credentials.SessionToken!
+  }
+}
+
+function validateS3(settings: Settings, audienceSettings: AudienceSettings) {
+  if (!settings.iam_role_arn) {
+    throw new InvalidAuthenticationError('Selected S3 upload mode, but missing IAM Role ARN')
+  }
+  if (!settings.s3_aws_bucket_name) {
+    throw new InvalidAuthenticationError('Selected S3 upload mode, but missing AWS S3 bucket name')
+  }
+  if (audienceSettings.s3_aws_folder_name && !audienceSettings.s3_aws_folder_name.endsWith('/')) {
+    throw new InvalidAuthenticationError('Selected S3 upload mode, but the folder name must end with "/"')
+  }
+  if (!settings.s3_aws_region) {
+    throw new InvalidAuthenticationError('Selected S3 upload mode, but missing AWS Region')
+  }
+}
+
+// Function to upload a CSV content to S3
+const uploadCSV = async (
+  credentials: Credentials,
+  fileContent: Buffer | string,
+  bucketName: string,
+  folderName: string,
+  key: string,
+  region: string
+) => {
+  const s3Client = new S3Client({
+    region,
+    credentials: {
+      accessKeyId: credentials.accessKeyId,
+      secretAccessKey: credentials.secretAccessKey,
+      sessionToken: credentials.sessionToken
+    }
+  })
+  const objectKey = folderName ? `${folderName}${key}` : key
+  const uploadParams = {
+    Bucket: bucketName,
+    Key: objectKey,
+    Body: fileContent,
+    ContentType: 'text/csv'
+  }
+
+  try {
+    await s3Client.send(new PutObjectCommand(uploadParams))
+  } catch (err) {
+    throw new Error(`Non-retryable error: ${err}`)
+  }
+  return { statusCode: 200, message: 'Upload successful' }
+}
+
+// Function to get credentials
+const getCredentials = async (roleArn: string, roleSessionName: string): Promise<Credentials> => {
+  try {
+    const credentials = await assumeRole(roleArn, roleSessionName)
+    return credentials
+  } catch (err) {
+    throw new Error(`Non-retryable error: ${err}`)
+  }
+}
+
+async function uploadS3(
+  settings: Settings,
+  audienceSettings: AudienceSettings,
+  filename: string,
+  fileContent: Buffer
+): Promise<{ statusCode: number; message: string }> {
+  try {
+    roleArn = settings.iam_role_arn
+    roleSessionName = uuidv4()
+    region = settings.s3_aws_region
+    const credentials = await getCredentials(roleArn, roleSessionName)
+    const dateSuffix = new Date().toISOString().replace(/[:.]/g, '-')
+
+    if (filename.endsWith('.csv')) {
+      // Insert the date suffix before the .csv extension
+      filename = filename.replace('.csv', `_${dateSuffix}.csv`)
+    } else {
+      // Append the date suffix followed by .csv
+      filename = `${filename}_${dateSuffix}.csv`
+    }
+
+    const bucketName = settings.s3_aws_bucket_name
+    const folderName = audienceSettings.s3_aws_folder_name || ''
+
+    await uploadCSV(credentials, fileContent, bucketName, folderName, filename, region)
+    return { statusCode: 200, message: 'Upload successful' }
+  } catch (err) {
+    return { statusCode: 500, message: 'Upload failed' }
+  }
+}
+
+export { validateS3, uploadS3 }
