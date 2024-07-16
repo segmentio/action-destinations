b17100b26520e322a4463b076a1e3321894cb394
Arvind Nedumaran <arvindamirtaa@gmail.com>
2024-07-16 12:51:11 -0700

    Initial destination scaffold + auth check

---
+++ packages/destination-actions/src/destinations/dawn-analytics/generated-types.ts
@@ -0,0 +1,8 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Settings {
+  /**
+   * Your write key from Dawn Analytics.
+   */
+  writeKey: string
+}
---
+++ packages/destination-actions/src/destinations/dawn-analytics/index.ts
@@ -0,0 +1,40 @@
+import type { DestinationDefinition } from '@segment/actions-core'
+import type { Settings } from './generated-types'
+
+const destination: DestinationDefinition<Settings> = {
+  name: 'Dawn Analytics',
+  slug: 'actions-dawn-analytics',
+  mode: 'cloud',
+
+  authentication: {
+    scheme: 'custom',
+    fields: {
+      writeKey: {
+        label: 'Dawn write key',
+        description: 'Your write key from Dawn Analytics.',
+        type: 'string',
+        required: true
+      }
+    },
+    testAuthentication: (request, { settings }) => {
+      return request(`https://api.dawnai.com/validate-auth`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${settings.writeKey}`
+        },
+        body: null
+      })
+    }
+  },
+
+  // onDelete: async (request, { settings, payload }) => {
+  //   // Return a request that performs a GDPR delete for the provided Segment userId or anonymousId
+  //   // provided in the payload. If your destination does not support GDPR deletion you should not
+  //   // implement this function and should remove it completely.
+  // },
+
+  actions: {}
+}
+
+export default destination
