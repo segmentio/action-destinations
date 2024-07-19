ae1d818f5b8c40a7d17b2965142cbf37d920586d
Marín Alcaraz <marin.alcarazcordova@segment.com>
2024-07-19 14:48:29 -0700

    Add simplified mappings "UploadClickConversion" action

--- packages/destination-actions/src/destinations/google-enhanced-conversions/index.ts
+++ packages/destination-actions/src/destinations/google-enhanced-conversions/index.ts
@@ -5,6 +5,8 @@ import uploadCallConversion from './uploadCallConversion'
 import uploadClickConversion from './uploadClickConversion'
 import uploadConversionAdjustment from './uploadConversionAdjustment'
 
+import uploadClickConversion2 from './uploadClickConversion2'
+
 interface RefreshTokenResponse {
   access_token: string
   scope: string
@@ -75,7 +77,8 @@ const destination: DestinationDefinition<Settings> = {
     postConversion,
     uploadClickConversion,
     uploadCallConversion,
-    uploadConversionAdjustment
+    uploadConversionAdjustment,
+    uploadClickConversion2
   }
 }
 
