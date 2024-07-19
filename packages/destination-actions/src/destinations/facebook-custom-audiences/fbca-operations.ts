da759ff074684d34cdf8a17357cb2334d5dd149d
Nick Aguilar <nicholas.aguilar@segment.com>
2024-07-19 13:26:02 -0700

    Squashed commit of the following:
    commit 0a1285a1fc6419f3a498e97aafd760db0993c9f1
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Thu Jul 18 15:08:29 2024 -0700
    
        Generates types
    
    commit 8f5f5ed48a05cd2b24d121925fc81bb35033b1b3
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Thu Jul 18 15:04:14 2024 -0700
    
        WIP on an inexplicably not passing unit test
    
    commit fc4f5ed07e908ad29e434a854410aa4351aa3be0
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Thu Jul 18 14:05:48 2024 -0700
    
        Checks for existence of user selected audience. Saves audience name and ID when returning from hook
    
    commit a2a642c395e9f004f65950b09643490da5dbecfa
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Thu Jul 18 12:58:03 2024 -0700
    
        WIP - verifying selected audience exists and saving it's ID as output
    
    commit a022704c9668d8ca92ee0651cd64a024f690e78b
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Thu Jul 18 12:47:32 2024 -0700
    
        Removes description param from creating an audience per PRD
    
    commit 49add301d15fbaec2c658cfddd3af65a525595d5
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Thu Jul 18 12:44:07 2024 -0700
    
        Pulls down 200 most recent audiences in dynamic field response
    
    commit c2f3e32e477e5df962721b93eac17066ed933284
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Thu Jul 18 12:35:50 2024 -0700
    
        Removes pagination related code - that work will be saved for later :)
    
    commit 6078690694e7041d1020f6b39d03c930fbcd7099
    Merge: b831f7b50 c9ecf66fa
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Thu Jul 18 12:31:29 2024 -0700
    
        Merge branch 'main' into fbca-retl-hook
    
    commit b831f7b506006ce1e202ea897ce4e7ec583004f2
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Wed Jul 17 15:14:15 2024 -0700
    
        Sets paging return such that the previous page was the current page
    
    commit 365690e16ef5b6ca9576ab1cec6b76925a0b2db8
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Wed Jul 17 15:09:14 2024 -0700
    
        Can pass dynamicFieldContext & paging into local server request
    
    commit 037534c439b66e6d73c1926ca37457c7f9943c4d
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Wed Jul 17 14:37:23 2024 -0700
    
        Fixes build
    
    commit 1e9d6168094a3f35787c7cb3fff43903ab9f4dca
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Tue Jul 16 16:55:05 2024 -0700
    
        WIP on a pagination mechanism for dynamic fields
    
    commit 151d26cc349530af6201680a36a84a247ad16efe
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Tue Jul 16 16:14:03 2024 -0700
    
        Adds a dynamic audience field, pulls down all customaudiences as choices
    
    commit 27e49035faf4ff80640b14654be9e33ca14a93b8
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Tue Jul 16 15:29:46 2024 -0700
    
        Adds required customer_file_source field
    
    commit 60d22911d80df4c98eaaee7a31fbe800ccc0884d
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Tue Jul 16 12:26:51 2024 -0700
    
        WIP - first draft of a FacebookClient class, including an untested createAudience hook for retl
    
    commit 5ba6c09e430b11d90554ae6ef12da0353a6fe2ff
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Tue Jul 16 12:05:23 2024 -0700
    
        Introduces shared adAccountId top-level setting and audienceSetting
    
    commit 0a20b76db6342aa9718c8a3a60a14a7ef143273b
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Tue Jul 16 12:02:16 2024 -0700
    
        Removes delete action, add action. Introduces single sync action

---
+++ packages/destination-actions/src/destinations/facebook-custom-audiences/fbca-operations.ts
@@ -0,0 +1,85 @@
+import { DynamicFieldItem, DynamicFieldError, RequestClient } from '@segment/actions-core'
+
+const FACEBOOK_API_VERSION = 'v20.0'
+// exported for unit testing
+export const BASE_URL = `https://graph.facebook.com/${FACEBOOK_API_VERSION}/`
+
+interface AudienceCreationResponse {
+  id: string
+}
+
+interface GetAllAudienceResponse {
+  data: {
+    id: string
+    name: string
+  }[]
+}
+
+interface GetSingleAudienceResponse {
+  name: string
+  id: string
+}
+
+interface FacebookResponseError {
+  error: {
+    message: string
+    type: string
+    code: number
+  }
+}
+
+export default class FacebookClient {
+  request: RequestClient
+  adAccountId: string
+
+  constructor(request: RequestClient, adAccountId: string) {
+    this.request = request
+    this.adAccountId = this.formatAdAccount(adAccountId)
+  }
+
+  createAudience = async (name: string) => {
+    return await this.request<AudienceCreationResponse>(`${BASE_URL}${this.adAccountId}/customaudiences`, {
+      method: 'post',
+      json: {
+        name,
+        subtype: 'CUSTOM',
+        customer_file_source: 'BOTH_USER_AND_PARTNER_PROVIDED'
+      }
+    })
+  }
+
+  getSingleAudience = async (
+    audienceId: string
+  ): Promise<{ data?: GetSingleAudienceResponse; error?: FacebookResponseError }> => {
+    try {
+      const fields = '?fields=id,name'
+      const { data } = await this.request<GetSingleAudienceResponse>(`${BASE_URL}${audienceId}${fields}`)
+      return { data, error: undefined }
+    } catch (error) {
+      return { data: undefined, error: error as FacebookResponseError }
+    }
+  }
+
+  getAllAudiences = async (): Promise<{ choices: DynamicFieldItem[]; error: DynamicFieldError | undefined }> => {
+    const { data } = await this.request<GetAllAudienceResponse>(
+      `${BASE_URL}${this.adAccountId}/customaudiences?fields=id,name&limit=200`
+    )
+
+    const choices = data.data.map(({ id, name }) => ({
+      value: id,
+      label: name
+    }))
+
+    return {
+      choices,
+      error: undefined
+    }
+  }
+
+  private formatAdAccount(adAccountId: string) {
+    if (adAccountId.startsWith('act_')) {
+      return adAccountId
+    }
+    return `act_${adAccountId}`
+  }
+}
