726544a280b7ed12df7803e24bc06b851f814fd7
Pooya Jaferian <pooya.j@gmail.com>
2024-07-19 10:10:29 -0700

    mappings 2

---
+++ packages/destination-actions/src/destinations/mappings-two-test/sendEvent/generated-types.ts
@@ -0,0 +1,52 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * A test string field
+   */
+  test_field?: string
+  /**
+   * A dynamic object
+   */
+  dynamic_object: {
+    [k: string]: unknown
+  }
+  /**
+   * Choose a pet or plant and provide details
+   */
+  dynamic_structured_object: {
+    /**
+     * Pet type
+     */
+    pet?: string
+    /**
+     * Plant type
+     */
+    plant?: string
+    /**
+     * Name
+     */
+    name?: string
+    /**
+     * Age
+     */
+    age?: number
+  }
+  /**
+   * A dynamic array
+   */
+  dynamic_array?: {
+    /**
+     * Product Category
+     */
+    product_category?: string
+    /**
+     * Product Name
+     */
+    product_name?: string
+    /**
+     * Product Price
+     */
+    product_price?: number
+  }[]
+}
