ff347767ed448a0bb8c3e5710db05971d6069eeb
spiderman <spiderman@Michaels-MacBook-Pro.local>
2024-07-22 16:09:00 +0300

    comment

--- packages/destination-actions/src/destinations/blend-ai/trackEvents/index.ts
+++ packages/destination-actions/src/destinations/blend-ai/trackEvents/index.ts
@@ -3,6 +3,11 @@ import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 import { BASE_URL } from '../consts'
 
+/**
+ * This action replaces sendData which is depracated
+ * At the point of replacement - finaloop were still using the
+ * old action and sending events to us
+ */
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Track events',
   description: 'Send data to Blend AI for product usage insights',
