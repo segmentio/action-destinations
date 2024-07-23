c4735f42fbb35faa838463635572e662ceaff699
Romani-Ramzi <romani.ramzi@optimizely.com>
2024-07-23 10:13:26 +0100

    implementing changes

--- packages/destination-actions/src/destinations/optimizely-data-platform/upsertContact/index.ts
+++ packages/destination-actions/src/destinations/optimizely-data-platform/upsertContact/index.ts
@@ -3,6 +3,34 @@ import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 import { user_identifiers } from '../fields'
 import { hosts, getDOBDetails } from '../utils'
+import { RequestClient } from '@segment/actions-core'
+
+const sendRequest = async (request: RequestClient, payloads: Payload[], settings: Settings) => {
+  const host = hosts[settings.region]
+
+  const requestBody = payloads.map((payload) => {
+    return {
+      ...payload.additional_traits,
+      user_identifiers: payload.user_identifiers,
+      title: payload.title,
+      name: payload.name,
+      first_name: payload.firstname,
+      last_name: payload.lastname,
+      age: payload.age,
+      ...getDOBDetails(payload.DOB),
+      gender: payload.gender,
+      phone: payload.phone,
+      address: payload.address,
+      company: payload.company,
+      image_url: payload.avatar
+    }
+  })
+
+  return request(`${host}/batch_upsert_contact`, {
+    method: 'post',
+    json: requestBody
+  })
+}
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Upsert Contact',
@@ -114,32 +142,14 @@ const action: ActionDefinition<Settings, Payload> = {
       label: 'Addition User Traits',
       type: 'object',
       defaultObjectUI: 'keyvalue',
-      description: "Additional user profile details"
+      description: 'Additional user profile details'
     }
   },
   perform: (request, { payload, settings }) => {
-    const host = hosts[settings.region]
-
-    const body = {
-      ...payload.additional_traits,
-      user_identifiers: payload.user_identifiers,
-      title: payload.title,
-      name: payload.name,
-      first_name: payload.firstname,
-      last_name: payload.lastname,
-      age: payload.age,
-      ...getDOBDetails(payload.DOB),
-      gender: payload.gender,
-      phone: payload.phone,
-      address: payload.address,
-      company: payload.company,
-      image_url: payload.avatar
-    }
-
-    return request(`${host}/upsert_contact`, {
-      method: 'post',
-      json: body
-    })
+    return sendRequest(request, [payload], settings)
+  },
+  performBatch: (request, { payload, settings }) => {
+    return sendRequest(request, payload, settings)
   }
 }
 
