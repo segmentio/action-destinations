a54e845d90798987508f7135294c662fc1f0ca6e
Nick Aguilar <nicholas.aguilar@segment.com>
2024-07-24 14:05:05 -0700

    Generates types

--- packages/destination-actions/src/destinations/facebook-custom-audiences/sync/generated-types.ts
+++ packages/destination-actions/src/destinations/facebook-custom-audiences/sync/generated-types.ts
@@ -7,6 +7,10 @@ export interface HookBundle {
   retlOnMappingSave: {
     inputs?: {
       /**
+       * Choose to either create a new custom audience or use an existing one. If you opt to create a new audience, we will display the required fields for audience creation. If you opt to use an existing audience, a drop-down menu will appear, allowing you to select from all the custom audiences in your ad account.
+       */
+      operation?: string
+      /**
        * The name of the audience in Facebook.
        */
       audienceName?: string
