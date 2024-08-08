1af99b24dc40b4756884d25671dd68a4fbf6abdb
agustinespildora <jaespildora@uc.cl>
2024-08-08 09:25:56 -0400

    tests & snapshots

--- packages/destination-actions/src/destinations/topsort/click/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/topsort/click/__tests__/index.test.ts
@@ -1,9 +1,45 @@
-// import nock from 'nock'
-// import { createTestEvent, createTestIntegration } from '@segment/actions-core'
-// import Destination from '../../index'
+import nock from 'nock'
+import { AggregateAjvError } from '@segment/ajv-human-errors'
+import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import Destination from '../../index'
 
-// const testDestination = createTestIntegration(Destination)
+const testDestination = createTestIntegration(Destination)
 
 describe('Topsort.click', () => {
-  // TODO: Test your action
+  it('should successfully build a click and send', async () => {
+    nock(/.*/).persist().post(/.*/).reply(200)
+
+    const event = createTestEvent({
+      properties: {
+        resolvedBidId: 'thisisaresolvedbidid'
+      }
+    })
+
+    const responses = await testDestination.testAction('click', {
+      event,
+      settings: {
+        api_key: 'bar'
+      },
+      useDefaultMappings: true
+    })
+
+    expect(responses.length).toBe(1)
+    expect(responses[0].status).toBe(200)
+  })
+
+  it('should fail to build a click because it misses a required field (resolvedBidId)', async () => {
+    nock(/.*/).persist().post(/.*/).reply(200)
+
+    const event = createTestEvent({})
+
+    await expect(
+      testDestination.testAction('click', {
+        event,
+        settings: {
+          api_key: 'bar'
+        },
+        useDefaultMappings: true
+      })
+    ).rejects.toThrowError(AggregateAjvError)
+  })
 })
--- packages/destination-actions/src/destinations/topsort/purchase/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/topsort/purchase/__tests__/index.test.ts
@@ -1,9 +1,51 @@
-// import nock from 'nock'
-// import { createTestEvent, createTestIntegration } from '@segment/actions-core'
-// import Destination from '../../index'
+import nock from 'nock'
+import { AggregateAjvError } from '@segment/ajv-human-errors'
+import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import Destination from '../../index'
 
-// const testDestination = createTestIntegration(Destination)
+const testDestination = createTestIntegration(Destination)
 
 describe('Topsort.purchase', () => {
-  // TODO: Test your action
+  it('should successfully build a purchase and send', async () => {
+    nock(/.*/).persist().post(/.*/).reply(200)
+
+    const event = createTestEvent({
+      properties: {
+        products: [
+          {
+            product_id: '123',
+            price: 100,
+            quantity: 1
+          }
+        ]
+      }
+    })
+
+    const responses = await testDestination.testAction('purchase', {
+      event,
+      settings: {
+        api_key: 'bar'
+      },
+      useDefaultMappings: true
+    })
+
+    expect(responses.length).toBe(1)
+    expect(responses[0].status).toBe(200)
+  })
+
+  it('should fail to build an event because it misses a required field (products)', async () => {
+    nock(/.*/).persist().post(/.*/).reply(200)
+
+    const event = createTestEvent({})
+
+    await expect(
+      testDestination.testAction('purchase', {
+        event,
+        settings: {
+          api_key: 'bar'
+        },
+        useDefaultMappings: true
+      })
+    ).rejects.toThrowError(AggregateAjvError)
+  })
 })
