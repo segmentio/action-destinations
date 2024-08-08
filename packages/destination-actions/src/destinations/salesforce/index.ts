2fa973185104d5d699e8c84ad40e4ca910ab88c0
Marín Alcaraz <marin.alcarazcordova@segment.com>
2024-08-08 13:32:32 -0700

    Add Opportunity2 to support SyncMode

--- packages/destination-actions/src/destinations/salesforce/index.ts
+++ packages/destination-actions/src/destinations/salesforce/index.ts
@@ -11,6 +11,8 @@ import { authenticateWithPassword } from './sf-operations'
 
 import lead2 from './lead2'
 
+import opportunity2 from './opportunity2'
+
 interface RefreshTokenResponse {
   access_token?: string
   error?: string
@@ -121,7 +123,8 @@ const destination: DestinationDefinition<Settings> = {
     contact,
     opportunity,
     account,
-    lead2
+    lead2,
+    opportunity2
   }
 }
 
