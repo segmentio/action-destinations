4bd526d90ce368b2552b288a10133537f2752907
Gaurav Kochar <gaurav.kochar@segment.com>
2024-08-09 13:36:32 +0530

    Removed integrations.All from all segment-profiles actions

--- packages/destination-actions/src/destinations/segment-profiles/sendSubscription/index.ts
+++ packages/destination-actions/src/destinations/segment-profiles/sendSubscription/index.ts
@@ -310,11 +310,11 @@ const action: ActionDefinition<Settings, Payload> = {
         consent: isValidConsentObject ? { ...payload?.consent } : {}
       },
       timestamp: payload?.timestamp,
-      integrations: {
-        // Setting 'integrations.All' to false will ensure that we don't send events
-        // to any destinations which is connected to the Segment Profiles space
-        All: false
-      },
+      // integrations: {
+      //   // Setting 'integrations.All' to false will ensure that we don't send events
+      //   // to any destinations which is connected to the Segment Profiles space
+      //   All: false
+      // },
       messageId: payload?.message_id,
       type: 'identify'
     }
