d21f488837a5f780b059d32aeae3d1059a9f4d21
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2024-08-13 16:50:22 -0700

    Separating insert from update conversion type in CM360.

--- packages/destination-actions/src/destinations/campaign-manager-360/conversionAdjustmentUpload/index.ts
+++ packages/destination-actions/src/destinations/campaign-manager-360/conversionAdjustmentUpload/index.ts
@@ -6,6 +6,19 @@ const action: ActionDefinition<Settings, Payload> = {
   title: 'Conversion Adjustment Upload',
   description: "Inserts a conversion into Campaign Manager 360's profile configured under Settings.",
   fields: {
+    floodlightConfigurationId: {
+      label: 'Floodlight Configuration ID',
+      description: 'The Floodlight configuration ID associated with the conversion.',
+      type: 'string',
+      required: false,
+      dynamic: true
+    },
+    floodlightActivityId: {
+      label: 'Floodlight Activity ID',
+      description: 'The Floodlight activity ID associated with the conversion.',
+      type: 'string',
+      required: false
+    },
     gclid: {
       label: 'Google Click ID',
       description: 'The Google Click ID (gclid) associated with the conversion.',
@@ -17,6 +30,93 @@ const action: ActionDefinition<Settings, Payload> = {
       description: 'The Display Click ID (dclid) associated with the conversion.',
       type: 'string',
       required: false
+    },
+    encryptedUserId: {
+      label: 'Encrypted User ID',
+      description: 'The encrypted user ID associated with the conversion.',
+      type: 'string',
+      required: false
+    },
+    mobileDeviceId: {
+      label: 'Mobile Device ID',
+      description: 'The mobile device ID associated with the conversion.',
+      type: 'string',
+      required: false
+    },
+    timestampMicros: {
+      label: 'Timestamp (Microseconds)',
+      description: 'The timestamp of the conversion in microseconds.',
+      type: 'number',
+      required: true
+    },
+    value: {
+      label: 'Value',
+      description: 'The value of the conversion.',
+      type: 'number',
+      required: true
+    },
+    quantity: {
+      label: 'Quantity',
+      description: 'The quantity of the conversion.',
+      type: 'number',
+      required: true
+    },
+    ordinal: {
+      label: 'Ordinal',
+      description: 'The ordinal value of the conversion.',
+      type: 'number',
+      required: false
+    },
+    limitAdTracking: {
+      label: 'Limit Ad Tracking',
+      description:
+        'Whether Limit Ad Tracking is enabled. When set to true, the conversion will be used for reporting but not targeting. This will prevent remarketing.',
+      type: 'boolean',
+      required: false
+    },
+    childDirectedTreatment: {
+      label: 'Child Directed Treatment',
+      description: 'Whether this particular request may come from a user under the age of 13, under COPPA compliance.',
+      type: 'boolean',
+      required: false
+    },
+    nonPersonalizedAd: {
+      label: 'Non-Personalized Ad',
+      description: 'Whether the conversion was for a non personalized ad.',
+      type: 'boolean',
+      required: false
+    },
+    treatmentForUnderage: {
+      label: 'Treatment For Underage',
+      description:
+        "Whether this particular request may come from a user under the age of 16 (may differ by country), under compliance with the European Union's General Data Protection Regulation (GDPR).",
+      type: 'boolean',
+      required: false
+    },
+    matchId: {
+      label: 'Match ID',
+      description:
+        'The match ID field. A match ID is your own first-party identifier that has been synced with Google using the match ID feature in Floodlight.',
+      type: 'string',
+      required: false
+    },
+    userIdentifiers: {
+      label: 'User Identifiers',
+      description:
+        'User identifiers associated with the conversion. The maximum number of user identifiers for each conversion is 5.',
+      type: 'string',
+      required: false,
+      multiple: true
+    },
+    adUserDataConsent: {
+      label: 'Ad User Data Consent',
+      description: 'The user data consent status for the conversion.',
+      type: 'string',
+      required: false,
+      choices: [
+        { label: 'Granted', value: 'GRANTED' },
+        { label: 'Denied', value: 'DENIED' }
+      ]
     }
   },
   perform: (request, data) => {
--- packages/destination-actions/src/destinations/campaign-manager-360/conversionUpload/index.ts
+++ packages/destination-actions/src/destinations/campaign-manager-360/conversionUpload/index.ts
@@ -7,18 +7,6 @@ const action: ActionDefinition<Settings, Payload> = {
   title: 'Conversion Upload',
   description: "Inserts a conversion into Campaign Manager 360's profile configured under Settings.",
   fields: {
-    gclid: {
-      label: 'Google Click ID',
-      description: 'The Google Click ID (gclid) associated with the conversion.',
-      type: 'string',
-      required: false
-    },
-    dclid: {
-      label: 'Display Click ID',
-      description: 'The Display Click ID (dclid) associated with the conversion.',
-      type: 'string',
-      required: false
-    },
     floodlightConfigurationId: {
       label: 'Floodlight Configuration ID',
       description: 'The Floodlight configuration ID associated with the conversion.',
@@ -32,27 +20,51 @@ const action: ActionDefinition<Settings, Payload> = {
       type: 'string',
       required: false
     },
-    ordinal: {
-      label: 'Ordinal',
-      description: 'The ordinal value of the conversion.',
-      type: 'number',
+    gclid: {
+      label: 'Google Click ID',
+      description: 'The Google Click ID (gclid) associated with the conversion.',
+      type: 'string',
       required: false
     },
-    quantity: {
-      label: 'Quantity',
-      description: 'The quantity of the conversion.',
-      type: 'number',
+    dclid: {
+      label: 'Display Click ID',
+      description: 'The Display Click ID (dclid) associated with the conversion.',
+      type: 'string',
+      required: false
+    },
+    encryptedUserId: {
+      label: 'Encrypted User ID',
+      description: 'The encrypted user ID associated with the conversion.',
+      type: 'string',
+      required: false
+    },
+    mobileDeviceId: {
+      label: 'Mobile Device ID',
+      description: 'The mobile device ID associated with the conversion.',
+      type: 'string',
       required: false
     },
     timestampMicros: {
       label: 'Timestamp (Microseconds)',
       description: 'The timestamp of the conversion in microseconds.',
       type: 'number',
-      required: false
+      required: true
     },
     value: {
       label: 'Value',
       description: 'The value of the conversion.',
+      type: 'number',
+      required: true
+    },
+    quantity: {
+      label: 'Quantity',
+      description: 'The quantity of the conversion.',
+      type: 'number',
+      required: true
+    },
+    ordinal: {
+      label: 'Ordinal',
+      description: 'The ordinal value of the conversion.',
       type: 'number',
       required: false
     },
@@ -61,6 +73,65 @@ const action: ActionDefinition<Settings, Payload> = {
       description: 'Custom variables associated with the conversion.',
       type: 'object',
       required: false
+    },
+    limitAdTracking: {
+      label: 'Limit Ad Tracking',
+      description:
+        'Whether Limit Ad Tracking is enabled. When set to true, the conversion will be used for reporting but not targeting. This will prevent remarketing.',
+      type: 'boolean',
+      required: false
+    },
+    childDirectedTreatment: {
+      label: 'Child Directed Treatment',
+      description: 'Whether this particular request may come from a user under the age of 13, under COPPA compliance.',
+      type: 'boolean',
+      required: false
+    },
+    encryptedUserIdCandidates: {
+      label: 'Encrypted User ID Candidates',
+      description:
+        'A list of the alphanumeric encrypted user IDs. Any user ID with exposure prior to the conversion timestamp will be used in the inserted conversion. If no such user ID is found then the conversion will be rejected with INVALID_ARGUMENT error. When set, `encryptionInfo` should also be specified.',
+      type: 'string',
+      required: false,
+      multiple: true
+    },
+    nonPersonalizedAd: {
+      label: 'Non-Personalized Ad',
+      description: 'Whether the conversion was for a non personalized ad.',
+      type: 'boolean',
+      required: false
+    },
+    treatmentForUnderage: {
+      label: 'Treatment For Underage',
+      description:
+        "Whether this particular request may come from a user under the age of 16 (may differ by country), under compliance with the European Union's General Data Protection Regulation (GDPR).",
+      type: 'boolean',
+      required: false
+    },
+    matchId: {
+      label: 'Match ID',
+      description:
+        'The match ID field. A match ID is your own first-party identifier that has been synced with Google using the match ID feature in Floodlight.',
+      type: 'string',
+      required: false
+    },
+    userIdentifiers: {
+      label: 'User Identifiers',
+      description:
+        'User identifiers associated with the conversion. The maximum number of user identifiers for each conversion is 5.',
+      type: 'string',
+      required: false,
+      multiple: true
+    },
+    adUserDataConsent: {
+      label: 'Ad User Data Consent',
+      description: 'The user data consent status for the conversion.',
+      type: 'string',
+      required: false,
+      choices: [
+        { label: 'Granted', value: 'GRANTED' },
+        { label: 'Denied', value: 'DENIED' }
+      ]
     }
   },
 
