d05b96dc9bb28124e72c3386f7e9d998e796c2ba
Marín Alcaraz <marin.alcarazcordova@segment.com>
2024-08-14 10:55:49 -0700

    Remove matcher in SFDC traits

--- packages/destination-actions/src/destinations/salesforce/sf-properties.ts
+++ packages/destination-actions/src/destinations/salesforce/sf-properties.ts
@@ -161,7 +161,6 @@ export const traits2: InputField = {
   type: 'object',
   defaultObjectUI: 'keyvalue:only',
   depends_on: {
-    match: 'any',
     conditions: [
       {
         type: 'syncMode',
