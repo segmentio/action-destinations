0e8ea1f2572940f4365393c20078e8977b33e042
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2024-08-15 16:03:30 -0700

    Adding something to test authentication.

--- packages/destination-actions/src/destinations/campaign-manager-360/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/campaign-manager-360/__tests__/index.test.ts
@@ -1,5 +1,5 @@
 import nock from 'nock'
-import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import { createTestIntegration } from '@segment/actions-core'
 import Definition from '../index'
 
 const testDestination = createTestIntegration(Definition)
@@ -9,8 +9,13 @@ describe('Campaign Manager 360', () => {
     it('should validate authentication inputs', async () => {
       nock('https://your.destination.endpoint').get('*').reply(200, {})
 
-      // This should match your authentication.fields
-      const authData = {}
+      // For now we won't need this.
+      // This is just a placeholder to test the authentication function.
+      const authData = {
+        profileId: '12345',
+        defaultFloodlightActivityId: '23456',
+        defaultFloodlightConfigurationId: '34567'
+      }
 
       await expect(testDestination.testAuthentication(authData)).resolves.not.toThrowError()
     })
