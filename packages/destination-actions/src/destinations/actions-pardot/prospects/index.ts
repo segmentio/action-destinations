ac963f76fa34771cd491963d28876b943b5a8f59
rvadera12 <89420099+rvadera12@users.noreply.github.com>
2024-08-19 18:21:54 -0400

    No longer update the status code when content-type includes 'application/json' (#2306)

--- packages/destination-actions/src/destinations/actions-pardot/prospects/index.ts
+++ packages/destination-actions/src/destinations/actions-pardot/prospects/index.ts
@@ -200,7 +200,7 @@ const action: ActionDefinition<Settings, Payload> = {
           `Pardot responded witha error code ${data.code}: ${data.message}. This means Pardot has received the call, but consider the payload to be invalid.  To identify the exact error, please refer to ` +
             `https://developer.salesforce.com/docs/marketing/pardot/guide/error-codes.html?q=error#numerical-list-of-error-codes and search for the error code you received.`,
           'PARDOT_ERROR',
-          400
+          statusCode
         )
       }
       //XML error response handles the error in headers.
