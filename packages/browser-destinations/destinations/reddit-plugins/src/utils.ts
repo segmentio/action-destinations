d3d2a3d335e69261491dc084c342634cf0ecfa38
Joe Ayoub <joe.ayoub@segment.com>
2024-08-21 18:17:58 +0200

    removing some comments

--- packages/browser-destinations/destinations/reddit-plugins/src/utils.ts
+++ packages/browser-destinations/destinations/reddit-plugins/src/utils.ts
@@ -1,17 +1,3 @@
-// // The name of the storage location where we'll cache the Snap click_id Querystring value
-// export const storageClickIdKey = 'analytics_snap_capi_click_id'
-
-// // The name of the storage location where we'll cache the Snap scid cookie value
-// export const storageSCIDCookieKey = 'analytics_snap_capi_scid_cookie'
-
-
-
-// // The name of the Snap cookie to retrieve to retrieve when the page loads
-// export const scidCookieName = '_scid'
-
-// // The field name to include for the Snap scid cookie in the context.integrations.snap_conversions_api
-// export const scidIntegrationFieldName = 'uuid_c1'
-
 // The field name to include for the Reddit click_id Querystring in the context.integrations.Reddit Conversions Api.click_id
 export const clickIdIntegrationFieldName = 'click_id'
 
@@ -24,7 +10,7 @@ export const clickIdCookieName = 'rdt_cookie'
 // The field name to include for the Reddit rdt_uuid in the context.integrations.Reddit Conversions Api.rdt_uuid
 export const rdtUUIDIntegrationFieldName = 'uuid'
 
-// The name of the Reddit rdt_uuid cookie 
+// The name of the Reddit rdt_uuid cookie
 export const rdtCookieName = '_rdt_uuid'
 
 export const getCookieValue = (cookieName: string): string | null => {
