1acd904b8a07545663d1d698e8123c86635f6b66
Manik Mehta <manik.mehta@reddit.com>
2024-08-22 13:10:37 -0700

    adding reddit plugin tests

--- packages/browser-destinations/destinations/reddit-plugins/src/__tests__/index.test.ts
+++ packages/browser-destinations/destinations/reddit-plugins/src/__tests__/index.test.ts
@@ -1,4 +1,86 @@
-import { Analytics, Context } from '@segment/analytics-next'
-import plugins, { destination } from '../index'
+import { Analytics, Context, Plugin } from '@segment/analytics-next'
+import redditPluginsDestination from '../'
+import {
+  clickIdIntegrationFieldName,
+  clickIdQuerystringName,
+  rdtUUIDIntegrationFieldName,
+  rdtCookieName
+} from '../utils'
 
-describe('Reddit Plugins', () => {})
+const example: Array<Record<string, any>> = [
+  {
+    partnerAction: 'redditPlugin',
+    name: 'Reddit Browser Plugin',
+    enabled: true,
+    subscribe: 'type = "track"',
+    mapping: {}
+  }
+]
+
+let browserActions: Plugin[]
+let redditPlugin: Plugin
+let ajs: Analytics
+
+beforeEach(async () => {
+  browserActions = await redditPluginsDestination({ subscriptions: example })
+  redditPlugin = browserActions[0] // Get the first plugin instance
+
+  ajs = new Analytics({
+    writeKey: 'w_123'
+  })
+
+  Object.defineProperty(window, 'location', {
+    value: {
+      search: ''
+    },
+    writable: true
+  })
+  document.cookie = `${rdtCookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
+})
+
+describe('ajs-integration', () => {
+  test('updates the original event with a Reddit click_id from the querystring', async () => {
+    Object.defineProperty(window, 'location', {
+      value: {
+        search: `?${clickIdQuerystringName}=dummyQuerystringValue`
+      },
+      writable: true
+    })
+
+    await redditPlugin.load(Context.system(), ajs)
+
+    const ctx = new Context({
+      type: 'track',
+      event: 'Test Event',
+      properties: {
+        greeting: 'Hello Reddit!'
+      }
+    })
+
+    const updatedCtx = await redditPlugin.track?.(ctx)
+
+    const redditIntegrationsObj = updatedCtx?.event?.integrations?.['Reddit Conversions Api']
+
+    expect(redditIntegrationsObj?.[clickIdIntegrationFieldName]).toEqual('dummyQuerystringValue')
+  })
+
+  test('updates the original event with a Reddit rdt_uuid cookie value', async () => {
+    document.cookie = `${rdtCookieName}=dummyCookieValue`
+
+    await redditPlugin.load(Context.system(), ajs)
+
+    const ctx = new Context({
+      type: 'track',
+      event: 'Test Event',
+      properties: {
+        greeting: 'Hello Reddit!'
+      }
+    })
+
+    const updatedCtx = await redditPlugin.track?.(ctx)
+
+    const redditIntegrationsObj = updatedCtx?.event?.integrations?.['Reddit Conversions Api']
+
+    expect(redditIntegrationsObj?.[rdtUUIDIntegrationFieldName]).toEqual('dummyCookieValue')
+  })
+})
