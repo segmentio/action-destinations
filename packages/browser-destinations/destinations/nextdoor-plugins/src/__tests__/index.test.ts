d6587afad335a7fff2c8a3a71cb9163608e826c3
Joe Ayoub <joe.ayoub@segment.com>
2024-08-23 15:59:47 +0200

    adding tests

--- packages/browser-destinations/destinations/nextdoor-plugins/src/__tests__/index.test.ts
+++ packages/browser-destinations/destinations/nextdoor-plugins/src/__tests__/index.test.ts
@@ -1,4 +1,62 @@
-import { Analytics, Context } from '@segment/analytics-next'
-import plugins, { destination } from '../index'
+import { Analytics, Context, JSONValue, Plugin } from '@segment/analytics-next'
+import { Subscription } from '@segment/browser-destination-runtime/types'
+import browserPluginsDestination from '../'
+import { clickIdIntegrationFieldName, clickIdQuerystringName } from '../utils'
 
-describe('Nextdoor Plugins', () => {})
+const example: Subscription[] = [
+  {
+    partnerAction: 'nextdoorPlugin',
+    name: 'Nextdoor Browser Plugin',
+    enabled: true,
+    subscribe: 'type = "track"',
+    mapping: {}
+  }
+]
+
+let browserActions: Plugin[]
+let nextdoorPlugin: Plugin
+let ajs: Analytics
+
+beforeEach(async () => {
+  browserActions = await browserPluginsDestination({ subscriptions: example } as unknown as JSONValue)
+  nextdoorPlugin = browserActions[0]
+
+  ajs = new Analytics({
+    writeKey: 'w_123'
+  })
+
+  Object.defineProperty(window, 'location', {
+    value: {
+      search: ''
+    },
+    writable: true
+  })
+})
+
+describe('ajs-integration', () => {
+  test('updates the original event with a Nextdoor click_id from the querystring', async () => {
+    Object.defineProperty(window, 'location', {
+      value: {
+        search: `?${clickIdQuerystringName}=dummyQuerystringValue`
+      },
+      writable: true
+    })
+
+    await nextdoorPlugin.load(Context.system(), ajs)
+
+    const ctx = new Context({
+      type: 'track',
+      event: 'Test Event',
+      properties: {
+        greeting: 'Yo!'
+      }
+    })
+
+    const updatedCtx = (await nextdoorPlugin.track?.(ctx)) as Context
+
+    console.log('updatedCtx', updatedCtx)
+
+    const nextdoorIntegrationsObj = updatedCtx?.event?.integrations['Nextdoor Conversions Api'] as Record<string, any>
+    expect(nextdoorIntegrationsObj[clickIdIntegrationFieldName]).toEqual('dummyQuerystringValue')
+  })
+})
--- packages/destination-actions/src/destinations/nextdoor-capi/__tests__/snapshot.test.ts
+++ packages/destination-actions/src/destinations/nextdoor-capi/__tests__/snapshot.test.ts
@@ -57,7 +57,26 @@ describe(`Testing snapshot for ${destinationSlug} destination:`, () => {
 
       const responses = await testDestination.testAction(actionSlug, {
         event: event,
-        mapping: event.properties,
+        mapping: {
+          ...event.properties,
+          ...{
+            customer: {
+              email: 'test@test.com',
+              phone_number: '1234567890',
+              first_name: 'John',
+              last_name: 'Doe',
+              date_of_birth: '1990-01-01',
+              street_address: '123 Main St',
+              city: 'San Francisco',
+              state: 'CA',
+              country: 'US',
+              zip_code: '94105',
+              pixel_id: 'test_pixel_id',
+              click_id: 'test_click_id',
+              client_ip_address: '111.111.111.111'
+            }
+          }
+        },
         settings: settingsData,
         auth: undefined
       })
