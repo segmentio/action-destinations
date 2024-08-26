78b9655afdda22abdf33947bc2092ec4f3626cc6
Maneesh Dharma <maneesh1996@gmail.com>
2024-08-26 13:15:01 +0530

    add oauth2 for webhook2.0

--- packages/destination-actions/src/destinations/webhook-extensible/index.ts
+++ packages/destination-actions/src/destinations/webhook-extensible/index.ts
@@ -9,7 +9,7 @@ const destination: DestinationDefinition<Settings> = {
   slug: 'actions-webhook-extensible',
   mode: 'cloud',
   authentication: {
-    scheme: 'custom',
+    scheme: 'oauth2',
     fields: {
       sharedSecret: {
         type: 'string',
@@ -17,16 +17,49 @@ const destination: DestinationDefinition<Settings> = {
         description:
           'If set, Segment will sign requests with an HMAC in the "X-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this shared secret and the request body.'
       }
+    },
+    testAuthentication: async (request, { settings }) => {
+      const res = await request<RefreshTokenResponse>(settings.authenticationUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${Buffer.from(settings.key_id + ':' + settings.client_secret).toString('base64')}`
+        },
+        body: '{"grant_type":"client_credentials"}'
+      })
+      if (res.status == 200) {
+        return { accessToken: res.data.access_token }
+      } else {
+        throw new Error(res.status + res.statusText)
+      }
+    },
+    refreshAccessToken: async (request, { settings }) => {
+      // Refresh access token using client_id and client_secret provided in the Settings
+      const res = await request<RefreshTokenResponse>(settings.refreshTokenUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${Buffer.from(settings.key_id + ':' + settings.client_secret).toString('base64')}`
+        },
+        body: '{"grant_type":"client_credentials"}'
+      })
+
+      return { accessToken: res.data.access_token }
     }
   },
-  extendRequest: ({ settings, payload }) => {
+  extendRequest: ({ settings, payload, auth }) => {
     const payloadData = payload.length ? payload[0]['data'] : payload['data']
     if (settings.sharedSecret && payloadData) {
       const digest = createHmac('sha1', settings.sharedSecret).update(JSON.stringify(payloadData), 'utf8').digest('hex')
-      return { headers: { 'X-Signature': digest } }
+      return { 
+        headers: { 
+          'X-Signature': digest,
+          authorization: `Bearer ${auth?.accessToken}`
+        }   
+      }
     }
     return {}
-  },
+},
   actions: {
     send
   }
