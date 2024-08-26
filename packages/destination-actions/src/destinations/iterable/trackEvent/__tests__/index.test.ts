658599f36007a58a8d0ee26d62d458ec6a10e99a
Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-08-26 10:59:48 +0200

    fix for iterable date format bug (#2305)
    * fix for iterable date format bug
    
    * v2 Fix for Iterable date format bug  (#2321)
    
    * No longer update the status code when content-type includes 'application/json' (#2306)
    
    * does not modify dates in yyyy-mm-dd format
    
    ---------
    
    Co-authored-by: rvadera12 <89420099+rvadera12@users.noreply.github.com>
    Co-authored-by: Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
    
    * removing code to make test pass
    
    ---------
    
    Co-authored-by: Jake Kopulsky <jakekopulsky@gmail.com>
    Co-authored-by: rvadera12 <89420099+rvadera12@users.noreply.github.com>

--- packages/destination-actions/src/destinations/iterable/trackEvent/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/iterable/trackEvent/__tests__/index.test.ts
@@ -63,6 +63,56 @@ describe('Iterable.trackEvent', () => {
     })
   })
 
+  it('does not modify a yyyy-mm-dd date', async () => {
+    const event = createTestEvent({
+      type: 'track',
+      userId: 'user1234',
+      properties: {
+        myDate: '2023-05-17'
+      }
+    })
+
+    nock('https://api.iterable.com/api').post('/events/track').reply(200, {})
+
+    const responses = await testDestination.testAction('trackEvent', {
+      event,
+      useDefaultMappings: true
+    })
+
+    expect(responses.length).toBe(1)
+    expect(responses[0].options.json).toMatchObject({
+      userId: 'user1234',
+      dataFields: {
+        myDate: '2023-05-17'
+      }
+    })
+  })
+
+  it('does not alter a midnight UTC datetime', async () => {
+    const event = createTestEvent({
+      type: 'track',
+      userId: 'user1234',
+      properties: {
+        myDate: '2023-05-17T00:00:00.000Z'
+      }
+    })
+
+    nock('https://api.iterable.com/api').post('/events/track').reply(200, {})
+
+    const responses = await testDestination.testAction('trackEvent', {
+      event,
+      useDefaultMappings: true
+    })
+
+    expect(responses.length).toBe(1)
+    expect(responses[0].options.json).toMatchObject({
+      userId: 'user1234',
+      dataFields: {
+        myDate: '2023-05-17 00:00:00 +00:00'
+      }
+    })
+  })
+
   it('does not convert a non date string into a standard Iterable date format', async () => {
     const event = createTestEvent({
       type: 'track',
