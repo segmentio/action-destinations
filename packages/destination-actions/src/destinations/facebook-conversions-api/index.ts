521061a848b9ee8065538d11705efbbf3b0bb7b2
Maryam Sharif <maryam.sharif@segment.com>
2024-08-28 16:14:58 -0700

    Duplicate action

--- packages/destination-actions/src/destinations/facebook-conversions-api/index.ts
+++ packages/destination-actions/src/destinations/facebook-conversions-api/index.ts
@@ -8,6 +8,8 @@ import search from './search'
 import pageView from './pageView'
 import custom from './custom'
 
+import viewContent2 from './viewContent2'
+
 const destination: DestinationDefinition<Settings> = {
   name: 'Facebook Conversions API (Actions)',
   slug: 'actions-facebook-conversions-api',
@@ -44,7 +46,8 @@ const destination: DestinationDefinition<Settings> = {
     viewContent,
     search,
     pageView,
-    custom
+    custom,
+    viewContent2
   }
 }
 
