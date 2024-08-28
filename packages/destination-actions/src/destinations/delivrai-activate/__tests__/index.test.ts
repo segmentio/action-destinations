cc0dc65e7300af345f0e5471dce32b61f0564fe9
Gaurav Kochar <gaurav.kochar@segment.com>
2024-08-28 13:03:23 +0530

    remove unnecesary spacing changes

--- packages/destination-actions/src/destinations/delivrai-activate/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/delivrai-activate/__tests__/index.test.ts
@@ -2,14 +2,15 @@ import { gen_update_segment_payload } from '../utils-rt'
 import { Payload } from '../updateSegment/generated-types'
 
 describe('Delivr AI Audiences', () => {
+  
   describe('gen_update_segment_payload() function', () => {
     describe('Success cases', () => {
       it('trivial', () => {
         // Given
         const payloads: Payload[] = [{} as Payload]
-        const client_identifier_id = 'delivrai'
+        const client_identifier_id = 'delivrai';
         // When
-        const result = gen_update_segment_payload(payloads, client_identifier_id)
+        const result = gen_update_segment_payload(payloads,client_identifier_id)
 
         // Then
         expect(result).toBeTruthy()
@@ -45,7 +46,7 @@ describe('Delivr AI Audiences', () => {
         ]
         const client_identifier_id = 'delivrai'
         // When
-        const result = gen_update_segment_payload(payloads, client_identifier_id)
+        const result = gen_update_segment_payload(payloads,client_identifier_id)
 
         // Then
         expect(result).toBeTruthy()
