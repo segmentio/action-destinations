0dd6e65de63023165ac1ee24b2c2190ed50d2104
Joe Ayoub <joe.ayoub@segment.com>
2024-08-28 11:31:28 +0200

    DC-936 - email and utk fields hide unless object type is contact

--- packages/destination-actions/src/destinations/hubspot/customEvent/constants.ts
+++ packages/destination-actions/src/destinations/hubspot/customEvent/constants.ts
@@ -1,6 +1,19 @@
+import { DependsOnConditions } from '@segment/actions-core/destination-kit/types'
+
 export const SUPPORTED_HUBSPOT_OBJECT_TYPES = [
   { label: 'Contact', value: 'contact' },
   { label: 'Company', value: 'company' },
   { label: 'Deal', value: 'deal' },
   { label: 'Ticket', value: 'ticket' }
 ]
+
+export const DEPENDS_ON_OBJECT_TYPE_CONTACT: DependsOnConditions = {
+  match: 'all',
+  conditions: [
+    {
+      fieldKey: 'object_type',
+      operator: 'is',
+      value: 'contact'
+    }
+  ]
+}
--- packages/destination-actions/src/destinations/hubspot/customEvent/index.ts
+++ packages/destination-actions/src/destinations/hubspot/customEvent/index.ts
@@ -42,20 +42,27 @@ const action: ActionDefinition<Settings, Payload> = {
     }
   },
   perform: async (request, { payload, syncMode }) => {
-    if (!payload.record_details.object_id && !payload.record_details.email && !payload.record_details.utk) {
-      throw new PayloadValidationError('At least one of object_id, email or utk is required')
-    }
-
-    if (payload.record_details.object_id && typeof payload.record_details.object_id !== 'number') {
-      throw new PayloadValidationError('Object ID must be numeric')
-    }
-
     if (payload.record_details.email && payload.record_details.object_type !== 'contact') {
-      throw new PayloadValidationError('Email can only be used to associate an event with a Contacts object')
+      delete payload.record_details.email
     }
 
     if (payload.record_details.utk && payload.record_details.object_type !== 'contact') {
-      throw new PayloadValidationError('User Token can only be used to associate an event with a Contacts object')
+      delete payload.record_details.utk
+    }
+
+    if (payload.record_details.object_type !== 'contact' && typeof payload.record_details.object_id !== 'number') {
+      throw new PayloadValidationError('object_id is required and must be numeric')
+    }
+
+    if (
+      payload.record_details.object_type === 'contact' &&
+      typeof payload.record_details.object_id !== 'number' &&
+      !payload.record_details.email &&
+      !payload.record_details.utk
+    ) {
+      throw new PayloadValidationError(
+        'Contact requires at least one of object_id (as number), email or utk to be provided'
+      )
     }
 
     const hubspotClient = new HubspotClient(request, syncMode as SyncMode)
