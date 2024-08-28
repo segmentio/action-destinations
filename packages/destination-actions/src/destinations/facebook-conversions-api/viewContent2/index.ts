0aa80fc465054f1135240a5f92b57c82129e4676
Maryam Sharif <maryam.sharif@segment.com>
2024-08-28 16:16:58 -0700

    Add syncMode

--- packages/destination-actions/src/destinations/facebook-conversions-api/viewContent2/index.ts
+++ packages/destination-actions/src/destinations/facebook-conversions-api/viewContent2/index.ts
@@ -29,6 +29,12 @@ const action: ActionDefinition<Settings, Payload> = {
   title: 'View Content',
   description: 'Send event when a user views content or a product',
   defaultSubscription: 'type = "track" and event = "Product Viewed"',
+  syncMode: {
+    description: 'Define how the records from your destination will be synced.',
+    label: 'How to sync records',
+    default: 'add',
+    choices: [{ label: 'Insert Records', value: 'add' }]
+  },
   fields: {
     action_source: { ...action_source, required: true },
     event_time: { ...event_time, required: true },
@@ -67,71 +73,75 @@ const action: ActionDefinition<Settings, Payload> = {
     data_processing_options_country: data_processing_options_country,
     data_processing_options_state: data_processing_options_state
   },
-  perform: (request, { payload, settings, features, statsContext }) => {
-    if (payload.currency && !CURRENCY_ISO_CODES.has(payload.currency)) {
-      throw new IntegrationError(
-        `${payload.currency} is not a valid currency code.`,
-        'Misconfigured required field',
-        400
-      )
-    }
-
-    if (!payload.user_data) {
-      throw new IntegrationError('Must include at least one user data property', 'Misconfigured required field', 400)
-    }
-
-    if (payload.action_source === 'website' && payload.user_data.client_user_agent === undefined) {
-      throw new IntegrationError(
-        'If action source is "Website" then client_user_agent must be defined',
-        'Misconfigured required field',
-        400
-      )
-    }
-
-    if (payload.contents) {
-      const err = validateContents(payload.contents)
-      if (err) throw err
-    }
-
-    const [data_options, country_code, state_code] = dataProcessingOptions(
-      payload.data_processing_options,
-      payload.data_processing_options_country,
-      payload.data_processing_options_state
-    )
-
-    return request(
-      `https://graph.facebook.com/v${get_api_version(features, statsContext)}/${settings.pixelId}/events`,
-      {
-        method: 'POST',
-        json: {
-          data: [
-            {
-              event_name: 'ViewContent',
-              event_time: payload.event_time,
-              action_source: payload.action_source,
-              event_id: payload.event_id,
-              event_source_url: payload.event_source_url,
-              user_data: hash_user_data({ user_data: payload.user_data }),
-              custom_data: {
-                ...payload.custom_data,
-                currency: payload.currency,
-                value: payload.value,
-                content_ids: payload.content_ids,
-                content_name: payload.content_name,
-                content_type: payload.content_type,
-                contents: payload.contents,
-                content_category: payload.content_category
-              },
-              app_data: generate_app_data(payload.app_data_field),
-              data_processing_options: data_options,
-              data_processing_options_country: country_code,
-              data_processing_options_state: state_code
-            }
-          ],
-          ...(settings.testEventCode && { test_event_code: settings.testEventCode })
-        }
+  perform: (request, { payload, settings, features, statsContext, syncMode }) => {
+    if (syncMode === 'add') {
+      if (payload.currency && !CURRENCY_ISO_CODES.has(payload.currency)) {
+        throw new IntegrationError(
+          `${payload.currency} is not a valid currency code.`,
+          'Misconfigured required field',
+          400
+        )
       }
-    )
+
+      if (!payload.user_data) {
+        throw new IntegrationError('Must include at least one user data property', 'Misconfigured required field', 400)
+      }
+
+      if (payload.action_source === 'website' && payload.user_data.client_user_agent === undefined) {
+        throw new IntegrationError(
+          'If action source is "Website" then client_user_agent must be defined',
+          'Misconfigured required field',
+          400
+        )
+      }
+
+      if (payload.contents) {
+        const err = validateContents(payload.contents)
+        if (err) throw err
+      }
+
+      const [data_options, country_code, state_code] = dataProcessingOptions(
+        payload.data_processing_options,
+        payload.data_processing_options_country,
+        payload.data_processing_options_state
+      )
+
+      return request(
+        `https://graph.facebook.com/v${get_api_version(features, statsContext)}/${settings.pixelId}/events`,
+        {
+          method: 'POST',
+          json: {
+            data: [
+              {
+                event_name: 'ViewContent',
+                event_time: payload.event_time,
+                action_source: payload.action_source,
+                event_id: payload.event_id,
+                event_source_url: payload.event_source_url,
+                user_data: hash_user_data({ user_data: payload.user_data }),
+                custom_data: {
+                  ...payload.custom_data,
+                  currency: payload.currency,
+                  value: payload.value,
+                  content_ids: payload.content_ids,
+                  content_name: payload.content_name,
+                  content_type: payload.content_type,
+                  contents: payload.contents,
+                  content_category: payload.content_category
+                },
+                app_data: generate_app_data(payload.app_data_field),
+                data_processing_options: data_options,
+                data_processing_options_country: country_code,
+                data_processing_options_state: state_code
+              }
+            ],
+            ...(settings.testEventCode && { test_event_code: settings.testEventCode })
+          }
+        }
+      )
+    } else {
+      throw new IntegrationError(`Sync mode ${syncMode} is not supported`, 'Misconfigured sync mode', 400)
+    }
   }
 }
 
