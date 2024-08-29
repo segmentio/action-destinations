c382fda4d006affa82a044684052a72a6967a521
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2024-08-29 10:32:28 -0700

    Batch tests for CM360.

--- packages/destination-actions/src/destinations/campaign-manager-360/conversionAdjustmentUpload/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/campaign-manager-360/conversionAdjustmentUpload/__tests__/index.test.ts
@@ -1,5 +1,5 @@
 import nock from 'nock'
-import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import { createTestEvent, createTestIntegration, SegmentEvent } from '@segment/actions-core'
 import Destination from '../../index'
 
 const testDestination = createTestIntegration(Destination)
@@ -9,66 +9,311 @@ const floodlightActivityId = '23456'
 const floodlightConfigurationId = '34567'
 
 describe('CampaignManager360.conversionAdjustmentUpload', () => {
-  it('sends an event with default mappings + default settings', async () => {
-    const event = createTestEvent({
-      timestamp,
-      event: 'Test Event',
-      properties: {
-        ordinal: '1',
-        quantity: '2',
-        value: '100',
-        gclid: '54321',
-        userDetails: {
-          email: 'daffy@warnerbros.com',
-          phone: '1234567890',
-          firstName: 'Daffy',
-          lastName: 'Duck',
-          streetAddress: '123 Daffy St',
-          city: 'Burbank',
-          state: 'CA',
-          postalCode: '98765',
-          countryCode: 'US'
-        }
-      }
+  describe('Successful scenarios', () => {
+    describe('Single event', () => {
+      it('sends an event with default mappings + default settings, plain data', async () => {
+        const event = createTestEvent({
+          timestamp,
+          event: 'Test Event',
+          properties: {
+            ordinal: '1',
+            quantity: '2',
+            value: '100',
+            gclid: '54321',
+            userDetails: {
+              email: 'daffy@warnerbros.com',
+              phone: '1234567890',
+              firstName: 'Daffy',
+              lastName: 'Duck',
+              streetAddress: '123 Daffy St',
+              city: 'Burbank',
+              state: 'CA',
+              postalCode: '98765',
+              countryCode: 'US'
+            }
+          }
+        })
+
+        nock(`https://www.googleapis.com/oauth2/v4/token`).post('').reply(200, {
+          access_token: 'my.access.token'
+        })
+
+        nock(`https://dfareporting.googleapis.com/dfareporting/v4/userprofiles/${profileId}/conversions/batchupdate`)
+          .post('')
+          .reply(200, { results: [{}] })
+
+        const responses = await testDestination.testAction('conversionAdjustmentUpload', {
+          event,
+          mapping: {
+            gclid: {
+              '@path': '$.properties.gclid'
+            },
+            timestamp: {
+              '@path': '$.timestamp'
+            },
+            value: {
+              '@path': '$.properties.value'
+            },
+            quantity: {
+              '@path': '$.properties.quantity'
+            },
+            ordinal: {
+              '@path': '$.properties.ordinal'
+            }
+          },
+          useDefaultMappings: true,
+          settings: {
+            profileId,
+            defaultFloodlightActivityId: floodlightActivityId,
+            defaultFloodlightConfigurationId: floodlightConfigurationId
+          }
+        })
+
+        expect(responses.length).toBe(2)
+        expect(responses[0].status).toBe(200)
+        expect(responses[1].status).toBe(200)
+      })
+
+      it('sends an event with default mappings + default settings, hashed data', async () => {
+        const event = createTestEvent({
+          timestamp,
+          event: 'Test Event',
+          properties: {
+            ordinal: '1',
+            quantity: '2',
+            value: '100',
+            gclid: '54321',
+            userDetails: {
+              email: '8e46bd4eaabb5d6324e327751b599f190dbaacd90066e66c94a046640bed60d0',
+              phone: 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646',
+              firstName: 'a628aa64f14c8196c8c82c7ffb6482b2db7431e4cb5b28cd313004ce7ba4eb66',
+              lastName: '3b67f1c91f4f245f6e219b364782ac53e912420f2284bf6a700e9cf71fbeafac',
+              streetAddress: '75d8b0ba2a20d1855cf768d65016a066b856b442fec6b33264b11016ba88efc3',
+              city: 'Burbank',
+              state: 'CA',
+              postalCode: '98765',
+              countryCode: 'US'
+            }
+          }
+        })
+
+        nock(`https://www.googleapis.com/oauth2/v4/token`).post('').reply(200, {
+          access_token: 'my.access.token'
+        })
+
+        nock(`https://dfareporting.googleapis.com/dfareporting/v4/userprofiles/${profileId}/conversions/batchupdate`)
+          .post('')
+          .reply(200, { results: [{}] })
+
+        const responses = await testDestination.testAction('conversionAdjustmentUpload', {
+          event,
+          mapping: {
+            gclid: {
+              '@path': '$.properties.gclid'
+            },
+            timestamp: {
+              '@path': '$.timestamp'
+            },
+            value: {
+              '@path': '$.properties.value'
+            },
+            quantity: {
+              '@path': '$.properties.quantity'
+            },
+            ordinal: {
+              '@path': '$.properties.ordinal'
+            }
+          },
+          useDefaultMappings: true,
+          settings: {
+            profileId,
+            defaultFloodlightActivityId: floodlightActivityId,
+            defaultFloodlightConfigurationId: floodlightConfigurationId
+          }
+        })
+
+        expect(responses.length).toBe(2)
+        expect(responses[0].status).toBe(200)
+        expect(responses[1].status).toBe(200)
+      })
     })
 
-    nock(`https://www.googleapis.com/oauth2/v4/token`).post('').reply(200, {
-      access_token: 'my.access.token'
+    describe('Batch', () => {
+      it('sends a batch of events with default mappings + default settings, plain data', async () => {
+        const goodBatch: SegmentEvent[] = [
+          {
+            type: 'track',
+            event: 'Test Event',
+            timestamp,
+            properties: {
+              ordinal: '1',
+              quantity: '1',
+              value: '123',
+              gclid: '54321',
+              userDetails: {
+                email: 'daffy@warnerbros.com',
+                phone: '1234567890',
+                firstName: 'Daffy',
+                lastName: 'Duck',
+                streetAddress: '123 Daffy St',
+                city: 'Burbank',
+                state: 'CA',
+                postalCode: '98765',
+                countryCode: 'US'
+              }
+            }
+          },
+          {
+            type: 'track',
+            event: 'Test Event',
+            timestamp,
+            properties: {
+              ordinal: '1',
+              quantity: '1',
+              value: '234',
+              gclid: '54322',
+              userDetails: {
+                email: 'bugs@warnerbros.com',
+                phone: '1234567891',
+                firstName: 'Bugs',
+                lastName: 'Bunny',
+                streetAddress: '123 Doc St',
+                city: 'Burbank',
+                state: 'CA',
+                postalCode: '98765',
+                countryCode: 'US'
+              }
+            }
+          }
+        ]
+
+        nock(`https://www.googleapis.com/oauth2/v4/token`).post('').reply(200, {
+          access_token: 'my.access.token'
+        })
+
+        nock(`https://dfareporting.googleapis.com/dfareporting/v4/userprofiles/${profileId}/conversions/batchupdate`)
+          .post('')
+          .reply(201, { results: [{}] })
+
+        const responses = await testDestination.testBatchAction('conversionAdjustmentUpload', {
+          events: goodBatch,
+          mapping: {
+            gclid: {
+              '@path': '$.properties.gclid'
+            },
+            timestamp: {
+              '@path': '$.timestamp'
+            },
+            value: {
+              '@path': '$.properties.value'
+            },
+            quantity: {
+              '@path': '$.properties.quantity'
+            },
+            ordinal: {
+              '@path': '$.properties.ordinal'
+            }
+          },
+          useDefaultMappings: true,
+          settings: {
+            profileId,
+            defaultFloodlightActivityId: floodlightActivityId,
+            defaultFloodlightConfigurationId: floodlightConfigurationId
+          }
+        })
+
+        expect(responses.length).toBe(2)
+        expect(responses[0].status).toBe(200)
+        expect(responses[1].status).toBe(201)
+      })
+
+      it('sends a batch of events with default mappings + default settings, hashed data', async () => {
+        const goodBatch: SegmentEvent[] = [
+          {
+            type: 'track',
+            event: 'Test Event',
+            timestamp,
+            properties: {
+              ordinal: '1',
+              quantity: '1',
+              value: '123',
+              gclid: '54321',
+              userDetails: {
+                email: '8e46bd4eaabb5d6324e327751b599f190dbaacd90066e66c94a046640bed60d0',
+                phone: 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646',
+                firstName: 'a628aa64f14c8196c8c82c7ffb6482b2db7431e4cb5b28cd313004ce7ba4eb66',
+                lastName: '3b67f1c91f4f245f6e219b364782ac53e912420f2284bf6a700e9cf71fbeafac',
+                streetAddress: '75d8b0ba2a20d1855cf768d65016a066b856b442fec6b33264b11016ba88efc3',
+                city: 'Burbank',
+                state: 'CA',
+                postalCode: '98765',
+                countryCode: 'US'
+              }
+            }
+          },
+          {
+            type: 'track',
+            event: 'Test Event',
+            timestamp,
+            properties: {
+              ordinal: '1',
+              quantity: '1',
+              value: '234',
+              gclid: '54322',
+              userDetails: {
+                email: 'ce4c7b4a4b35b8b4c8bfb38d4e881d8b3f563939f4e6c5e873b556c9313980af',
+                phone: '523aa18ecb892c51fbdbe28c57e10a92419e0a73e8931e578b98baffccf99cdd',
+                firstName: 'ff7c5467ce496637e5ba10662b7a90cde4ed9f8ef33f06fab0893b1c6c800845',
+                lastName: '03dac94651a68d28f655022e51379654976ad275481fa006ffd0eff9e9e2b08a',
+                streetAddress: '4ed79adb368d0fe12221a8587761006ffdc58ae87a4d78af999792cf56882dc9',
+                city: 'Burbank',
+                state: 'CA',
+                postalCode: '98765',
+                countryCode: 'US'
+              }
+            }
+          }
+        ]
+
+        nock(`https://www.googleapis.com/oauth2/v4/token`).post('').reply(200, {
+          access_token: 'my.access.token'
+        })
+
+        nock(`https://dfareporting.googleapis.com/dfareporting/v4/userprofiles/${profileId}/conversions/batchupdate`)
+          .post('')
+          .reply(201, { results: [{}] })
+
+        const responses = await testDestination.testBatchAction('conversionAdjustmentUpload', {
+          events: goodBatch,
+          mapping: {
+            gclid: {
+              '@path': '$.properties.gclid'
+            },
+            timestamp: {
+              '@path': '$.timestamp'
+            },
+            value: {
+              '@path': '$.properties.value'
+            },
+            quantity: {
+              '@path': '$.properties.quantity'
+            },
+            ordinal: {
+              '@path': '$.properties.ordinal'
+            }
+          },
+          useDefaultMappings: true,
+          settings: {
+            profileId,
+            defaultFloodlightActivityId: floodlightActivityId,
+            defaultFloodlightConfigurationId: floodlightConfigurationId
+          }
+        })
+
+        expect(responses.length).toBe(2)
+        expect(responses[0].status).toBe(200)
+        expect(responses[1].status).toBe(201)
+      })
     })
-
-    nock(`https://dfareporting.googleapis.com/dfareporting/v4/userprofiles/${profileId}/conversions/batchupdate`)
-      .post('')
-      .reply(200, { results: [{}] })
-
-    const responses = await testDestination.testAction('conversionAdjustmentUpload', {
-      event,
-      mapping: {
-        gclid: {
-          '@path': '$.properties.gclid'
-        },
-        timestamp: {
-          '@path': '$.timestamp'
-        },
-        value: {
-          '@path': '$.properties.value'
-        },
-        quantity: {
-          '@path': '$.properties.quantity'
-        },
-        ordinal: {
-          '@path': '$.properties.ordinal'
-        }
-      },
-      useDefaultMappings: true,
-      settings: {
-        profileId,
-        defaultFloodlightActivityId: floodlightActivityId,
-        defaultFloodlightConfigurationId: floodlightConfigurationId
-      }
-    })
-
-    expect(responses.length).toBe(2)
-    expect(responses[0].status).toBe(200)
-    expect(responses[1].status).toBe(200)
   })
 })
