2f6ee8241d072909134a0d2bf5cfa7ae14bef2f8
Philipp Munin <7120440+pmunin@users.noreply.github.com>
2024-09-01 16:26:45 -0400

    moving core caching logic to MessageSendPerformer => EngageActionPerformer

--- packages/actions-shared/src/engage/utils/MessageSendPerformer.ts
+++ packages/actions-shared/src/engage/utils/MessageSendPerformer.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { EngageActionPerformer } from './EngageActionPerformer'
+import { CacheSerializer, EngageActionPerformer } from './EngageActionPerformer'
 import { isRetryableError } from './isRetryableError'
 import { AggregateError } from './AggregateError'
 import { MaybePromise } from '@segment/actions-core/destination-kit/types'
@@ -9,7 +9,6 @@ import { getProfileApiEndpoint, Region } from './getProfileApiEndpoint'
 import { track } from './track'
 import { IntegrationError, PayloadValidationError } from '@segment/actions-core'
 import { getErrorDetails } from './ResponseError'
-import { StatsTagsMap } from './operationTracking'
 import { CachedError, CachedResponseType, CachedValue, CachedValueFactory } from './CachedResponse'
 
 export enum SendabilityStatus {
@@ -125,174 +124,6 @@ export abstract class MessageSendPerformer<
         serializer: SendToRecepientResponseSerializer
       }
     )
-  }
-
-  @track()
-  async getOrAddCache<T>(
-    key: string,
-    createValue: () => Promise<T>,
-    options: {
-      /**
-       * The group of cache used for stats tags
-       */
-      cacheGroup?: string
-      serializer?: CacheSerializer<T>
-      expiryInSeconds?: number
-      lockOptions?: Parameters<typeof MessageSendPerformer.prototype.withDistributedLock>[2]
-    }
-  ): Promise<T> {
-    const cache_group = options.cacheGroup || this.currentOperation?.parent?.func.name || ''
-    const finalStatsTags: StatsTagsMap & { cache_hit: boolean } = {
-      cache_group,
-      cache_hit: false
-    }
-
-    this.currentOperation?.onFinally.push(() => {
-      this.currentOperation?.tags.push(...this.statsClient.tagsMapToArray(finalStatsTags))
-    })
-
-    if (!this.engageDestinationCache) return createValue()
-
-    const serializer = options.serializer || DefaultSerializer
-
-    const cacheRead = await getOrCatch(() => this.engageDestinationCache!.getByKey(key))
-
-    // we respect lockOptions only if cache is not found and no error happened while reading cache
-    if (options.lockOptions && !(cacheRead.error || cacheRead.value)) {
-      if (!options.lockOptions.cacheGroup) options.lockOptions.cacheGroup = cache_group
-      return this.withDistributedLock(
-        `cache:${key}`,
-        () => this.getOrAddCache(key, createValue, { ...options, lockOptions: undefined }),
-        options.lockOptions
-      )
-    }
-
-    if (cacheRead.error) {
-      finalStatsTags.cache_reading_error = true
-      this.logInfo('cache_reading_error', { key, cacheGroup: cache_group })
-    } else if (cacheRead.value) {
-      const { value: parsedCache, error: parsingError } = getOrCatch(() => serializer.parse(cacheRead.value!))
-
-      if (parsingError) {
-        //exception happened while parsing the cache.
-        // Log it and execute as if we don't have cache
-        finalStatsTags.cache_parsing_error = true
-        this.logInfo('cache_parsing_error', { key, value: cacheRead.value, parsingError })
-      } else if (parsedCache) {
-        //parsed cache successfully && cache is not expired
-        // parsedValue - either value or error was parsed
-        finalStatsTags.cache_hit = true
-        finalStatsTags.cached_error = !!parsedCache?.error
-        this.statsIncr('cache_hit', 1, finalStatsTags)
-        if (parsedCache?.error) throw parsedCache.error
-        return parsedCache.value
-      } else {
-        //cache parsed successfully but cache needs to be ignored (e.g. expired) - re-execute
-        finalStatsTags.cache_ignored = true
-        this.logInfo('cache_ignored', { key, value: cacheRead.value, cacheGroup: cache_group })
-      }
-    }
-    // re-executing, because cache not found or ignored or failed to read or parse
-    finalStatsTags.cache_hit = false
-    this.statsIncr('cache_miss', 1, finalStatsTags)
-    this.logInfo('cache_miss', { key, cacheGroup: cache_group })
-    const { value: result, error: resultError } = await getOrCatch(() => createValue())
-
-    //before returning result - we need to try to serialize it and store it in cache
-    const stringified = getOrCatch(() => serializer.stringify(resultError ? { error: resultError } : { value: result }))
-    if (stringified.error) {
-      finalStatsTags.cache_stringify_error = true
-      this.logInfo('cache_stringify_error', { key, error: stringified.error, cacheGroup: cache_group })
-    } else if (stringified.value) {
-      //result stringified and contains cacheable value - cache it
-      const { error: cacheSavingError } = await getOrCatch(() =>
-        this.engageDestinationCache!.setByKey(key, stringified.value!, options.expiryInSeconds)
-      )
-      if (cacheSavingError) {
-        finalStatsTags.cache_saving_error = true
-        this.logInfo('cache_saving_error', { key, error: cacheSavingError, cacheGroup: cache_group })
-      }
-    }
-
-    if (resultError) throw resultError
-    else return result as T
-  }
-
-  @track()
-  async withDistributedLock<T>(
-    key: string,
-    createValue: () => Promise<T>,
-    options: {
-      acquireLockMaxWaitTimeMs: number
-      acquireLockRetryIntervalMs?: number
-      lockMaxTimeMs: number
-      cacheGroup?: string
-    }
-  ): Promise<T> {
-    const redisClient = (this.engageDestinationCache as any)?.redis
-    const cache_group = options.cacheGroup || this.currentOperation?.parent?.func.name || ''
-    const statsTags: StatsTagsMap = { cache_group }
-    this.currentOperation?.onFinally.push(() => {
-      this.currentOperation?.tags.push(...this.statsClient.tagsMapToArray(statsTags))
-    })
-
-    if (!redisClient) {
-      return await createValue()
-    }
-
-    /**
-     * LOGIC:
-     * Trying to aquire lock. Possible outcomes:
-     * 1. Lock acquired:
-     *    - createValue() and finally release lock
-     * 2. Lock not acquired because of timeout:
-     *   - throw timeout error up
-     * 3. Lock not acquired because of error accessing redis:
-     *   - fallback to createValue()
-     */
-
-    const lockKey = `engage-messaging-lock:${key}`
-    const acquireLock = async () => {
-      //tries to acquire lock for acquireLockMaxWaitInSeconds seconds
-      statsTags.lock_acquired = false
-      const startTime = Date.now()
-      while (Date.now() - startTime < options.acquireLockMaxWaitTimeMs) {
-        if ((await redisClient.set(lockKey, 'locked', 'NX', 'PX', options.lockMaxTimeMs)) === 'OK') {
-          // lock acquired, returning release function
-          statsTags.lock_acquired = true
-          return {
-            release: async () => await redisClient.del(lockKey)
-          }
-        }
-        await new Promise((resolve) => setTimeout(resolve, options.acquireLockRetryIntervalMs || 500)) // Wait 500ms before retrying
-      }
-      //no lock acquired because of waiting timeout
-      return
-    }
-
-    const { value: lock, error: redisError } = await getOrCatch(() => acquireLock())
-
-    if (redisError || lock?.release)
-      //if lock obtained or there was redis error - execute createValue and finally release lock
-      try {
-        if (redisError) {
-          this.logInfo('lock_acquire_error', { key, redisError, statsTags })
-          statsTags.lock_acquired_error = true
-        }
-        return await createValue()
-      } finally {
-        const { error: releaseError } = lock?.release ? await getOrCatch(() => lock.release()) : { error: undefined }
-        if (releaseError) {
-          this.logInfo('lock_release_error', { key, releaseError, statsTags })
-          statsTags.lock_release_error = true
-        }
-      }
-    else {
-      // no redis error and no lock acquired - means it was acquiring timeout
-      const acquireLockTimeoutError = new IntegrationError('Timeout while acquiring lock', 'ETIMEDOUT', 500)
-      acquireLockTimeoutError.retry = true
-      throw acquireLockTimeoutError
-    }
   }
 
   async doPerform() {
@@ -608,39 +439,6 @@ export abstract class MessageSendPerformer<
   }
 }
 
-type ValueOrError<T> = { value?: T; error?: any } //& ({ value: T } | { error: any });
-
-function getOrCatch<T>(getValue: () => Promise<T>): Promise<ValueOrError<T>>
-function getOrCatch<T>(getValue: () => T): ValueOrError<T>
-function getOrCatch(getValue: () => any): Promise<ValueOrError<any>> | ValueOrError<any> {
-  try {
-    const value = getValue()
-
-    if (value instanceof Promise) {
-      return value.then((value) => ({ value })).catch((error) => ({ error }))
-    } else {
-      return { value }
-    }
-  } catch (error) {
-    return { error }
-  }
-}
-
-type CacheSerializer<T> = {
-  /**
-   * Stringyfies value or error to string
-   * @param cacheable value or error to be stringified
-   * @returns if undefined returned, then the value will not be cached
-   */
-  stringify: (cacheable: ValueOrError<T>) => string | void
-  /**
-   * parses cached string to value or error
-   * @param cachedValue
-   * @returns if undefined returned, then the cache is either corrupted or expired and will be re-executed
-   */
-  parse: (cachedValue: string) => ValueOrError<T> | void
-}
-
 /**
  * Serializer optimized for caching sendToRecepient response.
  * As success it only serializes status, as error it serializes status, message and code.
@@ -666,36 +464,6 @@ export const SendToRecepientResponseSerializer: CacheSerializer<any> = {
       return { error }
     } else if (parsed.type === CachedResponseType.Success) {
       return { value: { status: parsed.status } }
-    }
-  }
-}
-
-/**
- * Default serializer that supports caching of non-retriable error as well as any JSON.stringify-able value
- */
-export const DefaultSerializer: CacheSerializer<any> = {
-  stringify: (valueOrError) => {
-    let cacheObj = undefined
-    if (valueOrError.error && !isRetryableError(valueOrError.error)) {
-      const errorDetails = getErrorDetails(valueOrError.error)
-      if (errorDetails?.status) {
-        cacheObj = { error: new CachedError(errorDetails.status, errorDetails.message, errorDetails.code).serialize() }
-      }
-    } else if (valueOrError.value !== undefined) {
-      cacheObj = { value: valueOrError.value }
-    }
-    return cacheObj ? JSON.stringify(cacheObj) : undefined
-  },
-
-  parse: (cachedValue) => {
-    const parsedValOrError = JSON.parse(cachedValue) as { value?: any; error?: string }
-    if (parsedValOrError.error) {
-      const parsed = CachedValueFactory.fromString(parsedValOrError.error) as CachedError
-      const error = new IntegrationError(parsed.message, parsed.code, parsed.status)
-      error.retry = false
-      return { error }
-    } else if (parsedValOrError.value !== undefined) {
-      return { value: parsedValOrError.value }
     }
   }
 }
