d09ffb292cea3887be67113d4e1a258f7da362db
Gaurav Kochar <gaurav.kochar@segment.com>
2024-09-03 16:12:32 +0530

    Added namespace

--- packages/destination-actions/src/destinations/yahoo-audiences/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/yahoo-audiences/__tests__/index.test.ts
@@ -21,20 +21,20 @@ const createAudienceInput = {
   audienceSettings: {},
   personas: {
     computation_key: AUDIENCE_KEY,
-    computation_id: AUDIENCE_ID
+    computation_id: AUDIENCE_ID,
+    namespace: 'spa_12124214124'
   }
 }
+const testDestination = createTestIntegration(Destination)
 
 describe('Yahoo Audiences', () => {
   describe('createAudience() function', () => {
-    let testDestination: any
     const OLD_ENV = process.env
     beforeEach(() => {
       jest.resetModules() // Most important - it clears the cache
       process.env = { ...OLD_ENV } // Make a copy
       process.env.ACTIONS_YAHOO_AUDIENCES_TAXONOMY_CLIENT_SECRET = 'yoda'
       process.env.ACTIONS_YAHOO_AUDIENCES_TAXONOMY_CLIENT_ID = 'luke'
-      testDestination = createTestIntegration(Destination)
     })
 
     afterAll(() => {
