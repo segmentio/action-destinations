b56229e6cb572d1dcd02fceae7c8b3f085ac35fc
Maryam Sharif <maryam.sharif@segment.com>
2024-09-04 16:42:09 -0700

    Add V2

--- packages/destination-actions/src/destinations/facebook-conversions-api/pageView2/index.ts
+++ packages/destination-actions/src/destinations/facebook-conversions-api/pageView2/index.ts
@@ -17,7 +17,7 @@ import { get_api_version } from '../utils'
 import { generate_app_data, app_data_field } from '../fb-capi-app-data'
 
 const action: ActionDefinition<Settings, Payload> = {
-  title: 'Page View',
+  title: 'Page View V2',
   description: 'Send a page view event when a user lands on a page',
   defaultSubscription: 'type = "page"',
   syncMode: {
