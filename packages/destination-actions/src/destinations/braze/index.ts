0e30b66706175b0ffcabeea0eea7f65d9749303c
Nick Aguilar <nicholas.aguilar@segment.com>
2024-09-06 09:32:35 -0700

    Squashed commit of the following:
    commit e636db19290ccac7b4ed7297ab483f31a492925e
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Fri Sep 6 09:29:54 2024 -0700
    
        Consolidates v2 action definitions into the same file as v1 definitions
    
    commit 4e0b114bd4a2e966009d82d3d5e7430651bb97d9
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Thu Sep 5 16:21:27 2024 -0700
    
        Rewords error message
    
    commit c93da25fe8fc7595d48e99f6573ccb2472159b99
    Author: Nick Aguilar <nicholas.aguilar@segment.com>
    Date:   Wed Sep 4 14:44:58 2024 -0700
    
        Scaffolds new createAlias2 and identifyUser2 actions

--- packages/destination-actions/src/destinations/braze/index.ts
+++ packages/destination-actions/src/destinations/braze/index.ts
@@ -1,8 +1,8 @@
 import type { DestinationDefinition } from '@segment/actions-core'
 import type { Settings } from './generated-types'
 import { defaultValues } from '@segment/actions-core'
-import createAlias from './createAlias'
-import identifyUser from './identifyUser'
+import { createAlias, createAliasV2 } from './createAlias'
+import { identifyUser, identifyUserV2 } from './identifyUser'
 import trackEvent from './trackEvent'
 import trackPurchase from './trackPurchase'
 import updateUserProfile from './updateUserProfile'
@@ -71,7 +71,9 @@ const destination: DestinationDefinition<Settings> = {
     trackEvent,
     trackPurchase,
     createAlias,
-    identifyUser
+    identifyUser,
+    createAliasV2,
+    identifyUserV2
   },
   presets: [
     {
