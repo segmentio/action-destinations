af0fb1aba902092bd8b17fa065a993061735a7fd
ByteDance <jaehyuk.rhee@tiktok.com>
2024-09-06 12:17:19 -0400

    add "autoPageView" setting & Pageview event preset

--- packages/browser-destinations/destinations/tiktok-pixel/src/index.ts
+++ packages/browser-destinations/destinations/tiktok-pixel/src/index.ts
@@ -66,6 +66,15 @@ export const destination: BrowserDestinationDefinition<Settings, TikTokPixel> = 
   mode: 'device',
   presets: [
     {
+      name: 'Page View',
+      subscribe: 'type = "page"',
+      partnerAction: 'reportWebEvent',
+      mapping: {
+        event: 'Pageview'
+      },
+      type: 'automatic'
+    },
+    {
       name: 'Complete Payment',
       subscribe: 'event = "Order Completed"',
       partnerAction: 'reportWebEvent',
@@ -219,6 +228,12 @@ export const destination: BrowserDestinationDefinition<Settings, TikTokPixel> = 
       type: 'boolean',
       description:
         'In order to help facilitate advertiser\'s compliance with the right to opt-out of sale and sharing of personal data under certain U.S. state privacy laws, TikTok offers a Limited Data Use ("LDU") feature. For more information, please refer to TikTok\'s [documentation page](https://business-api.tiktok.com/portal/docs?id=1770092377990145).'
+    },
+    autoPageView: {
+      label: 'Auto Page View Event Trigger',
+      type: 'boolean',
+      description: 'Configure TikTok Pixel to fire "Pageview" event whenevent the pixel is loaded on the page.',
+      default: true
     },
     useExistingPixel: {
       // TODO: HOW TO DELETE (reusing will not include Segment Partner name)
