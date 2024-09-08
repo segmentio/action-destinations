dfee853ca1e1ab4d1368e714267bdc332b138d0e
Maneesh Dharma <maneesh1996@gmail.com>
2024-09-09 04:52:47 +0530

    update extensible webhook

--- packages/destination-actions/src/destinations/webhook-extensible/generated-types.ts
+++ packages/destination-actions/src/destinations/webhook-extensible/generated-types.ts
@@ -5,20 +5,4 @@ export interface Settings {
    * If set, Segment will sign requests with an HMAC in the "X-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this shared secret and the request body.
    */
   sharedSecret?: string
-  /**
-   * Your client ID.
-   */
-  client_id: string
-  /**
-   * Your client secret.
-   */
-  client_secret: string
-  /**
-   * The URL to authenticate the client.
-   */
-  authenticationUrl: string
-  /**
-   * The URL to refresh the access token.
-   */
-  refreshTokenUrl: string
 }
