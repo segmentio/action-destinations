38a667d1695db7b6386a63c1f77863f7b34a1062
maryamsharif <99763167+maryamsharif@users.noreply.github.com>
2024-09-09 03:58:06 -0700

    [FACEBOOK] Add Sync Mode Support to "Add to Cart" Action (#2345)
    * Duplicate action
    
    * Add syncMode
    
    * Add test cases
    
    * Add invalid test case
    
    * Add V2 to name
    
    * generate types
    
    ---------
    
    Co-authored-by: Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>

--- packages/destination-actions/src/destinations/facebook-conversions-api/index.ts
+++ packages/destination-actions/src/destinations/facebook-conversions-api/index.ts
@@ -7,6 +7,7 @@ import viewContent from './viewContent'
 import search from './search'
 import pageView from './pageView'
 import custom from './custom'
+import addToCart2 from './addToCart2'
 import pageView2 from './pageView2'
 import purchase2 from './purchase2'
 import custom2 from './custom2'
@@ -48,6 +49,7 @@ const destination: DestinationDefinition<Settings> = {
     search,
     pageView,
     custom,
+    addToCart2,
     pageView2,
     purchase2,
     custom2
