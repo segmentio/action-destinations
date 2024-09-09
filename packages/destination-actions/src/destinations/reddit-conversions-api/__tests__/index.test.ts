04001249af04aa4b1cc861a2f04ebe224fa1c31d
Joe Ayoub <joe.ayoub@segment.com>
2024-09-09 12:56:59 +0100

    fixing breaking reddit tests

--- packages/destination-actions/src/destinations/reddit-conversions-api/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/reddit-conversions-api/__tests__/index.test.ts
@@ -51,49 +51,48 @@ describe('Reddit Conversions Api', () => {
           custom_event_name: 'Some Custom Event Name'
         }
       })
+      console.log(responses[0].options.json)
 
       expect(responses.length).toBe(1)
       expect(responses[0].status).toBe(200)
       expect(responses[0].options.json).toMatchObject({
-        data: {
-          events: [
-            {
-              click_id: 'click_id_1',
-              event_at: '2024-01-08T13:52:50.212Z',
-              event_metadata: {
-                conversion_id: 'conversion_id_1',
-                currency: 'USD',
-                item_count: 10,
-                products: [
-                  {
-                    category: 'category_1',
-                    id: 'product_id_1',
-                    name: 'name_1'
-                  },
-                  {
-                    category: 'category_2',
-                    id: 'product_id_2',
-                    name: 'name_2'
-                  }
-                ],
-                value_decimal: 100
-              },
-              event_type: {
-                custom_event_name: 'Some Custom Event Name',
-                tracking_type: 'Custom'
-              },
-              user: {
-                email: 'f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a',
-                external_id: '3482ae91c8ec52c06e19d618d400b3985814bf705e00947a302ec849a6575c4c',
-                ip_address: '5feaf188de296cd3b17f7c66fd3a2aec9b694815f2b1180631f7b52f57029777',
-                user_agent: 'test-user-agent',
-                uuid: 'uuid_1'
-              }
+        events: [
+          {
+            click_id: 'click_id_1',
+            event_at: '2024-01-08T13:52:50.212Z',
+            event_metadata: {
+              conversion_id: 'conversion_id_1',
+              currency: 'USD',
+              item_count: 10,
+              products: [
+                {
+                  category: 'category_1',
+                  id: 'product_id_1',
+                  name: 'name_1'
+                },
+                {
+                  category: 'category_2',
+                  id: 'product_id_2',
+                  name: 'name_2'
+                }
+              ],
+              value_decimal: 100
+            },
+            event_type: {
+              custom_event_name: 'Some Custom Event Name',
+              tracking_type: 'Custom'
+            },
+            user: {
+              email: 'f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a',
+              external_id: '3482ae91c8ec52c06e19d618d400b3985814bf705e00947a302ec849a6575c4c',
+              ip_address: '5feaf188de296cd3b17f7c66fd3a2aec9b694815f2b1180631f7b52f57029777',
+              user_agent: 'test-user-agent',
+              uuid: 'uuid_1'
             }
-          ],
-          partner: 'SEGMENT',
-          test_mode: false
-        }
+          }
+        ],
+        partner: 'SEGMENT',
+        test_mode: false
       })
     })
   })
@@ -141,44 +140,42 @@ describe('Reddit Conversions Api', () => {
       expect(responses.length).toBe(1)
       expect(responses[0].status).toBe(200)
       expect(responses[0].options.json).toMatchObject({
-        data: {
-          events: [
-            {
-              click_id: 'click_id_1',
-              event_at: '2024-01-08T13:52:50.212Z',
-              event_metadata: {
-                conversion_id: 'conversion_id_1',
-                currency: 'USD',
-                item_count: 10,
-                products: [
-                  {
-                    category: 'category_1',
-                    id: 'product_id_1',
-                    name: 'name_1'
-                  },
-                  {
-                    category: 'category_2',
-                    id: 'product_id_2',
-                    name: 'name_2'
-                  }
-                ],
-                value_decimal: 100
-              },
-              event_type: {
-                tracking_type: 'Purchase'
-              },
-              user: {
-                email: 'f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a',
-                external_id: '3482ae91c8ec52c06e19d618d400b3985814bf705e00947a302ec849a6575c4c',
-                ip_address: '5feaf188de296cd3b17f7c66fd3a2aec9b694815f2b1180631f7b52f57029777',
-                user_agent: 'test-user-agent',
-                uuid: 'uuid_1'
-              }
+        events: [
+          {
+            click_id: 'click_id_1',
+            event_at: '2024-01-08T13:52:50.212Z',
+            event_metadata: {
+              conversion_id: 'conversion_id_1',
+              currency: 'USD',
+              item_count: 10,
+              products: [
+                {
+                  category: 'category_1',
+                  id: 'product_id_1',
+                  name: 'name_1'
+                },
+                {
+                  category: 'category_2',
+                  id: 'product_id_2',
+                  name: 'name_2'
+                }
+              ],
+              value_decimal: 100
+            },
+            event_type: {
+              tracking_type: 'Purchase'
+            },
+            user: {
+              email: 'f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a',
+              external_id: '3482ae91c8ec52c06e19d618d400b3985814bf705e00947a302ec849a6575c4c',
+              ip_address: '5feaf188de296cd3b17f7c66fd3a2aec9b694815f2b1180631f7b52f57029777',
+              user_agent: 'test-user-agent',
+              uuid: 'uuid_1'
             }
-          ],
-          partner: 'SEGMENT',
-          test_mode: false
-        }
+          }
+        ],
+        partner: 'SEGMENT',
+        test_mode: false
       })
     })
 
@@ -223,43 +220,41 @@ describe('Reddit Conversions Api', () => {
       expect(responses.length).toBe(1)
       expect(responses[0].status).toBe(200)
       expect(responses[0].options.json).toMatchObject({
-        data: {
-          events: [
-            {
-              click_id: 'click_id_1',
-              event_at: '2024-01-08T13:52:50.212Z',
-              event_metadata: {
-                conversion_id: 'conversion_id_1',
-                currency: 'USD',
-                products: [
-                  {
-                    category: 'category_1',
-                    id: 'product_id_1',
-                    name: 'name_1'
-                  },
-                  {
-                    category: 'category_2',
-                    id: 'product_id_2',
-                    name: 'name_2'
-                  }
-                ],
-                value_decimal: 100
-              },
-              event_type: {
-                tracking_type: 'Lead'
-              },
-              user: {
-                email: 'f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a',
-                external_id: '3482ae91c8ec52c06e19d618d400b3985814bf705e00947a302ec849a6575c4c',
-                ip_address: '5feaf188de296cd3b17f7c66fd3a2aec9b694815f2b1180631f7b52f57029777',
-                user_agent: 'test-user-agent',
-                uuid: 'uuid_1'
-              }
+        events: [
+          {
+            click_id: 'click_id_1',
+            event_at: '2024-01-08T13:52:50.212Z',
+            event_metadata: {
+              conversion_id: 'conversion_id_1',
+              currency: 'USD',
+              products: [
+                {
+                  category: 'category_1',
+                  id: 'product_id_1',
+                  name: 'name_1'
+                },
+                {
+                  category: 'category_2',
+                  id: 'product_id_2',
+                  name: 'name_2'
+                }
+              ],
+              value_decimal: 100
+            },
+            event_type: {
+              tracking_type: 'Lead'
+            },
+            user: {
+              email: 'f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a',
+              external_id: '3482ae91c8ec52c06e19d618d400b3985814bf705e00947a302ec849a6575c4c',
+              ip_address: '5feaf188de296cd3b17f7c66fd3a2aec9b694815f2b1180631f7b52f57029777',
+              user_agent: 'test-user-agent',
+              uuid: 'uuid_1'
             }
-          ],
-          partner: 'SEGMENT',
-          test_mode: false
-        }
+          }
+        ],
+        partner: 'SEGMENT',
+        test_mode: false
       })
     })
   })
