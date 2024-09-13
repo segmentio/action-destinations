65361cc5e52574c0680967abecc4dafd8c86526e
Joe Ayoub <joe.ayoub@segment.com>
2024-09-13 17:39:00 +0100

    build passing

--- packages/destination-actions/src/destinations/s3/syncAudienceToS3/index.ts
+++ packages/destination-actions/src/destinations/s3/syncAudienceToS3/index.ts
@@ -1,7 +1,7 @@
 import type { ActionDefinition } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
-// import { send } from '../functions'
+import { send } from '../functions'
 import { commonFields, audienceOnlyFields } from '../fields'
 
 const action: ActionDefinition<Settings, Payload> = {
@@ -13,14 +13,10 @@ const action: ActionDefinition<Settings, Payload> = {
     ...audienceOnlyFields
   },
   perform: async (_, { payload, settings }) => {
-    console.log(payload)
-    console.log(settings)
-    // return send([payload], settings)
+    return send([payload], settings)
   },
   performBatch: (_, { payload, settings }) => {
-    console.log(payload)
-    console.log(settings)
-    // return send(payload, settings)
+    return send(payload, settings)
   }
 }
 
