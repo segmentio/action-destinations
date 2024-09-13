cb8b550f62a0467857f21a63a8588a13c2929bba
Austin Hsueh <austin.hsueh@reddit.com>
2024-09-13 14:07:12 -0700

    generating types

--- packages/destination-actions/src/destinations/reddit-audiences/generated-types.ts
+++ packages/destination-actions/src/destinations/reddit-audiences/generated-types.ts
@@ -5,6 +5,18 @@ export interface Settings {
    * Unique identifier of an ad account. This can be found in the Reddit UI.
    */
   ad_account_id: string
+  /**
+   * Unique identifier of an ad account. This can be found in the Reddit UI.
+   */
+  client_id: string
+  /**
+   * Unique identifier of an ad account. This can be found in the Reddit UI.
+   */
+  client_secret: string
+  /**
+   * Unique identifier of an ad account. This can be found in the Reddit UI.
+   */
+  refresh_token: string
 }
 // Generated file. DO NOT MODIFY IT BY HAND.
 
