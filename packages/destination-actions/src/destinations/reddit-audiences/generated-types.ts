160f052ba0a7c2c30fe5b54e7a6cb9508950e49a
Joe Ayoub <joe.ayoub@segment.com>
2024-09-17 15:32:25 +0100

    committing changes

--- packages/destination-actions/src/destinations/reddit-audiences/generated-types.ts
+++ packages/destination-actions/src/destinations/reddit-audiences/generated-types.ts
@@ -5,18 +5,6 @@ export interface Settings {
    * Unique identifier of an ad account. This can be found in the Reddit UI.
    */
   ad_account_id: string
-  /**
-   * Unique identifier of an ad account. This can be found in the Reddit UI.
-   */
-  client_id: string
-  /**
-   * Unique identifier of an ad account. This can be found in the Reddit UI.
-   */
-  client_secret: string
-  /**
-   * Unique identifier of an ad account. This can be found in the Reddit UI.
-   */
-  refresh_token: string
 }
 // Generated file. DO NOT MODIFY IT BY HAND.
 
--- packages/destination-actions/src/destinations/reddit-audiences/index.ts
+++ packages/destination-actions/src/destinations/reddit-audiences/index.ts
@@ -1,60 +1,29 @@
 import type { AudienceDestinationDefinition } from '@segment/actions-core'
 import type { Settings, AudienceSettings } from './generated-types'
 import { AuthSettings, RequestOptions } from './types'
-import { setAccessToken } from './functions'
+import syncAudience from './syncAudience'
 
-import addToAudience from './addToAudience'
-
-import createAudience from './createAudience'
-
-import deleteFromAudience from './deleteFromAudience'
-
-// For an example audience destination, refer to webhook-audiences. The Readme section is under 'Audience Support'
 const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
   name: 'Reddit Audiences',
   slug: 'actions-reddit-audiences',
   mode: 'cloud',
-
   authentication: {
-    scheme: 'oauth-managed',
+    scheme: 'oauth2',
     fields: {
       ad_account_id: {
         type: 'string',
         label: 'Ad Account ID',
         description: 'Unique identifier of an ad account. This can be found in the Reddit UI.',
         required: true
-      },
-      client_id: {
-        type: 'string',
-        label: 'Client ID',
-        description: 'Unique identifier of an ad account. This can be found in the Reddit UI.',
-        required: true
-      },
-      client_secret: {
-        type: 'string',
-        label: 'Client Secret',
-        description: 'Unique identifier of an ad account. This can be found in the Reddit UI.',
-        required: true
-      },
-      refresh_token: {
-        type: 'string',
-        label: 'Refresh Token',
-        description: 'Unique identifier of an ad account. This can be found in the Reddit UI.',
-        required: true
       }
     },
-
-    // comment out below for now and go back to it
-    // testAuthentication: (request) => {
-    //   // Return a request that tests/validates the user's credentials.
-    //   // If you do not have a way to validate the authentication fields safely,
-    //   // you can remove the `testAuthentication` function, though discouraged.
-    // },
-
-    refreshAccessToken: async (request: (url: string, options?: RequestOptions) => Promise<any>, { auth }: { auth: AuthSettings }) => {
-      // Return a request that refreshes the access_token if the API supports it
+    refreshAccessToken: async (
+      request: (url: string, options?: RequestOptions) => Promise<any>,
+      { auth }: { auth: AuthSettings }
+    ) => {
+     
       const baseUrl = 'https://www.reddit.com/api/v1/access_token'
-      const body = `grant_type=refresh_token&refresh_token=${auth.refreshToken}`;
+      const body = `grant_type=refresh_token&refresh_token=${auth.refreshToken}`
       const res = await request(baseUrl, {
         method: 'POST',
         username: auth.clientId,
@@ -63,42 +32,42 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
       })
       const responseData = res.data as { access_token: string }
 
-      setAccessToken(responseData.access_token);
+      setAccessToken(responseData.access_token)
       return {
-        accessToken: responseData.access_token,
+        accessToken: responseData.access_token
       }
     }
   },
-
+  
   audienceFields: {
-    adAccountId: {
-      label: 'An audience id required by the destination',
-      description: 'An audience id required by the destination',
+    audienceName: {
+      label: 'Audience Name',
+      description: 'An audience name to display in Reddit',
       type: 'string',
       required: true
-    },
-    audienceId: {
-      label: 'An audience id required by the destination',
-      description: 'An audience id required by the destination',
-      type: 'string',
-      required: false
     }
   },
-
   audienceConfig: {
     mode: {
-      type: 'synced', // Indicates that the audience is synced on some schedule; update as necessary
-      full_audience_sync: false // If true, we send the entire audience. If false, we just send the delta.
+      type: 'synced', 
+      full_audience_sync: false 
     },
-
+    async createAudience(request, createAudienceInput) {
+      
+      // Must respond with the below object
+      // return {
+      //   externalId: String(audience_id) 
+      // }
+    },
+    async getAudience(_, getAudienceInput) {
+      return {
+        externalId: getAudienceInput.externalId
+      }
+    }
   },
-
   actions: {
-    addToAudience,
-    createAudience,
-    deleteFromAudience
+    syncAudience
   }
 }
 
-
-export default destination;
+export default destination
