63879bd6c40ddbf1bfcd160befccf9159f361e3a
Philipp Munin <7120440+pmunin@users.noreply.github.com>
2024-09-25 14:34:54 -0400

    Update types.ts: newline at end of file

--- packages/destination-actions/src/destinations/reddit-audiences/types.ts
+++ packages/destination-actions/src/destinations/reddit-audiences/types.ts
@@ -1,23 +1,22 @@
-
 import { MAID_SCHEMA_NAME, EMAIL_SCHEMA_NAME } from './const'
 
 export interface CreateAudienceResp {
-    id: string
+  id: string
 }
 
 export interface CreateAudienceReq {
-    data: {
-        name: string
-        type: string
-    }
+  data: {
+    name: string
+    type: string
+  }
 }
 
 export interface UpdateAudienceReq {
-    data: {
-        column_order: Columns
-        user_data: string[][],
-        action_type: 'ADD' | 'REMOVE'
-    }
+  data: {
+    column_order: Columns
+    user_data: string[][]
+    action_type: 'ADD' | 'REMOVE'
+  }
 }
 
 export type Columns = (typeof MAID_SCHEMA_NAME | typeof EMAIL_SCHEMA_NAME)[]
