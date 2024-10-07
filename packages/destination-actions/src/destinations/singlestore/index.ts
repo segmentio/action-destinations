eecb761c0a7414f3b86761a95830db868594cd66
Joe Ayoub <joe.ayoub@segment.com>
2024-10-07 11:33:09 +0100

    scaffold

---
+++ packages/destination-actions/src/destinations/singlestore/index.ts
@@ -0,0 +1,32 @@
+import type { DestinationDefinition } from '@segment/actions-core'
+import type { Settings } from './generated-types'
+
+import sendData from './sendData'
+
+const destination: DestinationDefinition<Settings> = {
+  name: 'Singlestore',
+  slug: 'actions-singlestore',
+  mode: 'cloud',
+
+  authentication: {
+    scheme: 'custom',
+    fields: {},
+    testAuthentication: (request) => {
+      // Return a request that tests/validates the user's credentials.
+      // If you do not have a way to validate the authentication fields safely,
+      // you can remove the `testAuthentication` function, though discouraged.
+    }
+  },
+
+  onDelete: async (request, { settings, payload }) => {
+    // Return a request that performs a GDPR delete for the provided Segment userId or anonymousId
+    // provided in the payload. If your destination does not support GDPR deletion you should not
+    // implement this function and should remove it completely.
+  },
+
+  actions: {
+    sendData
+  }
+}
+
+export default destination
