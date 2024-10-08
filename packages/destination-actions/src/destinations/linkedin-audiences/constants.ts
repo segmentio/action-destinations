f0f9986322af02add5f489e5d8668f58f2036690
Harsh Joshi <harsh.joshi@segment.com>
2024-10-08 11:24:34 +0530

    update linkedin API version to 202409

--- packages/destination-actions/src/destinations/linkedin-audiences/constants.ts
+++ packages/destination-actions/src/destinations/linkedin-audiences/constants.ts
@@ -1,3 +1,3 @@
-export const LINKEDIN_API_VERSION = '202311'
+export const LINKEDIN_API_VERSION = '202409'
 export const BASE_URL = 'https://api.linkedin.com/rest'
 export const LINKEDIN_SOURCE_PLATFORM = 'SEGMENT'
