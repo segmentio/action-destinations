254e66e687ccf771c938864c8fc1a2c743302ab5
Varadarajan V <109586712+varadarajan-tw@users.noreply.github.com>
2024-10-21 14:27:08 +0530

    fix intercom tests

--- packages/browser-destinations/destinations/intercom/src/__tests__/utils.test.ts
+++ packages/browser-destinations/destinations/intercom/src/__tests__/utils.test.ts
@@ -3,7 +3,7 @@ import { convertDateToUnix, filterCustomTraits, getWidgetOptions, isEmpty } from
 
 describe('Utils test', () => {
   describe('date handling tests', () => {
-    test.skip('handles ISO datestrings', () => {
+    test('handles ISO datestrings', () => {
       const date = new Date()
       const isoDate = date.toISOString()
       const unixDate = dayjs(isoDate).unix()
@@ -26,7 +26,7 @@ describe('Utils test', () => {
   })
 
   describe('custom trait filtering tests', () => {
-    test.skip('objects & arrays will be filtered out of traits', () => {
+    test('objects & arrays will be filtered out of traits', () => {
       const traits = {
         name: 'ibum',
         badObj: {
@@ -47,24 +47,24 @@ describe('Utils test', () => {
   })
 
   describe('isEmpty tests', () => {
-    test.skip('isEmpty returns true if object is empty', () => {
+    test('isEmpty returns true if object is empty', () => {
       const obj = {}
       expect(isEmpty(obj)).toBe(true)
     })
 
-    test.skip('isEmpty returns false if object is not empty', () => {
+    test('isEmpty returns false if object is not empty', () => {
       const obj = { prop: 'value' }
       expect(isEmpty(obj)).toBe(false)
     })
 
-    test.skip('isEmpty works for undefined obj', () => {
+    test('isEmpty works for undefined obj', () => {
       const obj = undefined
       expect(isEmpty(obj)).toBe(true)
     })
   })
 
   describe('widget options tests', () => {
-    test.skip('attaches `activator` if activator is not default', () => {
+    test('attaches `activator` if activator is not default', () => {
       const activator = '#my-widget'
       const hide_default_launcher = undefined
 
@@ -75,7 +75,7 @@ describe('Utils test', () => {
       })
     })
 
-    test.skip('attaches `hide_default_launcher` if its not undefined ', () => {
+    test('attaches `hide_default_launcher` if its not undefined ', () => {
       const activator = '#my-widget'
       const hide_default_launcher = false
 
