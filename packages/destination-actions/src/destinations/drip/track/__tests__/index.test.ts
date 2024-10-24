0c75f0fb35d2edb3467f816bb56edbd562f7b452
Stephen Webber <stephen.webber@drip.com>
2024-10-24 15:08:19 -0500

    wip

--- packages/destination-actions/src/destinations/drip/track/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/drip/track/__tests__/index.test.ts
@@ -30,33 +30,37 @@ describe('Drip.track', () => {
     // TODO: not-match cases?
   })
 
-  // it('should track events with custom mappings', async () => {
-  //   nock('https://in.accoil.com').post('/segment').reply(200, {})
+  it('should track events with default mappings', async () => {
+    nock('https://api-staging.getdrip.com').post('/v2/3977335/events').reply(200, {})
 
-  //   const responses = await testDestination.testAction('track', {
-  //     settings: { api_key: 'apikey' },
-  //     event: createTestEvent({
-  //       event: 'Purchase Complete',
-  //       userId: 'bobby',
-  //       properties: { event_detail: 'Purchased New Book' }
-  //     }),
-  //     mapping: {
-  //       userId: {
-  //         '@path': '$.userId'
-  //       },
-  //       event: {
-  //         '@path': '$.properties.event_detail'
-  //       },
-  //       timestamp: {
-  //         '@path': '$.timestamp'
-  //       }
-  //     }
-  //   })
+    const event = createTestEvent({
+      event: 'Custom',
+      traits: { properties: { fizz: 'buzz' } },
+      properties: { email: 'foo@bar.com', action: 'Custom' }
+    })
 
-  //   expect(responses.length).toBe(1)
-  //   expect(responses[0].status).toBe(200)
-  //   expect(responses[0].options.body).toMatch(/"type":\s*"track"/g)
-  //   expect(responses[0].options.body).toContain('bobby')
-  //   expect(responses[0].options.body).toContain('Purchased New Book')
-  // })
+    const responses = await testDestination.testAction('track', {
+      settings: { apiKey: 'key' },
+      event: event,
+      mapping: {
+        action: {
+          '@path': '$.properties.action'
+        },
+        email: {
+          '@path': '$.properties.email'
+        },
+        properties: {
+          '@path': '$.traits.properties'
+        }
+      }
+    })
+
+    expect(responses.length).toBe(1)
+    expect(responses[0].status).toBe(200)
+    expect(responses[0].options.body).toMatch(/"type":\s*"track"/g)
+    expect(responses[0].options.body).toContain('Custom')
+    expect(responses[0].options.body).toContain('foo@bar.com')
+    expect(responses[0].options.body).toContain('buzz')
+    // TODO: not-match cases?
+  })
 })
