af5c5f335f8170030a9e7000030994bbba23da9a
Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
2024-11-12 11:19:41 +0000

    bug fixes for sendEmail Action (#2582)

--- packages/destination-actions/src/destinations/sendgrid/sendEmail/index.ts
+++ packages/destination-actions/src/destinations/sendgrid/sendEmail/index.ts
@@ -3,7 +3,13 @@ import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 import { send } from './utils'
 import { fields } from './fields'
-import { dynamicTemplateId, dynamicIpPoolNames, dynamicDomain, dynamicGroupId } from './dynamic-fields'
+import {
+  dynamicTemplateId,
+  dynamicIpPoolNames,
+  dynamicDomain,
+  dynamicGroupId,
+  dynamicTemplateData
+} from './dynamic-fields'
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Send email with Dynamic Template',
@@ -15,6 +21,11 @@ const action: ActionDefinition<Settings, Payload> = {
     },
     ip_pool_name: async (request) => {
       return await dynamicIpPoolNames(request)
+    },
+    dynamic_template_data: {
+      __keys__: async (request, { payload }) => {
+        return await dynamicTemplateData(request, payload)
+      }
     },
     domain: async (request) => {
       return await dynamicDomain(request)
