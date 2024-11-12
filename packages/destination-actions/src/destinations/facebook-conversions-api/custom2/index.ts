5e76015c055eab68a3e77c692632ae848cd35b76
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2024-11-12 02:58:57 -0800

    [Facebook Conversions] Adding Test Event Code to all mappings. (#2516)
    * [Facebook CAPI] Adding Test Event Code to all mappings.
    
    * Adding corresponding unit tests.

--- packages/destination-actions/src/destinations/facebook-conversions-api/custom2/index.ts
+++ packages/destination-actions/src/destinations/facebook-conversions-api/custom2/index.ts
@@ -8,7 +8,8 @@ import {
   data_processing_options,
   data_processing_options_country,
   data_processing_options_state,
-  dataProcessingOptions
+  dataProcessingOptions,
+  test_event_code
 } from '../fb-capi-properties'
 import { hash_user_data, user_data_field } from '../fb-capi-user-data'
 import type { Settings } from '../generated-types'
@@ -45,7 +46,8 @@ const action: ActionDefinition<Settings, Payload> = {
     event_source_url: event_source_url,
     data_processing_options: data_processing_options,
     data_processing_options_country: data_processing_options_country,
-    data_processing_options_state: data_processing_options_state
+    data_processing_options_state: data_processing_options_state,
+    test_event_code: test_event_code
   },
   perform: (request, { payload, settings, features, statsContext, syncMode }) => {
     if (syncMode === 'add') {
@@ -58,6 +60,8 @@ const action: ActionDefinition<Settings, Payload> = {
         payload.data_processing_options_country,
         payload.data_processing_options_state
       )
+
+      const testEventCode = payload.test_event_code || settings.testEventCode
 
       return request(
         `https://graph.facebook.com/v${get_api_version(features, statsContext)}/${settings.pixelId}/events`,
@@ -79,7 +83,7 @@ const action: ActionDefinition<Settings, Payload> = {
                 data_processing_options_state: state_code
               }
             ],
-            ...(settings.testEventCode && { test_event_code: settings.testEventCode })
+            ...(testEventCode && { test_event_code: testEventCode })
           }
         }
       )
