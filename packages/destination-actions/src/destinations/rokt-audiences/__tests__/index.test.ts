8fa0100bad2cf498117cda6d2f5c420e15a8c754
Ben Goodman <52042939+GoodmanBen@users.noreply.github.com>
2024-11-26 07:49:06 -0500

    Migrate rokt-audiences to new authentication method (#2606)
    * build: migrate to rpub rsec based auth
    
    * build: updates for authentication tests
    
    * build: small tweaks and test fixes
    
    * Update packages/destination-actions/src/destinations/rokt-audiences/upsertCustomAudiences/index.ts
    
    Co-authored-by: Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>

--- packages/destination-actions/src/destinations/rokt-audiences/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/rokt-audiences/__tests__/index.test.ts
@@ -6,7 +6,9 @@ import { CONSTANTS } from '../constants'
 const testDestination = createTestIntegration(Definition)
 
 const VALID_SETTINGS = {
-  apiKey: 'key'
+  rpub: 'rpub-***',
+  rsec: 'rsec-***',
+  accountid: '8675309'
 }
 
 const MOCK_TOKEN_RESPONSE = {
@@ -15,14 +17,14 @@ const MOCK_TOKEN_RESPONSE = {
 
 describe('Rokt Audiences', () => {
   describe('testAuthentication', () => {
-    it('should validate valid auth token', async () => {
-      nock(CONSTANTS.ROKT_API_BASE_URL).get(CONSTANTS.ROKT_API_AUTH_ENDPOINT).reply(200, MOCK_TOKEN_RESPONSE)
+    it('should validate proper rpub-rsec credentials', async () => {
+      nock(CONSTANTS.ROKT_API_BASE_URL).post(CONSTANTS.ROKT_API_AUTH_ENDPOINT).reply(200, MOCK_TOKEN_RESPONSE)
       const settings = VALID_SETTINGS
       await expect(testDestination.testAuthentication(settings)).resolves.not.toThrowError()
     })
 
     it('should test that authentication fails', async () => {
-      nock(CONSTANTS.ROKT_API_BASE_URL).get(CONSTANTS.ROKT_API_AUTH_ENDPOINT).reply(401)
+      nock(CONSTANTS.ROKT_API_BASE_URL).post(CONSTANTS.ROKT_API_AUTH_ENDPOINT).reply(401)
       const settings = VALID_SETTINGS
       await expect(testDestination.testAuthentication(settings)).rejects.toThrowError('')
     })
