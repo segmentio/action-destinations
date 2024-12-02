e7f04d780d0cadc90251e83a624d23ec31c37da4
Sayan Das <sayan.das@segment.com>
2024-12-03 02:51:41 +0530

    Multistatus WIP

--- packages/destination-actions/src/destinations/marketo-static-lists/functions.ts
+++ packages/destination-actions/src/destinations/marketo-static-lists/functions.ts
@@ -1,4 +1,12 @@
-import { IntegrationError, RetryableError, RequestClient, StatsContext } from '@segment/actions-core'
+import {
+  MultiStatusResponse,
+  ErrorCodes,
+  IntegrationError,
+  RetryableError,
+  RequestClient,
+  StatsContext
+} from '@segment/actions-core'
+import { ActionDestinationErrorResponseType } from '@segment/actions-core/destination-kit/types'
 import { Settings } from './generated-types'
 import { Payload as AddToListPayload } from './addToList/generated-types'
 import { Payload as RemoveFromListPayload } from './removeFromList/generated-types'
@@ -34,11 +42,23 @@ export async function addToList(
   statsContext?: StatsContext,
   hookOutputs?: { id: string; name: string }
 ) {
+  const isBatchPayload = payloads.length > 1
+
   // If the list ID is provided in the hook outputs, use it
   const list_id = hookOutputs?.id ?? payloads[0].external_id
 
   if (!list_id) {
-    throw new IntegrationError('No list ID found in payload', 'INVALID_REQUEST_DATA', 400)
+    const errormessage = 'No list ID found in payload'
+
+    if (isBatchPayload) {
+      setMultiStatusErrorResponse(payloads.length, {
+        status: 400,
+        errortype: ErrorCodes.PAYLOAD_VALIDATION_FAILED,
+        errormessage
+      })
+    }
+
+    throw new IntegrationError(errormessage, ErrorCodes.PAYLOAD_VALIDATION_FAILED, 400)
   }
 
   const api_endpoint = formatEndpoint(settings.api_endpoint)
@@ -46,8 +66,18 @@ export async function addToList(
   const csvData = formatData(payloads)
   const csvSize = Buffer.byteLength(csvData, 'utf8')
   if (csvSize > CSV_LIMIT) {
+    const errormessage = `CSV data size exceeds limit of ${CSV_LIMIT} bytes`
+
+    if (isBatchPayload) {
+      setMultiStatusErrorResponse(payloads.length, {
+        status: 400,
+        errortype: ErrorCodes.PAYLOAD_TOO_LARGE,
+        errormessage
+      })
+    }
+
     statsContext?.statsClient?.incr('addToAudience.error', 1, statsContext?.tags)
-    throw new IntegrationError(`CSV data size exceeds limit of ${CSV_LIMIT} bytes`, 'INVALID_REQUEST_DATA', 400)
+    throw new IntegrationError(errormessage, 'PAYLOAD_TOO_LARGE', 400)
   }
 
   const url =
@@ -76,8 +106,20 @@ export async function removeFromList(
   payloads: RemoveFromListPayload[],
   statsContext?: StatsContext
 ) {
+  const isBatchPayload = payloads.length > 1
+
   if (!payloads[0].external_id) {
-    throw new IntegrationError('No external_id found in payload', 'INVALID_REQUEST_DATA', 400)
+    const errormessage = 'No "external_id" found in payload'
+
+    if (isBatchPayload) {
+      setMultiStatusErrorResponse(payloads.length, {
+        status: 400,
+        errortype: ErrorCodes.PAYLOAD_VALIDATION_FAILED,
+        errormessage
+      })
+    }
+
+    throw new IntegrationError(errormessage, ErrorCodes.PAYLOAD_VALIDATION_FAILED, 400)
   }
 
   const api_endpoint = formatEndpoint(settings.api_endpoint)
@@ -277,3 +319,94 @@ export async function createList(request: RequestClient, input: CreateListInput,
 
   return listId
 }
+
+const setMultiStatusErrorResponse = (payloadSize: number, error: ActionDestinationErrorResponseType) => {
+  const multiStatusResponse = new MultiStatusResponse()
+
+  for (let i = 0; i < payloadSize; i++) {
+    multiStatusResponse.setErrorResponseAtIndex(i, error)
+  }
+
+  return multiStatusResponse
+}
+
+type ErrorCodeMapDefinition = {
+  status: number
+  description: string
+}
+
+export const MarketoErrorCodes = new Map<number, ErrorCodeMapDefinition>([
+  // [502, { status: 502, description: 'Bad Gateway' }],
+  [601, { status: 401, description: 'Access token invalid' }],
+  [602, { status: 401, description: 'Access token expired' }],
+  [603, { status: 403, description: 'Access denied' }],
+  [604, { status: 504, description: 'Request time-out' }],
+  [605, { status: 405, description: 'HTTP Method not supported' }],
+  [606, { status: 429, description: 'Max rate limit exceeded' }],
+  [607, { status: 429, description: 'Daily quota reached' }],
+  [608, { status: 503, description: 'API Temporarily Unavailable' }],
+  [609, { status: 400, description: 'Invalid JSON' }],
+  [610, { status: 404, description: 'Requested resource not found' }],
+  [611, { status: 500, description: 'System error' }],
+  [612, { status: 400, description: 'Invalid Content Type' }],
+  [613, { status: 400, description: 'Invalid Multipart Request' }],
+  [614, { status: 404, description: 'Invalid Subscription' }],
+  [615, { status: 429, description: 'Concurrent access limit reached' }],
+  [616, { status: 400, description: 'Invalid subscription type' }],
+  [701, { status: 400, description: '%s cannot be blank' }],
+  [702, { status: 404, description: 'No data found for a given search scenario' }],
+  [703, { status: 404, description: 'The feature is not enabled for the subscription' }],
+  [704, { status: 400, description: 'Invalid date format' }],
+  [709, { status: 400, description: 'Business Rule Violation' }],
+  [710, { status: 404, description: 'Parent Folder Not Found' }],
+  [711, { status: 400, description: 'Incompatible Folder Type' }],
+  [712, { status: 400, description: 'Merge to person Account operation is invalid' }],
+  [713, { status: 503, description: 'Transient Error' }],
+  [714, { status: 404, description: 'Unable to find the default record type' }],
+  [718, { status: 404, description: 'ExternalSalesPersonID not found' }],
+  [719, { status: 500, description: 'Lock wait timeout exception' }],
+  //
+  [1001, { status: 400, description: 'Invalid value, Required type is different' }],
+  [1002, { status: 400, description: 'Missing value for the required parameter' }],
+  [1003, { status: 400, description: 'Invalid data' }],
+  [1004, { status: 400, description: 'Lead not found' }],
+  [1005, { status: 400, description: 'Lead already exists' }],
+  [1006, { status: 400, description: 'Field not found' }],
+  [1007, { status: 400, description: 'Multiple leads match the lookup criteria' }],
+  [1008, { status: 400, description: 'Access denied to partition' }],
+  [1009, { status: 400, description: 'Partition name must be specified' }],
+  [1010, { status: 400, description: 'Partition update not allowed' }],
+  [1011, { status: 400, description: 'Field not supported' }],
+  [1012, { status: 400, description: 'Invalid cookie value' }],
+  [1013, { status: 400, description: 'Object not found' }],
+  [1014, { status: 400, description: 'Failed to create Object' }],
+  [1015, { status: 400, description: 'Lead not in list' }],
+  [1016, { status: 400, description: 'Too many imports' }],
+  [1017, { status: 400, description: 'Object already exists' }],
+  [1018, { status: 400, description: 'CRM Enabled' }],
+  [1019, { status: 400, description: 'Import in progress' }],
+  [1020, { status: 400, description: 'Too many clones to program' }],
+  [1021, { status: 400, description: 'Company update not allowed' }],
+  [1022, { status: 400, description: 'Object in use' }],
+  [1025, { status: 400, description: 'Program status not found' }],
+  [1026, { status: 400, description: 'Custom object not enabled' }],
+  [1027, { status: 400, description: 'Max Activity Type Limit Reached' }],
+  [1028, { status: 400, description: 'Max field limit reached' }],
+  [1029, { status: 400, description: 'Too many jobs in queue' }],
+  [1035, { status: 400, description: 'Unsupported filter type' }],
+  [1036, { status: 400, description: 'Duplicate object found in input' }],
+  [1037, { status: 400, description: 'Lead was skipped' }],
+  [1042, { status: 400, description: 'Invalid runAt date' }],
+  [1048, { status: 400, description: 'Custom Object Discard Draft Failed' }],
+  [1049, { status: 400, description: 'Failed to Create Activity' }],
+  [1076, { status: 400, description: 'Merge Leads call with mergeInCRM flag is 4' }],
+  [1077, { status: 400, description: 'Merge Leads call failed due to `SFDC Field` length' }],
+  [
+    1078,
+    {
+      status: 400,
+      description:
+        "Merge Leads call failed due to deleted entity, not a lead/contact, or field filter criteria doesn't match"
+    }
+  ]
+])
