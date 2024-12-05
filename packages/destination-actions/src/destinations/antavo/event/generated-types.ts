49a64b979948f9388a409b6c3e385a2a71bf9fdc
balint.bozoki <balint.bozoki@antavo.com>
2024-12-05 17:12:37 +0100

    ran yarn types command

--- packages/destination-actions/src/destinations/antavo/event/generated-types.ts
+++ packages/destination-actions/src/destinations/antavo/event/generated-types.ts
@@ -1,9 +1,21 @@
 // Generated file. DO NOT MODIFY IT BY HAND.
 
 export interface Payload {
+  /**
+   * User ID, selected in Antavo as customer identifier
+   */
   customer: string
+  /**
+   * Loyalty event name in Antavo
+   */
   action: string
+  /**
+   * Antavo Account ID — if the Multi Accounts extension is enabled
+   */
   account?: string
+  /**
+   * Event data
+   */
   data?: {
     [k: string]: unknown
   }
