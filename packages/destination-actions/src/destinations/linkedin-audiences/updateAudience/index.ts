f8d47a4ad5e9dedcd68796f64e361c7063ffad77
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2024-12-12 15:32:37 -0800

    PR suggestions.

--- packages/destination-actions/src/destinations/linkedin-audiences/updateAudience/index.ts
+++ packages/destination-actions/src/destinations/linkedin-audiences/updateAudience/index.ts
@@ -1,8 +1,7 @@
 import type { ActionDefinition, StatsContext } from '@segment/actions-core'
-import { RequestClient, RetryableError, IntegrationError } from '@segment/actions-core'
+import { RequestClient, RetryableError, IntegrationError, sha256SmartHash } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
-import { createHash } from 'crypto'
 import { LinkedInAudiences } from '../api'
 
 const action: ActionDefinition<Settings, Payload> = {
@@ -31,9 +30,9 @@ const action: ActionDefinition<Settings, Payload> = {
       type: 'string',
       default: {
         '@if': {
-          exists: { '@path': '$.traits.email' },
-          then: { '@path': '$.traits.email' },
-          else: { '@path': '$.context.traits.email' }
+          exists: { '@path': '$.context.traits.email' },
+          then: { '@path': '$.context.traits.email' },
+          else: { '@path': '$.traits.email' }
         }
       }
     },
@@ -224,15 +223,9 @@ function getUserIds(settings: Settings, payload: Payload): Record<string, string
   const users = []
 
   if (payload.email && settings.send_email === true) {
-    let email = payload.email
-    const isHashed = new RegExp(/[0-9abcdef]{64}/gi).test(payload.email)
-    if (!isHashed) {
-      email = createHash('sha256').update(payload.email).digest('hex')
-    }
-
     users.push({
       idType: 'SHA256_EMAIL',
-      idValue: email
+      idValue: sha256SmartHash(payload.email)
     })
   }
 
