1dc229c3a2ed34c88475d804ad1d355c2f3b8701
Karn <karn@customer.io>
2024-12-16 14:20:21 +0530

    add content event to customerio destination

---
+++ packages/destination-actions/src/destinations/customerio/__tests__/reportContentEvent.test.ts
@@ -0,0 +1,141 @@
+import { createTestEvent, createTestIntegration } from '@segment/actions-core'
+import CustomerIO from '../index'
+import dayjs from '../../../lib/dayjs'
+import { AccountRegion } from '../utils'
+import { nockTrackInternalEndpoint } from '../test-helper'
+
+const trackService = nockTrackInternalEndpoint(AccountRegion.US)
+
+describe('CustomerIO', () => {
+  describe('reportContentEvent', () => {
+    const testDestination = createTestIntegration(CustomerIO)
+
+    type testCase = {
+      name: string
+      anonymousId: string
+      context?: { [key: string]: any }
+      properties: { [key: string]: any }
+      expected: { [key: string]: any }
+    }
+
+    const testCases: testCase[] = [
+      {
+        name: 'should work with viewed',
+        anonymousId: 'abc123',
+        properties: {
+          actionType: 'viewed_content',
+          contentType: 'ctype',
+          contentId: 1,
+          templateId: 1
+        },
+        expected: {
+          action: 'viewed_content',
+          attributes: {
+            contentType: 'ctype',
+            contentId: 1,
+            templateId: 1
+          },
+          identifiers: {
+            anonymous_id: 'abc123'
+          },
+          name: 'viewed_content',
+          type: 'person'
+        }
+      },
+      {
+        name: 'should work with clicked',
+        anonymousId: 'abc123',
+        properties: {
+          actionType: 'clicked_content',
+          contentType: 'ctype',
+          contentId: 1,
+          templateId: 1,
+          actionName: 'aname',
+          actionValue: 'avalue'
+        },
+        expected: {
+          action: 'clicked_content',
+          attributes: {
+            contentType: 'ctype',
+            contentId: 1,
+            templateId: 1,
+            actionName: 'aname',
+            actionValue: 'avalue'
+          },
+          identifiers: {
+            anonymous_id: 'abc123'
+          },
+          name: 'clicked_content',
+          type: 'person'
+        }
+      },
+      {
+        name: 'should use the journeys identifiers',
+        anonymousId: 'unused',
+        context: {
+          journeys: {
+            identifiers: {
+              id: 'user_123'
+            }
+          }
+        },
+        properties: {
+          actionType: 'clicked_content',
+          contentType: 'ctype',
+          contentId: 1,
+          templateId: 1,
+          actionName: 'aname',
+          actionValue: 'avalue'
+        },
+        expected: {
+          action: 'clicked_content',
+          attributes: {
+            anonymous_id: 'unused',
+            contentType: 'ctype',
+            contentId: 1,
+            templateId: 1,
+            actionName: 'aname',
+            actionValue: 'avalue'
+          },
+          identifiers: {
+            id: 'user_123'
+          },
+          name: 'clicked_content',
+          type: 'person'
+        }
+      }
+    ]
+
+    testCases.forEach((testCase) => {
+      it(testCase.name, async () => {
+        trackService.post(`/api/v2/entity`).reply(200, {}, { 'x-customerio-region': 'US' })
+
+        const now = dayjs.utc()
+        const event = createTestEvent({
+          anonymousId: testCase.anonymousId,
+          timestamp: now.toISOString(),
+          type: 'track',
+          event: 'Report Content Event',
+          context: testCase.context,
+          properties: testCase.properties
+        })
+        const responses = await testDestination.testAction('reportContentEvent', {
+          event,
+          useDefaultMappings: true
+        })
+
+        expect(responses.length).toBe(1)
+        expect(responses[0].status).toBe(200)
+        expect(responses[0].headers.toJSON()).toMatchObject({
+          'x-customerio-region': 'US',
+          'content-type': 'application/json'
+        })
+        expect(responses[0].data).toMatchObject({})
+        expect(responses[0].options.json).toEqual({
+          ...testCase.expected,
+          timestamp: now.unix()
+        })
+      })
+    })
+  })
+})
---
+++ packages/destination-actions/src/destinations/customerio/reportContentEvent/generated-types.ts
@@ -0,0 +1,32 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * ID for the anonymous user.
+   */
+  anonymous_id?: string
+  /**
+   * The type of content event.
+   */
+  actionType: string
+  /**
+   * A timestamp of when the event took place. Default is current date and time.
+   */
+  timestamp?: string
+  /**
+   * Optional data to include with the event.
+   */
+  data?: {
+    [k: string]: unknown
+  }
+  /**
+   * Convert dates to Unix timestamps (seconds since Epoch).
+   */
+  convert_timestamp?: boolean
+  /**
+   * Non-user-provided context fields needed for auto-track.
+   */
+  context?: {
+    [k: string]: unknown
+  }
+}
