08d2652aa6d91295c30f640745199ccc67a5c631
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2024-12-17 09:22:29 -0800

    Responsys new Action: Send to PET + Adjustments in Send Audience as PET (#2615)
    * - New Action for Responsys: Send to PET, creating the PET if it doesn't exist yet;\n- Adding  as an optional parameter in Settings;\n-  should be a password field;\n- Profile List Name now it's an optional field.
    
    * PET fields can't have more than 30 characteres in their names.
    
    * New Action for Responsys: Send to PET.
    
    * - Having  truly work with sync/async endpoints;\n- For async requests, retrieving the corresponding response;\n- Improving profile lists update criteria.
    
    * Adding unit tests for  action.
    
    * Adding rate limits to all actions that are not marked as legacy.
    
    * With rate limits, 'sendToPet' doesn't require a retry mechanism per parameter.
    
    * Adding default permission status to Responsys mappings;\nRate limits for async request fetching.
    
    * Additional generations.
    
    * Making 'sendAudienceAsPet' to work with async endpoints.
    
    * Unit Tests adjustments.
    
    * Splitting requests in batches of 200 records. Updating unit tests.
    
    * Implementing batches of 200 records for upsertListMember.
    
    * Reversing 'Legacy' naming for 'sendCustomTraits'.
    
    * - Adding verification when PET exists in another folder;
    - Forcing US as default region for debug messages.
    
    * On profile list update, splitting batch into multiple record sets, to avoid overwriting empty values in a profile list.
    
    * Updating unit tests for Responsys.
    
    * Updating geerated types.
    
    * Fixing issue with .

--- packages/destination-actions/src/destinations/responsys/sendAudienceAsPet/functions.ts
+++ packages/destination-actions/src/destinations/responsys/sendAudienceAsPet/functions.ts
@@ -1,4 +1,4 @@
-import { RequestClient } from '@segment/actions-core'
+import { ModifiedResponse, RequestClient } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import { Payload } from './generated-types'
 import {
@@ -7,9 +7,29 @@ import {
   ResponsysMatchField,
   ResponsysMatchType,
   ResponsysMergeRule,
-  ResponsysRecordData
+  ResponsysRecordData,
+  ResponsysAsyncResponse
 } from '../types'
-import { sendDebugMessageToSegmentSource } from '../utils'
+import { getAsyncResponse, sendDebugMessageToSegmentSource } from '../utils'
+import { AuthTokens } from '@segment/actions-core/destination-kit/parse-settings'
+
+// Rate limits per endpoint.
+// Can be obtained through `/rest/api/ratelimit`, but at the point
+// this project is, there's no good way to calling it without a huge
+// drop in performance.
+// We are using here the most common values observed in our customers.
+
+// updateProfileListMembers (`lists/${settings.profileListName}/members`, POST): 400 requests per minute.
+// Around 1 request every 150ms.
+const upsertListMembersWaitInterval = 150
+
+// sendToPet (`lists/${settings.profileListName}/listExtensions/${petName}/members`, POST): 400 requests per minute.
+// Around 1 request every 150ms.
+const sendToPetWaitInterval = 150
+
+// getAsyncResponse (`requests/${requestId}`, GET): 1000 requests per minute.
+// Around 1 request every 60ms.
+const getAsyncResponseWaitInterval = 60
 
 export const petExists = async (request: RequestClient, settings: Settings, computationKey: string) => {
   const path = `/rest/api/v1.3/lists/${settings.profileListName}/listExtensions`
@@ -29,11 +49,11 @@ export const createPet = async (request: RequestClient, settings: Settings, payl
   const requestBody = {
     profileExtension: {
       objectName: payload.pet_name,
-      folderName: payload.folder_name
+      folderName: payload.folder_name || settings.defaultFolderName
     },
     fields: [
       {
-        fieldName: payload.pet_name,
+        fieldName: payload.pet_name.substring(0, 30),
         fieldType: 'STR500'
       }
     ]
@@ -50,20 +70,26 @@ export const createPet = async (request: RequestClient, settings: Settings, payl
   return response
 }
 
-export const updateProfileListAndPet = async (request: RequestClient, settings: Settings, payloads: Payload[]) => {
+export const updateProfileListAndPet = async (
+  request: RequestClient,
+  authTokens: AuthTokens,
+  settings: Settings,
+  payloads: Payload[]
+) => {
   const records: {
     [audienceKey: string]: {
       recordsWithUserId: Payload[]
       recordsWithEmail: Payload[]
       recordsWithRiid: Payload[]
-      requestBodyUserId?: ResponsysAudiencePetUpdateRequestBody
-      requestBodyEmail?: ResponsysAudiencePetUpdateRequestBody
-      requestBodyRiid?: ResponsysAudiencePetUpdateRequestBody
+      requestBodyUserId?: ResponsysAudiencePetUpdateRequestBody[]
+      requestBodyEmail?: ResponsysAudiencePetUpdateRequestBody[]
+      requestBodyRiid?: ResponsysAudiencePetUpdateRequestBody[]
     }
   } = {}
 
   // First we need to make sure that all users exist in the Profile List.
-  await updateProfileListMembers(request, settings, payloads)
+  await new Promise((resolve) => setTimeout(resolve, upsertListMembersWaitInterval))
+  await updateProfileListMembers(request, authTokens, settings, payloads)
 
   // This endpoint works better with only one identifier at a time, so we need to split the payloads into groups.
   for (const payload of payloads) {
@@ -86,13 +112,21 @@ export const updateProfileListAndPet = async (request: RequestClient, settings: 
 
   for (const [audienceKey, recordCategories] of Object.entries(records)) {
     if (recordCategories.recordsWithUserId.length > 0) {
-      records[audienceKey].requestBodyUserId = buildPetUpdatePayload(recordCategories.recordsWithUserId, 'CUSTOMER_ID')
+      records[audienceKey].requestBodyUserId = buildPetUpdatePayloads(
+        recordCategories.recordsWithUserId,
+        'CUSTOMER_ID',
+        settings
+      )
     }
     if (recordCategories.recordsWithEmail.length > 0) {
-      records[audienceKey].requestBodyEmail = buildPetUpdatePayload(recordCategories.recordsWithEmail, 'EMAIL_ADDRESS')
+      records[audienceKey].requestBodyEmail = buildPetUpdatePayloads(
+        recordCategories.recordsWithEmail,
+        'EMAIL_ADDRESS',
+        settings
+      )
     }
     if (recordCategories.recordsWithRiid.length > 0) {
-      records[audienceKey].requestBodyRiid = buildPetUpdatePayload(recordCategories.recordsWithRiid, 'RIID')
+      records[audienceKey].requestBodyRiid = buildPetUpdatePayloads(recordCategories.recordsWithRiid, 'RIID', settings)
     }
   }
 
@@ -101,22 +135,30 @@ export const updateProfileListAndPet = async (request: RequestClient, settings: 
   const results = []
   for (const [computationKey, recordCategories] of Object.entries(records)) {
     if (recordCategories.requestBodyUserId) {
-      results.push(await sendPetUpdate(request, settings, computationKey, recordCategories.requestBodyUserId))
+      results.push(
+        await sendPetUpdate(request, authTokens, settings, computationKey, recordCategories.requestBodyUserId)
+      )
     }
 
     if (recordCategories.requestBodyEmail) {
-      results.push(await sendPetUpdate(request, settings, computationKey, recordCategories.requestBodyEmail))
+      results.push(
+        await sendPetUpdate(request, authTokens, settings, computationKey, recordCategories.requestBodyEmail)
+      )
     }
 
     if (recordCategories.requestBodyRiid) {
-      results.push(await sendPetUpdate(request, settings, computationKey, recordCategories.requestBodyRiid))
+      results.push(await sendPetUpdate(request, authTokens, settings, computationKey, recordCategories.requestBodyRiid))
     }
   }
 
   return results
 }
 
-const buildPetUpdatePayload = (payloads: Payload[], matchField: ResponsysMatchField) => {
+const buildPetUpdatePayloads = (
+  payloads: Payload[],
+  matchField: ResponsysMatchField,
+  settings: Settings
+): ResponsysAudiencePetUpdateRequestBody[] => {
   const resolvedMatchType = (matchField + '_') as ResponsysMatchType
   const firstPayload = payloads[0]
   const records = payloads.map((payload) => {
@@ -127,21 +169,36 @@ const buildPetUpdatePayload = (payloads: Payload[], matchField: ResponsysMatchFi
     }
   }) as string[][]
 
-  const requestBody = {
-    recordData: {
-      fieldNames: [resolvedMatchType, firstPayload.computation_key],
-      records: records,
-      mapTemplateName: null
-    },
-    insertOnNoMatch: true,
-    updateOnMatch: 'REPLACE_ALL',
-    matchColumnName1: matchField
+  const requestBodies = []
+  // Per https://docs.oracle.com/en/cloud/saas/marketing/responsys-develop/API/REST/Async/asyncApi-v1.3-lists-listName-members-post.htm,
+  // we can only send 200 records at a time.
+  for (let i = 0; i < records.length; i += 200) {
+    const chunk = records.slice(i, i + 200)
+    const requestBody = {
+      recordData: {
+        // Per https://docs.oracle.com/en/cloud/saas/marketing/responsys-user/List_DataTypeFieldname.htm,
+        // the field name should have a maximum of 30 characters.
+        fieldNames: [resolvedMatchType, firstPayload.computation_key.substring(0, 30)],
+        records: chunk,
+        mapTemplateName: null
+      },
+      insertOnNoMatch: settings.insertOnNoMatch,
+      updateOnMatch: settings.updateOnMatch,
+      matchColumnName1: matchField
+    }
+
+    requestBodies.push(requestBody)
   }
 
-  return requestBody
+  return requestBodies
 }
 
-const updateProfileListMembers = async (request: RequestClient, settings: Settings, payloads: Payload[]) => {
+const updateProfileListMembers = async (
+  request: RequestClient,
+  authTokens: AuthTokens,
+  settings: Settings,
+  payloads: Payload[]
+) => {
   const fieldNames = ['EMAIL_ADDRESS_', 'CUSTOMER_ID_']
   const records: string[][] = []
 
@@ -150,56 +207,79 @@ const updateProfileListMembers = async (request: RequestClient, settings: Settin
     for (const fieldName of fieldNames) {
       const resolvedFieldName = fieldName as 'EMAIL_ADDRESS_' | 'CUSTOMER_ID_' | 'RIID_'
       if (payload.userData && payload.userData[resolvedFieldName]) {
-        if (payload.userData && payload.userData[resolvedFieldName]) {
-          const value = payload.userData[resolvedFieldName]
-          record.push(value || '')
-        }
+        const value = payload.userData[resolvedFieldName]
+        record.push(value || '')
       }
     }
 
     records.push(record)
   }
 
-  const recordData: ResponsysRecordData = {
-    fieldNames: fieldNames,
-    records: records,
-    mapTemplateName: ''
+  // Per https://docs.oracle.com/en/cloud/saas/marketing/responsys-develop/API/REST/Async/asyncApi-v1.3-lists-listName-members-post.htm,
+  // we can only send 200 records at a time.
+  for (let i = 0; i < records.length; i += 200) {
+    const chunk = records.slice(i, i + 200)
+    const recordData: ResponsysRecordData = {
+      fieldNames: fieldNames,
+      records: chunk,
+      mapTemplateName: ''
+    }
+
+    const mergeRule: ResponsysMergeRule = {
+      insertOnNoMatch: true,
+      updateOnMatch: 'REPLACE_ALL',
+      matchColumnName1: settings.matchColumnName1 + '_',
+      optinValue: settings.optinValue,
+      optoutValue: settings.optoutValue,
+      rejectRecordIfChannelEmpty: settings.rejectRecordIfChannelEmpty,
+      defaultPermissionStatus: payloads[0].default_permission_status || settings.defaultPermissionStatus
+    }
+
+    const requestBody: ResponsysListMemberRequestBody = {
+      recordData,
+      mergeRule
+    }
+
+    const path = `/rest/asyncApi/v1.3/lists/${settings.profileListName}/members`
+
+    const endpoint = new URL(path, settings.baseUrl)
+
+    const response: ModifiedResponse<ResponsysAsyncResponse> = await request(endpoint.href, {
+      method: 'POST',
+      body: JSON.stringify(requestBody)
+    })
+
+    const requestId = response.data.requestId
+    await new Promise((resolve) => setTimeout(resolve, getAsyncResponseWaitInterval))
+    const asyncResponse = await getAsyncResponse(requestId, authTokens, settings)
+    await sendDebugMessageToSegmentSource(request, requestBody, asyncResponse, settings)
   }
-
-  const mergeRule: ResponsysMergeRule = {
-    insertOnNoMatch: true,
-    updateOnMatch: 'REPLACE_ALL',
-    matchColumnName1: settings.matchColumnName1 + '_'
-  }
-
-  const requestBody: ResponsysListMemberRequestBody = {
-    recordData,
-    mergeRule
-  }
-
-  const path = `/rest/asyncApi/v1.3/lists/${settings.profileListName}/members`
-
-  const endpoint = new URL(path, settings.baseUrl)
-
-  const response = await request(endpoint.href, {
-    method: 'POST',
-    body: JSON.stringify(requestBody)
-  })
-
-  await sendDebugMessageToSegmentSource(request, requestBody, response, settings)
 }
 
 const sendPetUpdate = async (
   request: RequestClient,
+  authTokens: AuthTokens,
   settings: Settings,
   computationKey: string,
-  requestBody: ResponsysAudiencePetUpdateRequestBody
-) => {
-  const path = `/rest/api/v1.3/lists/${settings.profileListName}/listExtensions/${computationKey}/members`
+  requestBodies: ResponsysAudiencePetUpdateRequestBody[]
+): Promise<ModifiedResponse<unknown>[]> => {
+  const path = `/rest/asyncApi/v1.3/lists/${settings.profileListName}/listExtensions/${computationKey}/members`
   const endpoint = new URL(path, settings.baseUrl)
 
-  return request(endpoint.href, {
-    method: 'POST',
-    body: JSON.stringify(requestBody)
-  })
+  const responses = []
+  for (const requestBody of requestBodies) {
+    await new Promise((resolve) => setTimeout(resolve, sendToPetWaitInterval * 2))
+    const response: ModifiedResponse<ResponsysAsyncResponse> = await request(endpoint.href, {
+      method: 'POST',
+      body: JSON.stringify(requestBody)
+    })
+
+    const requestId = response.data.requestId
+    await new Promise((resolve) => setTimeout(resolve, getAsyncResponseWaitInterval))
+    const asyncResponse = await getAsyncResponse(requestId, authTokens, settings)
+    await sendDebugMessageToSegmentSource(request, requestBody, asyncResponse, settings)
+    responses.push(asyncResponse)
+  }
+
+  return responses
 }
--- packages/destination-actions/src/destinations/responsys/shared-properties.ts
+++ packages/destination-actions/src/destinations/responsys/shared-properties.ts
@@ -1,6 +1,74 @@
 import { InputField } from '@segment/actions-core/destination-kit/types'
 
-export const enable_batching: InputField = {
+export const folder_name: InputField = {
+  label: 'Folder Name',
+  description:
+    'The name of the folder where the new Profile Extension Table will be created. Overrides the default folder name in Settings.',
+  type: 'string',
+  required: false,
+  default: 'Segment'
+}
+
+export const recipient_data: InputField = {
+  label: 'Recipient Data',
+  description: 'Record data that represents field names and corresponding values for each profile.',
+  type: 'object',
+  defaultObjectUI: 'keyvalue',
+  required: true,
+  additionalProperties: false,
+  properties: {
+    EMAIL_ADDRESS_: {
+      label: 'Email address',
+      description: "The user's email address.",
+      type: 'string',
+      format: 'email',
+      required: false
+    },
+    CUSTOMER_ID_: {
+      label: 'Customer ID',
+      description: 'Responsys Customer ID.',
+      type: 'string',
+      required: false
+    },
+    RIID_: {
+      label: 'Recipient ID',
+      description:
+        'Recipient ID (RIID). RIID is required if Email Address and Customer ID are empty. Only use it if the corresponding profile already exists in Responsys.',
+      type: 'string',
+      required: false
+    }
+  },
+  default: {
+    EMAIL_ADDRESS_: {
+      '@if': {
+        exists: { '@path': '$.traits.email' },
+        then: { '@path': '$.traits.email' },
+        else: { '@path': '$.context.traits.email' }
+      }
+    },
+    CUSTOMER_ID_: { '@path': '$.userId' }
+  }
+}
+
+export const retry: InputField = {
+  label: 'Delay (seconds)',
+  description: `A delay of the selected seconds will be added before retrying a failed request.
+                Max delay allowed is 600 secs (10 mins). The default is 0 seconds.`,
+  type: 'number',
+  choices: [
+    { label: '0 secs', value: 0 },
+    { label: '30 secs', value: 30 },
+    { label: '120 secs', value: 120 },
+    { label: '300 secs', value: 300 },
+    { label: '480 secs', value: 480 },
+    { label: '600 secs', value: 600 }
+  ],
+  required: false,
+  unsafe_hidden: true,
+  default: 0
+}
+
+export const use_responsys_async_api: InputField = {
   label: 'Use Responsys Async API',
   description: 'Once enabled, Segment will collect events into batches of 200 before sending to Responsys.',
   type: 'boolean',
@@ -15,4 +83,16 @@ export const batch_size: InputField = {
   required: false,
   unsafe_hidden: true,
   default: 200
+}
+
+export const default_permission_status: InputField = {
+  label: 'Default Permission Status',
+  description:
+    'This value must be specified as either OPTIN or OPTOUT. It defaults to the value defined in this destination settings.',
+  type: 'string',
+  required: false,
+  choices: [
+    { label: 'Opt In', value: 'OPTIN' },
+    { label: 'Opt Out', value: 'OPTOUT' }
+  ]
 }
