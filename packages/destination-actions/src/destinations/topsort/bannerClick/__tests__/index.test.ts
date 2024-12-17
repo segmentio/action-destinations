4c54c0276b82163c8d40ebb56f1c1b8fcb2cd5e8
Agustín Espíldora Vial <42187995+agustinespildora@users.noreply.github.com>
2024-12-17 10:41:49 -0300

    feat: banners events (#19)

--- packages/destination-actions/src/destinations/topsort/bannerClick/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/topsort/bannerClick/__tests__/index.test.ts
@@ -5,7 +5,7 @@ import Destination from '../../index'
 
 const testDestination = createTestIntegration(Destination)
 
-describe('Topsort.click', () => {
+describe('Topsort.bannerClick', () => {
   it('should be successful with default mappings and resolvedBidId', async () => {
     nock(/.*/).persist().post(/.*/).reply(200)
 
@@ -15,7 +15,7 @@ describe('Topsort.click', () => {
       }
     })
 
-    const responses = await testDestination.testAction('click', {
+    const responses = await testDestination.testAction('bannerClick', {
       event,
       settings: {
         api_key: 'bar'
@@ -38,43 +38,13 @@ describe('Topsort.click', () => {
     })
   })
 
-  it('should be successful with additional attribution', async () => {
-    nock(/.*/).persist().post(/.*/).reply(200)
-
-    const event = createTestEvent({
-      properties: {
-        additionalAttribution: { id: '123', type: 'user' },
-        resolvedBidId: 'thisisaresolvedbidid'
-      }
-    })
-
-    const responses = await testDestination.testAction('click', {
-      event,
-      settings: {
-        api_key: 'bar'
-      },
-      useDefaultMappings: true
-    })
-
-    expect(responses.length).toBe(1)
-    expect(responses[0].status).toBe(200)
-    expect(responses[0].options.headers).toMatchSnapshot()
-    expect(responses[0].options.json).toMatchObject({
-      clicks: expect.arrayContaining([
-        expect.objectContaining({
-          additionalAttribution: { id: '123', type: 'user' }
-        })
-      ])
-    })
-  })
-
   it('should fail because it misses a required field (resolvedBidId)', async () => {
     nock(/.*/).persist().post(/.*/).reply(200)
 
     const event = createTestEvent({})
 
     await expect(
-      testDestination.testAction('click', {
+      testDestination.testAction('bannerClick', {
         event,
         settings: {
           api_key: 'bar'
