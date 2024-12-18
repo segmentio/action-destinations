7f313f40809ab382719c54f8405c08dc8d9edfb8
Harsh Joshi <harsh.joshi@segment.com>
2024-12-18 18:22:49 +0530

    refactor multistatus code

--- packages/destination-actions/src/destinations/salesforce-marketing-cloud/sfmc-operations.ts
+++ packages/destination-actions/src/destinations/salesforce-marketing-cloud/sfmc-operations.ts
@@ -9,31 +9,18 @@ import { Payload as payload_dataExtension } from './dataExtension/generated-type
 import { Payload as payload_contactDataExtension } from './contactDataExtension/generated-types'
 import { ErrorResponse } from './types'
 
-export async function upsertRows(
+export function upsertRows(
   request: RequestClient,
   subdomain: String,
   payloads: payload_dataExtension[] | payload_contactDataExtension[]
 ) {
-  const multiStatusResponse = new MultiStatusResponse()
-  let url: string
-  let response: ModifiedResponse | undefined
   const { key, id } = payloads[0]
   if (!key && !id) {
-    if (payloads.length === 1) {
-      throw new IntegrationError(
-        `In order to send an event to a data extension either Data Extension ID or Data Extension Key must be defined.`,
-        'Misconfigured required field',
-        400
-      )
-    }
-    payloads.forEach((_, index) => {
-      multiStatusResponse.setErrorResponseAtIndex(index, {
-        status: 400,
-        errortype: 'PAYLOAD_VALIDATION_FAILED',
-        errormessage: `In order to send an event to a data extension either Data Extension ID or Data Extension Key must be defined.`
-      })
-    })
-    return multiStatusResponse
+    throw new IntegrationError(
+      `In order to send an event to a data extension either Data Extension ID or Data Extension Key must be defined.`,
+      'Misconfigured required field',
+      400
+    )
   }
   const rows: Record<string, any>[] = []
   payloads.forEach((payload: payload_dataExtension | payload_contactDataExtension) => {
@@ -43,17 +30,27 @@ export async function upsertRows(
     })
   })
   if (key) {
-    url = `https://${subdomain}.rest.marketingcloudapis.com/hub/v1/dataevents/key:${key}/rowset`
-  } else {
-    url = `https://${subdomain}.rest.marketingcloudapis.com/hub/v1/dataevents/${id}/rowset`
-  }
-
-  try {
-    response = await request(url, {
+    return request(`https://${subdomain}.rest.marketingcloudapis.com/hub/v1/dataevents/key:${key}/rowset`, {
       method: 'POST',
       json: rows
     })
+  } else {
+    return request(`https://${subdomain}.rest.marketingcloudapis.com/hub/v1/dataevents/${id}/rowset`, {
+      method: 'POST',
+      json: rows
+    })
+  }
+}
 
+export async function handleMultistatusResponse(
+  request: RequestClient,
+  subdomain: String,
+  payloads: payload_dataExtension[] | payload_contactDataExtension[]
+): Promise<MultiStatusResponse> {
+  const multiStatusResponse = new MultiStatusResponse()
+  let response: ModifiedResponse | undefined
+  try {
+    response = await upsertRows(request, subdomain, payloads)
     payloads.forEach((payload, index) => {
       multiStatusResponse.setSuccessResponseAtIndex(index, {
         status: 200,
@@ -62,6 +59,15 @@ export async function upsertRows(
       })
     })
   } catch (error) {
+    if (error instanceof IntegrationError && error.code === 'Misconfigured required field') {
+      payloads.forEach((_, index) => {
+        multiStatusResponse.setErrorResponseAtIndex(index, {
+          status: 400,
+          errortype: 'PAYLOAD_VALIDATION_FAILED',
+          errormessage: `In order to send an event to a data extension either Data Extension ID or Data Extension Key must be defined.`
+        })
+      })
+    }
     const err = error as ErrorResponse
     if (err?.response?.data?.message === 'Not Authorized' || payloads.length === 1) {
       throw error
@@ -81,10 +87,5 @@ export async function upsertRows(
       })
     })
   }
-
-  if (payloads.length > 1) {
-    return multiStatusResponse
-  }
-
-  return response
+  return multiStatusResponse
 }
