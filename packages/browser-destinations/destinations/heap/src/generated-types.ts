01e524994c82cb8787f170f6df7922e41ef71dec
Corey Psoinos <coreypsoinos@gmail.com>
2025-01-20 07:06:15 -0500

    [HEAP] update browser destination code to load heap js v5 script (#2665)
    * feat(heap): update browser destination code to load heap js v5 script
    
    * remove try/catch fallback for self-hosted
    
    * update tests
    
    * fix tests
    
    * apply deprecated trackingServer config value for backward compatibility
    
    * remove non-functional script loading mock
    
    * better backward compatibility with hjsv4
    
    * assert heap.envId, not heap.appid

--- packages/browser-destinations/destinations/heap/src/generated-types.ts
+++ packages/browser-destinations/destinations/heap/src/generated-types.ts
@@ -6,19 +6,23 @@ export interface Settings {
    */
   appId: string
   /**
-   * Setting to true will redact all target text on your website. For more information visit the heap [docs page](https://developers.heap.io/docs/web#global-data-redaction-via-disabling-text-capture).
+   * Setting to true will redact all target text on your website. For more information visit the Heap [docs page](https://developers.heap.io/docs/web#global-data-redaction-via-disabling-text-capture).
    */
   disableTextCapture?: boolean
   /**
-   * This option is turned off by default to accommodate websites not served over HTTPS. If your application uses HTTPS, we recommend enabling secure cookies to prevent Heap cookies from being observed by unauthorized parties. For more information visit the heap [docs page](https://developers.heap.io/docs/web#securecookie).
+   * This option is turned off by default to accommodate websites not served over HTTPS. If your application uses HTTPS, we recommend enabling secure cookies to prevent Heap cookies from being observed by unauthorized parties. For more information visit the Heap [docs page](https://developers.heap.io/docs/web#securecookie).
    */
   secureCookie?: boolean
   /**
-   * This is an optional setting. This is used to set up first-party data collection. For most cased this should not be set. For more information visit the heap [docs page](https://developers.heap.io/docs/set-up-first-party-data-collection-in-heap).
+   * This is an optional setting. This is used to set up first-party data collection. For most cased this should not be set. For more information visit the Heap [docs page](https://developers.heap.io/docs/set-up-first-party-data-collection-in-heap). This field is deprecated in favor of `ingestServer`. If `trackingServer` is set and `ingestServer` is not set, then the Classic SDK will be loaded. If both are set, `ingestServer` will take precedence, and the latest stable version of the Heap SDK will be loaded.
    */
   trackingServer?: string
   /**
-   * This is an optional setting used to set the host that loads heap-js. This setting is used when heapJS is self-hosted. In most cased this should be left unset. The hostname should not contain https or app id it will be populated like so: https://${hostname}/js/heap-${appId}.js. For more information visit the heap [docs page](https://developers.heap.io/docs/self-hosting-heapjs).
+   * This is an optional setting. This is used to set up first-party data collection. For most cased this should not be set. For more information visit the Heap [docs page](https://developers.heap.io/docs/web#ingestserver).
+   */
+  ingestServer?: string
+  /**
+   * This is an optional setting used to set the host that loads the Heap SDK. This setting is used when the Heap SDK is self-hosted. In most cased this should be left unset. The hostname should not contain https or app id. When _both_ `hostname` and `trackingServer` are set, the Classic SDK will be loaded via: `https://${hostname}/js/heap-${appId}.js`. If `hostname` is set and `trackingServer` is not set, then the latest version of the Heap SDK will be loaded via: `https://${settings.hostname}/config/${settings.appId}/heap_config.js`. For more information visit the Heap [docs page](https://developers.heap.io/docs/self-hosting-heapjs).
    */
   hostname?: string
   /**
