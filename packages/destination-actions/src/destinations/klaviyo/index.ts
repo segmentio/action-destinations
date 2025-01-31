23727014637de89b3bd8de928e6863948148f811
Ankit Gupta <ankit.gupta@segment.com>
2025-01-31 10:39:52 +0530

    Updated defaultAudienceId for klaviyo

--- packages/destination-actions/src/destinations/klaviyo/index.ts
+++ packages/destination-actions/src/destinations/klaviyo/index.ts
@@ -107,14 +107,10 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
       }
     },
     async getAudience(request, getAudienceInput) {
-      let listId = getAudienceInput.externalId
+      const listId = getAudienceInput.externalId
       const apiKey = getAudienceInput.settings.api_key
 
       const defaultAudienceId = getAudienceInput.audienceSettings?.listId
-
-      if (defaultAudienceId) {
-        listId = defaultAudienceId
-      }
 
       const response = await request(`${API_URL}/lists/${listId}`, {
         method: 'GET',
@@ -126,6 +122,10 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
         const errorResponse = await response.json()
         const klaviyoErrorDetail = errorResponse.errors[0].detail
         throw new APIError(klaviyoErrorDetail, response.status)
+      }
+
+      if (defaultAudienceId) {
+        return { externalId: defaultAudienceId }
       }
 
       const r = await response.json()
