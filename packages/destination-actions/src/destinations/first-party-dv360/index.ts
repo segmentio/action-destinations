baecd704358afeb1b47cee2516b62038f0cfa60b
Rajul Vadera <rajul.vadera@segment.com>
2025-02-10 23:58:04 -0800

    merge DV360Updates

--- packages/destination-actions/src/destinations/first-party-dv360/index.ts
+++ packages/destination-actions/src/destinations/first-party-dv360/index.ts
@@ -1,15 +1,50 @@
-import { AudienceDestinationDefinition, defaultValues, IntegrationError } from '@segment/actions-core'
+import { AudienceDestinationDefinition, IntegrationError, PayloadValidationError } from '@segment/actions-core'
 import type { AudienceSettings, Settings } from './generated-types'
-import { createAudienceRequest, getAudienceRequest, getAuthSettings, getAuthToken } from './functions'
+import { createAudienceRequest, getAudienceRequest } from './functions'
 import removeFromAudContactInfo from './removeFromAudContactInfo'
 import removeFromAudMobileDeviceId from './removeFromAudMobileDeviceId'
 import addToAudContactInfo from './addToAudContactInfo'
 import addToAudMobileDeviceId from './addToAudMobileDeviceId'
+import { _CreateAudienceInput, _GetAudienceInput } from './types'
+
+export interface RefreshTokenResponse {
+  access_token: string
+  scope: string
+  expires_in: number
+  token_type: string
+}
 
 const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
   name: 'First Party Dv360',
   slug: 'actions-first-party-dv360',
   mode: 'cloud',
+  authentication: {
+    scheme: 'oauth2',
+    fields: {},
+    testAuthentication: async (_request) => {
+      return true
+    },
+    refreshAccessToken: async (request, { auth }) => {
+      const res = await request<RefreshTokenResponse>('https://www.googleapis.com/oauth2/v4/token', {
+        method: 'POST',
+        body: new URLSearchParams({
+          refresh_token: auth.refreshToken,
+          client_id: auth.clientId,
+          client_secret: auth.clientSecret,
+          grant_type: 'refresh_token'
+        })
+      })
+
+      return { accessToken: res.data.access_token }
+    }
+  },
+  extendRequest({ auth }) {
+    return {
+      headers: {
+        authorization: `Bearer ${auth?.accessToken}`
+      }
+    }
+  },
   audienceFields: {
     advertiserId: {
       type: 'string',
@@ -56,9 +91,10 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
       full_audience_sync: false
     },
 
-    createAudience: async (_request, createAudienceInput) => {
+    createAudience: async (_request, _CreateAudienceInput: _CreateAudienceInput) => {
       // Extract values from input
-      const { audienceName, audienceSettings, statsContext } = createAudienceInput
+      const { audienceName, audienceSettings, statsContext } = _CreateAudienceInput
+      const auth = _CreateAudienceInput.settings.oauth
       const advertiserId = audienceSettings?.advertiserId?.trim()
       const description = audienceSettings?.description
       const membershipDurationDays = audienceSettings?.membershipDurationDays
@@ -96,9 +132,25 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
         throw new IntegrationError('Missing audience type value', 'MISSING_REQUIRED_FIELD', 400)
       }
 
-      //Get access token
-      const authSettings = getAuthSettings()
-      const token = await getAuthToken(_request, authSettings)
+      if (
+        !auth?.refresh_token ||
+        !process.env.ACTIONS_FIRST_PARTY_DV360_CLIENT_ID ||
+        !process.env.ACTIONS_FIRST_PARTY_DV360_CLIENT_SECRET
+      ) {
+        throw new PayloadValidationError('Oauth credentials missing.')
+      }
+
+      const res = await _request<RefreshTokenResponse>('https://www.googleapis.com/oauth2/v4/token', {
+        method: 'POST',
+        body: new URLSearchParams({
+          refresh_token: auth.refresh_token,
+          client_id: process.env.ACTIONS_FIRST_PARTY_DV360_CLIENT_ID,
+          client_secret: process.env.ACTIONS_FIRST_PARTY_DV360_CLIENT_SECRET,
+          grant_type: 'refresh_token'
+        })
+      })
+
+      const token = res.data.access_token
 
       // Make API request to create the audience
       const response = await createAudienceRequest(_request, {
@@ -119,10 +171,11 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
       }
     },
 
-    getAudience: async (_request, getAudienceInput) => {
+    getAudience: async (_request, _GetAudienceInput: _GetAudienceInput) => {
       // Extract values from input
-      const { audienceSettings, statsContext } = getAudienceInput
-      const audienceId = getAudienceInput.externalId
+      const { audienceSettings, statsContext } = _GetAudienceInput
+      const auth = _GetAudienceInput.settings.oauth
+      const audienceId = _GetAudienceInput.externalId
       const advertiserId = audienceSettings?.advertiserId?.trim()
 
       // Update statistics tags and sends a call metric to Datadog. Ensures that datadog is infomred 'getAudience' operation was invoked
@@ -132,8 +185,25 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
       statsClient?.incr(`${statsName}.call`, 1, statsTags)
 
       //Get access token
-      const authSettings = getAuthSettings()
-      const token = await getAuthToken(_request, authSettings)
+      if (
+        !auth?.refresh_token ||
+        !process.env.ACTIONS_FIRST_PARTY_DV360_CLIENT_ID ||
+        !process.env.ACTIONS_FIRST_PARTY_DV360_CLIENT_SECRET
+      ) {
+        throw new PayloadValidationError('Oauth credentials missing.')
+      }
+
+      const res = await _request<RefreshTokenResponse>('https://www.googleapis.com/oauth2/v4/token', {
+        method: 'POST',
+        body: new URLSearchParams({
+          refresh_token: auth.refresh_token,
+          client_id: process.env.ACTIONS_FIRST_PARTY_DV360_CLIENT_ID,
+          client_secret: process.env.ACTIONS_FIRST_PARTY_DV360_CLIENT_SECRET,
+          grant_type: 'refresh_token'
+        })
+      })
+
+      const token = res.data.access_token
 
       if (!advertiserId) {
         statsTags?.push('error:missing-settings')
@@ -171,34 +241,7 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
     addToAudMobileDeviceId,
     removeFromAudContactInfo,
     removeFromAudMobileDeviceId
-  },
-  presets: [
-    {
-      name: 'Entities Audience Entered',
-      partnerAction: 'addToAudContactInfo',
-      mapping: {
-        ...defaultValues(addToAudContactInfo.fields),
-        properties: {
-          '@path': '$.properties'
-        }
-      },
-      type: 'specificEvent',
-      eventSlug: 'warehouse_audience_entered_track'
-    },
-
-    {
-      name: 'Entities Audience Exited',
-      partnerAction: 'removeFromAudContactInfo',
-      mapping: {
-        ...defaultValues(removeFromAudContactInfo.fields),
-        properties: {
-          '@path': '$.properties'
-        }
-      },
-      type: 'specificEvent',
-      eventSlug: 'warehouse_audience_exited_track'
-    }
-  ]
+  }
 }
 
 export default destination
