dd7569e9e5ab8406ebaaf623bc65f8e8d00cbe05
Nick Aguilar <nicholas.aguilar@segment.com>
2025-02-24 13:49:21 -0800

    Sets Fields to dynamic. Attempts to pull data extension ID from payload/mapping directly

--- packages/destination-actions/src/destinations/salesforce-marketing-cloud/contactDataExtension/index.ts
+++ packages/destination-actions/src/destinations/salesforce-marketing-cloud/contactDataExtension/index.ts
@@ -53,13 +53,13 @@ const action: ActionDefinition<Settings, Payload> = {
   dynamicFields: {
     keys: {
       __keys__: async (request, { settings, hookOutputs, payload }) => {
-        const dataExtensionId = hookOutputs?.onMappingSave?.outputs.id || payload.id || ''
+        const dataExtensionId = hookOutputs?.onMappingSave?.outputs.id || payload.onMappingSave.outputs.id || ''
         return await getDataExtensionFields(request, settings.subdomain, settings, dataExtensionId, true)
       }
     },
     values: {
       __keys__: async (request, { settings, hookOutputs, payload }) => {
-        const dataExtensionId = hookOutputs?.onMappingSave?.outputs.id || payload.id || ''
+        const dataExtensionId = hookOutputs?.onMappingSave?.outputs.id || payload.onMappingSave.outputs.id || ''
         return await getDataExtensionFields(request, settings.subdomain, settings, dataExtensionId, false)
       }
     }
