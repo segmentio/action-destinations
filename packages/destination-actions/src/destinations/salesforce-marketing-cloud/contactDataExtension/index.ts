781340bd80656d2c64bef4a341e2c932788f852e
Nick Aguilar <nicholas.aguilar@segment.com>
2025-02-24 14:51:02 -0800

    Removes unused and incorrect hook output reference

--- packages/destination-actions/src/destinations/salesforce-marketing-cloud/contactDataExtension/index.ts
+++ packages/destination-actions/src/destinations/salesforce-marketing-cloud/contactDataExtension/index.ts
@@ -52,16 +52,14 @@ const action: ActionDefinition<Settings, Payload> = {
   },
   dynamicFields: {
     keys: {
-      __keys__: async (request, { settings, hookOutputs, payload }) => {
-        const dataExtensionId =
-          hookOutputs?.onMappingSave?.outputs.id || (payload as any).onMappingSave.outputs.id || ''
+      __keys__: async (request, { settings, payload }) => {
+        const dataExtensionId = (payload as any).onMappingSave.outputs.id || ''
         return await getDataExtensionFields(request, settings.subdomain, settings, dataExtensionId, true)
       }
     },
     values: {
-      __keys__: async (request, { settings, hookOutputs, payload }) => {
-        const dataExtensionId =
-          hookOutputs?.onMappingSave?.outputs.id || (payload as any).onMappingSave.outputs.id || ''
+      __keys__: async (request, { settings, payload }) => {
+        const dataExtensionId = (payload as any).onMappingSave.outputs.id || ''
         return await getDataExtensionFields(request, settings.subdomain, settings, dataExtensionId, false)
       }
     }
