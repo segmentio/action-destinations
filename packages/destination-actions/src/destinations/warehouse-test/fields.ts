d7c88b52af0c180c890aec5ca21757901d7e3deb
Valerie Ernst <vernst@twilio.com>
2025-03-03 09:46:11 -0800

    Squashed commit of the following:
    commit 5f8707608047ff94400df49452ee06e95edfdec9
    Author: Valerie Ernst <vernst@twilio.com>
    Date:   Mon Mar 3 09:45:07 2025 -0800
    
        Add warehouseId to settings and file name
    
    commit 0f0de7b197e0242aed597be6e80c98a1ebb3a9b5
    Merge: 39d5ad425 f2ec6af91
    Author: Valerie Ernst <vernst@twilio.com>
    Date:   Mon Mar 3 09:04:59 2025 -0800
    
        Merge branch 'main' into warehouse-s3-destination
    
    commit 39d5ad4252db4705f73424ba4ad16855826012ac
    Merge: 13dc722dd 81731841b
    Author: Valerie Ernst <vernst@twilio.com>
    Date:   Thu Feb 20 11:42:06 2025 -0800
    
        Merge branch 'main' into warehouse-s3-destination
    
    commit 13dc722dd01920c35c27fa094913d57488a14444
    Author: Valerie Ernst <vernst@twilio.com>
    Date:   Thu Feb 20 11:38:12 2025 -0800
    
        remove unused param
    
    commit a8d08b1232e35554fd8dc321397fc349312892ac
    Author: Valerie Ernst <vernst@twilio.com>
    Date:   Wed Feb 19 16:51:22 2025 -0800
    
        Create temporary warehouse-test destination for poc of linked audience writeback

---
+++ packages/destination-actions/src/destinations/warehouse-test/fields.ts
@@ -0,0 +1,70 @@
+import { ActionDefinition } from '@segment/actions-core'
+import { Settings } from './generated-types'
+
+export const commonFields: ActionDefinition<Settings>['fields'] = {
+  columns: {
+    label: 'Columns',
+    description: `Column write to the DWH.`,
+    type: 'object',
+    defaultObjectUI: 'keyvalue',
+    required: true,
+    additionalProperties: true,
+    default: {
+      event: {
+        '@path': '$.event'
+      },
+      type: {
+        '@path': '$.type'
+      },
+      userId: {
+        '@path': '$.userId'
+      },
+      anonymousId: {
+        '@path': '$.anonymousId'
+      },
+      email: {
+        '@if': {
+          exists: { '@path': '$.traits.email' },
+          then: { '@path': '$.traits.email' },
+          else: { '@path': '$.context.traits.email' }
+        }
+      },
+      properties: {
+        '@path': '$.properties'
+      },
+      traits: {
+        '@if': {
+          exists: { '@path': '$.traits' },
+          then: { '@path': '$.traits' },
+          else: { '@path': '$.context.traits' }
+        }
+      },
+      context: {
+        '@path': '$.context'
+      },
+      timestamp: {
+        '@path': '$.timestamp'
+      },
+      messageId: {
+        '@path': '$.messageId'
+      },
+      audienceName: {
+        '@path': '$.context.personas.computation_key'
+      },
+      audienceId: {
+        '@path': '$.context.personas.computation_id'
+      },
+      spaceId: {
+        '@path': '$.context.personas.space_id'
+      },
+      entityContext: {
+        '@json': {
+          mode: 'encode',
+          value: {
+            '@path': '$.properties.data_graph_entity_context'
+          }
+        }
+      }
+    }
+  }
+}
---
+++ packages/destination-actions/src/destinations/warehouse-test/syncToWarehouse/generated-types.ts
@@ -0,0 +1,10 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * Column write to the DWH.
+   */
+  columns: {
+    [k: string]: unknown
+  }
+}
