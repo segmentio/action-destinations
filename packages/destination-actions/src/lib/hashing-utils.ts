4f145d68c02d23b158254509bd78d7558a9ad21d
Harsh Joshi <harsh.joshi@segment.com>
2025-03-16 18:58:41 +0530

    Implement Smart hashing for Snap Audience

--- packages/destination-actions/src/lib/hashing-utils.ts
+++ packages/destination-actions/src/lib/hashing-utils.ts
@@ -24,7 +24,11 @@ export const hashConfigs: {
   sha512: { lengthHex: 128, lengthBase64: 88 }
 }
 
-const slugsToBypassFeatureFlag = ['actions-facebook-custom-audiences', 'actions-linkedin-audiences']
+const slugsToBypassFeatureFlag = [
+  'actions-facebook-custom-audiences',
+  'actions-linkedin-audiences',
+  'actions-snap-audiences'
+]
 
 class SmartHashing {
   private preHashed: boolean
