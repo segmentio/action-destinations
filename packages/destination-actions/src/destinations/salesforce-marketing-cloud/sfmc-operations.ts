3c72e06551ef7c693f23bf8889b2a59071b85eb4
Nick Aguilar <nicholas.aguilar@segment.com>
2025-03-17 13:46:09 -0700

    Reverts SFMC data hook extension PR

--- packages/destination-actions/src/destinations/salesforce-marketing-cloud/sfmc-operations.ts
+++ packages/destination-actions/src/destinations/salesforce-marketing-cloud/sfmc-operations.ts
@@ -3,18 +3,11 @@ import {
   MultiStatusResponse,
   JSONLikeObject,
   ModifiedResponse,
-  IntegrationError,
-  ActionHookResponse,
-  DynamicFieldResponse,
-  DynamicFieldError,
-  DynamicFieldItem
+  IntegrationError
 } from '@segment/actions-core'
 import { Payload as payload_dataExtension } from './dataExtension/generated-types'
 import { Payload as payload_contactDataExtension } from './contactDataExtension/generated-types'
 import { ErrorResponse } from './types'
-import { OnMappingSaveInputs } from './dataExtension/generated-types'
-import { Settings } from './generated-types'
-import { xml2js } from 'xml-js'
 
 function generateRows(payloads: payload_dataExtension[] | payload_contactDataExtension[]): Record<string, any>[] {
   const rows: Record<string, any>[] = []
@@ -30,11 +23,10 @@ function generateRows(payloads: payload_dataExtension[] | payload_contactDataExt
 export function upsertRows(
   request: RequestClient,
   subdomain: String,
-  payloads: payload_dataExtension[] | payload_contactDataExtension[],
-  dataExtensionId?: string,
-  dataExtensionKey?: string
+  payloads: payload_dataExtension[] | payload_contactDataExtension[]
 ) {
-  if (!dataExtensionKey && !dataExtensionId) {
+  const { key, id } = payloads[0]
+  if (!key && !id) {
     throw new IntegrationError(
       `In order to send an event to a data extension either Data Extension ID or Data Extension Key must be defined.`,
       'Misconfigured required field',
@@ -42,16 +34,13 @@ export function upsertRows(
     )
   }
   const rows = generateRows(payloads)
-  if (dataExtensionKey) {
-    return request(
-      `https://${subdomain}.rest.marketingcloudapis.com/hub/v1/dataevents/key:${dataExtensionKey}/rowset`,
-      {
-        method: 'POST',
-        json: rows
-      }
-    )
+  if (key) {
+    return request(`https://${subdomain}.rest.marketingcloudapis.com/hub/v1/dataevents/key:${key}/rowset`, {
+      method: 'POST',
+      json: rows
+    })
   } else {
-    return request(`https://${subdomain}.rest.marketingcloudapis.com/hub/v1/dataevents/${dataExtensionId}/rowset`, {
+    return request(`https://${subdomain}.rest.marketingcloudapis.com/hub/v1/dataevents/${id}/rowset`, {
       method: 'POST',
       json: rows
     })
@@ -61,15 +50,13 @@ export function upsertRows(
 export async function executeUpsertWithMultiStatus(
   request: RequestClient,
   subdomain: String,
-  payloads: payload_dataExtension[] | payload_contactDataExtension[],
-  dataExtensionId?: string,
-  dataExtensionKey?: string
+  payloads: payload_dataExtension[] | payload_contactDataExtension[]
 ): Promise<MultiStatusResponse> {
   const multiStatusResponse = new MultiStatusResponse()
   let response: ModifiedResponse | undefined
   const rows = generateRows(payloads)
   try {
-    response = await upsertRows(request, subdomain, payloads, dataExtensionId, dataExtensionKey)
+    response = await upsertRows(request, subdomain, payloads)
     if (response) {
       const responseData = response.data as JSONLikeObject[]
       payloads.forEach((_, index) => {
@@ -126,472 +113,4 @@ export async function executeUpsertWithMultiStatus(
     })
   }
   return multiStatusResponse
-}
-
-interface DataExtensionCreationResponse {
-  data: {
-    id?: string
-    name?: string
-    key?: string
-    message?: string
-    errorcode?: number
-  }
-}
-
-interface DataExtensionSelectionResponse {
-  data: {
-    id?: string
-    name?: string
-    key?: string
-  }
-}
-
-interface DataExtensionSearchResponse {
-  count: number
-  items: {
-    id: string
-    name: string
-    key: string
-  }[]
-}
-
-interface DataExtensionFieldsResponse {
-  id: string
-  fields: {
-    name: string
-    type: string
-    isPrimaryKey: boolean
-  }[]
-}
-
-interface RefreshTokenResponse {
-  access_token: string
-  soap_instance_url: string
-}
-
-interface SoapResponseResult {
-  ID: {
-    _text: string
-  }
-  Name: {
-    _text: string
-  }
-  ContentType: {
-    _text: string
-  }
-}
-
-const getAccessToken = async (
-  request: RequestClient,
-  settings: Settings
-): Promise<{ accessToken: string; soapInstanceUrl: string }> => {
-  const baseUrl = `https://${settings.subdomain}.auth.marketingcloudapis.com/v2/token`
-  const res = await request<RefreshTokenResponse>(`${baseUrl}`, {
-    method: 'POST',
-    body: new URLSearchParams({
-      account_id: settings.account_id,
-      client_id: settings.client_id,
-      client_secret: settings.client_secret,
-      grant_type: 'client_credentials'
-    })
-  })
-
-  return { accessToken: res.data.access_token, soapInstanceUrl: res.data.soap_instance_url }
-}
-
-const dataExtensionRequest = async (
-  request: RequestClient,
-  hookInputs: OnMappingSaveInputs,
-  auth: { subdomain: string; accessToken: string }
-): Promise<{ id?: string; key?: string; error?: string }> => {
-  if (!hookInputs) {
-    return { id: '', key: '', error: 'No inputs provided' }
-  }
-
-  if (!hookInputs.columns) {
-    return { id: '', key: '', error: 'No columns provided' }
-  }
-
-  const fields = hookInputs.columns.map((column, i) => {
-    return {
-      name: column.name,
-      type: column.type,
-      isNullable: column.isNullable,
-      isPrimaryKey: column.isPrimaryKey,
-      length: column.length,
-      scale: column.scale,
-      description: column.description || '',
-      // these are required but we don't give the user an option
-      ordinal: i,
-      isTemplateField: false,
-      isHidden: false,
-      isReadOnly: false,
-      isInheritable: false,
-      isOverridable: false,
-      mustOverride: false
-    }
-  })
-
-  try {
-    const response = await request<DataExtensionCreationResponse>(
-      `https://${auth.subdomain}.rest.marketingcloudapis.com/data/v1/customobjects`,
-      {
-        method: 'POST',
-        json: {
-          name: hookInputs.name,
-          description: hookInputs.description,
-          categoryId: hookInputs.categoryId,
-          isSendable: hookInputs.isSendable,
-          sendableCustomObjectField: hookInputs.sendableCustomObjectField,
-          sendableSubscriberField: hookInputs.sendableSubscriberField,
-          fields
-        },
-        headers: {
-          authorization: `Bearer ${auth.accessToken}`
-        }
-      }
-    )
-
-    if (response.status !== 201) {
-      return { id: '', key: '', error: `Failed to create Data Extension` }
-    }
-
-    return {
-      id: (response as DataExtensionCreationResponse).data.id,
-      key: (response as DataExtensionCreationResponse).data.key
-    }
-  } catch (error) {
-    return { id: '', key: '', error: error.response.data.message }
-  }
-}
-
-async function createDataExtension(
-  request: RequestClient,
-  subdomain: string,
-  hookInputs: OnMappingSaveInputs,
-  settings: Settings
-): Promise<ActionHookResponse<{ id: string; name: string }>> {
-  if (!hookInputs) {
-    return {
-      error: { message: 'No inputs provided', code: 'ERROR' }
-    }
-  }
-
-  const { accessToken } = await getAccessToken(request, settings)
-
-  const { id, key, error } = await dataExtensionRequest(request, hookInputs, { subdomain, accessToken })
-
-  if (error || !id || !key) {
-    return {
-      error: { message: error || 'Unknown Error', code: 'ERROR' }
-    }
-  }
-
-  return {
-    successMessage: `Data Extension ${hookInputs.name} created successfully with External Key ${key}`,
-    savedData: {
-      id,
-      name: hookInputs.name!
-    }
-  }
-}
-
-const selectDataExtensionRequest = async (
-  request: RequestClient,
-  hookInputs: OnMappingSaveInputs,
-  auth: { subdomain: string; accessToken: string }
-): Promise<{ id?: string; name?: string; error?: string }> => {
-  if (!hookInputs) {
-    return { id: '', name: '', error: 'No inputs provided' }
-  }
-
-  if (!hookInputs.dataExtensionId) {
-    return { id: '', name: '', error: 'No Data Extension Id provided' }
-  }
-
-  try {
-    const response = await request<DataExtensionSelectionResponse>(
-      `https://${auth.subdomain}.rest.marketingcloudapis.com/data/v1/customobjects/${hookInputs.dataExtensionId}`,
-      {
-        method: 'GET',
-        headers: {
-          authorization: `Bearer ${auth.accessToken}`
-        }
-      }
-    )
-
-    if (response.status !== 200) {
-      return { id: '', name: '', error: `Failed to select Data Extension` }
-    }
-
-    return {
-      id: (response as DataExtensionSelectionResponse).data.id,
-      name: (response as DataExtensionSelectionResponse).data.name
-    }
-  } catch (err) {
-    return { id: '', name: '', error: err.response.data.message }
-  }
-}
-
-async function selectDataExtension(
-  request: RequestClient,
-  subdomain: string,
-  hookInputs: OnMappingSaveInputs,
-  settings: Settings
-): Promise<ActionHookResponse<{ id: string; name: string }>> {
-  if (!hookInputs) {
-    return {
-      error: { message: 'No inputs provided', code: 'ERROR' }
-    }
-  }
-
-  const { accessToken } = await getAccessToken(request, settings)
-
-  const { id, name, error } = await selectDataExtensionRequest(request, hookInputs, { subdomain, accessToken })
-
-  if (error || !id) {
-    return {
-      error: { message: error || 'Unknown Error', code: 'ERROR' }
-    }
-  }
-
-  return {
-    successMessage: `Data Extension ${name} selected successfully with External ID ${id}`,
-    savedData: {
-      id,
-      name: name!
-    }
-  }
-}
-
-export const selectOrCreateDataExtension = async (
-  request: RequestClient,
-  subdomain: string,
-  hookInputs: OnMappingSaveInputs,
-  settings: Settings
-): Promise<ActionHookResponse<{ id: string; name: string }>> => {
-  if (hookInputs.operation === 'create') {
-    return await createDataExtension(request, subdomain, hookInputs, settings)
-  } else if (hookInputs.operation === 'select') {
-    return await selectDataExtension(request, subdomain, hookInputs, settings)
-  }
-
-  return {
-    error: { message: 'Invalid operation', code: 'ERROR' }
-  }
-}
-
-const getDataExtensionsRequest = async (
-  request: RequestClient,
-  searchQuery: string,
-  auth: { subdomain: string; accessToken: string }
-): Promise<{ results?: DynamicFieldItem[]; error?: DynamicFieldError }> => {
-  try {
-    const response = await request<DataExtensionSearchResponse>(
-      `https://${auth.subdomain}.rest.marketingcloudapis.com/data/v1/customobjects`,
-      {
-        method: 'get',
-        searchParams: {
-          $search: searchQuery
-        },
-        headers: {
-          authorization: `Bearer ${auth.accessToken}`
-        }
-      }
-    )
-
-    if (response.status !== 200) {
-      return { error: { message: 'Failed to fetch Data Extensions', code: 'BAD_REQUEST' } }
-    }
-
-    const choices = response.data.items
-
-    return {
-      results: choices.map((choice) => {
-        return {
-          value: choice.id,
-          label: choice.name
-        }
-      })
-    }
-  } catch (err) {
-    return { error: { message: err.response.data.message, code: 'BAD_REQUEST' } }
-  }
-}
-
-export const getDataExtensions = async (
-  request: RequestClient,
-  subdomain: string,
-  settings: Settings,
-  query?: string
-): Promise<DynamicFieldResponse> => {
-  let searchQuery = '_'
-  if (query && query !== '') {
-    searchQuery = query
-  }
-
-  const { accessToken } = await getAccessToken(request, settings)
-
-  const { results, error } = await getDataExtensionsRequest(request, searchQuery, { subdomain, accessToken })
-
-  if (error) {
-    return {
-      choices: [],
-      error: error
-    }
-  }
-
-  if (!results || (Array.isArray(results) && results.length === 0)) {
-    return {
-      choices: [],
-      error: { message: 'No Data Extensions found', code: 'NOT_FOUND' }
-    }
-  }
-
-  return {
-    choices: results
-  }
-}
-
-const getDataExtensionFieldsRequest = async (
-  request: RequestClient,
-  dataExtensionId: string,
-  auth: { subdomain: string; accessToken: string },
-  primaryKey: boolean
-): Promise<{ results?: DynamicFieldItem[]; error?: DynamicFieldError }> => {
-  try {
-    const response = await request<DataExtensionFieldsResponse>(
-      `https://${auth.subdomain}.rest.marketingcloudapis.com/data/v1/customobjects/${dataExtensionId}/fields`,
-      {
-        method: 'GET',
-        headers: {
-          authorization: `Bearer ${auth.accessToken}`
-        }
-      }
-    )
-
-    if (response.status !== 200) {
-      return { error: { message: 'Failed to fetch Data Extension fields', code: 'BAD_REQUEST' } }
-    }
-
-    const filteredItems = response.data.fields.filter((field) => field.isPrimaryKey === primaryKey)
-
-    const choices = filteredItems.map((field) => {
-      return { value: field.name, label: field.name }
-    })
-
-    return { results: choices }
-  } catch (err) {
-    return { error: { message: err.response.data.message, code: 'BAD_REQUEST' } }
-  }
-}
-
-export const getDataExtensionFields = async (
-  request: RequestClient,
-  subdomain: string,
-  settings: Settings,
-  dataExtensionID: string,
-  primaryKey: boolean
-): Promise<DynamicFieldResponse> => {
-  if (!dataExtensionID) {
-    return { choices: [], error: { message: 'No Data Extension ID provided', code: 'BAD_REQUEST' } }
-  }
-  const { accessToken } = await getAccessToken(request, settings)
-
-  const { results, error } = await getDataExtensionFieldsRequest(
-    request,
-    dataExtensionID,
-    { subdomain, accessToken },
-    primaryKey
-  )
-
-  if (error) {
-    return {
-      choices: [],
-      error: error
-    }
-  }
-
-  if (!results || (Array.isArray(results) && results.length === 0)) {
-    return {
-      choices: [],
-      error: { message: 'No Data Extension fields found', code: 'NOT_FOUND' }
-    }
-  }
-
-  return {
-    choices: results
-  }
-}
-
-const getCategoriesRequest = async (
-  request: RequestClient,
-  auth: { soapInstanceUrl: string; accessToken: string }
-): Promise<{ results?: DynamicFieldItem[]; error?: DynamicFieldError }> => {
-  try {
-    const response = await request(`${auth.soapInstanceUrl}/Service.asmx`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/xml',
-        SOAPAction: 'Retrieve'
-      },
-      body: `<?xml version="1.0" encoding="UTF-8"?>
-        <SOAP-ENV:Envelope
-          xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-          xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-          <SOAP-ENV:Header>
-            <fueloauth xmlns="http://exacttarget.com">${auth.accessToken}</fueloauth>
-          </SOAP-ENV:Header>
-          <SOAP-ENV:Body>
-            <RetrieveRequestMsg
-              xmlns="http://exacttarget.com/wsdl/partnerAPI">
-                <RetrieveRequest>
-                  <ObjectType>DataFolder</ObjectType>
-                  <Properties>ID</Properties>
-                  <Properties>Name</Properties>
-                  <Properties>ContentType</Properties>
-                </RetrieveRequest>
-            </RetrieveRequestMsg>
-          </SOAP-ENV:Body>
-        </SOAP-ENV:Envelope>
-        `
-    })
-
-    const convert: any = xml2js(response.content, { compact: true })
-    const items = (convert['soap:Envelope']['soap:Body']['RetrieveResponseMsg']['Results'] as SoapResponseResult[]).map(
-      (item) => {
-        const type = item.ContentType._text
-
-        return {
-          label: item.Name._text,
-          value: item.ID._text,
-          description: `ContentType: ${type}`
-        }
-      }
-    )
-
-    return {
-      results: items
-    }
-  } catch (err) {
-    return { error: { message: err.response.data.message, code: 'BAD_REQUEST' } }
-  }
-}
-
-export const getCategories = async (request: RequestClient, settings: Settings): Promise<DynamicFieldResponse> => {
-  const { accessToken, soapInstanceUrl } = await getAccessToken(request, settings)
-
-  const { results, error } = await getCategoriesRequest(request, { soapInstanceUrl, accessToken })
-
-  if (error) {
-    return {
-      choices: [],
-      error: error
-    }
-  }
-
-  return {
-    choices: results || []
-  }
 }
