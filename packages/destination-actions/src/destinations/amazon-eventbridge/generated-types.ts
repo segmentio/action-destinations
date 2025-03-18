ce93a0584fea00d49ce9f5e275bd9b479cd8c1fc
mayur-pitale <109548891+mayur-pitale@users.noreply.github.com>
2025-03-18 04:30:49 -0700

    Eventbridge cosmetic changes (#2807)
    * Description changes
    
    * Description changes

--- packages/destination-actions/src/destinations/amazon-eventbridge/generated-types.ts
+++ packages/destination-actions/src/destinations/amazon-eventbridge/generated-types.ts
@@ -15,11 +15,9 @@ export interface Settings {
    */
   partnerEventSourceName: string
   /**
-   * USE WITH CAUTION: This will create the partner event source if it does not already exist.
-   *                       Use this option if you want to create the partner event source.
-   *                       projectId or context.protocols.sourceId will be used as the sourceId to
-   *                       create the partner event source.
-   *                       Use with caution.
+   * If enabled, Segment will check whether Partner Source identified by Segment source ID
+   *                       exists in EventBridge.
+   *                       If Partner Source does not exist, Segment will create a new Partner Source.
    */
   createPartnerEventSource?: boolean
 }
