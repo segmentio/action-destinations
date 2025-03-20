44d87e935388b8b7fbe1c1eae6909d7630b5423c
harsh-joshi99 <129737395+harsh-joshi99@users.noreply.github.com>
2025-03-20 10:49:56 +0530

    Implement Smart hashing for Snap Conversions API (#2799)

--- packages/destination-actions/src/lib/hashing-utils.ts
+++ packages/destination-actions/src/lib/hashing-utils.ts
@@ -27,6 +27,7 @@ export const hashConfigs: {
 const slugsToBypassFeatureFlag = [
   'actions-facebook-custom-audiences',
   'actions-linkedin-audiences',
+  'actions-snap-conversions',
   'actions-google-enhanced-conversions',
   'actions-google-campaign-manager-360'
 ]
