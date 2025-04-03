0ece93e8bf42d102d6d3685deb814feb33e89d05
Varadarajan V <109586712+varadarajan-tw@users.noreply.github.com>
2025-04-03 19:36:32 +0530

    Update uri format

--- packages/destination-actions/src/destinations/sendgrid/index.ts
+++ packages/destination-actions/src/destinations/sendgrid/index.ts
@@ -27,6 +27,7 @@ const destination: DestinationDefinition<Settings> = {
         description:
           'The regional processing endpoint for your SendGrid account. [See more details](https://www.twilio.com/en-us/blog/send-emails-in-eu?_gl=1*7hyri9*_gcl_au*MTg0MTQwMjAzNi4xNzQzMDAyNzc4*_ga*MTk4OTI2MDk1LjE3NDMwMDI3Nzg.*_ga_8W5LR442LD*MTc0MzY3NTc2NC41LjAuMTc0MzY3NTc2NC4wLjAuMA..)',
         required: false,
+        format: 'uri',
         default: GLOBAL_ENDPOINT,
         choices: [
           { label: `Global (${GLOBAL_ENDPOINT})`, value: GLOBAL_ENDPOINT },
