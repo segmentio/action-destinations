4d8898e275190bb17d858ff8fd0adc896c75d120
Monu Kumar <monkumar@segment.com>
2025-04-23 19:30:53 +0530

    Revert "Reapply "merge STRATCONN-5564_add_sales_objects""
    This reverts commit 71dcecff88a47260179fc6035dde32f17fa1817b.

--- packages/destination-actions/src/destinations/hubspot/upsertObject/constants.ts
+++ packages/destination-actions/src/destinations/hubspot/upsertObject/constants.ts
@@ -8,11 +8,7 @@ export const SUPPORTED_HUBSPOT_OBJECT_TYPES = [
   { label: 'Contact', value: 'contact' },
   { label: 'Company', value: 'company' },
   { label: 'Deal', value: 'deal' },
-  { label: 'Ticket', value: 'ticket' },
-  { label: 'Lead', value: 'lead' },
-  { label: 'Line Item', value: 'line_item' },
-  { label: 'Quote', value: 'quote' },
-  { label: 'Subscription', value: 'subscription' }
+  { label: 'Ticket', value: 'ticket' }
 ]
 
 export const MAX_HUBSPOT_BATCH_SIZE = 100
