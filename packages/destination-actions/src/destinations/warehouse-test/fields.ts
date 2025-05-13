046e55d9c6c53a95018353098734d09d56929941
Valerie Ernst <vernst@twilio.com>
2025-05-13 16:08:33 -0700

    Remove all mappings and just pass the event through

--- packages/destination-actions/src/destinations/warehouse-test/fields.ts
+++ packages/destination-actions/src/destinations/warehouse-test/fields.ts
@@ -9,62 +9,6 @@ export const commonFields: ActionDefinition<Settings>['fields'] = {
     defaultObjectUI: 'keyvalue',
     required: true,
     additionalProperties: true,
-    default: {
-      event: {
-        '@path': '$.event'
-      },
-      type: {
-        '@path': '$.type'
-      },
-      userId: {
-        '@path': '$.userId'
-      },
-      anonymousId: {
-        '@path': '$.anonymousId'
-      },
-      email: {
-        '@if': {
-          exists: { '@path': '$.traits.email' },
-          then: { '@path': '$.traits.email' },
-          else: { '@path': '$.context.traits.email' }
-        }
-      },
-      properties: {
-        '@path': '$.properties'
-      },
-      traits: {
-        '@if': {
-          exists: { '@path': '$.traits' },
-          then: { '@path': '$.traits' },
-          else: { '@path': '$.context.traits' }
-        }
-      },
-      context: {
-        '@path': '$.context'
-      },
-      timestamp: {
-        '@path': '$.timestamp'
-      },
-      messageId: {
-        '@path': '$.messageId'
-      },
-      audienceName: {
-        '@path': '$.context.personas.computation_key'
-      },
-      audienceId: {
-        '@path': '$.context.personas.computation_id'
-      },
-      spaceId: {
-        '@path': '$.context.personas.space_id'
-      },
-      entityContext: {
-        '@json': {
-          mode: 'encode',
-          value: {
-            '@path': '$.properties.data_graph_entity_context'
-          }
-        }
-      }
-    }
+    default: { '@path': '$.event' }
   }
 }
