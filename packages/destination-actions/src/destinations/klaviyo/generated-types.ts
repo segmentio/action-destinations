3199191a0e55c18efcde0b2ba1871213e5e4e0f4
Ankit Gupta <ankit.gupta@segment.com>
2025-05-20 15:07:09 +0530

    Changed klaviyo authentication method to oauth2

--- packages/destination-actions/src/destinations/klaviyo/generated-types.ts
+++ packages/destination-actions/src/destinations/klaviyo/generated-types.ts
@@ -4,7 +4,7 @@ export interface Settings {
   /**
    * You can find this by going to Klaviyo's UI and clicking Account > Settings > API Keys > Create API Key
    */
-  api_key: string
+  api_key?: string
 }
 // Generated file. DO NOT MODIFY IT BY HAND.
 
