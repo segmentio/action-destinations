6eca75677492c960b1d6db24d0789e5ea27922ec
Valerie Ernst <vernst@twilio.com>
2025-05-23 13:43:29 -0700

    Update fields for snowflake

--- packages/warehouse-destinations/src/destinations/snowflake/sendCustomEvent/index.ts
+++ packages/warehouse-destinations/src/destinations/snowflake/sendCustomEvent/index.ts
@@ -6,30 +6,54 @@ const action: ActionDefinition<Settings, Payload> = {
   title: 'Send Custom Event',
   description: 'Record custom events in Snowflake',
   fields: {
-    columns: {
+    timestamp: {
+      label: 'Timestamp',
+      description: 'Timestamp of the event',
+      type: 'string',
+      required: true,
+      default: { '@path': '$.timestamp' }
+    },
+    messageId: {
+      label: 'Message ID',
+      description: 'Name of column for the unique identifier for the message.',
+      type: 'string',
+      required: true,
+      default: { '@path': '$.messageId' },
+      readOnly: true
+    },
+    event: {
+      label: 'Table Name',
+      description: 'Name of the table. This will be the event name.',
+      type: 'string',
+      required: true,
+      default: { '@path': '$.event' },
+      readOnly: true
+    },
+    type: {
+      label: 'Event Type',
+      description: 'The type of event',
+      type: 'string',
+      required: true,
+      default: { '@path': '$.type' },
+      readOnly: true
+    },
+    computationId: {
+      label: 'Computation ID',
+      description:
+        'Audience or journey ID - hidden as we drop it in the warehouse pipeline, but need it for observability metrics.',
+      type: 'string',
+      required: true,
+      default: { '@path': '$.context.personas.computation_id' },
+      unsafe_hidden: true,
+      readOnly: true
+    },
+    properties: {
       label: 'Columns',
-      description: `Columns to write to Snowflake.`,
+      description: `Additional columns to write to Snowflake.`,
       type: 'object',
       defaultObjectUI: 'keyvalue',
       required: true,
       additionalProperties: true,
-      properties: {
-        timestamp: {
-          label: 'Timestamp',
-          description: 'Timestamp of the event',
-          type: 'string'
-        },
-        messageId: {
-          label: 'Message ID',
-          description: 'Name of column for the unique identifier for the message.',
-          type: 'string'
-        },
-        computationId: {
-          label: 'Computation ID',
-          description: 'Audience ID',
-          type: 'string'
-        }
-      },
       default: {
         entityContext: {
           '@json': {
