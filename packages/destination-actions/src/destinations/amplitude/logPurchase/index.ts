359bc90e02515bbb6f3077d3ccb77429e2280115
Varadarajan V <109586712+varadarajan-tw@users.noreply.github.com>
2025-06-05 20:47:00 +0530

    Revert "[MAIN][STRATCONN] Added prefix [Segment] in event_type of amplitude (…" (#2968)
    This reverts commit bb7f15c9fb6d66cb0841b62a27b84b47ef525841.

--- packages/destination-actions/src/destinations/amplitude/logPurchase/index.ts
+++ packages/destination-actions/src/destinations/amplitude/logPurchase/index.ts
@@ -255,10 +255,6 @@ const action: ActionDefinition<Settings, Payload> = {
       properties.session_id = formatSessionId(session_id)
     }
 
-    if (properties.event_type) {
-      properties.event_type = '[Segment] ' + properties.event_type
-    }
-
     if (Object.keys(payload.utm_properties ?? {}).length || payload.referrer) {
       properties.user_properties = mergeUserProperties(
         convertUTMProperties({ utm_properties }),
@@ -290,7 +286,7 @@ const action: ActionDefinition<Settings, Payload> = {
         // Or track revenue per product
         ...(trackRevenuePerProduct ? getRevenueProperties(product as EventRevenue) : {}),
         event_properties: product,
-        event_type: '[Segment] Product Purchased',
+        event_type: 'Product Purchased',
         insert_id: properties.insert_id ? `${properties.insert_id}-${events.length + 1}` : undefined,
         library: 'segment'
       })
