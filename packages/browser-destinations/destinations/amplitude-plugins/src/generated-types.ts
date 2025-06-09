d332b93ca72f11cbdf2b3318456e8075ac898aec
Ankit Gupta <ankit.gupta@segment.com>
2025-06-09 14:59:46 +0530

    updated generated types

--- packages/browser-destinations/destinations/amplitude-plugins/src/generated-types.ts
+++ packages/browser-destinations/destinations/amplitude-plugins/src/generated-types.ts
@@ -1,8 +1,3 @@
 // Generated file. DO NOT MODIFY IT BY HAND.
 
-export interface Settings {
-  /**
-   * Whether to log session start and end events. If enabled, a session start event will be logged when the user first visits the site, and a session end event will be logged when the user leaves the site.
-   */
-  log_session_start_and_end?: boolean
-}
+export interface Settings {}
