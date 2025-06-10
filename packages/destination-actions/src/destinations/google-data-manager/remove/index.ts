c530b242c2272ad3c0c82decd9d94327fe5bec65
Arjun Bhandage <abhandage@twilio.com>
2025-06-10 16:27:31 +0530

    STRATCONN-5870: Register google data manager

---
+++ packages/destination-actions/src/destinations/google-data-manager/remove/index.ts
@@ -0,0 +1,18 @@
+import type { ActionDefinition } from '@segment/actions-core'
+import type { Settings } from '../generated-types'
+import type { Payload } from './generated-types'
+
+const action: ActionDefinition<Settings, Payload> = {
+  title: 'Remove',
+  description: '',
+  fields: {},
+  perform: (_request, _data) => {
+    // Make your partner api request here!
+    // return request('https://example.com', {
+    //   method: 'post',
+    //   json: data.payload
+    // })
+  }
+}
+
+export default action
