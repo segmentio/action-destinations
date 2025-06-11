0b4632e70f6ad3d54bbd1f00a61182a2a8bedd7a
Joe Ayoub <joe.ayoub@segment.com>
2025-06-11 16:50:46 +0100

    setting cache time as an env variable

--- packages/destination-actions/src/destinations/kafka/constants.ts
+++ packages/destination-actions/src/destinations/kafka/constants.ts
@@ -1,3 +1,3 @@
-export const PRODUCER_TTL_MS = 0.5 * 60 * 1000 // 30 seconds
+export const PRODUCER_TTL_MS = Number(process.env.KAFKA_PRODUCER_TTL_MS) || 0.5 * 60 * 1000 // defaults to 30 seconds
 
 export const FLAGON_NAME = 'actions-kafka-optimize-connection'
