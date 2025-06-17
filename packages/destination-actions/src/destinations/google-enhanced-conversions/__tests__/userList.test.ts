d4d0c5d3bd2eaac956d81b1c0e556559edadb289
Ankit Gupta <ankit.gupta@segment.com>
2025-06-17 18:49:58 +0530

    Unit test cases updated

--- packages/destination-actions/src/destinations/google-enhanced-conversions/__tests__/userList.test.ts
+++ packages/destination-actions/src/destinations/google-enhanced-conversions/__tests__/userList.test.ts
@@ -526,6 +526,76 @@ describe('GoogleEnhancedConversions', () => {
           customerId
         },
         features: {
+          'google-enhanced-phone-validation-check': false
+        }
+      })
+
+      expect(responses.length).toEqual(3)
+      expect(responses[0].options.body).toMatchInlineSnapshot(
+        `"{\\"job\\":{\\"type\\":\\"CUSTOMER_MATCH_USER_LIST\\",\\"customerMatchUserListMetadata\\":{\\"userList\\":\\"customers/1234/userLists/1234\\",\\"consent\\":{\\"adUserData\\":\\"GRANTED\\",\\"adPersonalization\\":\\"GRANTED\\"}}}}"`
+      )
+      expect(responses[1].options.body).toMatchInlineSnapshot(
+        `"{\\"operations\\":[{\\"create\\":{\\"userIdentifiers\\":[{\\"hashedEmail\\":\\"87924606b4131a8aceeeae8868531fbb9712aaa07a5d3a756b26ce0f5d6ca674\\"},{\\"hashedPhoneNumber\\":\\"15a36d1cf8c33da64ac3c45039986f871a796d44645d6d8efe3c8413359620eb\\"},{\\"addressInfo\\":{\\"hashedFirstName\\":\\"4f23798d92708359b734a18172c9c864f1d48044a754115a0d4b843bca3a5332\\",\\"hashedLastName\\":\\"fd53ef835b15485572a6e82cf470dcb41fd218ae5751ab7531c956a2a6bcd3c7\\",\\"countryCode\\":\\"\\",\\"postalCode\\":\\"\\"}}]}}],\\"enable_warnings\\":true}"`
+      )
+    })
+
+    it('Does not format phone if country code already added and country code is in properties', async () => {
+      const event = createTestEvent({
+        timestamp,
+        event: 'Audience Entered',
+        properties: {
+          gclid: '54321',
+          email: 'test@gmail.com',
+          orderId: '1234',
+          phone: '+3234567890',
+          phone_country_code: '+1',
+          firstName: 'Jane',
+          lastName: 'Doe',
+          currency: 'USD',
+          value: '123',
+          address: {
+            street: '123 Street SW',
+            city: 'San Diego',
+            state: 'CA',
+            postalCode: '982004'
+          }
+        }
+      })
+
+      nock(`https://googleads.googleapis.com/${API_VERSION}/customers/${customerId}/offlineUserDataJobs:create`)
+        .post(/.*/)
+        .reply(200, { data: 'offlineDataJob' })
+
+      nock(`https://googleads.googleapis.com/${API_VERSION}/offlineDataJob:addOperations`)
+        .post(/.*/)
+        .reply(200, { data: 'offlineDataJob' })
+
+      nock(`https://googleads.googleapis.com/${API_VERSION}/offlineDataJob:run`)
+        .post(/.*/)
+        .reply(200, { data: 'offlineDataJob' })
+
+      const responses = await testDestination.testAction('userList', {
+        event,
+        mapping: {
+          ad_user_data_consent_state: 'GRANTED',
+          ad_personalization_consent_state: 'GRANTED',
+          external_audience_id: '1234',
+          phone_country_code: {
+            '@path': '$.properties.phone_country_code'
+          },
+          retlOnMappingSave: {
+            outputs: {
+              id: '1234',
+              name: 'Test List',
+              external_id_type: 'CONTACT_INFO'
+            }
+          }
+        },
+        useDefaultMappings: true,
+        settings: {
+          customerId
+        },
+        features: {
           'google-enhanced-phone-validation-check': true
         }
       })
