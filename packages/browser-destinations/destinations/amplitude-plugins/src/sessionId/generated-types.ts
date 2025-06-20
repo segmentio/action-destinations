1b66f3ad87ff2c7842abdd5ad57ac5cbb1278c5d
Ankit Gupta <ankit.gupta@segment.com>
2025-06-20 11:36:04 +0530

    Squashed commit of the following:
    commit a127bf7b1496f41a3e9e930365e9d0bf950f3305
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Fri Jun 20 11:32:51 2025 +0530
    
        Enabled session plugin for amplitude
    
    commit be186fc71f4644bb384b136dcd4d9a65f57564cb
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Thu Jun 12 09:41:37 2025 +0530
    
        New field to track session
    
    commit 09cca9c0be0c0267e8134ee134e8b7a430a397d0
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Mon Jun 9 14:51:18 2025 +0530
    
        Updated start and end session event in amplitude plugin
    
    commit b1fb991b26a644d733e491aec0921fb6d7a2abac
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Fri May 30 16:58:53 2025 +0530
    
        Added unit test cases
    
    commit 24d0df7669ff1eca3813c9cec274c7208ccad58a
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Fri May 30 11:37:30 2025 +0530
    
        Added session_start track event whenever new session starts

--- packages/browser-destinations/destinations/amplitude-plugins/src/sessionId/generated-types.ts
+++ packages/browser-destinations/destinations/amplitude-plugins/src/sessionId/generated-types.ts
@@ -5,4 +5,8 @@ export interface Payload {
    * Time in milliseconds to be used before considering a session stale.
    */
   sessionLength?: number
+  /**
+   * Generate session start and session end events. This is useful for tracking user sessions.
+   */
+  allowSessionTracking?: boolean
 }
