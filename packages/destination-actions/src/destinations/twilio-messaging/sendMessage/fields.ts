a21ed2d77ae0d10be193bb3354baead0de3a1ac9
Rajul Vadera <rajul.vadera@segment.com>
2025-06-20 14:14:43 -0400

    wip

--- packages/destination-actions/src/destinations/twilio-messaging/sendMessage/fields.ts
+++ packages/destination-actions/src/destinations/twilio-messaging/sendMessage/fields.ts
@@ -11,11 +11,8 @@ export const fields: Record<string, InputField> = {
       { label: 'SMS', value: CHANNELS.SMS },
       { label: 'MMS', value: CHANNELS.MMS },
       { label: 'WhatsApp', value: CHANNELS.WHATSAPP },
-      //{ label: 'Facebook Messenger', value: CHANNELS.MESSENGER },
       { label: 'RCS', value: CHANNELS.RCS }
-    ],
-    default: CHANNELS.MMS
-    //disabledInputMethods: ['literal', 'variable', 'function', 'enrichment']
+    ]
   },
   senderType: {
     label: 'Sender Type',
