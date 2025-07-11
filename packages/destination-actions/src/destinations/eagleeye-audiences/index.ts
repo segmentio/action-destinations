a4751783d3d4e70a74033d5ade498cc425f34152
Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
2025-07-11 12:15:42 +0100

    [EagleEye] - Converting to Audience Destination (#3068)
    * [EagleEye] - Converting to Audience Destination
    
    * renaming destination
    
    * renaming folder
    
    * unregistering old eagleeye destination

---
+++ packages/destination-actions/src/destinations/eagleeye-audiences/index.ts
@@ -0,0 +1,68 @@
+import { AudienceDestinationDefinition, IntegrationError } from '@segment/actions-core'
+import type { Settings, AudienceSettings } from './generated-types'
+
+import triggerBehavioralAction from './triggerBehavioralAction'
+
+const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
+  name: 'Eagle Eye',
+  slug: 'actions-eagleeye',
+  mode: 'cloud',
+  description:
+    'Synchronize Segment audiences to trigger behavioral actions in Eagle Eye AIR, enabling customer rewards.',
+  authentication: {
+    scheme: 'custom',
+    fields: {
+      connectorUrl: {
+        label: 'Connector URL',
+        description: 'Eagle Eye URL of the Segment connector provided by your EagleEye CSM',
+        type: 'string',
+        format: 'uri',
+        required: true
+      },
+      externalKey: {
+        label: 'Connector External Key',
+        description: 'Key to authenticate with the connector provided by your EagleEye CSM',
+        type: 'password',
+        required: true
+      }
+    }
+  },
+  audienceFields: {
+    behavioralActionTriggerReferences: {
+      label: 'Behavioral Action trigger reference',
+      type: 'string',
+      description:
+        'Required if connecting to an Engage Audience. Accepts a comma delimited list of reference strings for the Behavioral Action to be executed. E.g.: A0001,P0001',
+      required: false
+    }
+  },
+  audienceConfig: {
+    mode: {
+      type: 'synced',
+      full_audience_sync: false
+    },
+    async createAudience(_, createAudienceInput) {
+      const triggerRef = createAudienceInput?.audienceSettings?.behavioralActionTriggerReferences?.trim()
+
+      if (!triggerRef) {
+        throw new IntegrationError(
+          'Behavioral Action trigger references cannot be empty',
+          'Invalid Audience Settings',
+          400
+        )
+      }
+
+      return { externalId: triggerRef }
+    },
+    async getAudience(_, getAudienceInput) {
+      return {
+        externalId: getAudienceInput.externalId
+      }
+    }
+  },
+  actions: {
+    triggerBehavioralAction
+  }
+}
+
+export default destination
