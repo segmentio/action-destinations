0f403c068f8dce89c3a2674fe517df765a9ac35d
Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
2025-07-14 09:08:31 +0100

    [Singlestore] - Singlestore new API (#3044)
    * started migrating to Singlestore data API
    
    * adding more fields
    
    * changes from Partner

--- packages/destination-actions/src/destinations/singlestore/send/generated-types.ts
+++ packages/destination-actions/src/destinations/singlestore/send/generated-types.ts
@@ -2,13 +2,61 @@
 
 export interface Payload {
   /**
-   * The name of the SingleStore database to send data to.
+   * A unique identifier for the message.
    */
-  database: string
+  messageid: string
   /**
-   * The complete event payload.
+   * The timestamp of the event.
    */
-  message: {
+  timestamp: string
+  /**
+   * The type of the event (e.g., "track", "identify", "page", "screen", "group", "alias").
+   */
+  type: string
+  /**
+   * The name of the event. Only required for "track" events.
+   */
+  event?: string
+  /**
+   * The name of the page or screen.
+   */
+  name?: string
+  /**
+   * The properties of the track, page or screen event.
+   */
+  properties?: {
     [k: string]: unknown
   }
+  /**
+   * The user ID associated with the event.
+   */
+  userId?: string
+  /**
+   * The anonymous ID associated with the event.
+   */
+  anonymousId?: string
+  /**
+   * The group ID associated with the event.
+   */
+  groupId?: string
+  /**
+   * The traits of the user associated with the event.
+   */
+  traits?: {
+    [k: string]: unknown
+  }
+  /**
+   * The context of the event. Contains user environment information.
+   */
+  context?: {
+    [k: string]: unknown
+  }
+  /**
+   * The maximum number of rows to include in a batch.
+   */
+  max_batch_size: number
+  /**
+   * Batch events to SingleStore
+   */
+  enable_batching?: boolean
 }
