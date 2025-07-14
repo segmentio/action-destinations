4b7b2c7d01dcc0691b63fac7654166c66ec124ac
Monu Kumar <monkumar@segment.com>
2025-07-14 18:01:02 +0530

    deleted eagleeye

--- packages/destination-actions/src/destinations/index.ts
+++ packages/destination-actions/src/destinations/index.ts
@@ -195,7 +195,6 @@ register('674f2453916dadbd36d899dc', './attentive')
 register('674f23ece330374dc1ecc874', './twilio-messaging')
 register('67be4b2aef865ee6e0484fe5', './amazon-eventbridge')
 register('67e285767bbb94fc090bf3c7', './twilio-messaging-omnichannel')
-register('682db61f6c600fdb90251392', './eagleeye')
 register('682db6914f35aafb2757ef24', './dub')
 register('682db7017819e7e055f55cb4', './ortto')
 register('68370e58cb330586908573b7', './dotdigital')
@@ -205,7 +204,6 @@ register('68516bd8ca73bd53f38a0104', './roadwayai')
 register('68516c107fc235f3572f8a64', './posthog')
 register('6863e71f2a1e1ddc4b4612bf', './nudge')
 register('686799c65fcebef2a98c8141', './ortto-audiences')
-
 
 function register(id: MetadataId, destinationPath: string) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
