748bfb24ff3b561ce4f91362ad808ab7cc7c17d5
nikumar <nikumar@twilio.com>
2025-07-24 13:40:02 +0530

    add enable-batch and batch-size

---
+++ packages/destination-actions/src/destinations/customerio/customerio-properties.ts
@@ -0,0 +1,19 @@
+import { InputField } from '@segment/actions-core'
+
+export const eventProperties: Record<string, InputField> = {
+  enable_batching: {
+    type: 'boolean',
+    label: 'Batch Data to Customer.io',
+    description: 'Set as true to ensure Segment sends data to Mixpanel in batches.',
+    default: true
+  },
+  batch_size: {
+    label: 'Batch Size',
+    description: 'Maximum number of events to include in each batch. Actual batch sizes may be lower.',
+    type: 'number',
+    required: false,
+    minimum: 2,
+    maximum: 15,
+    default: 10
+  }
+}
