1423f647951675f3c4b75396d46a35429c549f5f
nikumar <nikumar@twilio.com>
2025-08-04 11:05:22 +0530

    Publish Canary

--- packages/destination-actions/src/destinations/customerio/createUpdateObject/generated-types.ts
+++ packages/destination-actions/src/destinations/customerio/createUpdateObject/generated-types.ts
@@ -37,4 +37,12 @@ export interface Payload {
    * Convert dates to Unix timestamps (seconds since Epoch).
    */
   convert_timestamp?: boolean
+  /**
+   * Set as true to ensure Segment sends data to Customer.io in batches.
+   */
+  enable_batching?: boolean
+  /**
+   * Maximum number of events to include in each batch. Actual batch sizes may be lower.
+   */
+  batch_size?: number
 }
