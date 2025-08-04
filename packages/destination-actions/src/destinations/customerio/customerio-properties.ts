35f8ff383393dc8da7742aa7b9ca30316db28783
nikumar <nikumar@twilio.com>
2025-08-04 10:27:57 +0530

    change label

--- packages/destination-actions/src/destinations/customerio/customerio-properties.ts
+++ packages/destination-actions/src/destinations/customerio/customerio-properties.ts
@@ -3,7 +3,7 @@ import { InputField } from '@segment/actions-core'
 export const eventProperties: Record<string, InputField> = {
   enable_batching: {
     type: 'boolean',
-    label: 'Batch Data to Customer.io',
+    label: 'Enable Batching',
     description: 'Set as true to ensure Segment sends data to Customer.io in batches.',
     default: true
   },
