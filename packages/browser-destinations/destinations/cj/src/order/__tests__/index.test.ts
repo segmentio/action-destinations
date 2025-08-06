746a1e689e754a7f4ea52929bb138898eeab4de1
Joe Ayoub <joe.ayoub@segment.com>
2025-08-06 17:25:19 +0100

    test with prehashed email

--- packages/browser-destinations/destinations/cj/src/order/__tests__/index.test.ts
+++ packages/browser-destinations/destinations/cj/src/order/__tests__/index.test.ts
@@ -154,6 +154,125 @@ describe('CJ init', () => {
     expect(mockCJ.order).toBe(undefined)
   })
 
+  test('CJ pixel order event with pre-hashed email', async () => {
+    const subscriptions: Subscription[] = [
+      {
+        partnerAction: 'order',
+        name: 'order',
+        enabled: true,
+        subscribe: 'type = "track" and event = "Order Completed"',
+        mapping: {
+          userId: { '@path': '$.userId' },
+          enterpriseId: 999999,
+          pageType: 'conversionConfirmation',
+          emailHash: {
+            '@if': {
+              exists: { '@path': '$.context.traits.email' },
+              then: { '@path': '$.context.traits.email' },
+              else: { '@path': '$.properties.email' }
+            }
+          },
+          orderId: { '@path': '$.properties.order_id' },
+          currency: { '@path': '$.properties.currency' },
+          amount: { '@path': '$.properties.total' },
+          discount:  { '@path': '$.properties.discount' },
+          coupon: { '@path': '$.properties.coupon' },
+          cjeventOrderCookieName: testCookieName,
+          items: {
+            '@arrayPath': [
+              '$.properties.products',
+              {
+                itemPrice: { '@path': '$.price' },
+                itemId: { '@path': '$.id' },
+                quantity: { '@path': '$.quantity' },
+                discount: { '@path': '$.discount' }
+              }
+            ]
+          }
+        }
+      }
+    ]
+    const context = new Context({
+      type: 'track',
+      event: 'Order Completed',
+      userId: 'userId-abc123',
+      context: {
+        traits: {
+          email: 'f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a'
+        }
+      },
+      properties: {
+        order_id: 'abc12345',
+        currency: 'USD',
+        coupon: 'COUPON1',
+        quantity: 5,
+        total: 10.99,
+        discount: 1,
+        products: [
+          {
+            id: '123',
+            quantity: 1,
+            price: 1,
+            discount: 0.5
+          },
+          {
+            id: '456',
+            quantity: 2,
+            price: 2,
+            discount: 0
+          }
+        ]
+      }
+    })
+    const [event] = await CJDestination({
+      ...settings,
+      subscriptions
+    })
+
+    const orderJSON = {
+        trackingSource: 'Segment',
+        userId: 'userId-abc123',
+        enterpriseId: 999999,
+        pageType: 'conversionConfirmation',
+        emailHash: 'f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a',
+        orderId: 'abc12345',
+        actionTrackerId: "987654321",
+        currency: 'USD',
+        amount: 10.99,
+        discount: 1,
+        coupon: 'COUPON1',
+        cjeventOrder: 'testcCookieValue',
+        items:[
+          {
+            itemId: '123',
+            quantity: 1,
+            itemPrice: 1,
+            discount: 0.5
+          },
+          {
+            itemId: '456',
+            quantity: 2,
+            itemPrice: 2,
+            discount: 0
+          }
+        ]
+    }
+
+    orderEvent = event
+    const sendSpy = jest.spyOn(sendModule, 'send').mockResolvedValue(undefined)
+    await orderEvent.load(Context.system(), {} as Analytics)
+    await orderEvent.track?.(context)
+    expect(destination.initialize).toHaveBeenCalled()
+    expect(orderModule.setOrderJSON).toHaveBeenCalled()
+    expect(orderModule.setOrderJSON).toHaveBeenCalledWith(
+      {}, 
+      orderJSON
+    )
+    expect(sendSpy).toHaveBeenCalledWith('123456789') 
+    expect(mockCJ.sitePage).toBe(undefined)
+    expect(mockCJ.order).toBe(undefined)
+  })
+
   test('CJ pixel order event with All Verticals field data', async () => {
     const subscriptions: Subscription[] = [
       {
