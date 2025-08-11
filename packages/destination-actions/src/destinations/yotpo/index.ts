f6deb1affe04dff5fd2d75a79cbbe38a04ca17cf
Arjun Bhandage <abhandage@twilio.com>
2025-08-11 16:16:13 +0530

    Upgrade first-party-dv360 to vv4

--- packages/destination-actions/src/destinations/yotpo/index.ts
+++ packages/destination-actions/src/destinations/yotpo/index.ts
@@ -1,29 +1,11 @@
-```diff
---- OldCode.ts
-+++ NewCode.ts
-@@
--    testAuthentication: (request, data) => {
--      return request(`https://developers.yotpo.com/v2/${data.settings.store_id}/info`, {
-+    testAuthentication: (request, data) => {
-+      return request(`https://developers.yotpo.com/v23/${data.settings.store_id}/info`, {
-@@
--    refreshAccessToken: async (request, data) => {
--      const promise = await request<AccessTokenResponse>(`https://developers.yotpo.com/v2/oauth/token`, {
-+    refreshAccessToken: async (request, data) => {
-+      const promise = await request<AccessTokenResponse>(`https://developers.yotpo.com/v23/oauth/token`, {
-@@
--  extendRequest({ auth }) {
--    return {
--      headers: {
--        'X-Yotpo-Token': `${auth?.accessToken}`
--      }
--    }
--  },
-+  extendRequest({ auth }) {
-+    return {
-+      headers: {
-+        'X-Yotpo-Token': `${auth?.accessToken}`
-+      }
-+    }
-+  },
-```
+    testAuthentication: (request, data) => {
+      return request(`https://developers.yotpo.com/v4/${data.settings.store_id}/info`, {
+    refreshAccessToken: async (request, data) => {
+      const promise = await request<AccessTokenResponse>(`https://developers.yotpo.com/v4/oauth/token`, {
+  extendRequest({ auth }) {
+    return {
+      headers: {
+        'X-Yotpo-Token': `${auth?.accessToken}`
+      }
+    }
+  },
