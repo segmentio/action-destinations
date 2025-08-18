1ca0c2d648d703759603626145673e7f6809661c
Ankit Gupta <ankit.gupta@segment.com>
2025-08-18 14:41:23 +0530

    Squashed commit of the following:
    commit 90e0e8389442a2faced6f9f61378680656830414
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Thu Aug 7 14:54:34 2025 +0530
    
        Updated windows analytics
    
    commit 6d56ae799422e40635f908185d88dbd8c7fce5cf
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Wed Jul 9 10:27:31 2025 +0530
    
        Updated unit test cases
    
    commit f35b1156c18661bcbe487069f379084b0c64c0e2
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Mon Jul 7 15:09:31 2025 +0530
    
        Update end session case
    
    commit 67ca4876b6d67907a21dd055a74000b75e863681
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Thu Jul 3 17:18:15 2025 +0530
    
        Updated value and depends on fields added
    
    commit a127bf7b1496f41a3e9e930365e9d0bf950f3305
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Fri Jun 20 11:32:51 2025 +0530
    
        Enabled session plugin for amplitude
    
    commit be186fc71f4644bb384b136dcd4d9a65f57564cb
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Thu Jun 12 09:41:37 2025 +0530
    
        New field to track session
    
    commit 09cca9c0be0c0267e8134ee134e8b7a430a397d0
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Mon Jun 9 14:51:18 2025 +0530
    
        Updated start and end session event in amplitude plugin
    
    commit b1fb991b26a644d733e491aec0921fb6d7a2abac
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Fri May 30 16:58:53 2025 +0530
    
        Added unit test cases
    
    commit 24d0df7669ff1eca3813c9cec274c7208ccad58a
    Author: Ankit Gupta <ankit.gupta@segment.com>
    Date:   Fri May 30 11:37:30 2025 +0530
    
        Added session_start track event whenever new session starts

--- packages/browser-destinations/destinations/amplitude-plugins/src/sessionId/generated-types.ts
+++ packages/browser-destinations/destinations/amplitude-plugins/src/sessionId/generated-types.ts
@@ -5,4 +5,16 @@ export interface Payload {
    * Time in milliseconds to be used before considering a session stale.
    */
   sessionLength?: number
+  /**
+   * Generate session start and session end events. This is useful for tracking user sessions.
+   */
+  allowSessionTracking?: boolean
+  /**
+   * The event name to use for the session start event.
+   */
+  sessionStartEvent?: string
+  /**
+   * The event name to use for the session end event.
+   */
+  sessionEndEvent?: string
 }
--- packages/browser-destinations/destinations/amplitude-plugins/src/sessionId/index.ts
+++ packages/browser-destinations/destinations/amplitude-plugins/src/sessionId/index.ts
@@ -3,16 +3,37 @@ import { UniversalStorage } from '@segment/analytics-next'
 import type { BrowserActionDefinition } from '@segment/browser-destination-runtime/types'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
+import { Analytics } from '@segment/analytics-next'
 
 function newSessionId(): number {
   return now()
 }
 
-function now(): number {
-  return new Date().getTime()
+function startSession(analytics: Analytics, eventName = 'session_started') {
+  analytics
+    .track(eventName, {})
+    .then(() => true)
+    .catch(() => true)
+}
+
+function endSession(analytics: Analytics, eventName = 'session_ended') {
+  analytics
+    .track(eventName, {})
+    .then(() => true)
+    .catch(() => true)
 }
 
 const THIRTY_MINUTES = 30 * 60000
+
+function withinSessionLimit(newTimeStamp: number, updated: number | null, length: number = THIRTY_MINUTES): boolean {
+  // This checks if the new timestamp is within the specified length of the last updated timestamp
+  const deltaTime = newTimeStamp - (updated ?? 0)
+  return deltaTime < length
+}
+
+function now(): number {
+  return new Date().getTime()
+}
 
 function stale(id: number | null, updated: number | null, length: number = THIRTY_MINUTES): id is null {
   if (id === null || updated === null) {
@@ -32,7 +53,6 @@ const action: BrowserActionDefinition<Settings, {}, Payload> = {
   title: 'Session Plugin',
   description: 'Generates a Session ID and attaches it to every Amplitude browser based event.',
   platform: 'web',
-  hidden: true,
   defaultSubscription: 'type = "track" or type = "identify" or type = "group" or type = "page" or type = "alias"',
   fields: {
     sessionLength: {
@@ -40,6 +60,46 @@ const action: BrowserActionDefinition<Settings, {}, Payload> = {
       type: 'number',
       required: false,
       description: 'Time in milliseconds to be used before considering a session stale.'
+    },
+    allowSessionTracking: {
+      label: 'Allow Session Tracking',
+      type: 'boolean',
+      default: false,
+      required: false,
+      description:
+        'Generate session start and session end events. This is useful for tracking user sessions. NOTE: This will generate a Segment track() event which will also get send to all Destinations connected to the JS Source'
+    },
+    sessionStartEvent: {
+      label: 'Session Start Event',
+      type: 'string',
+      default: 'session_started',
+      required: false,
+      description: 'The event name to use for the session start event.',
+      depends_on: {
+        conditions: [
+          {
+            fieldKey: 'allowSessionTracking',
+            operator: 'is',
+            value: true
+          }
+        ]
+      }
+    },
+    sessionEndEvent: {
+      label: 'Session End Event',
+      type: 'string',
+      default: 'session_ended',
+      required: false,
+      description: 'The event name to use for the session end event.',
+      depends_on: {
+        conditions: [
+          {
+            fieldKey: 'allowSessionTracking',
+            operator: 'is',
+            value: true
+          }
+        ]
+      }
     }
   },
   lifecycleHook: 'enrichment',
@@ -64,10 +124,17 @@ const action: BrowserActionDefinition<Settings, {}, Payload> = {
     const raw = storage.get('analytics_session_id')
     const updated = storage.get('analytics_session_id.last_access')
 
+    const withInSessionLimit = withinSessionLimit(newSession, updated, payload.sessionLength)
+    if (!withInSessionLimit && payload.allowSessionTracking) {
+      // end previous session
+      endSession(analytics, payload.sessionEndEvent)
+    }
+
     let id: number | null = raw
     if (stale(raw, updated, payload.sessionLength)) {
       id = newSession
       storage.set('analytics_session_id', id)
+      if (payload.allowSessionTracking) startSession(analytics, payload.sessionStartEvent)
     } else {
       // we are storing the session id regardless, so it gets synced between different storage mediums
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- id can't be null because of stale check
