f7bdf859fcefea2ae8b9d49c7f4daf22db8f6945
Joe Ayoub <joe.ayoub@segment.com>
2025-08-25 16:08:30 +0100

    updating types

--- packages/destination-actions/src/destinations/dynamic-yield-audiences/generated-types.ts
+++ packages/destination-actions/src/destinations/dynamic-yield-audiences/generated-types.ts
@@ -18,11 +18,11 @@ export interface AudienceSettings {
    */
   audience_name: string
   /**
-   * The type of Identifier to send to Dynamic Yield by Mastercard. E.g. `email`, `anonymousId`, `userId` or any other custom identifier. Make sure to configure the identifier in the `Customized Setup` below so that it is sent to Dynamic Yield by Mastercard.
+   * The Segment identifier to send to Dynamic Yield by Mastercard. E.g. `email`, `anonymousId`, `userId` or any other custom identifier. Make sure to configure the identifier in the `Customized Setup` below so that it is sent to Dynamic Yield by Mastercard.
    */
   identifier_type: string
   /**
-   * The name of the identifier in Dynamic Yield. You can leave this field empty if the Segment identifier name is the same as the Dynamic Yield identifier name.
+   * The name of the identifier in Dynamic Yield by Mastercard. You can leave this field empty if the Segment identifier name is the same as the Dynamic Yield by Mastercard identifier name.
    */
-  dy_identifier_type: string
+  dy_identifier_type?: string
 }
