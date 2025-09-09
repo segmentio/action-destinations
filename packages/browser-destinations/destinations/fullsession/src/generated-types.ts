2744b354ca15267b5b3ba309949e5c69f1580d6a
Joe Ayoub <joe.ayoub@segment.com>
2025-09-09 11:18:44 +0100

    removing unregistered destination from stage

--- packages/browser-destinations/destinations/fullsession/src/generated-types.ts
+++
@@ -1,8 +0,0 @@
-// Generated file. DO NOT MODIFY IT BY HAND.
-
-export interface Settings {
-  /**
-   * Your FullSession Customer ID. You can find this in your FullSession dashboard under Settings .
-   */
-  customerId: string
-}
