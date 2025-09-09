7ac2e2cce5ec80b2076728871e9376434bc469de
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2025-09-09 18:20:50 +0530

    Add lru cache

--- packages/destination-actions/src/destinations/kafka/constants.ts
+++ packages/destination-actions/src/destinations/kafka/constants.ts
@@ -3,3 +3,5 @@ export const PRODUCER_TTL_MS = Number(process.env.KAFKA_PRODUCER_TTL_MS) || 0.5 
 export const PRODUCER_REQUEST_TIMEOUT_MS = Number(process.env.KAFKA_PRODUCER_REQUEST_TIMEOUT_MS) || 10 * 1000 // defaults to 10 seconds
 
 export const FLAGON_NAME = 'actions-kafka-optimize-connection'
+
+export const CONNECTIONS_CACHE_SIZE = 500
