4280cfc2b8d2aa1659c7de90f56646ec853b5e75
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2025-09-11 19:34:13 +0530

    Add stats to track kafkajserrors (#3267)
    * Add stats to track kafkajserrors
    
    * Add stats to track kafkajserrors
    
    * add logs
    
    * Add stats to track kafkajserrors

--- packages/destination-actions/src/destinations/kafka/utils.ts
+++ packages/destination-actions/src/destinations/kafka/utils.ts
@@ -4,7 +4,7 @@ import type { Settings } from './generated-types'
 import type { Payload } from './send/generated-types'
 import { DEFAULT_PARTITIONER, Message, TopicMessages, SSLConfig, CachedProducer } from './types'
 import { PRODUCER_REQUEST_TIMEOUT_MS, PRODUCER_TTL_MS, FLAGON_NAME } from './constants'
-import { StatsContext } from '@segment/actions-core/destination-kit'
+import { Logger, StatsContext } from '@segment/actions-core/destination-kit'
 
 export const producersByConfig: Record<string, CachedProducer> = {}
 
@@ -181,7 +181,8 @@ export const sendData = async (
   settings: Settings,
   payload: Payload[],
   features: Features | undefined,
-  statsContext: StatsContext | undefined
+  statsContext: StatsContext | undefined,
+  logger: Logger | undefined
 ) => {
   validate(settings)
 
@@ -217,17 +218,27 @@ export const sendData = async (
   }))
 
   let producer: Producer
-  if (features && features[FLAGON_NAME]) {
-    producer = await getOrCreateProducer(settings, statsContext)
-  } else {
-    producer = getProducer(settings)
-    await producer.connect()
+  try {
+    if (features && features[FLAGON_NAME]) {
+      producer = await getOrCreateProducer(settings, statsContext)
+    } else {
+      producer = getProducer(settings)
+      await producer.connect()
+    }
+  } catch (error) {
+    const tags = [...(statsContext?.tags ?? []), `kafka_error:${error.name}`]
+    statsContext?.statsClient.incr('kafka_connection_error', 1, tags)
+    logger?.crit(`Kafka Connection Error: ${(error as Error).stack}`)
+    throw error
   }
 
   for (const data of topicMessages) {
     try {
       await producer.send(data as ProducerRecord)
     } catch (error) {
+      const tags = [...(statsContext?.tags ?? []), `kafka_error:${error.name}`]
+      statsContext?.statsClient.incr('kafka_send_error', 1, tags)
+      logger?.crit(`Kafka Send Error: ${(error as Error).stack}`)
       throw new IntegrationError(
         `Kafka Producer Error: ${(error as KafkaJSError).message}`,
         'KAFKA_PRODUCER_ERROR',
