06e6b1aff7546f98f8d5b10125ac26bd7ae0ce40
Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
2025-09-17 08:40:03 +0100

    MS Bing msclkid plugin (#3288)

--- packages/destinations-manifest/src/index.ts
+++ packages/destinations-manifest/src/index.ts
@@ -73,6 +73,6 @@ register('66cc76e29693c9e5591bf029', '@segment/analytics-browser-actions-nextdoo
 register('682db6914f35aafb2757ef24', '@segment/analytics-browser-actions-dub-plugins')
 register('68383577d2c19626da376944', '@segment/analytics-browser-actions-reddit-pixel')
 register('686b6e058e93b64b2fe576a3', '@segment/analytics-browser-actions-facebook-conversions-api-web')
-register('68b82be249b48bae343517c7', '@segment/analytics-browser-actions-ms-bing-capi')
+register('68b82be249b48bae343517c7', '@segment/analytics-browser-actions-ms-bing-capi-plugins')
 register('68c14f4eefb643527597844c', '@segment/analytics-browser-actions-fullsession')
 register('68c9611ea98b898fa893cffc', '@segment/analytics-browser-actions-cj')
