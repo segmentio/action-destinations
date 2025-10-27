cdb0a5575144fe235daed10d53891ecd0ceefbe6
Md Mozammil Khan <mdkhan@twilio.com>
2025-10-27 13:22:43 +0530

    code

--- packages/destination-actions/src/destinations/aws-kinesis/send/index.ts
+++ packages/destination-actions/src/destinations/aws-kinesis/send/index.ts
@@ -4,14 +4,50 @@ import type { Payload } from './generated-types'
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Send',
-  description: '',
-  fields: {},
-  perform: (_, _1) => {
+  description: 'Send event to AWS Kinesis',
+  defaultSubscription: 'type = "track" or type = "identify" or type = "page" or type = "screen" or type = "group"',
+  fields: {
+    payload: {
+      label: 'Payload',
+      description: 'The data to send to AWS Kinesis',
+      type: 'object',
+      required: true,
+      default: { '@path': '$.' }
+    },
+    partitionKey: {
+      label: 'Partition Key',
+      description: 'The partition key to use for the record',
+      type: 'string',
+      required: true,
+      default: { '@path': '$.messageId' }
+    },
+    streamName: {
+      label: 'Stream Name',
+      description: 'The name of the Kinesis stream to send records to',
+      type: 'string',
+      required: true,
+      default: { '@path': '$.properties.streamName' }
+    },
+    awsRegion: {
+      label: 'AWS Region',
+      description: 'The AWS region where the Kinesis stream is located',
+      type: 'string',
+      required: true,
+      default: { '@path': '$.properties.awsRegion' }
+    }
+  },
+  perform: async (_requests, { _settings, _payload }) => {
     // Make your partner api request here!
     // return request('https://example.com', {
     //   method: 'post',
     //   json: data.payload
     // })
+  },
+  performBatch: async (_requests, { _settings, _payload }) => {
+    // Example performBatch implementation
+    // for (const batch of requests) {
+    //   await sendBatchedDataToKinesis(batch, settings)
+    // }
   }
 }
 
--- packages/destination-actions/src/destinations/aws-kinesis/utils.ts
+++ packages/destination-actions/src/destinations/aws-kinesis/utils.ts
@@ -2,3 +2,8 @@ export const validateIamRoleArnFormat = (arn: string): boolean => {
   const iamRoleArnRegex = /^arn:aws:iam::\d{12}:role\/[A-Za-z0-9+=,.@_\-/]+$/
   return iamRoleArnRegex.test(arn)
 }
+
+export const sendDataToKinesis = async (_settings: Settings, _payload: Payload[]): Promise<void> => {
+  // Implementation for sending data to AWS Kinesis goes here.
+  // This is a placeholder function.
+}
