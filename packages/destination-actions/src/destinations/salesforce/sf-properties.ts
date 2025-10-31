e7067d09eee1ef2e8e6b6494c18b31cfed049a1c
Prithviraj-rathore-segment <prathore@twilio.com>
2025-10-31 19:33:00 +0530

    STRATCONN-6258 changed the tooltip for custom object in actions salesforce

--- packages/destination-actions/src/destinations/salesforce/sf-properties.ts
+++ packages/destination-actions/src/destinations/salesforce/sf-properties.ts
@@ -160,7 +160,7 @@ export const traits: InputField = {
 export const customFields: InputField = {
   label: 'Other Fields',
   description: `
-  Additional fields to send to Salesforce. On the left-hand side, input the Salesforce field API name. On the right-hand side, map the Segment field that contains the value.
+  Additional fields to send to Salesforce. On the left-hand side, map the Segment field that contains the value. On the right-hand side, input the Salesforce field API name.
 
   This can include standard or custom fields. Custom fields must be predefined in your Salesforce account and the API field name should have __c appended.
 
@@ -195,7 +195,7 @@ export const traits2: InputField = {
   label: 'Record Matchers',
   description: `The fields used to find Salesforce records for updates. **This is required if the operation is Delete, Update or Upsert.**
 
-  Any field can function as a matcher, including Record ID, External IDs, standard fields and custom fields. On the left-hand side, input the Salesforce field API name. On the right-hand side, map the Segment field that contains the value.
+  Any field can function as a matcher, including Record ID, External IDs, standard fields and custom fields. On the left-hand side, map the Segment field that contains the value. On the right-hand side, input the Salesforce field API name.
 
   If multiple records are found, no changes will be made. **Please use fields that result in unique records.**
 
@@ -347,7 +347,7 @@ export const bulkUpdateRecordId2: InputField = {
 export const customFields2: InputField = {
   label: 'Other Fields',
   description: `
-  Additional fields to send to Salesforce. On the left-hand side, input the Salesforce field API name. On the right-hand side, map the Segment field that contains the value.
+  Additional fields to send to Salesforce. On the left-hand side, map the Segment field that contains the value. On the right-hand side, input the Salesforce field API name.
 
   This can include standard or custom fields. Custom fields must be predefined in your Salesforce account and the API field name should have __c appended.
 
