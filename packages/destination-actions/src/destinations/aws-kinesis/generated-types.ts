ae3cd097ba28a6d62e35b8b3b24407b7e5613267
mdkhan-tw <mdkhan@twilio.com>
2025-11-04 13:45:58 +0530

    Adding authentication setting and validation for kinesis destination (#3378)
    * authentication settings and implementation
    
    * adding tests
    
    * fixing tests
    
    * code

--- packages/destination-actions/src/destinations/aws-kinesis/generated-types.ts
+++ packages/destination-actions/src/destinations/aws-kinesis/generated-types.ts
@@ -1,3 +1,12 @@
 // Generated file. DO NOT MODIFY IT BY HAND.
 
-export interface Settings {}
+export interface Settings {
+  /**
+   * The ARN of the IAM Role to assume for sending data to Kinesis.
+   */
+  iamRoleArn: string
+  /**
+   * The external ID to use when assuming the IAM Role. Generate a secure string and treat it like a password.  This is often used as an additional security measure.
+   */
+  iamExternalId: string
+}
