e84db52a7b081f9ea360da82a8e8efb06a6d0433
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2025-11-06 15:16:03 +0530

    Add Actions and settings for Redshift (#3357)

--- packages/warehouse-destinations/src/destinations/redshift/index.ts
+++ packages/warehouse-destinations/src/destinations/redshift/index.ts
@@ -1,14 +1,80 @@
+import { defaultValues } from '@segment/actions-core'
 import type { WarehouseDestinationDefinition } from '@segment/actions-core'
 import { Settings } from './generated-types'
+import sendCustomEvent from './sendCustomEvent'
+import audienceDefaultFields from './sendCustomEvent/audience-default-fields'
+import journeysDefaultFields from './sendCustomEvent/journeys-default-fields'
 
 const destination: WarehouseDestinationDefinition<Settings> = {
   name: 'Redshift',
   slug: 'redshift',
   mode: 'warehouse',
 
-  settings: {},
+  settings: {
+    warehouseId: {
+      label: 'Warehouse ID',
+      description: 'The ID of the existing Redshift warehouse instance to use.',
+      type: 'string',
+      required: true
+    }
+  },
 
-  actions: {}
+  presets: [
+    {
+      name: 'Linked Audience Entity Added',
+      partnerAction: 'sendCustomEvent',
+      mapping: {
+        ...defaultValues(sendCustomEvent.fields),
+        ...defaultValues(audienceDefaultFields)
+      },
+      type: 'specificEvent',
+      eventSlug: 'warehouse_entity_added_track'
+    },
+    {
+      name: 'Linked Audience Associated Entity Removed',
+      partnerAction: 'sendCustomEvent',
+      mapping: {
+        ...defaultValues(sendCustomEvent.fields),
+        ...defaultValues(audienceDefaultFields)
+      },
+      type: 'specificEvent',
+      eventSlug: 'warehouse_entity_removed_track'
+    },
+    {
+      name: 'Linked Audience Profile Entered',
+      partnerAction: 'sendCustomEvent',
+      mapping: {
+        ...defaultValues(sendCustomEvent.fields),
+        ...defaultValues(audienceDefaultFields)
+      },
+      type: 'specificEvent',
+      eventSlug: 'warehouse_audience_entered_track'
+    },
+    {
+      name: 'Linked Audience Profile Exited',
+      partnerAction: 'sendCustomEvent',
+      mapping: {
+        ...defaultValues(sendCustomEvent.fields),
+        ...defaultValues(audienceDefaultFields)
+      },
+      type: 'specificEvent',
+      eventSlug: 'warehouse_audience_exited_track'
+    },
+    {
+      name: 'Journeys Step Entered',
+      partnerAction: 'sendCustomEvent',
+      mapping: {
+        ...defaultValues(sendCustomEvent.fields),
+        ...defaultValues(journeysDefaultFields)
+      },
+      type: 'specificEvent',
+      eventSlug: 'journeys_step_entered_track'
+    }
+  ],
+
+  actions: {
+    sendCustomEvent
+  }
 }
 
 export default destination
