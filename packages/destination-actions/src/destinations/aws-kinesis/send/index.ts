082c54e77ea1edafbd5caabfb921f0fb73ae831c
Md Mozammil Khan <mdkhan@twilio.com>
2025-11-12 14:36:12 +0530

    removing dynamic

--- packages/destination-actions/src/destinations/aws-kinesis/send/index.ts
+++ packages/destination-actions/src/destinations/aws-kinesis/send/index.ts
@@ -27,8 +27,7 @@ const action: ActionDefinition<Settings, Payload> = {
       label: 'Stream Name',
       description: 'The name of the Kinesis stream to send records to',
       type: 'string',
-      required: true,
-      dynamic: true
+      required: true
     },
     awsRegion: {
       label: 'AWS Region',
