f90375724124db6f652a29b987b0be8f13a00b9b
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2025-11-17 15:02:49 +0530

    Add Actions and settings for bigquery (#3356)

---
+++ packages/warehouse-destinations/src/destinations/bigquery/sendCustomEvent/audience-default-fields.ts
@@ -0,0 +1,28 @@
+export default {
+  // note that this must be `properties` to be processed by the warehouse pipeline
+  properties: {
+    label: 'Columns',
+    description: `Additional columns to write to BigQuery.`,
+    type: 'object',
+    defaultObjectUI: 'keyvalue',
+    required: true,
+    additionalProperties: true,
+    default: {
+      entity_context: {
+        '@json': {
+          mode: 'encode',
+          value: {
+            '@path': '$.properties.data_graph_entity_context'
+          }
+        }
+      },
+      user_id: { '@path': '$.userId' },
+      audience_key: { '@path': '$.properties.audience_key' },
+      personas_computation_key: { '@path': '$.context.personas.computation_key' },
+      personas_computation_id: { '@path': '$.context.personas.computation_id' },
+      personas_computation_run_id: { '@path': '$.context.personas.computation_run_id' },
+      personas_activation_id: { '@path': '$.context.personas.event_emitter_id' },
+      event_name: { '@path': '$.event' }
+    }
+  }
+}
