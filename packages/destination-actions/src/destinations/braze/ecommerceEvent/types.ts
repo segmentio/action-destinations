987630a727227ead7cbf88d3747b653818d32730
Joe Ayoub <joe.ayoub@segment.com>
2025-11-17 09:51:51 +0000

    working on types

--- packages/destination-actions/src/destinations/braze/ecommerceEvent/types.ts
+++ packages/destination-actions/src/destinations/braze/ecommerceEvent/types.ts
@@ -1,67 +1,156 @@
-export interface EcommerceEvent {
-    events: Array<MultiProductEvent | SingleProductEvent>
-} 
+export type SinglePropertyEventName = 'ecommerce.product_viewed' 
 
-export interface MultiProductEvent {
-    external_id: string
+export type CheckoutStartedEventName = 'ecommerce.checkout_started'
+
+export type CartUpdatedEventName = 'ecommerce.cart_updated' 
+
+export type OrderPlacedEventName = 'ecommerce.order_placed' 
+
+export type OrderCancelledEventName = 'ecommerce.order_cancelled'
+
+export type OrderRefundedEventName = 'ecommerce.order_refunded'
+
+export type MultiPropertyEventName = 
+   CheckoutStartedEventName |
+   CartUpdatedEventName | 
+   OrderPlacedEventName | 
+   OrderCancelledEventName | 
+   OrderRefundedEventName
+
+export interface EcommerceEvent {
+    events: Array<
+        SingleProductEvent | 
+        CartUpdatedEvent | 
+        CheckoutStartedEvent | 
+        OrderPlacedEvent | 
+        OrderRefundedEvent |
+        OrderCancelledEvent  
+    >
+} 
+export interface BaseEvent {
+    external_id?: string
+    braze_id?: string
+    email?: string
+    phone?: string
+    user_alias?: {
+        alias_name: string
+        alias_label: string
+    },
     app_id: string
-    name: 'ecommerce.cart_updated' 
-        | 'ecommerce.checkout_started' 
-        | 'ecommerce.order_placed' 
-        | 'ecommerce.order_cancelled' 
-        | 'ecommerce.order_refunded'
+    name: SinglePropertyEventName | MultiPropertyEventName
     time: string
     properties: {
-        cancel_reason: string
-        checkout_id: string
-        cart_id: string
-        total_value: number
-        products: Array<Product>
         currency: string
-        total_discount: number
-        discounts: Array<{
-            code: string
-            amount: number
-        }>
         source: string
-        metadata: {
-          checkout_url: string
-          order_status_url: string
-          [key: string]: unknown
+        metadata?: {
+            [key: string]: unknown
         }
     }
 }
 
-export interface SingleProductEvent {
-    external_id: string
-    app_id: string
-    name: 'ecommerce.product_viewed' 
-    time: string
-    properties: (Product & {
-        checkout_id: string
-        cart_id: string
+export interface SingleProductEvent extends BaseEvent {
+    name: SinglePropertyEventName
+    properties: BaseEvent['properties'] & {
+        product_id: string
+        product_name: string
+        variant_id: string
+        price: number
+        image_url?: string
+        product_url?: string
+        type?: Array<string>
+    }
+}
+
+export interface MultiProductBaseEvent extends BaseEvent {
+    name: MultiPropertyEventName
+    properties: BaseEvent['properties'] & {
         total_value: number
-        currency: string
-        total_discount: number
-        discounts: Array<{
+        products: Array<{
+            product_id: string
+            product_name: string
+            variant_id: string
+            quantity: number
+            price: number
+            image_url?: string
+            product_url?: string
+            metadata?: {
+                [key: string]: unknown
+            }
+        }>
+    }
+}
+
+export interface CartUpdatedEvent extends MultiProductBaseEvent {
+    name: CartUpdatedEventName
+    properties: MultiProductBaseEvent['properties'] & {
+        cart_id: string
+    }
+}
+
+export interface CheckoutStartedEvent extends MultiProductBaseEvent {
+    name: CheckoutStartedEventName
+    properties: MultiProductBaseEvent['properties'] & {
+         checkout_id: string
+         cart_id?: string
+         metadata: BaseEvent['properties']['metadata'] & {
+            checkout_url?: string
+         }
+    }
+}
+
+export interface OrderPlacedEvent extends MultiProductBaseEvent {
+    name: OrderPlacedEventName
+    properties: MultiProductBaseEvent['properties'] & {
+        order_id: string
+        cart_id?: string
+        total_discount?: number
+        discounts?: Array<{
             code: string
             amount: number
         }>
-        source: string,
-        metadata: {
-          [key: string]: unknown
-        },
-        type: Array<string>
-    })
-}
-
-export interface Product {
-    product_id: string
-    product_name: string
-    variant_id: string
-    quantity: number
-    price: number
-    metadata: {
-        [key: string]: unknown
+        metadata: BaseEvent['properties']['metadata'] & {
+            order_status_url?: string
+        }
     }
 }
+
+export interface OrderRefundedEvent extends MultiProductBaseEvent {
+    name: OrderRefundedEventName
+    properties: MultiProductBaseEvent['properties'] & {
+        order_id: string
+        total_discount?: number
+        discounts?: Array<{
+            code: string
+            amount: number
+        }>
+        metadata: BaseEvent['properties']['metadata'] & {
+            order_status_url?: string
+        }
+    }
+}
+
+export interface OrderCancelledEvent extends MultiProductBaseEvent {
+    name: OrderCancelledEventName
+    properties: MultiProductBaseEvent['properties'] & {
+        order_id: string
+        cancel_reason: string
+        total_discount?: number
+        discounts?: Array<{
+            code: string
+            amount: number
+        }>
+        metadata: BaseEvent['properties']['metadata'] & {
+            order_status_url?: string
+        }
+    }
+}
+
+
+
+
+
+
+
+       
+                    
+       
