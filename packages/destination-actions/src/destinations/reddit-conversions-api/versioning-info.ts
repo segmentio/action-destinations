32152e43ad455356a4363c48c2965cf7a39e7414
Arjun Bhandage <abhandage@twilio.com>
2025-11-27 16:35:00 +0530

    tier 1 batch 2

---
+++ packages/destination-actions/src/destinations/reddit-conversions-api/versioning-info.ts
@@ -0,0 +1,5 @@
+/** REDDIT_CONVERSIONS_API_VERSION
+ * Reddit conversions API version.
+ * API reference: https://ads-api.reddit.com/docs/v2/changelog
+ */
+export const REDDIT_CONVERSIONS_API_VERSION = 'v2.0'
