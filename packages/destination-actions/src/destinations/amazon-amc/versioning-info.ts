c19bd6977f2264b4c949932a6bee012189d53a52
Arjun Bhandage <abhandage@twilio.com>
2025-11-27 17:26:43 +0530

    tier 1 batch 4

---
+++ packages/destination-actions/src/destinations/amazon-amc/versioning-info.ts
@@ -0,0 +1,11 @@
+/** AMAZON_AMC_API_VERSION & AMAZON_AMC_AUTH_API_VERSION
+ * Audience media type: application/vnd.amcaudiences.v1+json
+ * API reference: https://advertising.amazon.com/API/docs/en-us/amc-advertiser-audience#tag/Audience%20Records/operation/ManageAudienceV2
+ */
+export const AMAZON_AMC_API_VERSION = 'v1'
+
+/** AMAZON_AMC_AUTH_API_VERSION
+ * API reference: https://advertising.amazon.com/API/docs/en-us/reference/2/profiles#tag/Profiles/operation/listProfiles
+ * Auth profiles endpoint: /amc/v2/profiles
+ */
+export const AMAZON_AMC_AUTH_API_VERSION = 'v2'
---
+++ packages/destination-actions/src/destinations/linkedin-conversions/versioning-info.ts
@@ -0,0 +1,5 @@
+/** LINKEDIN_CONVERSIONS_API_VERSION
+ * LinkedIn Conversions API version.
+ * API reference: https://learn.microsoft.com/en-us/linkedin/marketing/integrations/ads-reporting/conversions-api?view=li-lms-2025-11&tabs=curl
+ */
+export const LINKEDIN_CONVERSIONS_API_VERSION = '202505'
