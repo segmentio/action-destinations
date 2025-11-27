09737653ff61d1c15793bf6005b5872844669ac2
Arjun Bhandage <abhandage@twilio.com>
2025-11-27 17:45:52 +0530

    tier 1 batch 5

---
+++ packages/destination-actions/src/destinations/launchdarkly-audiences/versioning-info.ts
@@ -0,0 +1,5 @@
+/** LAUNCHDARKLY_AUDIENCES_API_VERSION
+ * LaunchDarkly API version.
+ * API reference: https://launchdarkly.com/docs/api/segments
+ */
+export const LAUNCHDARKLY_AUDIENCES_API_VERSION = 'v2'
