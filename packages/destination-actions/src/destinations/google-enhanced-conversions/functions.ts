69a85aab7f793acf6f435ea73918a7089226cd0e
Arjun Bhandage <abhandage@twilio.com>
2025-11-27 16:16:20 +0530

    tier 1 batch 1

--- packages/destination-actions/src/destinations/google-enhanced-conversions/functions.ts
+++ packages/destination-actions/src/destinations/google-enhanced-conversions/functions.ts
@@ -33,7 +33,9 @@ import type { Payload as UserListPayload } from './userList/generated-types'
 import { RefreshTokenResponse } from '.'
 import { STATUS_CODE_MAPPING } from './constants'
 import { processHashing } from '../../lib/hashing-utils'
-export const API_VERSION = 'v21'
+import { GOOGLE_ENHANCED_CONVERSIONS_API_VERSION } from './versioning-info'
+
+export const API_VERSION = GOOGLE_ENHANCED_CONVERSIONS_API_VERSION
 export const CANARY_API_VERSION = 'v21'
 export const FLAGON_NAME = 'google-enhanced-canary-version'
 export const FLAGON_NAME_PHONE_VALIDATION_CHECK = 'google-enhanced-phone-validation-check'
---
+++ packages/destination-actions/src/destinations/google-enhanced-conversions/versioning-info.ts
@@ -0,0 +1,11 @@
+/** GOOGLE_ENHANCED_CONVERSIONS_API_VERSION
+ * Google Ads API version for enhanced conversions.
+ * API reference: https://developers.google.com/google-ads/api/docs/upgrade
+ */
+export const GOOGLE_ENHANCED_CONVERSIONS_API_VERSION = 'v19'
+
+/** GOOGLE_ENHANCED_CONVERSIONS_EVENTS_API_VERSION
+ * Google Ads event API version.
+ * API reference: https://developers.google.com/google-ads/api/docs/upgrade
+ */
+export const GOOGLE_ENHANCED_CONVERSIONS_EVENTS_API_VERSION = 'v1'
