8d4a258d764a90d1885c7b16d88f2744dfc780e9
Arjun Bhandage <abhandage@twilio.com>
2025-11-27 16:16:20 +0530

    Refactor API versioning to use constants from versioning-info module

---
+++ packages/destination-actions/src/destinations/amplitude/versioning-info.ts
@@ -0,0 +1,5 @@
+/** AMPLITUDE_API_VERSION
+ * Used for deletions, httpapi, usersearch endpoints.
+ * API reference: https://developers.amplitude.com/docs/http-api-v2
+ */
+export const AMPLITUDE_API_VERSION = '2'
---
+++ packages/destination-actions/src/destinations/google-enhanced-conversions/versioning-info.ts
@@ -0,0 +1,12 @@
+/** GOOGLE_ENHANCED_CONVERSIONS_API_VERSION
+ * Google Ads API version for enhanced conversions.
+ * API reference: https://developers.google.com/google-ads/api/docs/upgrade
+ */
+export const GOOGLE_ENHANCED_CONVERSIONS_API_VERSION = 'v21'
+export const GOOGLE_ENHANCED_CONVERSIONS_CANARY_API_VERSION = 'v21'
+
+/** GOOGLE_ENHANCED_CONVERSIONS_EVENTS_API_VERSION
+ * Google Ads event API version.
+ * API reference: https://developers.google.com/google-ads/api/docs/upgrade
+ */
+export const GOOGLE_ENHANCED_CONVERSIONS_EVENTS_API_VERSION = 'v1'
