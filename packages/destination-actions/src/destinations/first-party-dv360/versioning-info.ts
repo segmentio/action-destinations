7674887236400b762432fa657c3ddbe062810e22
Arjun Bhandage <abhandage@twilio.com>
2025-11-28 22:47:51 +0530

    tier 1 batch 6

---
+++ packages/destination-actions/src/destinations/first-party-dv360/versioning-info.ts
@@ -0,0 +1,11 @@
+/** FIRST_PARTY_DV360_API_VERSION
+ * DV360 First Party Audience API version.
+ * API reference: https://developers.google.com/display-video/api/reference/rest/v4/firstPartyAndPartnerAudiences/create
+ */
+export const FIRST_PARTY_DV360_API_VERSION = 'v4'
+
+/** FIRST_PARTY_DV360_CANARY_API_VERSION
+ * DV360 First Party Audience API canary version.
+ * API reference: https://developers.google.com/display-video/api/reference/rest/v4/firstPartyAndPartnerAudiences/create
+ */
+export const FIRST_PARTY_DV360_CANARY_API_VERSION = 'v4'
