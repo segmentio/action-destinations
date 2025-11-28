49a7a2628f43fedeab492f6ed8de63d55536e50f
Arjun Bhandage <abhandage@twilio.com>
2025-11-28 22:47:51 +0530

    tier 1 batch 6

---
+++ packages/destination-actions/src/destinations/yahoo-audiences/versioning-info.ts
@@ -0,0 +1,5 @@
+/** YAHOO_AUDIENCES_TAXONOMY_API_VERSION
+ * Yahoo Audiences taxonomy API version.
+ * API reference: https://help.yahooinc.com/datax/docs/put-append-taxonomy-node
+ */
+export const YAHOO_AUDIENCES_TAXONOMY_API_VERSION = 'v1'
