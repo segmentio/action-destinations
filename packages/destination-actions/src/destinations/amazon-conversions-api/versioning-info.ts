ffff344016e709e8257c5a9148838a3c1beeaa21
Arjun Bhandage <abhandage@twilio.com>
2025-11-30 14:01:31 +0530

    tier 1 batch 7

---
+++ packages/destination-actions/src/destinations/amazon-conversions-api/versioning-info.ts
@@ -0,0 +1,11 @@
+/** AMAZON_CONVERSIONS_API_PROFILES_VERSION
+ * Profiles endpoint version: /v2/profiles
+ * API reference: https://advertising.amazon.com/API/docs/en-us/reference/2/profiles#tag/Profiles/operation/listProfiles
+ */
+export const AMAZON_CONVERSIONS_API_PROFILES_VERSION = 'v2'
+
+/** AMAZON_CONVERSIONS_API_EVENTS_VERSION
+ * Events endpoint version: /v1/events
+ * API reference: https://advertising.amazon.com/API/docs/en-us/amazon-ads/1-0/betas#tag/Events/operation/CreateEvent
+ */
+export const AMAZON_CONVERSIONS_API_EVENTS_VERSION = 'v1'
