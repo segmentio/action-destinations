3751d4d505ff6f014e836ea5e03ceeeebc537d10
Arjun Bhandage <abhandage@twilio.com>
2025-11-30 14:01:31 +0530

    tier 1 batch 7

---
+++ packages/destination-actions/src/destinations/facebook-custom-audiences/versioning-info.ts
@@ -0,0 +1,11 @@
+/** FACEBOOK_CUSTOM_AUDIENCES_API_VERSION
+ * Facebook Custom Audiences API version.
+ * API reference: https://developers.facebook.com/docs/marketing-api/audiences/guides/custom-audiences
+ */
+export const FACEBOOK_CUSTOM_AUDIENCES_API_VERSION = 'v24.0'
+
+/** FACEBOOK_CUSTOM_AUDIENCES_CANARY_API_VERSION
+ * Facebook Custom Audiences API canary version.
+ * API reference: https://developers.facebook.com/docs/marketing-api/audiences/guides/custom-audiences
+ */
+export const FACEBOOK_CUSTOM_AUDIENCES_CANARY_API_VERSION = 'v24.0'
