a4b5cd3873368fa1968a6af570a705cb17ec6831
Joe Ayoub <joe.ayoub@segment.com>
2025-12-01 12:45:11 +0000

    fix spelling

--- packages/destination-actions/src/destinations/amplitude/events-constants.ts
+++ packages/destination-actions/src/destinations/amplitude/events-constants.ts
@@ -1,4 +1,4 @@
-const REVENUE_KETS = ['revenue', 'price', 'productId', 'quantity', 'revenueType']
+const REVENUE_KEYS = ['revenue', 'price', 'productId', 'quantity', 'revenueType']
 const USER_PROPERTY_KEYS = ['utm_properties', 'referrer', 'setOnce', 'setAlways','add','autocaptureAttributionEnabled','autocaptureAttributionSet','autocaptureAttributionSetOnce','autocaptureAttributionUnset']
-export const KEYS_TO_OMIT = [...REVENUE_KETS, ...USER_PROPERTY_KEYS, 'trackRevenuePerProduct']
+export const KEYS_TO_OMIT = [...REVENUE_KEYS, ...USER_PROPERTY_KEYS, 'trackRevenuePerProduct']
 export const DESTINATION_INTEGRATION_NAME = 'Actions Amplitude'
