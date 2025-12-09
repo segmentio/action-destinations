eef160989747ebc8484d16eebe19bfa46a7ac337
Joe Ayoub <joe.ayoub@segment.com>
2025-12-09 09:51:27 +0000

    registering zapier

--- packages/destination-actions/src/destinations/zapier/index.ts
+++ packages/destination-actions/src/destinations/zapier/index.ts
@@ -2,7 +2,7 @@ import type { DestinationDefinition } from '@segment/actions-core'
 import type { Settings } from './generated-types'
 import send from './send'
 const destination: DestinationDefinition<Settings> = {
-  name: 'Zapier',
+  name: 'Zapier (Actions)',
   slug: 'actions-zapier',
   description: 'Send Segment event data to Zapier',
   mode: 'cloud',
