d747e13f8b2811ad98d25a3bbb438d0b03be42d8
Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
2025-12-09 09:31:17 +0000

    [Zapier] - new Destination (#3454)
    * starting zapier destination
    
    * unit tests added
    
    * removing unnecessary tests

---
+++ packages/destination-actions/src/destinations/zapier/index.ts
@@ -0,0 +1,18 @@
+import type { DestinationDefinition } from '@segment/actions-core'
+import type { Settings } from './generated-types'
+import send from './send'
+const destination: DestinationDefinition<Settings> = {
+  name: 'Zapier',
+  slug: 'actions-zapier',
+  description: 'Send Segment event data to Zapier',
+  mode: 'cloud',
+  authentication: {
+    scheme: 'custom',
+    fields: {}
+  },
+  actions: {
+    send
+  }
+}
+
+export default destination
