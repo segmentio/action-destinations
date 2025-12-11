00e4d403273bf7fd965539547e67f799e03c5a8e
Joe Ayoub <joe.ayoub@segment.com>
2025-12-11 09:21:04 +0000

    refactor linkedin audience destination

--- packages/destination-actions/src/destinations/linkedin-audiences/types.ts
+++ packages/destination-actions/src/destinations/linkedin-audiences/types.ts
@@ -1,4 +1,30 @@
 import { HTTPError } from '@segment/actions-core'
+import { SEGMENT_TYPES, AUDIENCE_ACTION } from './constants'
+import { Payload as UserPayload } from './updateAudience/generated-types'
+
+export type SegmentType = typeof SEGMENT_TYPES[keyof typeof SEGMENT_TYPES]
+
+export type AudienceAction = typeof AUDIENCE_ACTION[keyof typeof AUDIENCE_ACTION]
+
+export type ValidUserPayload = (UserPayload & { index: number })
+
+export interface AudienceJSON<E> {
+  elements: E[]
+}
+
+export interface LinkedInUserAudienceElement {
+  action: 'ADD' | 'REMOVE'
+  userIds: LinkedInUserId[]
+  firstName?: string
+  lastName?: string
+  title?: string
+  company?: string
+  country?: string
+}
+
+export type LinkedInUserId =
+  | { idType: 'SHA256_EMAIL'; idValue: string }
+  | { idType: 'GOOGLE_AID'; idValue: string }
 
 export interface RefreshTokenResponse {
   access_token: string
@@ -15,14 +41,22 @@ export interface AdAccountUserResponse {
   role: string
 }
 
-export interface LinkedInAudiencePayload {
-  action: 'ADD' | 'REMOVE'
-  userIds: Record<string, string>[]
-  firstName?: string
-  lastName?: string
-  title?: string
-  company?: string
-  country?: string
+export interface GetDMPSegmentResponse {
+  elements: Array<CreateDMPSegmentResponse>
+}
+
+export interface CreateDMPSegmentResponse {
+  id: string
+  type: 'USER' | 'COMPANY'
+}
+
+export interface LinkedInBatchUpdateResponse {
+  elements: Array<
+    {
+      status: number,
+      id?: string
+      message?: string
+    }>
 }
 
 export class LinkedInRefreshTokenError extends HTTPError {
--- packages/destination-actions/src/destinations/linkedin-audiences/updateAudience/index.ts
+++ packages/destination-actions/src/destinations/linkedin-audiences/updateAudience/index.ts
@@ -1,19 +1,42 @@
 import type { ActionDefinition} from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
-import { processPayload } from './functions'
+import { getSegmentSourceIdAndName, buildJSON, validate } from './functions'
+import { send } from '../common-functions'
 import { fields } from './fields'
+import { SEGMENT_TYPES } from '../constants'
+import { LinkedInUserAudienceElement } from '../types'
 
 const action: ActionDefinition<Settings, Payload> = {
-  title: 'Sync To LinkedIn DMP Segment',
-  description: 'Syncs contacts from a Personas Audience to a LinkedIn DMP Segment.',
+  title: 'Sync To LinkedIn DMP User Segment',
+  description: 'Syncs an Engage User based Audience to a LinkedIn DMP User Segment.',
   defaultSubscription: 'event = "Audience Entered" or event = "Audience Exited"',
   fields,
   perform: async (request, { settings, payload, statsContext }) => {
-    return processPayload(request, settings, [payload], statsContext)
+    return send<Payload, LinkedInUserAudienceElement>(
+      request, 
+      getSegmentSourceIdAndName,
+      buildJSON,
+      validate,
+      settings, 
+      [payload],  
+      SEGMENT_TYPES.USER, 
+      false, 
+      statsContext
+    )
   },
   performBatch: async (request, { settings, payload, statsContext }) => {
-    return processPayload(request, settings, payload, statsContext)
+    return send<Payload, LinkedInUserAudienceElement>(
+      request, 
+      getSegmentSourceIdAndName,
+      buildJSON,
+      validate,
+      settings, 
+      payload, 
+      SEGMENT_TYPES.USER, 
+      true, 
+      statsContext
+    )
   }
 }
 
