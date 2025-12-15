36e6e8981938f9ae664157382bcfa877efd2da10
Arjun Bhandage <abhandage@twilio.com>
2025-12-15 17:07:21 +0530

    address comment

--- packages/destination-actions/src/destinations/amazon-amc/versioning-info.ts
+++ packages/destination-actions/src/destinations/amazon-amc/versioning-info.ts
@@ -1,4 +1,4 @@
-/** AMAZON_AMC_API_VERSION & AMAZON_AMC_AUTH_API_VERSION
+/** AMAZON_AMC_API_VERSION
  * Audience media type: application/vnd.amcaudiences.v1+json
  * API reference: https://advertising.amazon.com/API/docs/en-us/amc-advertiser-audience#tag/Audience%20Records/operation/ManageAudienceV2
  */
--- packages/destination-actions/src/destinations/linkedin-conversions/versioning-info.ts
+++ packages/destination-actions/src/destinations/linkedin-conversions/versioning-info.ts
@@ -1,5 +1,5 @@
 /** LINKEDIN_CONVERSIONS_API_VERSION
  * LinkedIn Conversions API version.
- * API reference: https://learn.microsoft.com/en-us/linkedin/marketing/integrations/ads-reporting/conversions-api?view=li-lms-2025-11&tabs=curl
+ * API reference: https://learn.microsoft.com/en-us/linkedin/marketing/integrations/ads-reporting/conversions-api?view=li-lms-2025-05&tabs=curl
  */
 export const LINKEDIN_CONVERSIONS_API_VERSION = '202505'
