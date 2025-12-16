05875db591e123ebf1c7a5359c432ef6532eb5ef
Joe Ayoub <joe.ayoub@segment.com>
2025-12-16 14:24:17 +0000

    redeploy amc changes

--- packages/destination-actions/src/destinations/amazon-amc/utils.ts
+++ packages/destination-actions/src/destinations/amazon-amc/utils.ts
@@ -1,66 +1,17 @@
-import {
-  ErrorCodes,
-  HTTPError,
-  OAuth2ClientCredentials,
-  InvalidAuthenticationError,
-  RequestClient
-} from '@segment/actions-core'
+import { ErrorCodes, OAuth2ClientCredentials, InvalidAuthenticationError, RequestClient } from '@segment/actions-core'
 import { Settings } from './generated-types'
-import { AmazonRefreshTokenError, RefreshTokenResponse } from './types'
+import { SettingsWithOauth, AmazonRefreshTokenError, RefreshTokenResponse } from './types'
+import { AUTHORIZATION_URL } from './constants'  
 
-export class AmazonAdsError extends HTTPError {
-  response: Response & {
-    data: {
-      status: string
-      message: string
-    }
-  }
-}
-
-export interface AudiencePayload {
-  name: string
-  description: string
-  countryCode: string
-  targetResource: {
-    advertiserId: string
-  }
-  metadata: {
-    externalAudienceId: string
-    ttl?: number
-    audienceFees?: {
-      cpmCents: number
-      currency: string
-    }[]
-  }
-}
-export interface RecordsResponseType {
-  jobRequestId: string
-}
-
-export const AUTHORIZATION_URL: any = {
-  'https://advertising-api.amazon.com': 'https://api.amazon.com',
-  'https://advertising-api-eu.amazon.com': 'https://api.amazon.co.uk',
-  'https://advertising-api-fe.amazon.com': 'https://api.amazon.co.jp'
-}
-export const CONSTANTS = {
-  CREATE: 'CREATE',
-  DELETE: 'DELETE'
-}
-export const CURRENCY = ['USD', 'CAD', 'JPY', 'GBP', 'EUR', 'SAR', 'AUD', 'AED', 'CNY', 'MXN', 'INR', 'SEK', 'TRY']
-
-export const REGEX_AUDIENCEID = /"audienceId":(\d+)/
-export const REGEX_ADVERTISERID = /"advertiserId":"(\d+)"/
-type AmazonAMCCredentials = { refresh_token: string; access_token: string; client_id: string; client_secret: string }
-
-export function extractNumberAndSubstituteWithStringValue(responseString: string, regex: any, substituteWith: string) {
+export function extractNumberAndSubstituteWithStringValue(responseString: string, regex: RegExp, substituteWith: string) {
   const resString = responseString.replace(regex, substituteWith)
   return JSON.parse(resString)
 }
-type SettingsWithOauth = Settings & { oauth: AmazonAMCCredentials }
+
 // Use the refresh token to get a new access token.
 // Refresh tokens, Client_id and secret are long-lived and belong to the DMP.
 // Given the short expiration time of access tokens, we need to refresh them periodically.
-export const getAuthToken = async (request: RequestClient, settings: Settings, auth: OAuth2ClientCredentials) => {
+export const getAuthToken = async (request: RequestClient, settings: Settings, auth: OAuth2ClientCredentials): Promise<string> => {
   const endpoint = AUTHORIZATION_URL[`${settings.region}`]
   try {
     const { data } = await request<RefreshTokenResponse>(`${endpoint}/auth/o2/token`, {
@@ -78,7 +29,7 @@ export const getAuthToken = async (request: RequestClient, settings: Settings, a
       timeout: 15000
     })
     return data.access_token
-  } catch (e: any) {
+  } catch (e: unknown) {
     const error = e as AmazonRefreshTokenError
     if (error.response?.data?.error === 'invalid_grant') {
       throw new InvalidAuthenticationError(
@@ -95,12 +46,20 @@ export const getAuthToken = async (request: RequestClient, settings: Settings, a
 }
 
 export const getAuthSettings = (settings: SettingsWithOauth): OAuth2ClientCredentials => {
-  return {
-    refreshToken: settings.oauth?.refresh_token,
-    accessToken: settings.oauth?.access_token,
-    clientId: process.env.ACTIONS_AMAZON_AMC_CLIENT_ID,
-    clientSecret: process.env.ACTIONS_AMAZON_AMC_CLIENT_SECRET
-  } as OAuth2ClientCredentials
+  
+  const { 
+    refresh_token: refreshToken, 
+    access_token: accessToken, 
+    client_id: clientId, 
+    client_secret: clientSecret 
+  } = settings.oauth || {}
+
+  const oauth2ClientCredentials: OAuth2ClientCredentials = {
+    refreshToken,
+    accessToken,
+    clientId,
+    clientSecret
+  }
+
+  return oauth2ClientCredentials
 }
-export const REGEX_EXTERNALUSERID = /^[0-9a-zA-Z-_]{1,128}$/
-export const TTL_MAX_VALUE = 34300800
