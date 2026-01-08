2385ba47a6de31da8dcd87ae3c27e5263673e705
Joe Ayoub <joe.ayoub@segment.com>
2026-01-08 13:34:42 +0000

    started work on integration settings

--- packages/browser-destinations/destinations/moengage-web/src/trackEvent/index.ts
+++ packages/browser-destinations/destinations/moengage-web/src/trackEvent/index.ts
@@ -5,11 +5,15 @@ import type { Payload } from './generated-types'
 // Change from unknown to the partner SDK types
 const action: BrowserActionDefinition<Settings, unknown, Payload> = {
   title: 'Track Event',
-  description: '',
+  description: 'Send Segment track events to Moengage.',
   platform: 'web',
-  fields: {},
-  perform: (_client) => {
-    // Invoke Partner SDK here
+  fields: {
+
+  },
+  perform: ({_client}) => {
+    
+
+    // code which calls the moengage web SDK to send data to Moengage
   }
 }
 
