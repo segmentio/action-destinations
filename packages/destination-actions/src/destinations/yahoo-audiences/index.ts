701e5e0313553fdae2135e60ac460b4c00cce8d3
Prithviraj-rathore-segment <prathore@twilio.com>
2026-01-14 14:58:07 +0530

    STRATCONN-6246 added the console for yahoo realtime API

--- packages/destination-actions/src/destinations/yahoo-audiences/index.ts
+++ packages/destination-actions/src/destinations/yahoo-audiences/index.ts
@@ -97,6 +97,9 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
         }
       )
       const rt_access_token = res.data.access_token
+      if (rt_access_token) {
+        console.log('Successfully refreshed Yahoo Realtime API access token')
+      }
       return { accessToken: rt_access_token }
     }
   },
