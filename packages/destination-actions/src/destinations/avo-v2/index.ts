ef1ae63c0a1e7c967e942465afab045ccae43423
Alexey Verein <tpom6oh@gmail.com>
2026-02-06 12:41:48 +0300

    Disable hiding of public keys

--- packages/destination-actions/src/destinations/avo-v2/index.ts
+++ packages/destination-actions/src/destinations/avo-v2/index.ts
@@ -16,14 +16,14 @@ const destination: DestinationDefinition<Settings> = {
       apiKey: {
         label: 'Avo Inspector API Key',
         description: 'Avo Inspector API Key can be found in the Inspector setup page on your source in Avo.',
-        type: 'password',
+        type: 'string',
         required: true
       },
       publicEncryptionKey: {
         label: 'Avo Inspector Public Encryption Key',
         description:
           'Optional. Enables verification of the property values against your Tracking Plan (e.g. allowed values, regex patterns, min/max constraints). Values are end-to-end encrypted and Avo can not decrypt them. Read more: https://www.avo.app/docs/inspector/connect-inspector-to-segment#property-value-validation-optional',
-        type: 'password',
+        type: 'string',
         required: false
       },
       env: {
