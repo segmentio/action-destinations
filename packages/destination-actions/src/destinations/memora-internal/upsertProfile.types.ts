876233cf33a5d839e2d60d8438d16f995306098b
nikumar <nikumar@twilio.com>
2026-02-06 13:49:22 +0530

    add memora-internal destination

---
+++ packages/destination-actions/src/destinations/memora-internal/upsertProfile.types.ts
@@ -0,0 +1,35 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * Enable batching of requests to Memora. Batches are uploaded as CSV files.
+   */
+  enable_batching?: boolean
+  /**
+   * Maximum number of profiles to include in each CSV import. Actual batch sizes may be lower.
+   */
+  batch_size?: number
+  /**
+   * The Memora Store ID to use for this profile. This should be a valid Memora Store associated with your Twilio account.
+   */
+  memora_store: string
+  /**
+   * Contact identifiers (email and/or phone). At least one identifier is required.
+   */
+  contact_identifiers: {
+    /**
+     * User email address
+     */
+    email?: string
+    /**
+     * User phone number
+     */
+    phone?: string
+  }
+  /**
+   * Additional contact traits for the profile. These fields are dynamically loaded from the selected Memora Store.
+   */
+  contact_traits?: {
+    [k: string]: unknown
+  }
+}
