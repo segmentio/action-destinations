63667701a9801578162253367b0fc79e3369147c
nk1107 <97457275+nk1107@users.noreply.github.com>
2026-02-06 12:13:08 +0530

    register memora internal (#3579)
    adds registration for actions-memora-internal

--- packages/destination-actions/src/destinations/index.ts
+++ packages/destination-actions/src/destinations/index.ts
@@ -227,6 +227,7 @@ register('697088b36245ed59c958d615', './rokt-capi')
 register('69708853c934696ffa66b6ef', './qualified')
 register('69708875e5073cee011f9be2', './jimo-cloud')
 register('698085f267252abcf3254a52', './dotdigital-audiences')
+register('69857992cdfcc50bdf75d589', './memora-internal')
 
 function register(id: MetadataId, destinationPath: string) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
