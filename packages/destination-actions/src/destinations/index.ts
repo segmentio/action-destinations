c5bbbb02dcaea4fc9b15b3a384f8022097565336
Joe Ayoub <joe.ayoub@segment.com>
2026-02-10 09:39:08 +0000

    remobing registration

--- packages/destination-actions/src/destinations/index.ts
+++ packages/destination-actions/src/destinations/index.ts
@@ -228,7 +228,6 @@ register('69708853c934696ffa66b6ef', './qualified')
 register('69708875e5073cee011f9be2', './jimo-cloud')
 register('698085f267252abcf3254a52', './dotdigital-audiences')
 register('69857992cdfcc50bdf75d589', './memora-internal')
-register('000000000000000000000000', './appcues') // TODO: Replace with actual metadata ID from production
 
 function register(id: MetadataId, destinationPath: string) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
