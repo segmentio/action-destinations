5bd915c4be521c9ff419337ab8589833beb6613a
Arnav Dev <ardev@twilio.com>
2026-02-18 13:07:13 +0530

    remove logs

--- packages/destination-actions/src/destinations/yahoo-audiences/utils-tax.ts
+++ packages/destination-actions/src/destinations/yahoo-audiences/utils-tax.ts
@@ -79,7 +79,6 @@ export async function update_taxonomy(
   const tx_client_key = tx_creds.tx_client_key
   const url = `https://datax.yahooapis.com/v1/taxonomy/append${engage_space_id.length > 0 ? '/' + engage_space_id : ''}`
   const oauth1_auth_string = gen_oauth1_signature(tx_client_key, tx_client_secret, 'PUT', url)
-  console.log('Generated OAuth1.0 signature for taxonomy update', oauth1_auth_string)
   try {
     const add_segment_node = await request(url, {
       method: 'PUT',
