acbaa54414a1680404da42404005f210ed300a70
nikumar <nikumar@twilio.com>
2026-02-19 22:09:03 +0530

    update stage url

--- packages/destination-actions/src/destinations/memora/constants.ts
+++ packages/destination-actions/src/destinations/memora/constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL_PRODUCTION = 'https://memory.twilio.com'
-export const BASE_URL_STAGING = 'https://memory.dev.twilio.com'
+export const BASE_URL_STAGING = 'https://memory.stage.twilio.com'
