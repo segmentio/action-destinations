f4ce37831e013ee909d6eec68bd1ec23ce9a7048
Joe Ayoub <joe.ayoub@segment.com>
2026-03-04 12:13:02 +0000

    adding default mappings

--- packages/destination-actions/src/destinations/facebook-custom-audiences/sync/fields.ts
+++ packages/destination-actions/src/destinations/facebook-custom-audiences/sync/fields.ts
@@ -10,27 +10,41 @@ export const fields: Record<string, InputField> = {
     required: true,
     label: 'External ID',
     category: 'hashedPII',
-    description:
-      'Your company’s custom identifier for this user. This can be any unique ID, such as loyalty membership IDs, user IDs, and external cookie IDs.'
+    description: 'Your company’s custom identifier for this user. This can be any unique ID, such as loyalty membership IDs, user IDs, and external cookie IDs.',
+    default: { '@path': '$.userId' }
   },
   email: {
     type: 'string',
     label: 'Email',
     description: 'User’s email (ex: foo@bar.com)',
-    category: 'hashedPII'
+    category: 'hashedPII', 
+    default: {
+      '@if': {
+        exists: { '@path': '$.traits.email' },
+        then: { '@path': '$.traits.email' },
+        else: { '@path': '$.properties.email' }
+      }
+    }
   },
   phone: {
     type: 'string',
     label: 'Phone',
     description:
       'User’s phone number, including country code. Punctuation and spaces are ok (ex: 1-234-567-8910 or +44 844 412 4653)',
-    category: 'hashedPII'
+    category: 'hashedPII',
+    default: {
+      '@if': {
+        exists: { '@path': '$.traits.phone' },
+        then: { '@path': '$.traits.phone' },
+        else: { '@path': '$.properties.phone' }
+      }
+    }
   },
   birth: {
     type: 'object',
     label: 'Date of Birth',
-    description:
-      'User’s date of birth. Include as many fields as possible for better match rates (ex: year = YYYY, month = MM, day = DD)',
+    description: 'User’s date of birth. Include as many fields as possible for better match rates (ex: year = YYYY, month = MM, day = DD)',
+    defaultObjectUI: 'keyvalue',
     properties: {
       year: {
         type: 'string',
@@ -53,6 +67,7 @@ export const fields: Record<string, InputField> = {
     description:
       'User’s name. Include as many fields as possible for better match rates. Use a-z only. No punctuation. Special characters in UTF-8 format',
     category: 'hashedPII',
+    defaultObjectUI: 'keyvalue',
     properties: {
       first: {
         type: 'string',
@@ -68,39 +83,90 @@ export const fields: Record<string, InputField> = {
         type: 'string',
         label: 'First Initial'
       }
+    },
+    default: {
+      first: {
+        '@if': {
+          exists: { '@path': '$.traits.first_name' },
+          then: { '@path': '$.traits.first_name' },
+          else: { '@path': '$.properties.first_name' }
+        }
+      },
+      last: {
+        '@if': {
+          exists: { '@path': '$.traits.last_name' },
+          then: { '@path': '$.traits.last_name' },
+          else: { '@path': '$.properties.last_name' }
+        }
+      }
     }
   },
   gender: {
     type: 'string',
     label: 'Gender',
     description: 'User’s gender (m for male, f for female)',
-    category: 'hashedPII'
+    category: 'hashedPII',
+    default: {
+      '@if': {
+        exists: { '@path': '$.traits.gender' },
+        then: { '@path': '$.traits.gender' },
+        else: { '@path': '$.properties.gender' }
+      }
+    }
   },
   city: {
     type: 'string',
     label: 'City',
     description: 'User’s city. Use a-z only. No punctuation. No special characters.',
-    category: 'hashedPII'
+    category: 'hashedPII',
+    default: {
+      '@if': {
+        exists: { '@path': '$.traits.city' },
+        then: { '@path': '$.traits.city' },
+        else: { '@path': '$.properties.city' }
+      }
+    }
   },
   state: {
     type: 'string',
     label: 'State',
     description:
       'User’s state. Use the 2-character ANSI abbreviation code, Normalize states outside the US with no punctuation and no special characters.',
-    category: 'hashedPII'
+    category: 'hashedPII',
+    default: {
+      '@if': {
+        exists: { '@path': '$.traits.state' },
+        then: { '@path': '$.traits.state' },
+        else: { '@path': '$.properties.state' }
+      }
+    }
   },
   zip: {
     type: 'string',
     label: 'Postal Code',
     description:
       'User’s postal code. For the US, use only the first 5 digits. For the UK, use the Area/District/Sector format.',
-    category: 'hashedPII'
+    category: 'hashedPII',
+    default: {
+      '@if': {
+        exists: { '@path': '$.traits.postal_code' },
+        then: { '@path': '$.traits.postal_code' },
+        else: { '@path': '$.properties.postal_code' }
+      }
+    }
   },
   country: {
     type: 'string',
     label: 'Country',
     description: 'User’s country. Use 2-letter country codes in ISO 3166-1 alpha-2 format.',
-    category: 'hashedPII'
+    category: 'hashedPII',
+    default: {
+      '@if': {
+        exists: { '@path': '$.traits.country' },
+        then: { '@path': '$.traits.country' },
+        else: { '@path': '$.properties.country' }
+      }
+    }
   },
   mobileAdId: {
     type: 'string',
