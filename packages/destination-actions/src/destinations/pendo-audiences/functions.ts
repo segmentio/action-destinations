97ac5553841cdb1a745f31562e89f3bb03cb252a
Joe Ayoub <45374896+joe-ayoub-segment@users.noreply.github.com>
2026-03-10 10:23:02 +0000

    STRATCONN-6127 - [Pendo Audiences] - post partner meeting updates (#3646)
    * post partner meeting updates
    
    * unit tests
    
    * tidy up
    
    * fixing tests
    
    * adding region support

--- packages/destination-actions/src/destinations/pendo-audiences/functions.ts
+++ packages/destination-actions/src/destinations/pendo-audiences/functions.ts
@@ -1,11 +1,15 @@
-import { RequestClient, IntegrationError } from '@segment/actions-core'
-import { CONSTANTS } from './constants'
+import { RequestClient, IntegrationError, ErrorCodes } from '@segment/actions-core'
+import { PendoDomain } from './types'
 import type { CreateSegmentJSON, CreateSegmentResponse, GetSegmentResponse } from './types'
+import { REGIONS, SEGMENT_ENDPOINT } from './constants'
 
-export async function createSegment(request: RequestClient, name: string): Promise<string> {
-  const json: CreateSegmentJSON = { name, visitors: [] }
+export async function createSegment(request: RequestClient, region: string, name: string): Promise<string> {
+  const json: CreateSegmentJSON = { 
+    name, 
+    visitors: ["empty-visitor"] // Pendo requires at least one visitor to create a segment, but it can be an empty placeholder since we'll be updating the segment with the actual visitors in the syncAudience function
+  }
   const response = await request<CreateSegmentResponse>(
-    `${CONSTANTS.API_BASE_URL}${CONSTANTS.SEGMENT_ENDPOINT}/upload`,
+    `${getDomain(region)}/${SEGMENT_ENDPOINT}/upload`,
     {
       method: 'POST',
       json
@@ -15,9 +19,9 @@ export async function createSegment(request: RequestClient, name: string): Promi
   return response.data.segmentId
 }
 
-export async function getSegment(request: RequestClient, segmentId: string): Promise<string> {
+export async function getSegment(request: RequestClient, region: string, segmentId: string): Promise<string> {
   const response = await request<GetSegmentResponse>(
-    `${CONSTANTS.API_BASE_URL}${CONSTANTS.SEGMENT_ENDPOINT}/${segmentId}`,
+    `${getDomain(region)}/${SEGMENT_ENDPOINT}/${segmentId}`,
     {
       method: 'GET'
     }
@@ -28,4 +32,14 @@ export async function getSegment(request: RequestClient, segmentId: string): Pro
   }
 
   return segmentId
+}
+
+export function getDomain(regionName: string): PendoDomain {
+  const region = Object.values(REGIONS).find(r => r.name === regionName)
+
+  if (!region) {
+    throw new IntegrationError(`Invalid region: ${regionName}`, ErrorCodes.NOT_IMPLEMENTED, 400);
+  }
+
+  return region.domain
 }
