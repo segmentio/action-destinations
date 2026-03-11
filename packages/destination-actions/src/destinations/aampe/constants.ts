a6a544bb1421209fd28829fa1c4aa7023dfa54eb
Glen Willis <glencwillis@mac.lan>
2026-03-11 10:44:40 -0700

    [Aampe] Fix US endpoint URL to include /v1/

--- packages/destination-actions/src/destinations/aampe/constants.ts
+++ packages/destination-actions/src/destinations/aampe/constants.ts
@@ -1,4 +1,3 @@
-
 export const SingaporeURL = 'https://ingestion-service-asia-southeast1-toqowp62ka-as.a.run.app/v1/'
 export const GermanyURL = 'https://ingestion-service-europe-west3-toqowp62ka-ey.a.run.app/v1/'
-export const UsURL = 'https://ingestion.us-east1.api.aampe.com/'
+export const UsURL = 'https://ingestion.us-east1.api.aampe.com/v1/'
