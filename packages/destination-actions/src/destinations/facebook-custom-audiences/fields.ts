efd854cd7894a199cb9ccc68041cdde3bfd0a464
Joe Ayoub <joe.ayoub@segment.com>
2026-03-17 14:27:56 +0000

    STRATCONN-6562 - [Facebook Custom Audiences] - rewrite

---
+++ packages/destination-actions/src/destinations/facebook-custom-audiences/fields.ts
@@ -0,0 +1,15 @@
+import { GlobalSetting } from '@segment/actions-core'
+
+export const adAccountId: GlobalSetting = {
+  type: 'string',
+  label: 'Advertiser Account ID',
+  description: 'Your advertiser account id. Read [more](https://www.facebook.com/business/help/1492627900875762).',
+  required: true
+}
+
+export const audienceDescription: GlobalSetting = {
+  type: 'string',
+  label: 'Description',
+  description: 'A brief description about your audience.',
+  required: true
+}
