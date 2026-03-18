002906596adfdeed4be72e6d2f8b189c8f1a194d
Monu Kumar <monkumar@segment.com>
2026-03-18 21:09:11 +0530

    update comment

--- packages/destination-actions/src/destinations/memora-internal/upsertProfile.types.ts
+++ packages/destination-actions/src/destinations/memora-internal/upsertProfile.types.ts
@@ -27,7 +27,7 @@ export interface Payload {
     phone?: string
   }
   /**
-   * Traits for the profile from all trait groups. At least one trait is required. These fields are dynamically loaded from the selected Memora Store. All traits use the format "TraitGroupName.$.traitName" (e.g., "Contact.$.firstName", "PurchaseHistory.$.lastPurchaseDate").
+   * Traits for the profile from all trait groups. At least one trait is required. These fields are dynamically loaded from the selected Memora Store.
    */
   profile_traits: {
     [k: string]: unknown
