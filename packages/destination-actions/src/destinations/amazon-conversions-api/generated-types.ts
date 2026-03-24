0bf0f4a5abef80ac9c1e8da15afcdb812068a104
Joe Ayoub <joe.ayoub@segment.com>
2026-03-24 08:54:41 +0000

    testing amazon capi changes in stage

--- packages/destination-actions/src/destinations/amazon-conversions-api/generated-types.ts
+++ packages/destination-actions/src/destinations/amazon-conversions-api/generated-types.ts
@@ -6,7 +6,11 @@ export interface Settings {
    */
   region: string
   /**
-   * Your Amazon Advertiser Account ID.
+   * Your Amazon Advertiser Account ID. This must be a numeric value. Use Amazon DSP CFID and not Entity ID.
    */
   advertiserId: string
+  /**
+   * Amazon Ads organizes uploaded data into datasets, which are logical groupings used to separate and categorize events from your sources. All events within a dataset will appear in Amazon Ads Data Manager under the name you provide here. New destination? We recommend providing a dataset name during initial setup. Existing destination? We strongly recommend reading the [FAQ](https://www.twilio.com/docs/segment/connections/destinations/catalog/actions-amazon-conversions-api#what-is-a-dataset-and-how-does-amazon-use-the-dataset-name) before updating your dataset name, as changes may impact your existing events.
+   */
+  dataSetName?: string
 }
