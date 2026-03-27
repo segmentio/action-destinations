273da5a340f8d1f4fd2220a8bc8a404dea241d6a
Arnav Dev <98037912+arnav777dev@users.noreply.github.com>
2026-03-27 18:15:59 +0530

    Adding 1016 as retryable and default to 500 (#3698)
    * Adding 1016 as retryable and default to 500
    
    * default to 500 in non batched deliveries
    
    * default to 500 in non batched deliveries

--- packages/destination-actions/src/destinations/marketo-static-lists/functions.ts
+++ packages/destination-actions/src/destinations/marketo-static-lists/functions.ts
@@ -332,6 +332,7 @@ const MARKETO_RETRYABLE_CODES = new Set([
   '713', // Transient error – system resource temporarily unavailable
   '719', // Lock wait timeout
   '1019', // Batch import in progress on list
+  '1016', // Too many imports in progress
   '1029' // Queue/export limit exceeded
 ])
 
@@ -354,7 +355,7 @@ function parseErrorResponse(response: MarketoResponse) {
     throw new RetryableError(message)
   }
 
-  throw new IntegrationError(message, ErrorCodes.NOT_ACCEPTABLE, 406)
+  throw new IntegrationError(message, ErrorCodes.RETRYABLE_ERROR, 500)
 }
 
 function parseErrorResponseBatch(response: MarketoResponse, payloadSize: number) {
@@ -384,8 +385,8 @@ function parseErrorResponseBatch(response: MarketoResponse, payloadSize: number)
   }
 
   return buildMultiStatusErrorResponse(payloadSize, {
-    status: 406,
-    errortype: ErrorCodes.NOT_ACCEPTABLE,
+    status: 500,
+    errortype: ErrorCodes.RETRYABLE_ERROR,
     body: response.errors[0] as unknown as JSONLikeObject,
     errormessage: message
   })
