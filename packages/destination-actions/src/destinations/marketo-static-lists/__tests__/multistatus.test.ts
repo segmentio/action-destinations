d526244360ef0860c114043c78578087f5194a9e
Arnav Dev <ardev@twilio.com>
2026-03-27 18:04:19 +0530

    default to 500 in non batched deliveries

--- packages/destination-actions/src/destinations/marketo-static-lists/__tests__/multistatus.test.ts
+++ packages/destination-actions/src/destinations/marketo-static-lists/__tests__/multistatus.test.ts
@@ -126,15 +126,15 @@ describe('MultiStatus', () => {
 
       expect(response).toMatchObject([
         {
-          status: 406,
-          errortype: 'NOT_ACCEPTABLE',
+          status: 500,
+          errortype: 'RETRYABLE_ERROR',
           errormessage: 'Invalid Content Type',
           errorreporter: 'DESTINATION',
           body: { code: '612', message: 'Invalid Content Type' }
         },
         {
-          status: 406,
-          errortype: 'NOT_ACCEPTABLE',
+          status: 500,
+          errortype: 'RETRYABLE_ERROR',
           errormessage: 'Invalid Content Type',
           errorreporter: 'DESTINATION',
           body: { code: '612', message: 'Invalid Content Type' }
@@ -354,15 +354,15 @@ describe('MultiStatus', () => {
 
       expect(response).toMatchObject([
         {
-          status: 406,
-          errortype: 'NOT_ACCEPTABLE',
+          status: 500,
+          errortype: 'RETRYABLE_ERROR',
           errormessage: 'Invalid Content Type',
           errorreporter: 'DESTINATION',
           body: { code: '612', message: 'Invalid Content Type' }
         },
         {
-          status: 406,
-          errortype: 'NOT_ACCEPTABLE',
+          status: 500,
+          errortype: 'RETRYABLE_ERROR',
           errormessage: 'Invalid Content Type',
           errorreporter: 'DESTINATION',
           body: { code: '612', message: 'Invalid Content Type' }
@@ -398,15 +398,15 @@ describe('MultiStatus', () => {
 
       expect(response).toMatchObject([
         {
-          status: 406,
-          errortype: 'NOT_ACCEPTABLE',
+          status: 500,
+          errortype: 'RETRYABLE_ERROR',
           errormessage: 'Invalid Content Type',
           errorreporter: 'DESTINATION',
           body: { code: '612', message: 'Invalid Content Type' }
         },
         {
-          status: 406,
-          errortype: 'NOT_ACCEPTABLE',
+          status: 500,
+          errortype: 'RETRYABLE_ERROR',
           errormessage: 'Invalid Content Type',
           errorreporter: 'DESTINATION',
           body: { code: '612', message: 'Invalid Content Type' }
