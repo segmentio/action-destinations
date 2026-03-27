286cfae2aa08f9598a68075aab409ebe422d80e1
Joe Ayoub <joe.ayoub@segment.com>
2026-03-27 09:16:53 +0000

    STRATCONN-6671 - [LinkedIn Audiences] - membership update

--- packages/destination-actions/src/destinations/linkedin-audiences/updateAudience/functions.ts
+++ packages/destination-actions/src/destinations/linkedin-audiences/updateAudience/functions.ts
@@ -1,5 +1,5 @@
-import type { StatsContext } from '@segment/actions-core'
-import { RequestClient, RetryableError, IntegrationError, InvalidAuthenticationError } from '@segment/actions-core'
+import type { AudienceMembership, StatsContext } from '@segment/actions-core'
+import { RequestClient, RetryableError, IntegrationError, InvalidAuthenticationError, Features, FLAGS } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 import { LinkedInAudiences } from '../api'
@@ -11,7 +11,9 @@ export async function processPayload(
   request: RequestClient,
   settings: Settings,
   payloads: Payload[],
-  statsContext: StatsContext | undefined,
+  audienceMemberships?: AudienceMembership[],
+  features?: Features,
+  statsContext?: StatsContext,
   stateContext?: StateContext
 ) {
   validate(settings, payloads)
@@ -19,7 +21,7 @@ export async function processPayload(
   const linkedinApiClient: LinkedInAudiences = new LinkedInAudiences(request)
 
   const dmpSegmentId = await getDmpSegmentId(linkedinApiClient, settings, payloads[0], statsContext, stateContext)
-  const elements = extractUsers(settings, payloads)
+  const elements = extractUsers(settings, payloads, audienceMemberships, features)
 
   // We should never hit this condition because at least an email or a
   // google ad id is required in each payload, but if we do, returning early
@@ -67,7 +69,9 @@ export async function processPayload(
 }
 
 function validate(settings: Settings, payloads: Payload[]): void {
+
   const isAutoOrUndefined = ['AUTO', undefined].includes(payloads[0]?.dmp_user_action)
+  
   if (isAutoOrUndefined && payloads[0].source_segment_id !== payloads[0].personas_audience_key) {
     throw new IntegrationError(
       'The value of `source_segment_id` and `personas_audience_key` must match.',
@@ -131,16 +135,16 @@ async function createDmpSegment(
   return headers['x-linkedin-id']
 }
 
-function extractUsers(settings: Settings, payloads: Payload[]): LinkedInAudiencePayload[] {
+function extractUsers(settings: Settings, payloads: Payload[], audienceMemberships?: AudienceMembership[], features?: Features): LinkedInAudiencePayload[] {
   const elements: LinkedInAudiencePayload[] = []
 
-  payloads.forEach((payload: Payload) => {
+  payloads.forEach((payload: Payload, index: number) => {
     if (!payload.email && !payload.google_advertising_id) {
       return
     }
 
     const linkedinAudiencePayload: LinkedInAudiencePayload = {
-      action: getAction(payload),
+      action: getAction(payload, index, audienceMemberships, features),
       userIds: getUserIds(settings, payload)
     }
 
@@ -170,7 +174,7 @@ function extractUsers(settings: Settings, payloads: Payload[]): LinkedInAudience
   return elements
 }
 
-function getAction(payload: Payload): 'ADD' | 'REMOVE' {
+function getAction(payload: Payload, index: number, audienceMemberships?: AudienceMembership[], features?: Features): 'ADD' | 'REMOVE' {
   const { dmp_user_action = 'AUTO' } = payload
 
   if (dmp_user_action === 'ADD') {
@@ -182,11 +186,15 @@ function getAction(payload: Payload): 'ADD' | 'REMOVE' {
   }
 
   if (dmp_user_action === 'AUTO' || !dmp_user_action) {
-    if (payload.event_name === 'Audience Entered') {
+
+    const isAudienceMembershipFlagOn = features && features[FLAGS.ACTIONS_LINKEDIN_AUDIENCES_AUDIENCE_MEMBERSHIP]
+    const membership = isAudienceMembershipFlagOn && Array.isArray(audienceMemberships) ? audienceMemberships?.[index] : undefined
+
+    if (payload.event_name === 'Audience Entered' || membership === true) {
       return 'ADD'
     }
 
-    if (payload.event_name === 'Audience Exited') {
+    if (payload.event_name === 'Audience Exited' || membership === false) {
       return 'REMOVE'
     }
   }
