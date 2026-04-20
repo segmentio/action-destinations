58bc396af1af15b71bdd764f3b4559f3db9602c5
Arjun Bhandage <abhandage@twilio.com>
2026-04-20 16:16:02 +0530

    tier 1 batch 6 (#3700)

---
+++ packages/destination-actions/src/destinations/segment-profiles/versioning-info.ts
@@ -0,0 +1,5 @@
+/** SEGMENT_PROFILES_API_VERSION
+ * Segment Profiles API version.
+ * API reference : https://docs.segmentapis.com/tag/Audiences
+ */
+export const SEGMENT_PROFILES_API_VERSION = 'v1'
