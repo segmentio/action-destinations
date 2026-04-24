95746690bcf26fe333129da0c0ef8ef52b19a02a
Joe Ayoub <joe.ayoub@segment.com>
2026-04-24 15:06:29 +0100

    removing unused flag

--- packages/core/src/flags.ts
+++ packages/core/src/flags.ts
@@ -2,6 +2,5 @@
 export const FLAGS = {
     ACTIONS_GOOGLE_EC_AUDIENCE_MEMBERSHIP: 'actions-google-ec-audience-membership',
     ACTIONS_BRAZE_COHORTS_AUDIENCE_MEMBERSHIP: 'actions-braze-cohorts-audience-membership',
-    ACTIONS_LINKEDIN_AUDIENCES_AUDIENCE_MEMBERSHIP: 'actions-linkedin-audiences-audience-membership',
-    ACTIONS_TIKTOK_AUDIENCES_AUDIENCE_MEMBERSHIP: 'actions-tiktok-audiences-audience-membership'
+    ACTIONS_LINKEDIN_AUDIENCES_AUDIENCE_MEMBERSHIP: 'actions-linkedin-audiences-audience-membership'
 }
