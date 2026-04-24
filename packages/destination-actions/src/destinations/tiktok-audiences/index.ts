266a2d0ebf41f86815cf334c57f9d3f19a8455a2
Joe Ayoub <joe.ayoub@segment.com>
2026-04-24 14:59:02 +0100

    adding new preset

--- packages/destination-actions/src/destinations/tiktok-audiences/index.ts
+++ packages/destination-actions/src/destinations/tiktok-audiences/index.ts
@@ -204,6 +204,13 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
       mapping: defaultValues(addToAudience.fields),
       type: 'specificEvent',
       eventSlug: 'journeys_step_entered_track'
+    },
+    {
+      name: 'Journeys Step All Trigger',
+      partnerAction: 'addToAudience',
+      mapping: defaultValues(addToAudience.fields),
+      type: 'specificEvent',
+      eventSlug: 'journeys_step_all_triggers_track'
     }
   ]
 }
