72a9050a88501c28236a64efed2a910bb3140a84
Joe Ayoub <joe.ayoub@segment.com>
2026-04-27 08:05:53 +0100

    yarn types

--- packages/destination-actions/src/destinations/tiktok-audiences/syncAudience/generated-types.ts
+++ packages/destination-actions/src/destinations/tiktok-audiences/syncAudience/generated-types.ts
@@ -2,17 +2,9 @@
 
 export interface Payload {
   /**
-   * The user's email address to send to TikTok.
+   * The name of the current Segment event.
    */
-  email?: string
-  /**
-   * The user's phone number to send to TikTok.
-   */
-  phone?: string
-  /**
-   * The user's mobile advertising ID to send to TikTok. This could be a GAID, IDFA, or AAID
-   */
-  advertising_id?: string
+  event_name?: string
   /**
    * Send email to TikTok. Segment will hash this value before sending
    */
@@ -26,19 +18,27 @@ export interface Payload {
    */
   send_advertising_id?: boolean
   /**
-   * The name of the current Segment event.
+   * The user's email address to send to TikTok.
    */
-  event_name?: string
+  email?: string
+  /**
+   * The user's phone number to send to TikTok.
+   */
+  phone?: string
+  /**
+   * The user's mobile advertising ID to send to TikTok. This could be a GAID, IDFA, or AAID
+   */
+  advertising_id?: string
   /**
    * Enable batching of requests to the TikTok Audiences.
    */
   enable_batching?: boolean
   /**
-   * The keys to use for batching the events.
-   */
-  batch_keys?: string[]
-  /**
    * The Audience ID in TikTok's DB.
    */
   external_audience_id?: string
+  /**
+   * The keys to use for batching the events.
+   */
+  batch_keys?: string[]
 }
