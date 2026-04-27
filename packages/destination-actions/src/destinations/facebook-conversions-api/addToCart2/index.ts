be1b95ca6f7f3acf19b0bff971e68d0b6eb3b831
Joe Ayoub <joe.ayoub@segment.com>
2026-04-27 16:41:57 +0100

    removing flags from actions

--- packages/destination-actions/src/destinations/facebook-conversions-api/addToCart2/index.ts
+++ packages/destination-actions/src/destinations/facebook-conversions-api/addToCart2/index.ts
@@ -1,14 +1,9 @@
 import { ActionDefinition, IntegrationError } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
-import { validateContents, dataProcessingOptions } from '../fb-capi-properties'
-import { CURRENCY_ISO_CODES } from '../constants'
-import { hash_user_data } from '../fb-capi-user-data'
-import { get_api_version } from '../utils'
-import { generate_app_data } from '../fb-capi-app-data'
 import { addToCartFields } from '../shared/fields'
 import { send, getAddToCartEventData } from '../shared/functions'
-import { EventType, FEATURE_FLAG_ADD_TO_CART } from '../shared/constants'
+import { EventType } from '../shared/constants'
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Add to Cart V2',
@@ -24,75 +19,7 @@ const action: ActionDefinition<Settings, Payload> = {
 
   perform: (request, { payload, settings, features, statsContext, syncMode }) => {
     if (syncMode === 'add') {
-      if (features && features[FEATURE_FLAG_ADD_TO_CART]) {
-        return send(request, payload, settings, getAddToCartEventData, EventType.AddToCart, features, statsContext)
-      }
-
-      if (payload.currency && !CURRENCY_ISO_CODES.has(payload.currency)) {
-        throw new IntegrationError(
-          `${payload.currency} is not a valid currency code.`,
-          'Misconfigured required field',
-          400
-        )
-      }
-
-      if (!payload.user_data) {
-        throw new IntegrationError('Must include at least one user data property', 'Misconfigured required field', 400)
-      }
-
-      if (payload.action_source === 'website' && payload.user_data.client_user_agent === undefined) {
-        throw new IntegrationError(
-          'If action source is "Website" then client_user_agent must be defined',
-          'Misconfigured required field',
-          400
-        )
-      }
-
-      if (payload.contents) {
-        const err = validateContents(payload.contents)
-        if (err) throw err
-      }
-
-      const [data_options, country_code, state_code] = dataProcessingOptions(
-        payload.data_processing_options,
-        payload.data_processing_options_country,
-        payload.data_processing_options_state
-      )
-
-      const testEventCode = payload.test_event_code || settings.testEventCode
-
-      return request(
-        `https://graph.facebook.com/v${get_api_version(features, statsContext)}/${settings.pixelId}/events`,
-        {
-          method: 'POST',
-          json: {
-            data: [
-              {
-                event_name: 'AddToCart',
-                event_time: payload.event_time,
-                event_source_url: payload.event_source_url,
-                event_id: payload.event_id,
-                action_source: payload.action_source,
-                user_data: hash_user_data({ user_data: payload.user_data }),
-                custom_data: {
-                  ...payload.custom_data,
-                  currency: payload.currency,
-                  value: payload.value,
-                  content_ids: payload.content_ids,
-                  content_name: payload.content_name,
-                  contents: payload.contents,
-                  content_type: payload.content_type
-                },
-                app_data: generate_app_data(payload.app_data_field),
-                data_processing_options: data_options,
-                data_processing_options_country: country_code,
-                data_processing_options_state: state_code
-              }
-            ],
-            ...(testEventCode && { test_event_code: testEventCode })
-          }
-        }
-      )
+      return send(request, payload, settings, getAddToCartEventData, EventType.AddToCart, features, statsContext)
     } else {
       throw new IntegrationError(`Sync mode ${syncMode} is not supported`, 'Misconfigured required field', 400)
     }
--- packages/destination-actions/src/destinations/facebook-conversions-api/viewContent/index.ts
+++ packages/destination-actions/src/destinations/facebook-conversions-api/viewContent/index.ts
@@ -1,14 +1,9 @@
-import { ActionDefinition, IntegrationError } from '@segment/actions-core'
-import { validateContents, dataProcessingOptions } from '../fb-capi-properties'
-import { hash_user_data } from '../fb-capi-user-data'
+import { ActionDefinition } from '@segment/actions-core'
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
-import { CURRENCY_ISO_CODES } from '../constants'
-import { get_api_version } from '../utils'
-import { generate_app_data } from '../fb-capi-app-data'
 import { viewContentFields } from '../shared/fields'
 import { send, getViewContentEventData } from '../shared/functions'
-import { EventType, FEATURE_FLAG_VIEW_CONTENT } from '../shared/constants'
+import { EventType } from '../shared/constants'
 
 const action: ActionDefinition<Settings, Payload> = {
   title: 'View Content',
@@ -16,76 +11,7 @@ const action: ActionDefinition<Settings, Payload> = {
   defaultSubscription: 'type = "track" and event = "Product Viewed"',
   fields: viewContentFields,
   perform: (request, { payload, settings, features, statsContext }) => {
-    if (features && features[FEATURE_FLAG_VIEW_CONTENT]) {
-      return send(request, payload, settings, getViewContentEventData, EventType.ViewContent, features, statsContext)
-    }
-
-    if (payload.currency && !CURRENCY_ISO_CODES.has(payload.currency)) {
-      throw new IntegrationError(
-        `${payload.currency} is not a valid currency code.`,
-        'Misconfigured required field',
-        400
-      )
-    }
-
-    if (!payload.user_data) {
-      throw new IntegrationError('Must include at least one user data property', 'Misconfigured required field', 400)
-    }
-
-    if (payload.action_source === 'website' && payload.user_data.client_user_agent === undefined) {
-      throw new IntegrationError(
-        'If action source is "Website" then client_user_agent must be defined',
-        'Misconfigured required field',
-        400
-      )
-    }
-
-    if (payload.contents) {
-      const err = validateContents(payload.contents)
-      if (err) throw err
-    }
-
-    const [data_options, country_code, state_code] = dataProcessingOptions(
-      payload.data_processing_options,
-      payload.data_processing_options_country,
-      payload.data_processing_options_state
-    )
-
-    const testEventCode = payload.test_event_code || settings.testEventCode
-
-    return request(
-      `https://graph.facebook.com/v${get_api_version(features, statsContext)}/${settings.pixelId}/events`,
-      {
-        method: 'POST',
-        json: {
-          data: [
-            {
-              event_name: 'ViewContent',
-              event_time: payload.event_time,
-              action_source: payload.action_source,
-              event_id: payload.event_id,
-              event_source_url: payload.event_source_url,
-              user_data: hash_user_data({ user_data: payload.user_data }),
-              custom_data: {
-                ...payload.custom_data,
-                currency: payload.currency,
-                value: payload.value,
-                content_ids: payload.content_ids,
-                content_name: payload.content_name,
-                content_type: payload.content_type,
-                contents: payload.contents,
-                content_category: payload.content_category
-              },
-              app_data: generate_app_data(payload.app_data_field),
-              data_processing_options: data_options,
-              data_processing_options_country: country_code,
-              data_processing_options_state: state_code
-            }
-          ],
-          ...(testEventCode && { test_event_code: testEventCode })
-        }
-      }
-    )
+    return send(request, payload, settings, getViewContentEventData, EventType.ViewContent, features, statsContext)    
   }
 }
 
