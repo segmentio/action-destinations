74b3a2074804c37b1c4968fcdf56fab53327da92
Joe Ayoub <joe.ayoub@segment.com>
2026-05-06 09:40:40 +0100

    removing redundant function and flag - it's all in constants.ts and functions.ts

--- packages/destination-actions/src/destinations/facebook-conversions-api/__tests__/utils.test.ts
+++ packages/destination-actions/src/destinations/facebook-conversions-api/__tests__/utils.test.ts
@@ -1,20 +1,20 @@
-import { get_api_version } from '../utils'
+import { getApiVersion } from '../shared/functions'
 import { API_VERSION, CANARY_API_VERSION } from '../shared/constants'
 import { StatsContext } from '@segment/actions-core/destination-kit'
 
 describe('FacebookConversionsApi', () => {
-  describe('get_api_version', () => {
+  describe('getApiVersion', () => {
     it('should return the canary API version', async () => {
       const features = {
         'facebook-capi-actions-canary-version': true
       }
-      const version = get_api_version(features, {} as StatsContext)
+      const version = getApiVersion(features, {} as StatsContext)
       expect(version).toEqual(CANARY_API_VERSION)
     })
 
     it('should return the regular API version', async () => {
       const features = {}
-      const version = get_api_version(features, {} as StatsContext)
+      const version = getApiVersion(features, {} as StatsContext)
       expect(version).toEqual(API_VERSION)
     })
   })
