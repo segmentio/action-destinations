3ff0efb04ff9321c7887fcafce85d140bcc6610c
Joe Ayoub <joe.ayoub@segment.com>
2026-06-02 15:52:26 +0100

    adding google enhanced conversions example test

--- packages/destination-actions/src/destinations/google-enhanced-conversions/__e2e__/index.ts
+++ packages/destination-actions/src/destinations/google-enhanced-conversions/__e2e__/index.ts
@@ -1,8 +1,11 @@
 import type { E2EAudienceDestinationConfig } from '@segment/actions-core'
 
+const audienceName = `e2e_test_user_list_${Date.now()}` // to ensure the audience name is unique across test runs
+
 export const config: E2EAudienceDestinationConfig = {
   settings: {
     customerId: { $env: 'E2E_GOOGLE_ADS_CUSTOMER_ID' },
+    //loginCustomerId: { $env: 'E2E_GOOGLE_ADS_LOGIN_CUSTOMER_ID' },
     oauth: {
       access_token: 'will_be_refreshed',
       refresh_token: { $env: 'E2E_GOOGLE_ADS_REFRESH_TOKEN' },
@@ -11,7 +14,7 @@ export const config: E2EAudienceDestinationConfig = {
     }
   },
   audience: {
-    audienceName: 'e2e_test_user_list',
+    audienceName,
     audienceSettings: {
       supports_conversions: false,
       external_id_type: 'CONTACT_INFO'
