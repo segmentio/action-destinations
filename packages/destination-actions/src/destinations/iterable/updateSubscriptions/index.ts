07a8aa19ab1b20f0d55d66eafa1ff2ee30ec153a
Joe Ayoub <joe.ayoub@segment.com>
2026-07-22 15:54:39 +0100

    updating description

--- packages/destination-actions/src/destinations/iterable/updateSubscriptions/index.ts
+++ packages/destination-actions/src/destinations/iterable/updateSubscriptions/index.ts
@@ -7,7 +7,7 @@ import { getSubscriptionGroupId } from './dynamic-fields'
 const action: ActionDefinition<Settings, Payload> = {
   title: 'Update Subscriptions',
   description:
-    'Manage subscription preferences for a user, including subscribing and unsubscribing from channels, message types, and email lists.',
+    'Manage subscription preferences for a user, including subscribing and unsubscribing from channels, message types, and email lists. This integration is only supported on Iterable projects with opt-in Message Type Subscriptions and the Subscriptions API enabled.',
   defaultSubscription: 'type = "track" and event = "Subscriptions Updated"',
   fields: {
     identifier: {
