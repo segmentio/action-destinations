28bd209df063feb9346a93773faab72133b47a97
nikumar <nikumar@twilio.com>
2026-07-31 13:57:53 +0530

    bug bash: flip required to false

--- packages/destination-actions/src/destinations/webhook/index.ts
+++ packages/destination-actions/src/destinations/webhook/index.ts
@@ -16,7 +16,7 @@ const destination: DestinationDefinition<Settings> = {
         label: 'Shared Secret',
         description:
           'If set, Segment will sign requests with an HMAC in the "X-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this shared secret and the request body.',
-        required: true
+        required: false
       },
       optionalAuthField: {
         type: 'string',
