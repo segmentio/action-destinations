0d9002063855f53e28573db67eeda4aefbd0317b
Sayan Das <sayan.das@segment.com>
2026-07-31 13:11:10 +0530

    Update TTD sync mode

--- packages/destination-actions/src/destinations/the-trade-desk-crm/index.ts
+++ packages/destination-actions/src/destinations/the-trade-desk-crm/index.ts
@@ -99,7 +99,7 @@ const destination: AudienceDestinationDefinition<Settings, AudienceSettings> = {
   audienceConfig: {
     mode: {
       type: 'synced',
-      full_audience_sync: true
+      full_audience_sync: false
     },
     async createAudience(request, createAudienceInput) {
       const audienceName = createAudienceInput.audienceName
