0afbceb530d69c06e048cecfa9bfc41fa57351af
nikumar <nikumar@twilio.com>
2026-07-31 12:37:05 +0530

    Revert "bug bash actions-webhook required auth field"
    This reverts commit 866bc9066ead65d22ac0cd1c7c814051da1bb77a.

--- packages/destination-actions/src/destinations/webhook-extensible/index.ts
+++ packages/destination-actions/src/destinations/webhook-extensible/index.ts
@@ -20,12 +20,6 @@ const destination: DestinationDefinition<SettingsWithDynamicAuth> = {
         label: 'Shared Secret',
         description:
           'If set, Segment will sign requests with an HMAC in the "X-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this shared secret and the request body.'
-      },
-      requiredAuthField: {
-        type: 'string',
-        label: 'Required Auth Field',
-        description: 'Required Field for destinations bot bug bash',
-        required: true
       }
     },
     refreshAccessToken: async (request, { settings, auth }) => {
