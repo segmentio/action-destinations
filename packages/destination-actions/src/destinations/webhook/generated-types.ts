e1d3db7866b6e4ff16bafda6e25c3ca95bfd1e71
nikumar <nikumar@twilio.com>
2026-07-31 13:50:14 +0530

    bug bash: mark sharedsecret required

--- packages/destination-actions/src/destinations/webhook/generated-types.ts
+++ packages/destination-actions/src/destinations/webhook/generated-types.ts
@@ -4,7 +4,7 @@ export interface Settings {
   /**
    * If set, Segment will sign requests with an HMAC in the "X-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this shared secret and the request body.
    */
-  sharedSecret?: string
+  sharedSecret: string
   /**
    * Optional field for bug bash
    */
--- packages/destination-actions/src/destinations/webhook/index.ts
+++ packages/destination-actions/src/destinations/webhook/index.ts
@@ -15,7 +15,8 @@ const destination: DestinationDefinition<Settings> = {
         type: 'password',
         label: 'Shared Secret',
         description:
-          'If set, Segment will sign requests with an HMAC in the "X-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this shared secret and the request body.'
+          'If set, Segment will sign requests with an HMAC in the "X-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this shared secret and the request body.',
+        required: true
       },
       optionalAuthField: {
         type: 'string',
