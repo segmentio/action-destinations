27886b1895b236aff6ea16adf60b1521f2d05e05
Md Mozammil Khan <mdkhan@twilio.com>
2026-08-04 15:19:02 +0530

    adding new test destinations

---
+++ packages/destination-actions/src/destinations/mdkhan-destination/generated-types.ts
@@ -0,0 +1,12 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Settings {
+  /**
+   * Your Mdkhan Destination username
+   */
+  username: string
+  /**
+   * Your Mdkhan Destination password.
+   */
+  password: string
+}
