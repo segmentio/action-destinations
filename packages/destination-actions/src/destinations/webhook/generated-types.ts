d2b7f276092cb11ab18b69ce841d6bdfd154d30a
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2026-08-05 20:34:21 +0530

    Sl18: Add an option literally named 'enabled' (any scheme)

--- packages/destination-actions/src/destinations/webhook/generated-types.ts
+++ packages/destination-actions/src/destinations/webhook/generated-types.ts
@@ -5,4 +5,8 @@ export interface Settings {
    * If set, Segment will sign requests with an HMAC in the "X-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this shared secret and the request body.
    */
   sharedSecret?: string
+  /**
+   * If set, Segment will also sign requests with an HMAC in the "X-Secondary-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this secondary secret and the request body.
+   */
+  enabled?: boolean
 }
