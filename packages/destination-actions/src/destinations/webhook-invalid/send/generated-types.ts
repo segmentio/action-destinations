562670b210ec9ce8b11aea0f2f1a398082dbdf2e
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2026-08-05 13:48:54 +0530

    test registration with invalid mode

---
+++ packages/destination-actions/src/destinations/webhook-invalid/send/generated-types.ts
@@ -0,0 +1,32 @@
+// Generated file. DO NOT MODIFY IT BY HAND.
+
+export interface Payload {
+  /**
+   * URL to deliver data to.
+   */
+  url: string
+  /**
+   * HTTP method to use.
+   */
+  method: string
+  /**
+   * Maximum number of events to include in each batch. Actual batch sizes may be lower.
+   */
+  batch_size?: number
+  /**
+   * HTTP headers to send with each request. Only ASCII characters are supported.
+   */
+  headers?: {
+    [k: string]: unknown
+  }
+  /**
+   * Payload to deliver to webhook URL (JSON-encoded).
+   */
+  data?: {
+    [k: string]: unknown
+  }
+  /**
+   * The mapping keys to batch events together by.
+   */
+  batch_keys?: string[]
+}
