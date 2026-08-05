fac54636ec2479ed237cf2ac1acd469280475fc1
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2026-08-05 18:54:52 +0530

    Sl5: Flip an existing field required:true->false

--- packages/destination-actions/src/destinations/webhook/index.ts
+++ packages/destination-actions/src/destinations/webhook/index.ts
@@ -20,7 +20,6 @@ const destination: DestinationDefinition<Settings> = {
       secondarySecret: {
         type: 'password',
         label: 'Secondary Secret',
-        required: true,
         description:
           'If set, Segment will also sign requests with an HMAC in the "X-Secondary-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this secondary secret and the request body.'
       }
