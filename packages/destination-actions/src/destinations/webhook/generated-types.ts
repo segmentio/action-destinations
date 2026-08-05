a01b904bc57ee0db9aab45b83c85505ef8ffa206
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2026-08-05 19:14:56 +0530

    Sl4: Flip an existing field required:false->true

--- packages/destination-actions/src/destinations/webhook/generated-types.ts
+++ packages/destination-actions/src/destinations/webhook/generated-types.ts
@@ -8,5 +8,5 @@ export interface Settings {
   /**
    * If set, Segment will also sign requests with an HMAC in the "X-Secondary-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this secondary secret and the request body.
    */
-  secondarySecret?: string
+  secondarySecret: string
 }
