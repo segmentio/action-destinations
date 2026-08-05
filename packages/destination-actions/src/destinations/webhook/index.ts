2d2b43eb63634f673019ba60e3510b5f80eb0506
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2026-08-05 18:26:02 +0530

    Sl1: Add a new required auth field

--- packages/destination-actions/src/destinations/webhook/index.ts
+++ packages/destination-actions/src/destinations/webhook/index.ts
@@ -16,6 +16,13 @@ const destination: DestinationDefinition<Settings> = {
         label: 'Shared Secret',
         description:
           'If set, Segment will sign requests with an HMAC in the "X-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this shared secret and the request body.'
+      },
+      secondarySecret: {
+        type: 'password',
+        label: 'Secondary Secret',
+        required: true,
+        description:
+          'If set, Segment will also sign requests with an HMAC in the "X-Secondary-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this secondary secret and the request body.'
       }
     }
   },
