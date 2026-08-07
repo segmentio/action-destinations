02a828fbbfaa205a36f2deec677376c5f09a06f4
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2026-08-07 17:00:38 +0530

    flip an action field required

--- packages/destination-actions/src/destinations/webhook/send/index.ts
+++ packages/destination-actions/src/destinations/webhook/send/index.ts
@@ -12,7 +12,7 @@ const action: ActionDefinition<Settings, Payload> = {
       label: 'URL',
       description: 'URL to deliver data to.',
       type: 'string',
-      required: false,
+      required: true,
       format: 'uri'
     },
     method: {
