0dd77c5db15a24e94abaffeabb6d1b47ceee0d8a
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2026-08-07 17:10:07 +0530

    flip an action field required

--- packages/destination-actions/src/destinations/webhook/index.ts
+++ packages/destination-actions/src/destinations/webhook/index.ts
@@ -14,7 +14,7 @@ const destination: DestinationDefinition<Settings> = {
       sharedSecret: {
         type: 'password',
         label: 'Shared Secret',
-        required: false,
+        required: true,
         description:
           'If set, Segment will sign requests with an HMAC in the "X-Signature" request header. The HMAC is a hex-encoded SHA1 hash generated using this shared secret and the request body.'
       }
