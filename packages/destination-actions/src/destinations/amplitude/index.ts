5a4ce4b480c34ffac18d2edf2104d669f9fab9e1
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2026-08-13 15:14:18 +0530

    Update: metadata.json

--- packages/destination-actions/src/destinations/amplitude/index.ts
+++ packages/destination-actions/src/destinations/amplitude/index.ts
@@ -91,6 +91,7 @@ const destination: DestinationDefinition<Settings> = {
         description: 'The region to send your data.',
         type: 'string',
         format: 'text',
+        multiple: false,
         choices: [
           {
             label: 'North America',
