b167067e7138a3a7a214071ba3c3173b2bf32328
Joe Ayoub <joe.ayoub@segment.com>
2026-08-18 14:35:52 +0100

    STRATCONN-6860: Copy Journey Step All Events preset to staging for testing

--- packages/destination-actions/src/destinations/marketo-static-lists/index.ts
+++ packages/destination-actions/src/destinations/marketo-static-lists/index.ts
@@ -135,6 +135,13 @@ const destination: AudienceDestinationDefinition<Settings> = {
       mapping: { ...defaultValues(addToList.fields) },
       type: 'specificEvent',
       eventSlug: 'journeys_step_entered_track'
+    },
+    {
+      name: 'Journey Step All Events',
+      partnerAction: 'syncList',
+      mapping: defaultValues(syncList.fields),
+      type: 'specificEvent',
+      eventSlug: 'journey_step_all_events_track'
     }
   ]
 }
