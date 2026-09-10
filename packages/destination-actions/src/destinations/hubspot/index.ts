49ac68313996af981c8cdc545b5fa979f99b5cc1
nikumar <nikumar@twilio.com>
2026-09-10 14:52:01 +0530

    no op

--- packages/destination-actions/src/destinations/hubspot/index.ts
+++ packages/destination-actions/src/destinations/hubspot/index.ts
@@ -29,6 +29,7 @@ const destination: DestinationDefinition<Settings> = {
     },
     testAuthentication: (request) => {
       // HubSpot doesn't have a test authentication endpoint, so we using a lightweight CRM API to validate access token
+      // no op change
       return request(`${HUBSPOT_BASE_URL}/crm/${HUBSPOT_CRM_API_VERSION}/objects/contacts?limit=1`)
     },
     refreshAccessToken: async (request, { auth }) => {
