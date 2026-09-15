73cc3c43722bdd7dfb44ebe09af7429d3e621a8e
Joe Ayoub <joe.ayoub@segment.com>
2026-09-15 13:36:36 +0100

    core changes for first party DV360

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,3 @@
-// no op change-2 for @segment/actions-core
 export { Destination, fieldsToJsonSchema } from './destination-kit'
 export { getAuthData } from './destination-kit/parse-settings'
 export { transform, Features } from './mapping-kit'
@@ -44,8 +43,10 @@ export {
   ErrorCodes,
   HttpErrorCodes,
   CustomErrorCodes,
-  getErrorCodeFromHttpStatus
+  getErrorCodeFromHttpStatus,
+  isRetryableStatus
 } from './errors'
+export type { RetryableStatusCodes } from './errors'
 export { retry } from './retry'
 export { get } from './get'
 export { omit } from './omit'
@@ -55,12 +56,7 @@ export { time, duration } from './time'
 export { realTypeOf, isObject, isArray, isString } from './real-type-of'
 
 export type { RequestOptions } from './request-client'
-export {
-  HTTPError,
-  DEFAULT_REQUEST_TIMEOUT,
-  RETRYABLE_NETWORK_ERROR_CODES,
-  isRetryableNetworkError
-} from './request-client'
+export { HTTPError, DEFAULT_REQUEST_TIMEOUT } from './request-client'
 export { ModifiedResponse } from './types'
 export { default as fetch, Request, Response, Headers } from './fetch'
 
